323a0a5e940fe9d2ff4a5e7061651e4311d0ad25
andreamari <andreamari84@gmail.com>
2019-11-04 18:33:51 +0100

    classical mnist classification

--- implementations/tutorial_quanvolutional.py
+++ implementations/tutorial_quanvolutional.py
@@ -16,25 +16,56 @@ Introduction
 
 General setup
 ------------------------
-This Python code requires *PennyLane* and the plotting library *matplotlib*.
+This Python code requires *PennyLane* with the *TensorFlow* interface and the plotting library *matplotlib*.
 """
 
 # Pennylane
 import pennylane as qml
 from pennylane import numpy as np
 
+# TensorFlow and Keras
+import tensorflow as tf
+from tensorflow import keras
+
 # Plotting
 import matplotlib.pyplot as plt
+
+
+
 
 ##############################################################################
 # Setting of the main hyper-parameters of the model
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 n_qubits = 3    # Number of system qubits.
-steps = 30      # Number of optimization steps
-eta = 0.8       # Learning rate
+num_epochs = 1  # Number of optimization epochs
 rng_seed = 0    # Seed for random number generator
 
+##############################################################################
+# We import the MNIST dataset from *Keras*.
+
+mnist_dataset = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist_dataset.load_data()
+
+# Normalize pixel values from 0 to 1.
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=num_epochs)
+
+predictions = model.predict(test_images)
+
+print('Label 0:', test_labels[0])
+print('Prediction 0:', np.argmax(predictions[0]))
 
 
 ##############################################################################
