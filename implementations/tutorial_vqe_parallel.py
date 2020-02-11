0326b05c071d3d9bc1c903f77823322391ea459f
trbromley <brotho02@gmail.com>
2020-02-11 09:18:41 -0500

    Update tutorial after read through

--- implementations/tutorial_vqe_parallel.py
+++ implementations/tutorial_vqe_parallel.py
@@ -10,7 +10,7 @@ Using a VQE setup, we task two devices from the
 [PennyLane-Forest](https://pennylane-forest.readthedocs.io/en/latest/) plugin with evaluating
 separate terms in the qubit Hamiltonian of $H_2$. As these devices are allowed to operate
 asynchronously, i.e., at the same time and without having to wait for each other,
-the calculation can be performed in half the time.
+the calculation can be performed in roughly half the time.
 
 We begin by importing the prerequisite libraries:
 """
@@ -35,25 +35,25 @@ from pennylane import qchem
 # separations.
 #
 # Each atomic separation results in a different qubit Hamiltonian. To find the corresponding
-# Hamiltonian, we use the ```generate_hamiltonian``` function of the ``qchem`` package. Further
-# details on the mapping from electronic Hamiltonian of a molecule to a qubit Hamiltonian can be
-# found in the :ref:`Quantum Chemistry with PennyLane <qchem-beginner>` and
+# Hamiltonian, we use the :func:`~.generate_hamiltonian` function of the :mod:`~.qchem` package.
+# Further details on the mapping from the electronic Hamiltonian of a molecule to a qubit
+# Hamiltonian can be found in the :ref:`Quantum Chemistry with PennyLane <qchem-beginner>` and
 # :ref:`Quantum Chemistry with PennyLane <qchem-implementations>` tutorials.
 #
 # We begin by creating a dictionary containing molecular separations and corresponding data files
 # saved in ```.xyz``` format.
 
 data = {  # keys: atomic separations (ångström), values: corresponding files
-    0.3: "xyz/h2_0.30.xyz",
-    0.5: "xyz/h2_0.50.xyz",
-    0.7: "xyz/h2_0.70.xyz",
-    0.9: "xyz/h2_0.90.xyz",
-    1.1: "xyz/h2_1.10.xyz",
-    1.3: "xyz/h2_1.30.xyz",
-    1.5: "xyz/h2_1.50.xyz",
-    1.7: "xyz/h2_1.70.xyz",
-    1.9: "xyz/h2_1.90.xyz",
-    2.1: "xyz/h2_2.10.xyz",
+    0.3: "tutorial_vqe_parallel/h2_0.30.xyz",
+    0.5: "tutorial_vqe_parallel/h2_0.50.xyz",
+    0.7: "tutorial_vqe_parallel/h2_0.70.xyz",
+    0.9: "tutorial_vqe_parallel/h2_0.90.xyz",
+    1.1: "tutorial_vqe_parallel/h2_1.10.xyz",
+    1.3: "tutorial_vqe_parallel/h2_1.30.xyz",
+    1.5: "tutorial_vqe_parallel/h2_1.50.xyz",
+    1.7: "tutorial_vqe_parallel/h2_1.70.xyz",
+    1.9: "tutorial_vqe_parallel/h2_1.90.xyz",
+    2.1: "tutorial_vqe_parallel/h2_2.10.xyz",
 }
 
 ##############################################################################
@@ -93,12 +93,16 @@ print([len(h.ops) for h in hamiltonians])
 #
 # Let's suppose we have access to two QPUs: ```Aspen-4-4Q-E``` and ```Aspen-7-4Q-C``` from
 # Rigetti. We can evaluate the expectation value of each Hamiltonian with eight terms run on
-# ```Aspen-4-4Q-D``` and seven terms run on ```Aspen-7-4Q-C```.
+# ```Aspen-4-4Q-E``` and seven terms run on ```Aspen-7-4Q-C```, as summarized by the diagram below:
+#
+# .. figure:: /implementations/vqe_parallel/diagram.png
+#    :width: 50%
+#    :align: center
 #
 # To do this, start by instantiating a device for each term:
 
 # Use forest.qpu if hardware access is available
-devs_4 = [qml.device("forest.qvm", device="Aspen-4-4Q-D") for _ in range(8)]
+devs_4 = [qml.device("forest.qvm", device="Aspen-4-4Q-E") for _ in range(8)]
 devs_7 = [qml.device("forest.qvm", device="Aspen-7-4Q-C") for _ in range(7)]
 devs = devs_4 + devs_7
 
@@ -117,16 +121,16 @@ def circuit(param, wires):
 
 
 ##############################################################################
-# Each molecular separation requires a different Y-rotation parameter. The values of these
+# Each molecular separation has a different Y-rotation parameter. The values of these
 # parameters can be found by minimizing the ground state energy as outlined in
-# :ref:`Quantum Chemistry with PennyLane <qchem-implementations>`. In this tutorial we load
+# :ref:`Quantum Chemistry with PennyLane <qchem-implementations>`. In this tutorial, we load
 # pre-optimized rotations and focus on comparing the speed of evaluating the potential energy
 # surface with sequential and parallel evaluation.
 
-params = np.load("RY_params.npy")
+params = np.load("tutorial_vqe_parallel/RY_params.npy")
 
 ##############################################################################
-# Finally, the energies as a function of rotation angle can be given using ```VQECost```.
+# Finally, the energies as functions of rotation angle can be given using :class:`~.VQECost`.
 
 energies = [qml.VQECost(circuit, h, devs) for h in hamiltonians]
 
@@ -134,18 +138,18 @@ energies = [qml.VQECost(circuit, h, devs) for h in hamiltonians]
 # Calculating the potential energy surface
 # ----------------------------------------
 #
-# ```VQECost``` returns a ```QNodeCollection``` which can be evaluated based using the input
-# parameters to the ansatz circuit. As of ```v0.8.0```, the ```QNodeCollection``` can be
-# evaluated asynchronously by specifying the ```parallel = True``` keyword argument. With the
-# default ```parallel = False```, the QNodes are evaluated one at a time.
+# :class:`~.VQECost` returns a :class:`QNodeCollection` which can be evaluated using the input
+# parameters to the ansatz circuit. As of ```v0.8.0``` of PennyLane, the :class:`QNodeCollection`
+# can be evaluated asynchronously by specifying the ```parallel = True``` keyword argument.
+# With the default ```parallel = False```, the QNodes are evaluated sequentially.
 #
-# We can use this feature to compare the sequential and parallel times to calculate the potential
-# energy surface. The following function calculates the surface:
+# We can use this feature to compare the sequential and parallel times required to calculate the
+# potential energy surface. The following function calculates the surface:
 
 
 def calculate_surface(parallel=True):
-    t0 = time.time()
     s = []
+    t0 = time.time()
 
     for i, e in enumerate(energies):
         print("Running for molecular separation {} Å".format(list(data.keys())[i]))
@@ -154,17 +158,22 @@ def calculate_surface(parallel=True):
     t1 = time.time()
 
     print("Evaluation time: {0:.2f} s".format(t1 - t0))
-    return s
+    return s, t1 - t0
 
 
 print("Evaluating the potential energy surface sequentially")
-surface = calculate_surface(False)
+surface, t_seq = calculate_surface(parallel=False)
 
 print("\nEvaluating the potential energy surface in parallel")
-surface = calculate_surface(True)
+surface, t_par = calculate_surface(parallel=True)
+
+print("Speedup: {0:.2f}".format(t_seq / t_par))
 
 ##############################################################################
-# We can finally plot the potential energy surface.
+# We have seen how a :class:`QNodeCollection` can be evaluated in parallel and the resultant
+# speed up in processing. Can you think of other ways to combine multiple QPUs to improve the
+# performance of quantum algorithms? To conclude the tutorial, let's plot the calculated
+# potential energy surface:
 
 plt.plot(surface, linewidth=2.2, marker="o")
 plt.title("Potential energy surface for molecular hydrogen", fontsize=12)
