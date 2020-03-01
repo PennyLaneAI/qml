baf931fa4dd15b4d2c46d0aaef631a452d1916e7
Josh Izaac <josh146@gmail.com>
2020-03-01 22:42:03 +1030

    fix bug

--- demonstrations/tutorial_vqe_parallel.py
+++ demonstrations/tutorial_vqe_parallel.py
@@ -110,9 +110,9 @@ for op in h.ops:
 #    fifteen quantum circuit runs. Nevertheless, these quantum circuit runs can still be
 #    parallelized to multiple QPUs.
 #
-# Let's suppose we have access to two QPUs: ``Aspen-4-4Q-E`` and ``Aspen-7-4Q-D`` from
+# Let's suppose we have access to two QPUs: ``Aspen-4-4Q-F`` and ``Aspen-7-4Q-D`` from
 # Rigetti. We can evaluate the expectation value of each Hamiltonian with eight terms run on
-# ``Aspen-4-4Q-E`` and seven terms run on ``Aspen-7-4Q-D``, as summarized by the diagram below:
+# ``Aspen-4-4Q-F`` and seven terms run on ``Aspen-7-4Q-D``, as summarized by the diagram below:
 #
 # .. figure:: /demonstrations/vqe_parallel/vqe_diagram.png
 #    :width: 65%
@@ -120,7 +120,7 @@ for op in h.ops:
 #
 # To do this, start by instantiating a device for each term:
 
-devs_4 = [qml.device("forest.qvm", device="Aspen-4-4Q-E") for _ in range(8)]
+devs_4 = [qml.device("forest.qvm", device="Aspen-4-4Q-F") for _ in range(8)]
 devs_7 = [qml.device("forest.qvm", device="Aspen-7-4Q-D") for _ in range(7)]
 devs = devs_4 + devs_7
 
