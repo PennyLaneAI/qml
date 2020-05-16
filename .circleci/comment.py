a47e81a76d0b81197e048e2f5ca703ff213db89d
Josh Izaac <josh146@gmail.com>
2020-05-16 21:04:15 +0930

    update requirements

--- .circleci/comment.py
+++ .circleci/comment.py
@@ -15,6 +15,7 @@ URL = "https://circleci.com/api/v2/project/github/XanaduAI/qml/{}/artifacts".for
 res = requests.get(URL, headers=HEADERS).json()
 
 zip_url = res["items"][0]["url"]
+demo_url = res["items"][1]["url"]
 
 for f in res["items"]:
     if "html/index.html" in f["url"]:
@@ -25,10 +26,11 @@ comment = """\
 <h3>Website build</h3>
 <strong>Commit:</strong> {hash}\n
 <strong>Circle build number:</strong> {job}\n
+<strong>Built demos:</strong> <a href={demo}>{demo}</a>\n
 <strong>Website zip:</strong> <a href={zip}>{zip}</a>\n
 <strong>Website url:</strong> <a href={web}>{web}</a>\n
 <em>Please double check the rendered website build to make sure everything is correct.</em>
-""".format(hash=SHA1, job=JOB_ID, zip=zip_url, web=web_url)
+""".format(hash=SHA1, job=JOB_ID, demo=demo_url, zip=zip_url, web=web_url)
 
 g = Github(GH_TOKEN)
 repo = g.get_repo("XanaduAI/qml")
