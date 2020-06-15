da2cbd79f3a4d14f9c5cf38ce443baf0fd15bda9
johannesjmeyer <jj.meyer@outlook.com>
2020-06-15 14:40:36 +0200

    Add tutorial file

---
+++ demonstrations/tutorial_quantum_metrology.py
@@ -0,0 +1,29 @@
+r"""
+Quantum Metrology
+==================================
+
+.. meta::
+    :property="og:description": In this demonstration, a variational algorithm is 
+        used to optimize a quantum sensing protocol.
+    :property="og:image": https://pennylane.ai/qml/_images/single_shot.png
+
+In this tutorial we use the variational quantum algorithm from
+`Johannes Jakob Meyer et al. (2020) <https://arxiv.org/abs/2006.06303>`__ to optimize a quantum
+sensing protocol.
+
+Background
+----------
+
+- Quantum Metrology
+- The Variational Algorithm
+
+"""
+
+
+##############################################################################
+# References
+# ----------
+#
+# 1. Johannes Jakob Meyer, Johannes Borregaard, Jens Eisert. 
+#    "A variational toolbox for quantum multi-parameter estimation." `arXiv:2006.06303
+#    <https://arxiv.org/abs/2006.06303>`__, 2020.
