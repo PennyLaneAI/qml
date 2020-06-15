541531b0f148282e87e8176cab26f684ba824364
johannesjmeyer <jj.meyer@outlook.com>
2020-06-15 14:59:08 +0200

    Begin intro text

--- demonstrations/tutorial_quantum_metrology.py
+++ demonstrations/tutorial_quantum_metrology.py
@@ -14,7 +14,30 @@ sensing protocol.
 Background
 ----------
 
-- Quantum Metrology
+Quantum metrology is a particular application of quantum technologies that exploit non-classical
+effects to enhance the sensitivity of measurement processes. A sensing protocol can be modeled in
+the following way:
+
+As a first step, a *probe state* :math:`\rho_0(\boldsymbol{\theta})` with variational parameters
+:math:`\boldsymbol{\theta}` is prepared. This probe state then undergoes a possibly noisy quantum
+evolution that depends on a vector of parameters :math:`\boldsymbol{\phi}`. The resulting state
+:math:`\rho(\boldsymbol{\theta}, \boldsymbol{\phi})` is then measured using a parametrized positive
+operator-valued measurement :math:`\{ \Pi_l(\boldsymbol{\mu}) \}`, yielding an output probability
+distribution
+
+.. math:: p_l(\boldsymbol{\theta}, \boldsymbol{\phi}, \boldsymbol{\mu}) =
+    \operatorname{Tr}(\rho(\boldsymbol{\theta}, \boldsymbol{\phi}), \Pi_l(\boldsymbol{\mu}))
+
+We now seek to estimate the vector of parameters :math:`\boldsymbol{\phi}` or a multi-variate function
+:math:`\boldsymbol{f}(\boldsymbol{\phi})` thereof from the output probability distribution. The best achievable
+estimation precision in doing this can be quantified by the *Cramér-Rao bound*: For any unbiased estimator
+:math:`\mathbb{E}(\hat{\boldsymbol{f}}) = \boldsymbol{f}`, we have
+
+.. math:: \operatorname{Cov}(\hat{\boldsymbol{f}}) \geq \frac{1}{n} I^{-1}_{\boldsymbol{f}},
+
+where :math:`n` is the number of samples and :math:`I_{\boldsymbol{f}}` is the *Classical Fisher Information Matrix*
+with respect to the entries of :math:`\boldsymbol{f}`.
+
 - The Variational Algorithm
 
 """
