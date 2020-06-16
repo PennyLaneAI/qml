9e1a5b466978db9af45226ef20ab4f53f5d0f78f
Jack Ceroni <jackceroni@gmail.com>
2020-06-16 13:55:03 -0400

    runs

--- demonstrations/tutorial_vqt.py
+++ demonstrations/tutorial_vqt.py
@@ -408,7 +408,7 @@ def exact_cost(params):
 iterations = 0
 
 params = [np.random.randint(-300, 300) / 100 for i in range(0, (N * (1 + depth * 4)))]
-out = minimize(exact_cost, x0=params, method="COBYLA", options={"maxiter": 1000})
+out = minimize(exact_cost, x0=params, method="COBYLA", options={"maxiter": 1600})
 out_params = out["x"]
 print(out)
 
