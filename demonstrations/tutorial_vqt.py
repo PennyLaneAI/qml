65117e6b205c7869db6378fc9c9d871bd40c33ad
Jack Ceroni <jackceroni@gmail.com>
2020-06-17 15:52:52 -0400

    code

--- demonstrations/tutorial_vqt.py
+++ demonstrations/tutorial_vqt.py
@@ -215,7 +215,7 @@ def prob_dist(params):
 # :math:`U(\phi)`, composed of rotational and coupling layers. The
 # rotation layer is simply ``RX``, ``RY```, and ``RZ``
 # gates applied to each qubit. We make use of the
-# ``AngleEmbedding`
+# ``AngleEmbedding``
 # function, which allows us to easily pass parameters into rotational
 # layers.
 #
