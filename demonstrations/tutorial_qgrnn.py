1d7357bd577c69bec5860600477f73d2391d8a64
Jack Ceroni <jackceroni@gmail.com>
2020-06-22 16:22:20 -0400

    Update tutorial_qgrnn.py

--- demonstrations/tutorial_qgrnn.py
+++ demonstrations/tutorial_qgrnn.py
@@ -15,7 +15,7 @@ The Quantum Graph Recurrrent Neural Network
 # recurrent neural networks (QGRNN), which are the quantum analogue of a
 # classical graph recurrent neural network and a subclass of the more 
 # general quantum graph
-# neural network ansatz (`QGNN <https://arxiv.org/abs/1909.12264>`__).
+# neural network ansatz (`QGNN <https://arxiv.org/abs/1909.12264>`__)
 
 ######################################################################
 # The Idea
