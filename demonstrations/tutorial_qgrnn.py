2851563df6b3e96a9b7ae3cc646303b1923adc39
Jack Ceroni <jackceroni@gmail.com>
2020-06-22 15:51:42 -0400

    Update demonstrations/tutorial_qgrnn.py
    Co-authored-by: ixfoduap <40441298+ixfoduap@users.noreply.github.com>

--- demonstrations/tutorial_qgrnn.py
+++ demonstrations/tutorial_qgrnn.py
@@ -403,7 +403,7 @@ def swap_test(control, register1, register2):
 
 ######################################################################
 # Before creating the full QGRNN and the cost function, we
-# define a few more fixed values. Among these fixed values is a "guessed"
+# define a few more fixed values. Among these is a "guessed"
 # interaction graph, which we define to be the complete graph. This choice 
 # is motivated by the fact that any target interaction graph will be a subgraph 
 # of this initial guess. 
