12469ae2b3b5f12861e689f53ba30926d902f4ad
Jack Ceroni <jackceroni@gmail.com>
2020-06-22 17:56:11 -0400

    testing 2

--- demonstrations/tutorial_qgrnn.py
+++ demonstrations/tutorial_qgrnn.py
@@ -540,7 +540,7 @@ qnode = qml.QNode(qgrnn, qgrnn_dev)
 iterations = 0
 optimizer = qml.AdamOptimizer(stepsize=0.3)
 steps = 1
-qgrnn_params = list([np.random.randint(-20, 20) / 50 for i in range(0, 10)])
+qgrnn_params = list([0 for i in range(0, 10)])
 
 # Executes the optimization method
 
