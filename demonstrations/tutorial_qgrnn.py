94d5f81b5210d9a8dc8f686edbf62927c21d679d
Jack Ceroni <jackceroni@gmail.com>
2020-06-23 19:44:35 -0400

    Update demonstrations/tutorial_qgrnn.py
    Co-authored-by: Aroosa <44415402+AroosaIjaz@users.noreply.github.com>

--- demonstrations/tutorial_qgrnn.py
+++ demonstrations/tutorial_qgrnn.py
@@ -26,7 +26,7 @@ The Quantum Graph Recurrent Neural Network
 ######################################################################
 # A graph is defined as a set of *nodes*, along with a set of
 # **edges**, which represent interactions or relationships between nodes.
-# Sometimes, we like to encode information into graphs by assigning numbers
+# Information can be encoded into graphs by assigning numbers
 # to nodes and edges, which we call **weights**.
 # It is usually convenient to think of a graph visually:
 #
