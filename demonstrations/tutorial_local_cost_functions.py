59650e053fc92b0a020a95ddb044246ca7456765
Thomas Storwick <tstorwick@gmail.com>
2020-07-02 19:57:05 -0400

    Update demonstrations/tutorial_local_cost_functions.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- demonstrations/tutorial_local_cost_functions.py
+++ demonstrations/tutorial_local_cost_functions.py
@@ -40,7 +40,7 @@ and attempts to analyze the behavior of the entire circuit from this limited sco
 
 Cerezo et al. also handily prove that these local cost functions are bounded by the 
 global ones, i.e. if a global cost function is formulated in the manner discribed
-by Cerezo, then its corrisponding local cost function will always be 
+by Cerezo, then its corresponding local cost function will always be 
 less than or equal to the global.
 
 Imports
