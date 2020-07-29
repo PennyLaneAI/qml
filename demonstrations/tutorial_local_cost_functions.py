2d215bbebe19b79bcfdfeedce13e7b1c445fb62a
Thomas Storwick <tstorwick@gmail.com>
2020-07-28 18:27:36 -0600

    Update demonstrations/tutorial_local_cost_functions.py
    Co-authored-by: Maria Schuld <mariaschuld@gmail.com>

--- demonstrations/tutorial_local_cost_functions.py
+++ demonstrations/tutorial_local_cost_functions.py
@@ -82,7 +82,7 @@ dev = qml.device("default.qubit", wires=wires, shots=10000, analytic=False)
 # learn the identity gate, a natural cost function is the probability of measuring the 
 # zero state, denoted here as :math:`p_{|0\rangle}`.
 #
-# .. math:: C = \langle | \psi(theta) | \left(I - |0\rangle \langle 0|_0\right) \rangle =1-p_{|0\rangle}
+# .. math:: C = \langle  \psi(\theta) | \left(I - |0\rangle \langle 0|_0\right) \rangle  | \psi(\theta)  \rangle   =1-p_{|0\rangle}
 #
 # We will apply this across all qubits for our global cost function, i.e.,:
 #
