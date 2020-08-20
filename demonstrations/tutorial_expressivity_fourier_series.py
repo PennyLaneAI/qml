fc53777c673cb7fbd92623767e4d9af80c7846f1
Maria Schuld <mariaschuld@gmail.com>
2020-08-20 12:23:55 +0200

    Update demonstrations/tutorial_expressivity_fourier_series.py
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

--- demonstrations/tutorial_expressivity_fourier_series.py
+++ demonstrations/tutorial_expressivity_fourier_series.py
@@ -61,7 +61,7 @@ Quantum models as Fourier series
 # As illustrated in the picture below (which is Figure 1 from the paper),
 # the "encoding Hamiltonians" in :math:`S(x)` determine the set
 # :math:`\Omega` of available "frequencies", and the remainder of the
-# circuit, including the trainable parameters, determine the coefficients
+# circuit, including the trainable parameters, determines the coefficients
 # :math:`c_{\omega}`.
 # 
 
