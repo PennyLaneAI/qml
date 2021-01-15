6a13a660e5c8ec7a66efd4496e1c60e41027cb1d
Theodor Isacsson <theodor.isacsson@gmail.com>
2021-01-15 12:32:10 -0500

    minor fixes

--- demonstrations/tutorial_data_reuploading_classifier.py
+++ demonstrations/tutorial_data_reuploading_classifier.py
@@ -36,7 +36,7 @@ single-qubit variational quantum circuit to achieve this goal. The data
 is generated as a set of random points in a plane :math:`(x_1, x_2)` and
 labeled as 1 (blue) or 0 (red) depending on whether they lie inside or
 outside a circle. The goal is to train a quantum circuit to predict the
-label (red or blue) given an input point’s coordinate.
+label (red or blue) given an input point's coordinate.
 
 .. figure:: ../demonstrations/data_reuploading/universal_circles.png
    :scale: 65%
@@ -114,7 +114,7 @@ which can be encoded as fixed vectors (Blue = :math:`[1, 0]`, Red =
 after transforming our input state through alternate applications of
 data layer and weights.
 
-We can use the idea of the “collapse” of our quantum state into
+We can use the idea of the "collapse" of our quantum state into
 one or other class. This happens when we measure the quantum state which
 leads to its projection as either the state 0 or 1. We can compute the
 fidelity (or closeness) of the output state to the class label making
@@ -159,7 +159,7 @@ Deep Neural Networks.
 
 Pérez-Salinas et al. (2019) make a connection to Deep Neural Networks by
 describing that in their approach the
-“layers” :math:`L_i(\vec \theta_i, \vec x )` are analogous to the size
+"layers" :math:`L_i(\vec \theta_i, \vec x )` are analogous to the size
 of the intermediate hidden layer of a neural network. And the concept of
 deep (multiple layers of the neural network) relates to the number
 of qubits. So, multiple qubits with entanglement between them could
@@ -455,8 +455,8 @@ plt.show()
 ##############################################################################
 # References
 # ----------
-# [1] Pérez-Salinas, Adrián, et al. “Data re-uploading for a universal
-# quantum classifier.” arXiv preprint arXiv:1907.02085 (2019).
+# [1] Pérez-Salinas, Adrián, et al. "Data re-uploading for a universal
+# quantum classifier." arXiv preprint arXiv:1907.02085 (2019).
 #
 # [2] Kingma, Diederik P., and Ba, J. "Adam: A method for stochastic
 # optimization." arXiv preprint arXiv:1412.6980 (2014).
