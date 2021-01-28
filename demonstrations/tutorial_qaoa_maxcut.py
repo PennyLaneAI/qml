23956c5a47db946714bd6818bfa11dc561069fa7
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2021-01-28 13:29:14 -0500

    Add seed to QAOA maxcut.

--- demonstrations/tutorial_qaoa_maxcut.py
+++ demonstrations/tutorial_qaoa_maxcut.py
@@ -11,7 +11,7 @@ QAOA for MaxCut
 .. related::
    tutorial_qaoa_intro Intro to QAOA
 
-*Author: PennyLane dev team. Last updated: 6 Aug 2020.*
+*Author: PennyLane dev team. Last updated: 28 Jan 2021.*
 
 """
 ##############################################################################
