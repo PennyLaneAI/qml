d80f20967d02379ff7786bca1990f16c4226b5ac
Maria Schuld <mariaschuld@gmail.com>
2021-01-29 13:20:13 +0200

    fix import bug

--- demonstrations/tutorial_kernel_based_training.py
+++ demonstrations/tutorial_kernel_based_training.py
@@ -132,7 +132,7 @@ import torch
 from torch.nn.functional import relu
 
 from sklearn.svm import SVC
-from sklearn.datasets import load_iris()
+from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
