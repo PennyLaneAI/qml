fe528d9c6367ba9e60a1a9415e0c50ce2cc79a22
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2021-03-18 13:36:54 -0400

    Finish discussion on concentration of measure.

--- demonstrations/tutorial_haar_measure.py
+++ demonstrations/tutorial_haar_measure.py
@@ -487,7 +487,7 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 # These are constraints are where step 2 comes in - the outcome of a generic
 # QR decomposition are an *orthonormal* matrix :math:`Q`, and upper
 # triangular matrix :math:`R`. Since our original matrix was complex-valued, we end
-# up with a :math:`Q` that is in fact already unitary. But why not stop there? Why 
+# up with a :math:`Q` that is in fact already unitary. But why not stop there? Why
 # do we then perform steps 3 and 4?
 #
 # Steps 3 and 4 are needed because, while the QR decomposition yields a unitary,
@@ -497,12 +497,12 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 # eigenvalue should be equally likely. Just using the QR decomposition out of
 # the box produces an *uneven* distribution of eigenvalues of the unitaries!
 #
-# This discrepancy stems from the fact that the QR decomposition is not unique. 
+# This discrepancy stems from the fact that the QR decomposition is not unique.
 # We can take any unitary diagonal matrix :math:`\Lambda`, and re-express the decomposition
 # as :math:`QR = Q\Lambda \Lambda^\dagger R = Q^\prime R^\prime`. Step 3 removes this
 # redundancy by fixing a :math:`\Lambda` that depends on :math:`R`, leading to a unique
 # value of :math:`Q^\prime = Q \Lambda`, and a uniform distribution of eigenvalues.
-# 
+#
 # .. admonition:: Try it!
 #
 #    Use the ``qr_haar`` function above to generate random unitaries and construct
@@ -525,14 +525,14 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 # Invariance of the Haar measure
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# Earlier, we showed that the Haar measure is used when integrating functions over 
+# Earlier, we showed that the Haar measure is used when integrating functions over
 # the unitary group:
 #
 # .. math::
 #
 #    \int_{V \in U(N)} f(V) d\mu_N(V).
 #
-# One interesting feature of the Haar measure is that it is both left and right *invariant* 
+# One interesting feature of the Haar measure is that it is both left and right *invariant*
 # under unitary transformations. That is,
 #
 # .. math::
@@ -551,51 +551,62 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 # so all distributions must be the same.  The specific distribution is complex
 # numbers :math:`a+bi` where both :math:`a` and :math:`b` has mean 0 and variance
 # :math:`1/N` [#Meckes2014]_ (so, much like Ginibre ensemble we used in the QR decomposition
-# above, but with a different variance and constraints due to orthonormality). 
+# above, but with a different variance and constraints due to orthonormality).
 #
 # Concentration of measure
 # ~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # An unfortunate (although interesting) property of the Haar measure is that it
 # suffers from the phenomenon of `concentration of measure
-# <https://en.wikipedia.org/wiki/Concentration_of_measure>`__. Most of the "stuff"
-# in the space concentrates around a certain area, and this gets worse as the 
-# size of the system increases.
+# <https://en.wikipedia.org/wiki/Concentration_of_measure>`__. Most of the
+# "stuff" in the space concentrates around a certain area, and this gets worse
+# as the size of the system increases. You can see the beginnings of by looking
+# at the sphere. For the 3-dimensional sphere, we saw graphically how there is
+# concentration around the equator. This becomes increasingly prominent for
+# `higher-dimensional spheres <https://en.wikipedia.org/wiki/N-sphere>`__.
 #
-# You can see the beginnings of by looking at the sphere. For the 3-dimensional
-# sphere, we saw how there is a concentration of "stuff" around the
-# equator. This becomes increasingly prominent for `higher-dimensional spheres
-# <https://en.wikipedia.org/wiki/N-sphere>`__. Suppose we have the sphere
-# :math:`S^{2n-1}`, and some function :math:`f` that maps points on that sphere
-# to real numbers. Sample a point :math:`x` on that sphere from the uniform
+# Let's consider an :math:`N`-dimensional unit sphere. Points on the sphere, or
+# vectors in this space, are parametrized by :math:`N-1` real coordinates.  Now
+# suppose we have some function :math:`f` that maps points on that sphere to
+# real numbers. Sample a point :math:`x` on that sphere from the uniform
 # measure, and compute the value of :math:`f(x)`. How close do you think the
-# result will be to the mean value of the function, :math:`E[f]`, over the entire
-# sphere?
+# result will be to the mean value of the function, :math:`E[f]`, over the
+# entire sphere?
 #
-# A result called *Levy's lemma* bounds the probability of a randomly selected
-# :math:`x` result that deviates from :math:`E[f]` by an amount :math:`\epsilon`:
+# A result called `Levy's lemma
+# <https://en.wikipedia.org/wiki/Concentration_of_measure#Concentration_on_the_sphere>`__
+# [#Gerken2013]_, [#Hayden2006]_ bounds the probability that :math:`f(x)` for a
+# randomly selected :math:`x` result deviates from :math:`E[f]` by some amount
+# :math:`\epsilon`:
 #
 # .. math::
-# 
-#    \hbox{Pr}(|f(x) - E[f]| \ge \epsilon) \leq 2 \exp\left[-\frac{n\epsilon^2}{9\pi^3 \eta^2}\right]
+#
+#    \hbox{Pr}(|f(x) - E[f]| \ge \epsilon) \leq 2 \exp\left[-\frac{N\epsilon^2}{9\pi^3 \eta^2}\right]
 #
 # A constraint on the function :math:`f` is that it must be `Lipschitz
 # continuous <https://en.wikipedia.org/wiki/Lipschitz_continuity>`__, where
-# :math:`\eta` is the *Lipschitz constant* of the function. Clearly the larger
-# the deviation :math:`\epsilon`, the less likely you would be to encounter that
-# value at random. Furthermore, increasing the dimension :math:`n` also makes
-# the deviation exponentially less likely.
+# :math:`\eta` is the *Lipschitz constant* of the function. The important aspect
+# here is the likelihood of deviating significantly from the mean by an amount
+# :math:`\epsilon` decreases exponentially with :math:`\epsilon.` Furthermore,
+# increasing the dimension :math:`N` also makes the deviation exponentially less
+# likely.
 #
 # Now, this result seems unrelated to quantum states -- it concerns higher-
 # dimensional spheres. However, recall that a quantum state vector is a complex
-# vector whose squared values sum to 1, just like a spheres. If you "unroll"
-# a quantum state vector of dimension :math:`2^n` by stacking its real and
-# complex parts, you end with a space isomorphic to that of a sphere of
-# dimension X [TODO: verify dimensions in this whole section]. Given that
-# measure concentrates on sphere, and quantum states can be trivially made to
-# look like spheres, functions on random quantum states will also demonstrate
-# concentration! This is bad news because it means that the more qubits you have
-# the more the random states will look the same.  [TODO: rephrase]
+# vector whose squared values sum to 1, similar to vectors on a sphere. If you
+# "unroll" a quantum state vector of dimension :math:`N = 2^n` by stacking its
+# real and complex parts, you end with a vector of length :math:`2 \cdot 2^{n}`
+# which ends up behaving just like a unit vector on the sphere in this
+# dimension. Given that measure concentrates on spheres, and quantum state
+# vectors can be converted to vectors on spheres, functions on random quantum
+# states will also demonstrate concentration.
+#
+# This is bad news! To do useful things in quantum computing, we need a lot of
+# qubits. But the more qubits we have, the more random states will look the same
+# (specifically, random states will concentrate around the maximally entangled
+# state [#Hayden2006]_). This has important consequences for near-term
+# algorithms (as detailed in the next section), and any algorithm that involves
+# sampling quantum states and operations uniformly at random.
 #
 # Haar measure and barren plateaus
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -615,13 +626,13 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 # most from the barren plateau problem [#McClean2018]_, [#Holmes2021]_.
 # :doc:`Barren plateaus </demos/tutorial_barren_plateaus>` are regions in the
 # space of a parametrized circuit where both the gradient and its variance
-# rapidly approach 0, leading your optimizer to get stuck in a local minimum.
+# rapidly approach 0, leading the optimizer to get stuck in a local minimum.
 # This was explored recently in the work of [#Holmes2021]_, wherein closeness to
 # the Haar measure was actually used as a metric for expressivity. The closer
 # things are to the Haar measure, the more expressive they are, but they are
 # also more prone to exhibiting barren plateaus.
 #
-# 
+#
 # .. figure:: /demonstrations/haar_measure/holmes-costlandscapes.png
 #    :align: center
 #    :width: 50%
@@ -631,7 +642,7 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 #    producing unitaries in something close to a Haar-random manner) is very
 #    likely to have flat cost landscapes and suffer from the barren plateau
 #    problem.
-#    
+#
 # It turns out that the types of ansatze know as *hardware efficient ansatze*
 # also suffer from this problem if they are "random enough" (this notion will be
 # formalized in a future demo). It was shown in [#McClean2018]_ that this is a
@@ -646,7 +657,7 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 ######################################################################
 # Conclusion
 # ----------
-#  
+#
 # The Haar measure plays an important role in quantum computing - anywhere
 # you might be dealing with sampling random circuits, or averaging over
 # all possible unitary operations, you'll want to do so with respect
@@ -667,47 +678,58 @@ plot_bloch_sphere(qr_haar_bloch_vectors)
 #
 # .. [#deGuise2018]
 #
-#     H. de Guise, O. Di Matteo, L. L. Sánchez-Soto. "Simple factorization of unitary
-#     transformations" `Phys. Rev. A 97 022328 <https://journals.aps.org/pra/abstract/10.1103/PhysRevA.97.022328>`__
-#     (2018). (`arXiv <https://arxiv.org/abs/1708.00735>`__)
+#     H. de Guise, O. Di Matteo L. L. Sánchez-Soto. (2018) "Simple factorization
+#     of unitary transformations", `Phys. Rev. A 97 022328
+#     <https://journals.aps.org/pra/abstract/10.1103/PhysRevA.97.022328>`__.
+#     (`arXiv <https://arxiv.org/abs/1708.00735>`__)
 #
 # .. [#Clements2016]
 #
-#     W. R. Clements, P. C. Humphreys, B. J. Metcalf, W. S.Kolthammer, and I. A. Walmsley, “Optimal design for universal multiport interferometers,” \
-#     `Optica 3, 1460–1465 <https://www.osapublishing.org/optica/fulltext.cfm?uri=optica-3-12-1460&id=355743>`__ (2016).
+#     W. R. Clements, P. C. Humphreys, B. J. Metcalf, W. S. Kolthammer, and
+#     I. A. Walmsley (2016) “Optimal design for universal multiport
+#     interferometers”, \ `Optica 3, 1460–1465
+#     <https://www.osapublishing.org/optica/fulltext.cfm?uri=optica-3-12-1460&id=355743>`__.
 #     (`arXiv <https://arxiv.org/abs/1603.08788>`__)
 #
 # .. [#Reck1994]
 #
-#    M. Reck, A. Zeilinger, H. J. Bernstein, and P. Bertani, “Experimental
-#    realization of any discrete unitary operator,” `Phys. Rev. Lett.73, 58–61
-#    <https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.73.58>`__
-#    (1994).
+#    M. Reck, A. Zeilinger, H. J. Bernstein, and P. Bertani (1994) “Experimental
+#    realization of any discrete unitary operator”, `Phys. Rev. Lett.73, 58–61
+#    <https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.73.58>`__.
 #
 # .. [#Mezzadri2006]
 #
-#     F. Mezzadri. "How to generate random matrices from the classical compact groups"
+#     F. Mezzadri (2006) "How to generate random matrices from the classical compact groups".
 #     (`arXiv <https://arxiv.org/abs/math-ph/0609050>`__)
 #
 # .. [#Meckes2014]
 #
-#     E. Meckes. `The Random Matrix Theory of the Classical Compact Groups  
-#     <https://case.edu/artsci/math/esmeckes/Haar_book.pdf>`_
+#     E. Meckes (2019) `"The Random Matrix Theory of the Classical Compact Groups"
+#     <https://case.edu/artsci/math/esmeckes/Haar_book.pdf>`_, Cambridge University Press.
 #
+# .. [#Gerken2013]
+#
+#     M. Gerken (2013) "Measure concentration: Levy's Lemma" (`lecture notes
+#     <http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.679.2560?`__).
+#
+# .. [#Hayden2006]
+#
+#     P. Hayden, D. W. Leung, and A. Winter (2006) "Aspects of generic entanglement", 
+#     `Comm. Math. Phys. Vol. 265, No. 1, pp. 95-117 <https://link.springer.com/article/10.1007%2Fs00220-006-1535-6>`__.
+#     (`arXiv <https://arxiv.org/abs/quant-ph/0407049>`__)
 #
 # .. [#McClean2018]
 #
-#     McClean, J. R., Boixo, S., Smelyanskiy, V. N., Babbush, R., & Neven,
-#     H. (2018). Barren plateaus in quantum neural network training
-#     landscapes. `Nature Communications, 9(1),
+#     J. R. McClean, S. Boixo, V. N. Smelyanskiy, R. Babbush, and H. Neven
+#     (2018) "Barren plateaus in quantum neural network training
+#     landscapes", `Nature Communications, 9(1)
 #     <http://dx.doi.org/10.1038/s41467-018-07090-4>`__.
-#     `(arXiv) <https://arxiv.org/abs/1803.11173>`__
+#     (`arXiv <https://arxiv.org/abs/1803.11173>`__)
 #
-# 
 # .. [#Holmes2021]
 #
-#     Holmes, Z., Sharma, K., Cerezo, M., & Coles, P. J. (2021). Connecting ansatz
-#     expressibility to gradient magnitudes and barren plateaus. `(arXiv) 
-#     <https://arxiv.org/abs/2101.02138>`__.
+#     Z. Holmes, K. Sharma, M. Cerezo, and P. J. Coles (2021) "Connecting ansatz
+#     expressibility to gradient magnitudes and barren plateaus". (`arXiv
+#     <https://arxiv.org/abs/2101.02138>`__)
 #
 #
