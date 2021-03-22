330c83149ab73acdecc34cf751b8d06b9b179c78
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2021-03-22 14:23:36 -0400

    Zoom in on Bloch plots. Fix alignment in SUn decomp figure.

--- demonstrations/tutorial_haar_measure.py
+++ demonstrations/tutorial_haar_measure.py
@@ -228,27 +228,26 @@ def plot_bloch_sphere(bloch_vectors):
     ax = fig.add_subplot(111, projection='3d')
     fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
+    ax.grid(False)
+    ax.set_axis_off()
+    ax.view_init(30, 45)
+    ax.dist = 7
+
     # Draw the axes (source: https://github.com/matplotlib/matplotlib/issues/13575)
     x, y, z = np.array([[-1.5,0,0], [0,-1.5,0], [0,0,-1.5]])
     u, v, w = np.array([[3,0,0], [0,3,0], [0,0,3]])
     ax.quiver(x, y, z, u, v, w, arrow_length_ratio=0.05, color="black", linewidth=0.5)
-
-    ax.grid(False)
-    ax.set_axis_off()
-
-    ax.view_init(30, 45)
 
     ax.text(0, 0, 1.7, r"|0⟩", color="black", fontsize=16)
     ax.text(0, 0, -1.9, r"|1⟩", color="black", fontsize=16)
     ax.text(1.9, 0, 0, r"|+⟩", color="black", fontsize=16)
     ax.text(-1.7, 0, 0, r"|–⟩", color="black", fontsize=16)
     ax.text(0, 1.7, 0, r"|i+⟩", color="black", fontsize=16)
-    ax.text(0,-1.9, 0, r"|i-⟩", color="black", fontsize=16)
+    ax.text(0,-1.9, 0, r"|i–⟩", color="black", fontsize=16)
 
     ax.scatter(
         bloch_vectors[:,0], bloch_vectors[:,1], bloch_vectors[:, 2], c='#e29d9e', alpha=0.3
     )
-    plt.show()
 
 plot_bloch_sphere(not_haar_bloch_vectors)
 
