6a3ae4e35d277520d2b36d8084aefe2262d65542
bdoolittle <brian.d.doolittle@gmail.com>
2021-05-06 14:46:53 -0500

    Create tutorial_classical_shadows.py

---
+++ demonstrations/tutorial_classical_shadows.py
@@ -0,0 +1,14 @@
+# Metadata
+
+# Introduction to classical shadows
+
+# Outline for Classical shadow algorithm.
+    # def classical_shadow(circuit_template, num_shadows) 
+
+# Simple example of classical shadow computation for qubits
+
+# Multi-qubit application of classical shadow
+    
+
+# Application of classical shadow/ performance comparison
+    # TODO: what is a good application?
