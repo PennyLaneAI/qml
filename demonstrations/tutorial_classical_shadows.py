16ea08b61ff4be65b8b9ae3021f75217017f1af6
rooler <roel.wier@gmail.com>
2021-05-06 23:00:53 -0400

    added comments, multiqubit is working

--- demonstrations/tutorial_classical_shadows.py
+++ demonstrations/tutorial_classical_shadows.py
@@ -8,17 +8,20 @@ import pennylane.numpy as np
 
 
 def classical_shadow(circuit_template, params, num_shadows: int) -> np.ndarray:
+    # create a dict so we can quickly call qml.Observables from integers.
     unitary_ensemble = {0: lambda n: qml.Identity(n),
                         1: lambda n: qml.PauliX(n),
                         2: lambda n: qml.PauliY(n),
                         3: lambda n: qml.PauliZ(n)}
-
+    # each shadow is one shot, so we set this parameter in the qml.device
     device_shadow = qml.device('default.qubit', wires=nqubits, shots=1)
-    # sample random unitaries, where 0 = I and 1,2,3 = X,Y,Z
-    randints = np.random.randint(1, 4, size=(number_of_shadows, nqubits))
+    # sample random unitaries uniformly, where 0 = I and 1,2,3 = X,Y,Z
+    randints = np.random.randint(0, 4, size=(number_of_shadows, nqubits))
     unitaries = []
     for ns in range(num_shadows):
+        # for each shadow, add a random Clifford observable at each location
         unitaries.extend(unitary_ensemble[int(randints[ns, i])](i) for i in range(nqubits))
+    # use the QNodeCollections through qml.map to calculate multiple observables for the same circuit
     samples = qml.map(circuit_template, observables=unitaries, device=device_shadow, measure='sample')(params)
     # reshape from dim 1 vector to matrix, each row now corresponds to a shadow
     samples = samples.reshape((number_of_shadows, nqubits))
@@ -27,15 +30,20 @@ def classical_shadow(circuit_template, params, num_shadows: int) -> np.ndarray:
 
 
 def estimate_shadow_obervable(shadows, observable):
+    # https://github.com/momohuang/predicting-quantum-properties
     sum_product, cnt_match = 0, 0
+    # loop over the shadows:
     for single_measurement in shadows:
         not_match = 0
         product = 1
+        # loop over all the paulis that we care about
         for pauli_XYZ, position in observable:
+            # if the pauli in our shadow does not match, we break and go to the next shadow
             if pauli_XYZ != single_measurement[position + nqubits]:
                 not_match = 1
                 break
             product *= single_measurement[position]
+        # don't record the shadow
         if not_match == 1: continue
 
         sum_product += product
@@ -47,17 +55,14 @@ def estimate_shadow_obervable(shadows, observable):
 # Simple example of classical shadow computation for qubits
 
 nqubits = 1
-
+theta = np.random.randn(nqubits, )
 device_exact = qml.device('default.qubit', wires=nqubits)
-
 number_of_shadows = 1000
 
 paulis = {0: qml.Identity(0).matrix,
           1: qml.PauliX(0).matrix,
           2: qml.PauliY(0).matrix,
           3: qml.PauliZ(0).matrix}
-
-theta = np.random.randn(nqubits, )
 
 
 def circuit(params, wires, **kwargs):
@@ -77,6 +82,17 @@ print(f"Shadow value: {shadow_observable}")
 
 # Multi-qubit application of classical shadow
 
+nqubits = 2
+theta = np.random.randn(nqubits, )
+device_exact = qml.device('default.qubit', wires=nqubits)
+number_of_shadows = 1000
+
+exact_observable = qml.map(circuit, observables=[qml.PauliX(0) @ qml.PauliX(1)], device=device_exact, measure='expval')(
+    theta)
+print(f"Exact value: {exact_observable[0]}")
+shadows = classical_shadow(circuit, theta, number_of_shadows)
+shadow_observable = estimate_shadow_obervable(shadows, [(1, 0), (1, 1)])
+print(f"Shadow value: {shadow_observable}")
 
 # Application of classical shadow/ performance comparison
 # TODO: what is a good application?
