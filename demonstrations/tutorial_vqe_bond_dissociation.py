d406e2da21a6c324a18c025fd69be82c9a0cc55c
Varun Rishi <varunrishi1987@gmail.com>
2021-05-21 15:22:54 +0530

    Alain's suggestions [skip ci]
    Co-authored-by: agran2018 <45397799+agran2018@users.noreply.github.com>

--- demonstrations/tutorial_vqe_bond_dissociation.py
+++ demonstrations/tutorial_vqe_bond_dissociation.py
@@ -249,7 +249,7 @@ plt.show()
 #
 # In a diatomic molecule such as :math:`H_2`, the potential energy curve as a function of
 # internuclear distance tells us the equilibrium bond length ---
-# the distance between the two atoms when the energy is at a minimum and the system is in
+# the distance between the two atoms that minimizes the total electronic energy
 # equilibrium --- and the bond dissociation energy.
 # The bond dissociation energy is calculated as the difference in energy of the system at equilibrium (minimum) and the energy
 # of the system at the dissociation limit, where the atoms are far apart and
