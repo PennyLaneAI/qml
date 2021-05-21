c05017506f9f3f7a9fe4bb7b4d8bcfb2f63e11fc
Varun Rishi <varunrishi1987@gmail.com>
2021-05-21 12:43:47 +0530

    Alain's suggestions [skip ci]
    Co-authored-by: agran2018 <45397799+agran2018@users.noreply.github.com>

--- demonstrations/tutorial_vqe_bond_dissociation.py
+++ demonstrations/tutorial_vqe_bond_dissociation.py
@@ -13,7 +13,7 @@ Modelling chemical reactions on a quantum computer
 *Author: PennyLane dev team. Posted: 19 May 2021. Last updated: 19 May 2021.*
 
 The term "chemical reaction" is another name for the transformation of molecules -- the breaking and 
-forming of bonds. Such transformations involve energy costs that determine 
+forming of bonds. Such transformations are characterized by an energy cost that determine 
 the feasibility of a particular transformation amongst many different possibilities. 
 Computational chemistry offers several theoretical methods for precisely determining this energy
 cost. It also opens a window into predicting the thermodynamic and kinetic aspects of any 
