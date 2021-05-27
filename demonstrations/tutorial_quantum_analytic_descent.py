22a1c12eb455914a8371d50d00d38d97bee495e5
dwierichs <davidwierichs@gmail.com>
2021-05-27 16:28:20 +0200

    Elies" changes

--- demonstrations/tutorial_quantum_analytic_descent.py
+++ demonstrations/tutorial_quantum_analytic_descent.py
@@ -19,7 +19,33 @@ Quantum analytic descent
 
 *Authors: David Wierichs, Elies Gil-Fuster (Xanadu Residents) Posted: ?? May 2021. Last updated: ?? May 2021.*
 
-SUMMARY
+One of the main problems of Many-Body Physics is that of finding the ground 
+state and ground state energy of a given Hamiltonian.
+The Variational Quantum Eigensolver (VQE) algorithm combines a smart circuit 
+design with a gradient based optimization to solve the ground state energy 
+problem.
+One issue for such an approach is, though, that the optimization landscape is 
+non-convex, so reaching a good enough local minimum easily requires hundreds or 
+thousands of update steps.
+
+At the same time, though, we do have a good understanding of the local shape
+of the cost landscape around any reference point.
+Cashing in on this, the Quantum Analytic Descent algorithm proposes using a
+local approximation to the landscape where gradient based optimisation is much
+cheaper.
+Indeed, this approach avoids using the quantum computer to estimate the gradient
+at every single optimization step.
+Instead, we can use the quantum device to estimate the cost values at a few
+points close to the reference one.
+With the cost values at these points, we can build a classical model which 
+approximates the landscape locally, and then perform the optimization routine
+solely on a classical device.
+
+Here we demonstrate how to implement Quantum Analytic Descent on a quantum
+computer using PennyLane.
+Next to the code snippets we provide with the theory required to understand
+what is going on at the different steps, too.
+So: sit down, relax, and enjoy your optimization!
 
 |
 
@@ -44,26 +70,26 @@ VQEs give rise to trigonometric cost functions
 
 In a few words, what we have in mind when we talk about VQEs is a quantum circuit.
 Typically, this :math:`n`-qubit quantum circuit is initialized in the :math:`|0\rangle^{\otimes n}` state (in an abuse of notation, we name it simply :math:`|0\rangle` in the rest of the demo).
-The body of the circuit is populated with a *variational form* :math:`V(\theta)` -- a fixed architecture of quantum gates parametrized by an array of real-numbers :math:`\theta\in\mathbb{R}^m`.
+The body of the circuit is populated with a *variational form* :math:`V(\boldsymbol{\theta})` -- a fixed architecture of quantum gates parametrized by an array of real-numbers :math:`\boldsymbol{\theta}\in\mathbb{R}^m`.
 After the variational form, the circuit ends with a measurement of an observable :math:`\mathcal{M}`, which is also fixed at the start.
 
-With these, the common choice of cost function :math:`E(\theta)` is
+With these, the common choice of cost function :math:`E(\boldsymbol{\theta})` is
 
-.. math:: E(\theta) = |\langle0|V^\dagger(\theta)\mathcal{M}V(\theta)|0\rangle|^2,
+.. math:: E(\boldsymbol{\theta}) = |\langle0|V^\dagger(\boldsymbol{\theta})\mathcal{M}V(\boldsymbol{\theta})|0\rangle|^2,
 
 or, in short
 
-.. math:: E(\theta) = \operatorname{tr}[\rho(\theta)\mathcal{M}],
+.. math:: E(\boldsymbol{\theta}) = \operatorname{tr}[\rho(\boldsymbol{\theta})\mathcal{M}],
 
-where :math:`\rho(\theta)=V(\theta)|0\rangle\!\langle0|V^\dagger(\theta)` is the density matrix of the quantum state after applying the variational form on the initial state.
+where :math:`\rho(\boldsymbol{\theta})=V(\boldsymbol{\theta})|0\rangle\!\langle0|V^\dagger(\boldsymbol{\theta})` is the density matrix of the quantum state after applying the variational form on the initial state.
 
 It can be seen that if the variational form is composed only of Pauli gates, then the cost function is a sum of multilinear trigonometric terms in each of the parameters.
 That's a scary sequence of words!
-What that means is that if we look at :math:`E(\theta)` but we focus on one of the parameter values only, say :math:`\theta_i`, then we can write the functional dependence as a linear combination of three terms: :math:`1`, :math:`\sin(\theta_i)`, and :math:`\cos(\theta_i)` assuming Pauli rotation gates in the circuit.
+What that means is that if we look at :math:`E(\boldsymbol{\theta})` but we focus on one of the parameter values only, say :math:`\boldsymbol{\theta}_i`, then we can write the functional dependence as a linear combination of three terms: :math:`1`, :math:`\sin(\boldsymbol{\theta}_i)`, and :math:`\cos(\boldsymbol{\theta}_i)` assuming Pauli rotation gates in the circuit.
 
-That is, for some coefficients :math:`a_i`, :math:`b_i`, and :math:`c_i` depending on all parameters but one (which we could write for instance as :math:`a_i = a_i(\theta_1, \ldots, \hat\theta_i, \ldots, \theta_m)`, but we don't do it everywhere for the sake of notation ease), we can write :math:`E(\theta)` as
+That is, for some coefficients :math:`a_i`, :math:`b_i`, and :math:`c_i` depending on all parameters but one (which we could write for instance as :math:`a_i = a_i(\boldsymbol{\theta}_1, \ldots, \hat\boldsymbol{\theta}_i, \ldots, \boldsymbol{\theta}_m)`, but we don't do it everywhere for the sake of notation ease), we can write :math:`E(\boldsymbol{\theta})` as
 
-.. math:: E(\theta) = a_i + b_i\sin(\theta_i) + c_i\cos(\theta_i).
+.. math:: E(\boldsymbol{\theta}) = a_i + b_i\sin(\boldsymbol{\theta}_i) + c_i\cos(\boldsymbol{\theta}_i).
 
 Let's look at a toy example to illustrate this!
 """
@@ -89,6 +115,9 @@ parameters = np.array([3.3, .5])
 # Evaluate the circuit for these parameters.
 print(f"At the parameters {np.round(parameters,4)} the energy is {circuit(parameters)}")
 ###############################################################################
+# It is this simple in PennyLane to obtain the energy of the given state
+# for a specific Hamiltonian!
+# Let us now look at how the energy value depends on each of the two parameters:
 
 # Create 1D sweeps through parameter space with the other parameter fixed.
 num_samples = 50
@@ -134,13 +163,13 @@ line2 = ax.plot(theta_func, [parameters[0]]*num_samples, C2, label="$E(\\theta_1
 # As explained in the paper, an approximation via trigonometric series up to second order is already a sound candidate!
 # In particular, we want to approximate
 #
-# .. math:: E(\theta) := \operatorname{tr}[\mathcal{M}\Phi(\theta)\rho_0]
+# .. math:: E(\boldsymbol{\theta}) := \operatorname{tr}[\mathcal{M}\Phi(\boldsymbol{\theta})\rho_0]
 #
-# in the vicinity of a reference point :math:`\theta_0`.
-# Here :math:`\rho_0` is the density matrix of the initial state, and :math:`\Phi(\theta)` is the quantum channel that implements the variational form.
+# in the vicinity of a reference point :math:`\boldsymbol{\theta}_0`.
+# Here :math:`\rho_0` is the density matrix of the initial state, and :math:`\Phi(\boldsymbol{\theta})` is the quantum channel that implements the variational form.
 # We then have:
 #
-# .. math:: \hat{E}(\theta_0+\theta) := A(\theta) E^{(A)} + \sum_{i=1}^m[B_i(\theta)E_i^{(B)} + C_i(\theta) E_i^{(C)}] + \sum_{i<j}^m[D_{ij}(\theta) E_{ij}^{(D)}].
+# .. math:: \hat{E}(\boldsymbol{\theta}_0+\boldsymbol{\theta}) := A(\boldsymbol{\theta}) E^{(A)} + \sum_{k=1}^m[B_k(\boldsymbol{\theta})E_k^{(B)} + C_k(\boldsymbol{\theta}) E_k^{(C)}] + \sum_{l>k}^m[D_{kl}(\boldsymbol{\theta}) E_{kl}^{(D)}].
 #
 # We have introduced a number of :math:`E`\ 's, in order to build each of these we need to sample some points in the landscape with a quantum computer.
 # Before eyeing the formulas for each of them, though, it is important we now only need to estimate :math:`2m^2+m+1` points (as we will see in the model formulas further below).
@@ -150,11 +179,11 @@ line2 = ax.plot(theta_func, [parameters[0]]*num_samples, C2, label="$E(\\theta_1
 # Granted, our model represents the true landscape less accurately the further we go away from the reference point :math:`\theta_0`, but nonetheless the global minimum *of the model* will bring us much closer to the global minimum *of the true cost* than a random walk.
 # And hereby, the complete strategy:
 #
-# #. Set an initial reference point :math:`\theta_0`.
-# #. Build the model :math:`\hat{E}(\theta)\approx E(\theta_0+\theta)` at this point.
-# #. Find the global minimum :math:`\theta_\text{min}` of the model.
-# #. Set :math:`\theta_\text{min}` as the new reference point :math:`\theta_0`, go back to step 2.
-# #. After convergence or a fixed number of iterations, output the last global minimum :math:`\theta_\text{opt}=\theta_\text{min}`.
+# #. Set an initial reference point :math:`\boldsymbol{\theta}_0`.
+# #. Build the model :math:`\hat{E}(\boldsymbol{\theta})\approx E(\boldsymbol{\theta}_0+\boldsymbol{\theta})` at this point.
+# #. Find the global minimum :math:`\boldsymbol{\theta}_\text{min}` of the model.
+# #. Set :math:`\boldsymbol{\theta}_\text{min}` as the new reference point :math:`\boldsymbol{\theta}_0`, go back to step 2.
+# #. After convergence or a fixed number of iterations, output the last global minimum :math:`\boldsymbol{\theta}_\text{opt}=\boldsymbol{\theta}_\text{min}`.
 #
 # This provides an iterative strategy which will take us to a good enough solution much faster (in number of iterations) than for example regular stochastic gradient descent (SGD).
 #
@@ -173,9 +202,9 @@ line2 = ax.plot(theta_func, [parameters[0]]*num_samples, C2, label="$E(\\theta_1
 # .. math::
 #
 #   E^{(A)} &= E(\boldsymbol{\theta})\\
-#   E^{(B)}_i &= E(\boldsymbol{\theta}+\frac{\pi}{2}\boldsymbol{v}_i)-E(\boldsymbol{\theta}-\frac{\pi}{2}\boldsymbol{v}_i)\\
-#   E^{(C)}_i &= E(\boldsymbol{\theta}+\pi\boldsymbol{v}_i)\\
-#   E^{(D)}_{ij} &= E(\boldsymbol{\theta}+\frac{\pi}{2}\boldsymbol{v}_i+\frac{\pi}{2}\boldsymbol{v}_j) + E(\boldsymbol{\theta}-\frac{\pi}{2}\boldsymbol{v}_i-\frac{\pi}{2}\boldsymbol{v}_j) - E(\boldsymbol{\theta}+\frac{\pi}{2}\boldsymbol{v}_i-\frac{\pi}{2}\boldsymbol{v}_j) - E(\boldsymbol{\theta}-\frac{\pi}{2}\boldsymbol{v}_i+\frac{\pi}{2}\boldsymbol{v}_j)
+#   E^{(B)}_k &= E(\boldsymbol{\theta}+\frac{\pi}{2}\boldsymbol{v}_k)-E(\boldsymbol{\theta}-\frac{\pi}{2}\boldsymbol{v}_k)\\
+#   E^{(C)}_k &= E(\boldsymbol{\theta}+\pi\boldsymbol{v}_k)\\
+#   E^{(D)}_{kl} &= E(\boldsymbol{\theta}+\frac{\pi}{2}\boldsymbol{v}_k+\frac{\pi}{2}\boldsymbol{v}_l) + E(\boldsymbol{\theta}-\frac{\pi}{2}\boldsymbol{v}_k-\frac{\pi}{2}\boldsymbol{v}_l) - E(\boldsymbol{\theta}+\frac{\pi}{2}\boldsymbol{v}_k-\frac{\pi}{2}\boldsymbol{v}_l) - E(\boldsymbol{\theta}-\frac{\pi}{2}\boldsymbol{v}_k+\frac{\pi}{2}\boldsymbol{v}_l)
 #
 # Let us create a function that will take care of evaluating :math:`E` at all these shifted parameter points and combines the results to obtain the above coefficients:
 
@@ -215,7 +244,7 @@ def get_model_data(fun, params, *args):
     return E_A, E_B, E_C, E_D
 
 ###############################################################################
-# Let's test our brand-new function at a random parameter position.
+# Let's test our brand-new function at a random parameter position!
 
 parameters = np.random.random(2) * 2 * np.pi
 print(f"Random parameters (params): {parameters}")
@@ -236,25 +265,25 @@ print(f"Coefficients at params:",
 #
 # .. math::
 #
-#   A(\boldsymbol{\theta}) &= \prod_{k=1}^m \cos\left(\frac{\theta_k}{2}\right)^2\\
-#   B_i(\boldsymbol{\theta}) &= \cos\left(\frac{\theta_i}{2}\right)\sin\left(\frac{\theta_i}{2}\right)\prod_{k\neq i} \cos\left(\frac{\theta_k}{2}\right)^2\\
-#   C_i(\boldsymbol{\theta}) &= \sin\left(\frac{\theta_i}{2}\right)^2\prod_{k\neq i} \cos\left(\frac{\theta_k}{2}\right)^2\\
-#   D_{ij}(\boldsymbol{\theta}) &= \cos\left(\frac{\theta_i}{2}\right)\sin\left(\frac{\theta_i}{2}\right)\cos\left(\frac{\theta_j}{2}\right)\sin\left(\frac{\theta_j}{2}\right)\prod_{j\neq k\neq i} \cos\left(\frac{\theta_k}{2}\right)^2
+#   A(\boldsymbol{\theta}) &= \prod_{i=1}^m \cos\left(\frac{\theta_i}{2}\right)^2\\
+#   B_k(\boldsymbol{\theta}) &= \cos\left(\frac{\theta_k}{2}\right)\sin\left(\frac{\theta_k}{2}\right)\prod_{i\neq k} \cos\left(\frac{\theta_i}{2}\right)^2\\
+#   C_k(\boldsymbol{\theta}) &= \sin\left(\frac{\theta_k}{2}\right)^2\prod_{i\neq k} \cos\left(\frac{\theta_i}{2}\right)^2\\
+#   D_{kl}(\boldsymbol{\theta}) &= \cos\left(\frac{\theta_k}{2}\right)\sin\left(\frac{\theta_k}{2}\right)\cos\left(\frac{\theta_l}{2}\right)\sin\left(\frac{\theta_l}{2}\right)\prod_{k\neq i\neq l} \cos\left(\frac{\theta_i}{2}\right)^2
 #
 # While this expression for these functions seems quite long, it shows us a nice relation between the four sets of trigonometric polynomials :math:`A`, :math:`B`, :math:`C`, and :math:`D`:
 #
 # .. math::
 #
-#   B_i(\boldsymbol{\theta}) &= \tan\left(\frac{\theta_i}{2}\right)A(\boldsymbol{\theta})\\
-#   C_i(\boldsymbol{\theta}) &= \tan\left(\frac{\theta_i}{2}\right)^2 A(\boldsymbol{\theta})\\
-#   D_{ij}(\boldsymbol{\theta}) &= \tan\left(\frac{\theta_i}{2}\right)\tan\left(\frac{\theta_j}{2}\right)A(\boldsymbol{\theta})
+#   B_k(\boldsymbol{\theta}) &= \tan\left(\frac{\theta_k}{2}\right)A(\boldsymbol{\theta})\\
+#   C_k(\boldsymbol{\theta}) &= \tan\left(\frac{\theta_k}{2}\right)^2 A(\boldsymbol{\theta})\\
+#   D_{kl}(\boldsymbol{\theta}) &= \tan\left(\frac{\theta_k}{2}\right)\tan\left(\frac{\theta_l}{2}\right)A(\boldsymbol{\theta})
 #
 # Using these, we can compute the classical surrogate model :math:`\tilde{E}(\boldsymbol{\theta})`:
 #
 # .. math::
 #
-#   \tilde{E}(\boldsymbol{\theta}) &= A(\theta) E^{(A)} + \sum_i B_i(\boldsymbol{\theta}) E^{(B)}_i + C_i(\boldsymbol{\theta}) E^{(C)}_i + \sum_{i<j} D_{ij}(\boldsymbol{\theta}) E^{(D)}_{ij}\\
-#   \phantom{\tilde{E}(\boldsymbol{\theta})}&=A(\boldsymbol{\theta})\left[E^{(A)}+\sum_i \tan\left(\frac{\theta_i}{2}\right)E^{(B)}_i + \tan\left(\frac{\theta_i}{2}\right)^2 E^{(C)}_i + \sum_{i<j} \tan\left(\frac{\theta_i}{2}\right)\tan\left(\frac{\theta_j}{2}\right)E^{(D)}_{ij}\right]
+#   \tilde{E}(\boldsymbol{\theta}) &= A(\theta) E^{(A)} + \sum_k B_k(\boldsymbol{\theta}) E^{(B)}_k + C_k(\boldsymbol{\theta}) E^{(C)}_k + \sum_{l>k} D_{kl}(\boldsymbol{\theta}) E^{(D)}_{kl}\\
+#   \phantom{\tilde{E}(\boldsymbol{\theta})}&=A(\boldsymbol{\theta})\left[E^{(A)}+\sum_k \tan\left(\frac{\theta_k}{2}\right)E^{(B)}_k + \tan\left(\frac{\theta_k}{2}\right)^2 E^{(C)}_k + \sum_{l>k} \tan\left(\frac{\theta_k}{2}\right)\tan\left(\frac{\theta_l}{2}\right)E^{(D)}_{kl}\right]
 #
 # Let's implement this model:
 
@@ -301,8 +330,8 @@ print(f"E_A and E_original are the same: {coeffs[0]==E_original}")
 print(f"E_model and E_original are the same: {E_model==E_original}")
 
 ###############################################################################
-# As we can see, the model works fine and reproduces the correct energy at the parameter position :math:`\theta_0` at which we constructed it.
-# When we move away from :math:`\theta_0`, the model starts to deviate:
+# As we can see, the model works fine and reproduces the correct energy at the parameter position :math:`\boldsymbol{\theta}_0` at which we constructed it.
+# When we move away from :math:`\boldsymbol{\theta}_0`, the model starts to deviate:
 
 # Obtain a random shift away from parameters
 shift = 0.1 * np.random.random(2)
@@ -318,7 +347,7 @@ print(f"E_model and E_original are the same: {E_model==E_original}")
 
 ###############################################################################
 # Let's try to visualize how sensible our approximate model is.
-# We let our toy model to be :math:`2`-dimensional so we could make the following :math:`3D`-plot.
+# We let our toy model to be 2-dimensional so we could make the following 3D-plot.
 # Here, in orange we see the true landscape, and in blue the approximate model.
 # The vertical rod marks the reference point, from where we computed the trigonometric expansion.
 #
@@ -465,15 +494,17 @@ plot_cost_and_model(circuit, mapped_model, past_parameters[2])
 ###############################################################################
 # Step 3: Both, the model and the original cost function show a minimum close to our parameter position, Quantum Analytic Descent converged.
 #
-# Quantum Analytic Descent in PennyLane
-# -------------------------------------
-#
-#[ELIES: Should we write extensively about the code in the text? or just be very thorough in documenting it via comments? David: Comments should be enough :-)]
-
-
-
-###############################################################################
-# CONCLUSION
+# In this demo we've seen how to implement the Quantum Analytic Descent algorithm
+# using PennyLane for a toy-example of a Variational Quantum Eigensolver.
+# By making extensive use of 3D-plots we have also tried to illustrate exactly
+# what is going on in both the true cost landscape and the trigonometric expansion
+# approximation.
+# Recall we wanted to avoid working on the true landscape itself because we can
+# only access it via very costly quantum computations.
+# Instead, a few runs on the quantum device allowed us to construct a classical
+# model on which we performed classical (cheap) optimization.
+# Don't forget to fasten your seat belts on your way home, it was a pleasure
+# having you here today.
 #
 # References
 # ----------
