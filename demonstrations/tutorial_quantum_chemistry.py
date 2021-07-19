7e9887e7f0b22a1f9add3c405a15d1f3149d1e86
agran2018 <nanoalain@gmail.com>
2021-07-19 18:22:10 -0400

    Fix typo

--- demonstrations/tutorial_quantum_chemistry.py
+++ demonstrations/tutorial_quantum_chemistry.py
@@ -213,7 +213,7 @@ multiplicity = 1
 # As mentioned above, molecular orbitals are represented as a linear combination
 # of atomic orbitals which are typically modeled as `Gaussian-type orbitals
 # <https://en.wikipedia.org/wiki/Gaussian_orbital>`_. We can specify different types
-# of `Gaussian atomic basis <https://www.basissetexchange.org/>`_. In this example we
+# of `Gaussian atomic bases <https://www.basissetexchange.org/>`_. In this example we
 # choose a `minimal basis set
 # <https://en.wikipedia.org/wiki/Basis_set_(chemistry)#Minimal_basis_sets>`_.
 
