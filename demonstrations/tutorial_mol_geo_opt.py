d643f3e8449e101f3f4e2eb86639a4a9f49de948
Jay Soni <jbsoni@uwaterloo.ca>
2021-09-29 07:49:55 -0400

    Update demonstrations/tutorial_mol_geo_opt.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- demonstrations/tutorial_mol_geo_opt.py
+++ demonstrations/tutorial_mol_geo_opt.py
@@ -221,7 +221,7 @@ num_wires = 6
 dev = qml.device("default.qubit", wires=num_wires)
 
 ##############################################################################
-# Next, we define the `cost`` function :math:`g(\theta, x)` which depends on
+# Next, we define the ``cost`` function :math:`g(\theta, x)` which depends on
 # both the circuit and the Hamiltonian parameters. Specifically we consider the
 # expectation values of the Hamiltonian.
 
