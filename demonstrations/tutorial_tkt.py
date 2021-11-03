14304a2196a8e7b9bdfec361e005e6070fe1ef0d
Catalina Albornoz <catalina@xanadu.ai>
2021-11-03 18:40:56 -0500

    Changed tutorial_tkt and built locally

--- demonstrations/tutorial_tkt.py
+++ demonstrations/tutorial_tkt.py
@@ -5,8 +5,16 @@ Carnival
 .. meta::
     :property="og:description": Party with PennyLane.
 
-*Author: PennyLane dev team.*
+Congratulations on finding this ticket! 🤩 Be sure to note down the code and submit it at the end of November.
 
+🖊️ `Sign up <https://pennylane.ai/carnival/register.html>`__ for the carnival if you haven't done so already.
+
+If you were looking for a demo and can't find it, why not create the demo yourself?
+Quantum computing is built on thousands of people making one contribution at a time.
+
+During November new community demos will recieve Quantum Carnival tickets 🎟️ worth several points.
+
+Keep exploring PennyLane and having fun doing quantum! 🥳
 """
 
 ##############################################################################
