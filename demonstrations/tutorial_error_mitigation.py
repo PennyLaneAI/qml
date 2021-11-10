7e27aa2e7b434aeb8379b2ca6605c474b8b35703
trbromley <brotho02@gmail.com>
2021-11-10 11:24:54 -0500

    Add to intro

--- demonstrations/tutorial_error_mitigation.py
+++ demonstrations/tutorial_error_mitigation.py
@@ -7,12 +7,19 @@ Error mitigation with Mitiq and PennyLane
 
     :property="og:image": https://pennylane.ai/qml/_images/laptop.png
 
-.. related::
-   tutorial_classical_shadows Classical shadows
-
-*Author: PennyLane dev team. Last updated: 10 November 2021*
+*Author: Mitiq and PennyLane dev teams. Last updated: 10 November 2021*
 
 Have you ever run a circuit on quantum hardware and not quite got the result you were expecting?
-If so, welcome to the world of noisy intermediate scale quantum (NISQ) devices!
+If so, welcome to the world of noisy intermediate scale quantum (NISQ) devices! These devices must
+function in noisy environments and are unable to execute quantum circuits perfectly, resulting in
+outputs that can have a significant error. The long-term plan of quantum computing is to develop a
+subsequent generation of error-corrected hardware. In the meantime, how can we best utilize our
+error-prone NISQ devices for practical tasks? One proposed solution is to adopt an approach called
+error *mitigation*, which aims to minimize the effects of noise by executing a family of related
+circuits and using the results to estimate an error-free value.
+
+This demo shows how error mitigation can be carried out by combining PennyLane with the
+`Mitiq <https://github.com/unitaryfund/mitiq>`__ package, which is a Python-based library providing
+a range of error mitigation techniques.
 
 """
