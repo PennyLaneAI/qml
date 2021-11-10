7b6bd0fa928a88fb1a379fe680be3b0c9f81dc13
trbromley <brotho02@gmail.com>
2021-11-10 10:49:09 -0500

    Add metadata and first sentences

--- demonstrations/tutorial_error_mitigation.py
+++ demonstrations/tutorial_error_mitigation.py
@@ -2,4 +2,17 @@
 Error mitigation with Mitiq and PennyLane
 =========================================
 
+.. meta::
+    :property="og:description": Learn how to mitigate quantum circuits using Mitiq and PennyLane.
+
+    :property="og:image": https://pennylane.ai/qml/_images/laptop.png
+
+.. related::
+   tutorial_classical_shadows Classical shadows
+
+*Author: PennyLane dev team. Last updated: 10 November 2021*
+
+Have you ever run a circuit on quantum hardware and not quite got the result you were expecting?
+If so, welcome to the world of noisy intermediate scale quantum (NISQ) devices!
+
 """
