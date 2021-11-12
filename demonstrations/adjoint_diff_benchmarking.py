6646f6afea0f0149b4ac9b893419e2f7a9660303
albi3ro <chrissie.c.l@gmail.com>
2021-11-12 17:43:46 +0000

    revamped benchmarking file

--- demonstrations/adjoint_diff_benchmarking.py
+++ demonstrations/adjoint_diff_benchmarking.py
@@ -23,89 +23,110 @@ from pennylane import numpy as np
 
 plt.style.use("bmh")
 
-rng = np.random.default_rng(seed=42)
+n_samples = 5
 
-# Scaling over wires section
+def get_time(qnode, params):
+    globals_dict = {'grad': qml.grad, 'circuit': qnode, 'params': params}
+    return timeit.timeit("grad(circuit)(params)", globals=globals_dict, number=n_samples)
 
-n_wires_ls = [3, 6,9,12,15, 18]
+def wires_scaling(n_wires, n_layers):
 
-n_layers = 5
+    rng = np.random.default_rng(seed=42)
 
-t_exec_wires = []
-t_grad_wires = []
+    t_adjoint = []
+    t_ps = []
+    t_backprop = []
 
-for i_wires in n_wires_ls:
-    
-    dev = qml.device("lightning.qubit", wires=i_wires)
-    
-    @qml.qnode(dev, diff_method="adjoint")
-    def circuit(params):
-        qml.StronglyEntanglingLayers(params, wires=range(i_wires))
+    def circuit(params, wires):
+        qml.StronglyEntanglingLayers(params, wires=range(wires))
         return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1) @ qml.PauliZ(2))
+
+    for i_wires in n_wires:
+        
+        dev = qml.device("lightning.qubit", wires=i_wires)
+        dev_python = qml.device("default.qubit", wires=i_wires)
+        
+        circuit_adjoint = qml.QNode(lambda x: circuit(x, wires=i_wires), dev, diff_method="adjoint")
+        circuit_ps = qml.QNode(lambda x: circuit(x, wires=i_wires), dev, diff_method="parameter-shift")
+        circuit_backprop = qml.QNode(lambda x: circuit(x, wires=i_wires), dev_python, diff_method="backprop")
+
+        # set up the parameters
+        param_shape = qml.StronglyEntanglingLayers.shape(n_wires=i_wires, n_layers=n_layers)
+        params = rng.standard_normal(param_shape, requires_grad=True)
+
+        t_adjoint.append(get_time(circuit_adjoint, params))
+        t_backprop.append(get_time(circuit_backprop, params))
+        t_ps.append(get_time(circuit_ps, params))
+
+    return t_adjoint, t_backprop, t_ps
+
+
+def layers_scaling(n_wires, n_layers):
     
-    # set up the parameters
-    param_shape = qml.StronglyEntanglingLayers.shape(n_wires=i_wires, n_layers=n_layers)
-    params = rng.standard_normal(param_shape, requires_grad=True)
-    
-    ti_exec = timeit.timeit("circuit(params)", globals=globals(), number=100)
-    t_exec_wires.append(ti_exec)
-    
-    ti_grad = timeit.timeit("qml.grad(circuit)(params)", globals=globals(), number=100)
-    t_grad_wires.append(ti_grad)
+    rng = np.random.default_rng(seed=42)
 
+    dev = qml.device("lightning.qubit", wires=n_wires)
+    dev_python = qml.device('default.qubit', wires=n_wires)
 
+    t_adjoint = []
+    t_ps = []
+    t_backprop = []
 
-# Scaling over layers section
-
-n_wires = 9
-dev = qml.device("lightning.qubit", wires=n_wires)
-
-n_layers_ls = [1,2,3,4,5,6,7,8]
-
-t_exec_layers = []
-t_grad_layers = []
-
-for i_layers in n_layers_ls:
-
-    @qml.qnode(dev, diff_method="adjoint")
     def circuit(params):
         qml.StronglyEntanglingLayers(params, wires=range(n_wires))
-        return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1) @ qml.PauliZ(2) @ qml.PauliZ(3))
+        return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1) @ qml.PauliZ(2))
+
+    circuit_adjoint = qml.QNode(circuit, dev, diff_method="adjoint")
+    circuit_ps = qml.QNode(circuit, dev, diff_method="parameter-shift")
+    circuit_backprop = qml.QNode(circuit, dev_python, diff_method="backprop")
+
+    for i_layers in n_layers:
+
+        # set up the parameters
+        param_shape = qml.StronglyEntanglingLayers.shape(n_wires=n_wires, n_layers=i_layers)
+        params = rng.standard_normal(param_shape, requires_grad=True)
+        
+        t_adjoint.append(get_time(circuit_adjoint, params))
+        t_backprop.append(get_time(circuit_backprop, params))
+        t_ps.append(get_time(circuit_ps, params))
+
+    return t_adjoint, t_backprop, t_ps
+
+if __name__ == "__main__":
+
+    layers_list= [1, 3, 5, 7, 9]
+    n_wires = 5
+    adjoint_layers, backprop_layers, ps_layers = layers_scaling(n_wires, layers_list)
+
     
-    # set up the parameters
-    param_shape = qml.StronglyEntanglingLayers.shape(n_wires=n_wires, n_layers=i_layers)
-    params = rng.standard_normal(param_shape, requires_grad=True)
-    
-    ti_exec = timeit.timeit("circuit(params)", globals=globals(), number=100)
-    t_exec_layers.append(ti_exec)
-    
-    ti_grad = timeit.timeit("qml.grad(circuit)(params)", globals=globals(), number=100)
-    t_grad_layers.append(ti_grad)
+    wires_list = [3, 6, 9, 12, 15]
+    n_layers = 3
+    adjoint_wires, backprop_wires, ps_wires = wires_scaling(wires_list, n_layers)
+    # Generating the graphic
 
-# Generating the graphic
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+    ax1.plot(wires_list, adjoint_wires, '.-', label="adjoint")
+    ax1.plot(wires_list, ps_wires, '.-', label="parameter-shift")
+    ax1.plot(wires_list, backprop_wires, '.-', label="backprop")
 
-ax1.plot(n_wires_ls, t_exec_wires, '.-', label="circuit")
-ax1.plot(n_wires_ls, t_grad_wires, '.-', label="gradient")
+    ax1.legend()
 
-ax1.legend()
+    ax1.set_xlabel("Number of wires")
+    ax1.set_xticks(wires_list)
+    ax1.set_ylabel("Log Time")
+    ax1.set_yscale("log")
+    ax1.set_title("Scaling with wires")
 
-ax1.set_xlabel("Number of wires")
-ax1.set_xticks(n_wires_ls)
-ax1.set_ylabel("Log Time")
-ax1.set_yscale("log")
-ax1.set_title("Scaling with wires")
+    ax2.plot(layers_list, adjoint_layers, '.-', label="adjoint")
+    ax2.plot(layers_list, ps_layers, '.-', label="parameter-shift")
+    ax2.plot(layers_list, backprop_layers, '.-', label="backprop")
 
-ax2.plot(n_layers_ls, t_exec_layers, '.-', label="circuit")
-ax2.plot(n_layers_ls, t_grad_layers, '.-', label="gradient")
+    ax2.legend()
 
-ax2.legend()
+    ax2.set_xlabel("Number of layers")
+    ax2.set_xticks(layers_list)
+    ax2.set_ylabel("Time")
+    ax2.set_title("Scaling with Layers")
 
-ax2.set_xlabel("Number of layers")
-ax2.set_ylabel("Time")
-ax2.set_title("Scaling with Layers")
-
-plt.savefig("adjoint_diff/scaling.png")
-
-
+    plt.savefig("adjoint_diff/scaling.png")
