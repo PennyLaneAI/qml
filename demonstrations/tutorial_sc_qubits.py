ac9ce109e5388544f25b082b2cae16b6ed65a1dd
alvaro-at-xanadu <alvaro@xanadu.ai>
2021-11-29 17:10:54 -0500

    added homodyne detection and J.Junction

--- demonstrations/tutorial_sc_qubits.py
+++ demonstrations/tutorial_sc_qubits.py
@@ -12,36 +12,32 @@ Superconducting qubits
 
 *Author: PennyLane dev team. Posted: XX November 2021. Last updated: XX November 2021.*
 
-Quantum computers are developing at a much faster rate than many physicists could 
-have predicted. The various research groups at companies and universities share the 
-vision to create a quantum computer that is useful and available to everyone. 
-While many seem to be on the right track, the claim that near-term quantum computers 
-will be used for everyday industrial applications is debatable. Realizing this vision 
-will take a bit more time and a continuous joint effort between the scientific 
-and engineering communities across the world. The main issue is scalability: the ability
-to control and measure many qubits without destroying their quantum properties.
-The good news is that we have a plethora of approaches to solving this problem. 
-We can try making quantum computers by trapping ions, creating artificial atoms 
-using superconductors, using groups of photons to encode photons, among many other options. 
-Each of the techniques seems promising on its own, but they also present 
-unique engineering challenges.
+Quantum computers are developing at a much faster rate than many physicists could have predicted.
+However, the claim that near-term quantum computers will be used for everyday industrial 
+applications is debatable. Realizing this vision will take a bit more time and a continuous 
+joint effort between the scientific and engineering communities across the world. The main 
+issue is scalability: the ability to control and measure many qubits without destroying
+their quantum properties. The good news is that we have a plethora of approaches 
+to solving this problem. We can try making quantum computers by trapping ions, 
+creating artificial atoms using superconductors, using groups of photons to encode 
+photons, among many other options. Each of the techniques seems promising on its own, 
+but they also present unique engineering challenges.
 
 This article is an introduction to **superconducting qubits** as an approach to 
 building quantum computers. This technology is being used by renowned companies 
 such as Google and IBM in their quest to pioneer the quantum era. Indeed, 
-superconducting qubits are pretty promising. Google's Sycamore claimed quantum 
-advantage back in 2019, and IBM recently built its Eagle quantum computer with 127 
-qubits, twice as many as its main competitors! The central insight that allows for 
-these quantum computers is that superconductivity is a quantum phenomenon, so we 
-can use superconducting circuits as quantum systems that we can control at will.  
-These circuits, although quantum, are rather large and need to be cooled down to 
-almost absolute zero to be functional. They are nothing but a modification of 
-current microchip technology adapted to work with superconductors, so we have most 
-of the infrastructure in place! However, the large size of the qubit makes it prone 
-to decoherence, making it more short-lived than other types of qubits. Nevertheless, 
-we can get around this, and the results speak for themselves.
+superconducting qubits are pretty promising. Google's Sycamore claimed quantum advantage 
+back in 2019, and IBM recently built its Eagle quantum computer with 127 qubits, 
+twice as many as its main competitors! The central insight that allows for these 
+quantum computers is that superconductivity is a quantum phenomenon, so we can use 
+superconducting circuits as quantum systems that we can control at will. 
+They are nothing but a modification of current microchip technology adapted 
+to work with superconductors, so we have most of the infrastructure in place! 
+However, the large size of the qubit makes it prone to decoherence, making 
+it more short-lived than other types of qubits. Nevertheless, we can get 
+around this, and the results speak for themselves.
 
-y the end of this demo, you will learn how superconductors are used to create, 
+By the end of this demo, you will learn how superconductors are used to create, 
 prepare, control, and measure the state of a qubit. Moreover, you will identify 
 the strengths and weaknesses of this technology in terms of Di Vincenzo's criteria, as 
 introduced in the box below. You will be armed with the basic concepts to understand 
@@ -78,28 +74,6 @@ that are bound to come soon.
 #
 # Superconductivity
 # ~~~~~~~~~~~~~~~~~~
-#
-# How many states of matter are there? Recalling middle-school science class, most
-# people would say three: solid, liquid, and gas (and maybe a fourth one known as plasma).
-# However, these states of matter are a particular manifestation of the more general
-# concept of a thermodynamic *phase*. A phase is a large-scale property of a material
-# that changes abruptly at a specific temperature, pressure, or other external stimuli.
-# The structure of water and other materials is undoubtedly an example, but others
-# might be less familiar. For example, if we heat a magnet enough, it will suddenly
-# lose its ferromagnetic properties. Magnetization, indeed, represents a  thermodynamic
-# phase of matter. A further example is electrical conductivity. The resistance to the
-# passage of electric current tends to go down as we reduce the temperature. At a low 
-# enough temperature, this resistance will suddenly drop to zero for certain materials 
-# known as **superconductors**. 
-#
-# The sudden change in physical properties remained a mystery for centuries. After all,
-# physics usually deals with continuous phenomena, so how can discontinuous jumps 
-# like these occur? One of the prominent triumphs of the discipline known as statistical 
-# mechanics was to explain these *phase transitions* by modelling the internal components
-# of matter and their interactions in various ways. While some of the models in this 
-# framework are heuristic, we can understand their validity better thanks to quantum 
-# mechanics. Let us try to understand, conceptually, why some materials become superconductors
-# in sufficiently cold environments.
 #
 # To introduce superconductivity, we need to understand why some materials resist the passage 
 # of electrons at the microscopic level. The electrons that flow through a conductor are 
@@ -177,7 +151,7 @@ that are bound to come soon.
 # or *superconducting resonator*, which turns out to be a physical realization 
 # of the quantum harmonic oscillator. The LC circuit has two components. 
 # The first one is a capacitor :math:`C`, which consists of two parallel metallic plates
-# with equal and opposite charges. The second one is an inductor :math:``,
+# with equal and opposite charges. The second one is an inductor :math:`L`,
 # which is connected in between the capacitor plates. When a varying
 # current goes through the inductor, it exerts a force on the charges opposite
 # to their motion. But the attraction between the plates is proportional
@@ -193,9 +167,133 @@ that are bound to come soon.
 #
 # .. math:: E = n\hbar\omega.
 # 
-# where $n$ is a natural number, $\hbar$ is Planck's constant, and $\omega$ is the resonant 
+# where :math:`n`is a natural number, :math:`\hbar` is Planck's constant, and :math:`\omega` is the resonant 
 # frequency of the circuit, which depends on the physical characteristics of the inductor 
-# and the capacitor. When a circuit has energy $n\hbar\omega$, we denote its state 
+# and the capacitor. When a circuit has energy :math:`\hbar\omega`, we denote its state 
 # as $\left\lvert n \right\rangle$, known as a *Fock state*. These discrete energy values 
 # in the circuit can be observed in the simple LC superconducting circuit. 
 #
+# Josephson junctions and artificial atoms
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Therefore, unlike atoms, where the energy levels are unevenly spaced, superconducting 
+# resonators cannot serve as a qubit. If we want to get around this issue, we should 
+# find a way to change the energy level differences in our circuit. The reason that the 
+# energy levels are evenly spaced boils down to the fact that the resistive force exerted
+# by an inductor is proportional to the change in the current going through it. If this 
+# relation between current and force weren't that simple, the energy levels could be 
+# spaced unevenly. Therefore what we need is a different type of inductor with a more 
+# complicated *non-linear behaviour*.
+#
+# There is in fact a ciruit element that does precisely this: a *Josephson junction*.
+# This element consists of a very thin piece of an insulating placed between two 
+# superconducting metals. At first thought, the Josephson junction should play the 
+# role of a resistor, and since we have superconducting metals on either side of it, 
+# no current should go through it. Here is where *tunneling*, another famous quantum 
+# effect, comes into play. Because of the spread-out nature of their wave function,
+# Cooper pairs can actually go through the Josephson junction with some probability. 
+# The net effect is that the junction acts as a nonlinear inductor, introducing 
+# anharmonicity in the energy levels.
+#
+# Let us study the variation in energy levels in more detail. In quantum mechanics, 
+# systems are described by Hamiltonians, which play a double role. Firstly, they allow 
+# us to calculate the allowed energies. Secondly, they tell us how an initial quantum 
+# state will change in time. Deriving the Hamiltonian is a crucial step when we want to 
+# study the properties of quantum systems, and it is generally hard to do. For the quantum 
+# harmonic oscillator, it turns out to be of the form (this will be put in a box)
+#
+# .. math:: \hat{H}= a\hat{X}^2 + b\hat{P}^2
+#
+# where :math:`\hat{X}` and :math:`\hat{P}`` are (infinite dimensional) matrices that satisfy 
+# :math:`\hat{X}\hat{P}-\hat{P}\hat{X}=i\hbar I`. The physical significance of these
+# matrices depends on the system in question. For waves in a solid, :math:`\hat{X}` 
+# represent the position of a particle, while :math:`\hat{P}` represents its momentum. 
+# In the case of a superconducting resonator, :math:`\hat{X}` stands for the magnetic 
+# flux through the inductor and is denoted by :math:`\hat{\phi}`, and $:math:`\hat{P}` corresponds 
+# to the charge in the capacitor, and is denoted by :math:`\hat{Q}` . The Hamiltonian for 
+# the resonator reads
+#
+# .. math:: \hat{H}_{res}=4 E_C (\hat{Q}-Q_g I)^2+4E_J \cos(\hat{\phi}).
+#
+# where blah blah. It is the last term that describes the energy contained in the 
+# Josephson junction. Finding the eigenvalues of :math:`\hat{H_{sc}}`, we can find 
+# what the allowed energy level are. They turn out to depend on the value of :math:`Q_g`. 
+# The dependence of the energy levels with :math:`Q_g` looks as follows: **(draw plot)**. 
+# for different values of :math:`E_J/E_C`. We see that if this quotient is too large, 
+# then the anharmonicity is lost, but if it's too low, then small variations in :math:`Q_g` 
+# can affect the energy levels. THe idea is to find a goldilocks zone, where 
+# these variations are do not affect the energy differences too much, so the 
+# value of :math:`Q_g` doesn't matter much, but we can also still resolve the energy differences. 
+# With our current experimental bandwidths available, this occurs for :math:`E_J/E_C \sim 80`, 
+# the **transmon regime**. 
+#
+# Homodyne measurement
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Here I still need to write this works. The idea is that the cavity+superconductor 
+# circuit is driven to create a coherent state, which is then rotated in phase space.
+# This drive is done away from the resonance frequency of the cavity. The angle of 
+# this rotation depends on the state of the qubit, so the coherent state is entangled 
+# with it. Here is the circuit that implements this:
+# 
+import pennylane as qml
+from pennylane import numpy as np
+import matplotlib.pyplot as plt 
+
+dev = qml.device("default.gaussian", wires=1, shots=50)
+
+@qml.qnode(dev)
+def measure_X_shots(alpha, phi):
+    qml.Displacement(alpha, 0, wires=0)
+    qml.Rotation(phi, wires=0)
+    return qml.sample(qml.X(0))
+
+@qml.qnode(dev)
+def measure_P_shots(alpha, phi):
+    qml.Displacement(alpha, 0, wires=0)
+    qml.Rotation(phi, wires=0)
+    return qml.sample(qml.P(0))
+
+##############################################################################
+# The larger alpha, the better we can resolve our quantum states when measuring. 
+# However, alpha is proportional to time, so there is decoherence badness. 
+# phi is the angle of rotation in phase space, positive for ground qubit state, 
+# and negative for excited qubit state. 
+
+X1=measure_X_shots(3,0.1)
+X2=measure_X_shots(3,-0.1)
+P1=measure_P_shots(3,0.1)
+P2=measure_P_shots(3,-0.1)
+
+plt.scatter(X1,P1)
+plt.scatter(X2,P2)
+
+##############################################################################
+
+X1=measure_X_shots(6,0.1)
+X2=measure_X_shots(6,-0.1)
+P1=measure_P_shots(6,0.1)
+P2=measure_P_shots(6,-0.1)
+
+plt.scatter(X1,P1)
+plt.scatter(X2,P2)
+
+##############################################################################
+
+X1=measure_X_shots(12,0.1)
+X2=measure_X_shots(12,-0.1)
+P1=measure_P_shots(12,0.1)
+P2=measure_P_shots(12,-0.1)
+
+plt.scatter(X1,P1)
+plt.scatter(X2,P2)
+
+##############################################################################
+# 
+# Vertical axis is momentum. So if we measure momentum, 
+# we can resolve given that we drive for a long enough time. 
+
+
+
+
+
