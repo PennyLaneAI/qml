fa5c69f224acd3f731bdd8df3d2e61cce2cfd7ba
soranjh <soranjh@yahoo.com>
2022-01-05 19:31:32 -0500

    add optimal sector

--- demonstrations/tutorial_qubit_tapering.py
+++ demonstrations/tutorial_qubit_tapering.py
@@ -36,17 +36,17 @@ with one of its eigenvalues.
 
 The molecular qubit Hamiltonian is constructed as a linear combination of Pauli strings as
 
-      .. math:: H = \sum_{i=1}^r c_i \eta_i
+.. math:: H = \sum_{i=1}^r c_i \eta_i
 
 where :math:`c_i` is a real coefficient and :math:`\eta_i` is a Pauli string acting on a number of
 qubits. The Hamiltonian :math:`H` is constructed such that :math:`\eta_i \in P_M` where
 
-      .. math:: P_M = \pm \left \{ I, \sigma_x, \sigma_y, \sigma_z  \right \}^{\bigotimes M},
+.. math:: P_M = \pm \left \{ I, \sigma_x, \sigma_y, \sigma_z  \right \}^{\bigotimes M},
 
 and :math:`M` is the total number of qubits. The aim here is to find a unitary operator :math:`U`
 that is applied to :math:`H` to provide a transformed Hamiltonian :math:`H'`
 
-      .. math:: H' = U^{\dagger} H U = \sum_{i=1}^r c_i \mu_i,
+.. math:: H' = U^{\dagger} H U = \sum_{i=1}^r c_i \mu_i,
 
 such that each :math:`\mu_i` term acts trivially on a set of :math:`k` qubits and
 :math:`\mu_i \equiv U^{\dagger} \eta_i U`.
@@ -60,7 +60,7 @@ We now define the Abelian group :math:`S \in P_M` such that each term in :math:`
 group :math:`S \in P_M` and :math:`-I \notin S`, there is a set set of generators
 :math:`S = \left \langle \tau_1, ..., \tau_k \right \rangle` such that
 
-      .. math:: \tau_i = U \sigma_x^i U^{\dagger}.
+.. math:: \tau_i = U \sigma_x^i U^{\dagger}.
 
 Based on the definition of :math:`S`, we already know that :math:`[\tau_i ,\eta_j] = 0`,
 then it is easy to show that :math:`[\mu_j, \sigma_x^i] = 0`. This implies that there are :math:`k`
@@ -86,15 +86,35 @@ print(hamiltonian)
 
 ##############################################################################
 # The Hamiltonian contains 15 terms acting on one to four qubits. This Hamiltonian can be
-# transformed such that it acts trivially on some of the qubits.
+# transformed such that it acts trivially on some of the qubits. To do that we first need to obtain
+# the generators and the Pauli-X operators that are used to construct the unitary Clifford
+# operators. These are obtained with the :func:`~.pennylane.hf.generate_symmetries` function
 
 generators, paulix_ops = qml.hf.generate_symmetries(hamiltonian, len(hamiltonian.wires))
 
-paulix_sector = [1, -1, -1]
+##############################################################################
+# Once the Clifford operator is applied, each of the Hamiltonian terms will act on the qubits ...
+# with a Pauli-X operator. For each of these ... wires, we can simply replace the Pauli-X operator
+# with either :math:`+1` or :math:`-1` and remove the corresponding qubit from the Hamiltonian
+# terms. This results in a total number of :math:`2^n` Hamiltonians each corresponding to one
+# eigenvalue sector. The optimal sector corresponding to the ground state energy of the molecule can
+# be obtained from the reference Hartree-Fock state and the generated symmetries using the
+# :func:`~.pennylane.hf.optimal_sector` function
+
+active_electrons = 2
+paulix_sector = qml.hf.optimal_sector(hamiltonian, generators, active_electrons)
+print(paulix_sector)
+
+##############################################################################
+# We can now build the tapered Hamiltonian with the :func:`~.pennylane.hf.transform_hamiltonian`
+# function
 
 H_tapered = qml.hf.transform_hamiltonian(hamiltonian, generators, paulix_ops, paulix_sector)
-
 print(H_tapered)
+
+##############################################################################
+# The new Hamiltonian has only three non-zero terms acting on only 1 wire! Let's now compute the
+# ground state energy at the Hartree-Fock level and with VQE.
 
 ##############################################################################
 # Conclusions
