eef5582634a4c3436b97f9b9e99377d3a02d3eef
jamesellis1999 <55260564+jamesellis1999@users.noreply.github.com>
2022-01-31 10:54:53 +0000

    Add hyperlinks to arXiv references

--- demonstrations/tutorial_quantum_gans.py
+++ demonstrations/tutorial_quantum_gans.py
@@ -595,8 +595,8 @@ plt.show()
 
 ######################################################################
 # [1] Ian J. Goodfellow et al. *Generative Adversarial Networks*.
-# arXiv:1406.2661 (2014).
+# `arXiv:1406.2661 <https://arxiv.org/abs/2010.06201>`__ (2014).
 #
 # [2] He-Liang Huang et al. *Experimental Quantum Generative Adversarial
-# Networks for Image Generation*. arXiv:2010.06201 (2020).
+# Networks for Image Generation*. `arXiv:2010.06201 <https://arxiv.org/abs/1406.2661>`__ (2020).
 #
