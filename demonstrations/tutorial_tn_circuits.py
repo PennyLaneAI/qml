4ede339da6f5fde4afa4c140cfc14ae73a061924
Diego <diego_guala@hotmail.com>
2022-03-04 17:18:15 -0500

    Addressed most review comments

--- demonstrations/tutorial_tn_circuits.py
+++ demonstrations/tutorial_tn_circuits.py
@@ -1,11 +1,11 @@
 r"""
 .. _tn_circuits:
 
-Tensor Network Quantum Circuits
+Tensor-Network Quantum Circuits
 ==============================
 
 .. meta::
-    :property="og:description": This demonstration explins how to simulate tensor network quantum circuits.
+    :property="og:description": This demonstration explins how to simulate tensor-network quantum circuits.
     :property="og:image": https://pennylane.ai/qml/_images/thumbnail_tn_circuits.png
 
 .. related::
@@ -16,39 +16,40 @@ Tensor Network Quantum Circuits
 
 *Author: PennyLane dev team. Last updated: 2 March 2022.*
 
-This demonstration explains how to use PennyLane templates to design and implement tensor network quantum circuits
-as in `Huggins et. al. (2018) <https://arxiv.org/abs/1803.11537>`__.
-
-Background
-------------
-
-Tensor network quantum circuits emulate the shape and connectivity of tensor networks such as matrix product states 
-(MPS) and tree tensor networks (TTN). By following tensor network architectures, quantum circuits can become easier
-to simulate classically and provide a gradual transition from circuits that are classically simulable to circuits 
-that require a quantum computer to evaluate (Huggins et. al., 2018)
+This demonstration explains how to use PennyLane templates to design and implement tensor-network quantum circuits
+as in `Huggins et. al. (2018) <https://arxiv.org/abs/1803.11537>`__. Tensor-network quantum circuits emulate the shape and connectivity of tensor networks such as matrix product states 
+(MPS) and tree tensor networks (TTN).
 
 We begin with a short introduction to tensor networks and explain their relationship to quantum circuits. Next, we 
 demonstrate how PennyLane's templates make it easy to design, customize, and simulate these circuits. Finally, we
-show how to use the templates to solve a toy machine learning problem.
+show how to use the templates to learn to classify the bars and stripes data set. This is a toy problem where the template
+learns to recognize whether an image exhibits horizontal stripes or vertical bars.
 
 Tensors and Tensor Networks
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-Tensors are multi-dimensional arrays of numbers. Intuitively, they can be interpreted as a generalization of scalars, vectors, and matrices. 
-Tensor can be described by their rank and bond dimension.
-The rank is the number of indices in a tensor -- a scalar has rank zero, a vector has rank one, and a matrix has rank two.
-Each index can take a certain number of values, we call this the bond dimension. A vector with three elements has a single index which can take three values.
-This vector can be considered a tensor of rank one and bond dimension three.
+Tensors are multi-dimensional arrays of numbers. 
+Intuitively, they can be interpreted as a
+generalization of scalars, vectors, and matrices. 
+Tensors can be described by their rank, indices, and the dimension of the indices.
+The rank is the number of indices in a tensor --- a scalar has 
+rank zero, a vector has rank one, and a matrix has rank two.
+The dimension of an index is how many values that index can take.
+For example, a vector with three elements has one index that can take three
+values. This is vector is therefore a rank one tensor and its index has dimension
+three.
 
 To define tensor networks, it is important to first understand tensor contration.
 Two or more tensors can be contracted by summing over repeated indices.
 In diagrammatic notation, the repeated indices appear as lines connecting tensors as in the figure below. 
-In this figure, we see two tensors of rank two, connected by one repeated index, :math:`k`.
+We see two tensors of rank two, connected by one repeated index, :math:`k`. The dimension of the
+repeated index is called the bond dimension.
 
 .. image:: ../demonstrations/tn_circuits/Simple_TN.PNG
     :align: center
-    :width: 80 %
+    :width: 50 %
 
-The contraction of the tensors above is equivalent to the standard matrix multiplication formula and can be expressed as
+The contraction of the tensors above is equivalent to the standard 
+matrix multiplication formula and can be expressed as
 
 .. math::
     C_{ij} = \sum_{k}A_{ik}B_{kj},
@@ -56,46 +57,53 @@ The contraction of the tensors above is equivalent to the standard matrix multip
 where :math:`C_{ij}` denotes the entry for the :math:`i`-th row and :math:`j`-th column of the product :math:`C=AB`. 
 Here, the number of terms in the summation is equal to the bond dimension of the index :math:`k`.
 
-A tensor network, then, is a collection of tensors where a subset of all indices are contracted based on the connections between tensors.
-Tensor networks can therefore represent complicated operations involving several tensors with many indices contracted in sophisticated patterns.
+A tensor network is a collection of tensors where a subset of 
+all indices are contracted. As mentioned above, we use diagrammatic notation
+to specify which indices and tensors will be contracted together by connecting
+individual tensors with lines.
+Tensor networks can therefore represent complicated operations involving
+several tensors with many indices contracted in sophisticated patterns.
 
-Two well-known tensor network architectures are the MPS and TTN shown below.
+Two well-known tensor network architectures are the MPS and TTN. These follow
+specific patterns of connections between tensors and can be extended to have
+many or few indices. Examples of these architectures with only a few tensors 
+can be seen in the figure below.
 
 .. image:: ../demonstrations/tn_circuits/MPS_TTN.PNG
     :align: center
-    :width: 80 %
+    :width: 50 %
 
-These tensor networks are commonly used to efficiently represent many-body quantum systems in classical simulations.
-We can reverse this, instead designing quantum systems, i.e. quantum circuits, that follow the structure and connectivity of tensor networks.
-We call these circuits *tensor network quantum circuits*.
+These tensor networks are commonly used to efficiently represent many-body quantum
+systems in classical simulations (`Orus, 2014 <https://arxiv.org/abs/1306.2164>`)__.
+We can reverse this, instead designing quantum systems, e.g., quantum circuits, that follow the structure and connectivity of tensor networks.
+We call these circuits *tensor-network quantum circuits*.
 
-In tensor network quantum circuits, the tensor network architecture acts a a meta-template for the quantum circuit.
-In other words, the tensors in the tensor networks above are replaced with unitary operations to obtain quantum circuits of the form:
+In tensor-network quantum circuits, the tensor network architecture acts as a
+guideline for the shape of the quantum circuit.
+More specifically, the tensors in the tensor networks above are replaced with
+unitary operations to obtain quantum circuits of the form:
 
 .. image:: ../demonstrations/tn_circuits/MPS_TTN_Circuit.PNG
     :align: center
-    :width: 99 %
+    :width: 50 %
 
-Since the unitary operations :math:`U_1` to :math:`U_3` are very general mathematical entities, it is not always clear how to implement them with
-the gate sets available in quantum hardware. Instead, we can replace the unitary operations with circuit ansatze that perform a desired operation.
-The PennyLane tensor network templates allow us to do precisely this, implement tensor network quantum circuits with user-defined circuit ansatze
-as the unitary operations.
+Since the unitary operations :math:`U_1` to :math:`U_3` are in principle completely general,
+it is not always clear how to implement them with the gate sets available in quantum hardware. 
+Instead, we can replace the unitary operations with variational quantum circuits,
+determined by a specific template of choice.
+The PennyLane tensor network templates allow us to do precisely this: implement tensor-network quantum
+circuits with user-defined circuit ansatze as the unitary operations. In this sense, just as a template
+is a strategy for arranging parametrized gates, tensor-network quantum circuits
+are strategies for structuring circuit templates. They can therefore be interpreted as templates of templates,
+i.e., as meta-templates. 
 
-not sastisfied with the following paragraph:
-In the following section, we demonstrate how to use PennyLane to build and simulate tensor network quantum circuits.
-We will refer to the circuit ansatze that replace the unitary operations as *blocks*  and note that the bond dimension
-of the tensor network, :math:`d`, is related to the number of wires per block, :math:`n`, as
-
-.. math::
-    d=2^{\frac{n}{2}}
 
 
 PennyLane Design
 ----------------
-
+In this section, we demonstrate how to use PennyLane to build and simulate tensor-network quantum circuits.
 
 Import PennyLane and Numpy
-^^^^^^^^^^^^^^^^^^^^^^^^^^
 """
 
 import pennylane as qml
@@ -103,142 +111,128 @@ from pennylane import numpy as np
 
 ##############################################################################
 # Build the circuit with PennyLane
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# The following cell builds a quantum circuit in the shape of a matrix product state tensor network and prints the result.
-# 
+#
+# The following cell builds a quantum circuit in the shape of a matrix product state tensor network, and prints the result.
+#
 # Block defines a variational quantum circuit that takes the position of tensors in the network.
 
 
-def block(weights,wires):
+def block(weights, wires):
+    qml.RX(weights[0], wires=wires[0])
+    qml.RY(weights[1], wires=wires[1])
     qml.CNOT(wires=wires)
-    qml.RX(weights[0],wires=wires[0])
-    qml.RY(weights[1],wires=wires[1])
 
 
-dev= qml.device('default.qubit',wires=4)
+dev = qml.device("default.qubit", wires=4)
 
-@qml.qnode(dev, expansion_strategy='device')
+
+@qml.qnode(dev, expansion_strategy="device")
 def circuit(template_weights):
-    qml.MPS(wires=range(4),n_block_wires=2,block=block,n_params_block=2,template_weights=template_weights)
+    qml.MPS(
+        wires=range(4),
+        n_block_wires=2,
+        block=block,
+        n_params_block=2,
+        template_weights=template_weights,
+    )
     return qml.expval(qml.PauliZ(wires=3))
 
 
-weights = np.random.random(size=[3,2])
-print(f'The circuit result is {circuit(weights):.02f}')
-print('The circuit looks like:')
-qml.drawer.use_style('black_white')
+weights = np.random.random(size=[3, 2])
+print(f"The circuit result is {circuit(weights):.02f}")
+qml.drawer.use_style("black_white")
 qml.draw_mpl(circuit)(weights)
 
 ##############################################################################
 # Easy circuit editing
-# ^^^^^^^^^^^^^^^^^^^^ 
 #
-# Using the `qml.MPS` template we can easily change the block type, depth, and size. For example:
 #
-# A different block:
-# ^^^^^^^^^^^^^^^^^^
-
-
-def different_block(weights, wires):
-    qml.CNOT(wires=wires)
-    qml.Hadamard(wires=wires[0])
-    qml.Hadamard(wires=wires[1])
-    qml.Rot(weights[0],weights[1],weights[2],wires=wires[0])
-
-dev= qml.device('default.qubit',wires=4)
-
-@qml.qnode(dev, expansion_strategy='device')
-def circuit(template_weights):
-    qml.MPS(wires=range(4),n_block_wires=2,block=different_block,n_params_block=3,template_weights=template_weights)
-    return qml.expval(qml.PauliZ(wires=3))
-
-weights = np.random.random(size=[3,3])
-print(f'The circuit result is {circuit(weights):.02f}')
-print('The circuit looks like:')
-qml.draw_mpl(circuit)(weights)
-
-##############################################################################
+# Using the :class:`~pennylane.MPS` template we can easily change the block type, depth, and size. For example:
 # A deeper block
-# ^^^^^^^^^^^^^^
 
+shape = qml.StronglyEntanglingLayers.shape(n_layers=2,n_wires =2)
 
 def deep_block(weights, wires):
-    qml.SWAP(wires=wires)
-    qml.Hadamard(wires=wires[0])
-    qml.Hadamard(wires=wires[1])
-    qml.Rot(weights[0],weights[1],weights[2],wires=wires[0])
-    qml.Rot(weights[1],weights[2],weights[0],wires=wires[1])
-    qml.SX(wires=wires[0])
-    qml.RX(weights[3],wires=wires[0])
-    qml.RY(weights[4],wires=wires[1])
+    qml.StronglyEntanglingLayers(weights,wires)
 
-dev= qml.device('default.qubit',wires=4)
 
-@qml.qnode(dev, expansion_strategy='device')
+dev = qml.device("default.qubit", wires=4)
+
+
+@qml.qnode(dev, expansion_strategy="device")
 def circuit(template_weights):
-    qml.MPS(wires=range(4),n_block_wires=2,block=deep_block,n_params_block=5,template_weights=template_weights)
+    qml.MPS(
+        wires=range(4),
+        n_block_wires=2,
+        block=deep_block,
+        n_params_block=3,
+        template_weights=template_weights,
+    )
     return qml.expval(qml.PauliZ(wires=3))
 
 
-weights = np.random.random(size=[3,5])
-print(f'The circuit result is {circuit(weights):.02f}')
-print('The circuit looks like:')
+weights = [np.random.random(size=shape)]*3
+print(f"The circuit result is {circuit(weights):.02f}")
 qml.draw_mpl(circuit)(weights)
 
 ##############################################################################
-# A larger block
-# ^^^^^^^^^^^^^^
+# A wider block
 
+
+shapes = qml.SimplifiedTwoDesign.shape(n_layers=1, n_wires=4)
+weights = [np.random.random(size=shape) for shape in shapes]
 
 def large_block(weights, wires):
-    qml.MultiControlledX(control_wires=wires[0:3],wires=wires[3])
-    qml.RX(weights[0],wires=wires[0])
-    qml.RX(weights[1],wires=wires[1])
-    qml.RX(weights[2],wires=wires[2])
-    qml.RX(weights[3],wires=wires[3])
+    qml.SimplifiedTwoDesign(initial_layer_weights=weights[0], weights=weights[1], wires=wires)
+
 
 dev= qml.device('default.qubit',wires=8)
 
-@qml.qnode(dev, expansion_strategy='device')
+@qml.qnode(dev,expansion_strategy='device')
 def circuit(template_weights):
-    qml.MPS(wires=range(8),n_block_wires=4,block=large_block,n_params_block=5,template_weights=template_weights)
+    qml.MPS(wires=range(8),n_block_wires=4,block=large_block,n_params_block=2,template_weights=template_weights)
     return qml.expval(qml.PauliZ(wires=7))
 
-
-weights = np.random.random(size=[3,5])
-print(f'The circuit result is {circuit(weights):.02f}')
-print('The circuit looks like:')
-qml.draw_mpl(circuit)(weights)
+template_weights = [weights]*3
+print(f'The circuit result is {circuit(template_weights):.02f}')
+qml.draw_mpl(circuit)(template_weights)
 
 ##############################################################################
 # A different tensor network
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
 # We can also easily call a different tensor network architecture by using ``qml.TTN``:
 
 
 def block(weights, wires):
     qml.CNOT(wires=wires)
-    qml.RX(weights[0],wires=wires[0])
-    qml.RX(weights[1],wires=wires[1])
+    qml.RX(weights[0], wires=wires[0])
+    qml.RX(weights[1], wires=wires[1])
 
-dev= qml.device('default.qubit',wires=4)
 
-@qml.qnode(dev, expansion_strategy='device')
+dev = qml.device("default.qubit", wires=4)
+
+
+@qml.qnode(dev, expansion_strategy="device")
 def circuit(template_weights):
-    qml.TTN(wires=range(4),n_block_wires=2,block=block,n_params_block=2,template_weights=template_weights)
+    qml.TTN(
+        wires=range(4),
+        n_block_wires=2,
+        block=block,
+        n_params_block=2,
+        template_weights=template_weights,
+    )
     return qml.expval(qml.PauliZ(wires=3))
 
 
-weights = np.random.random(size=[3,2])
-print(f'The circuit result is {circuit(weights):.02f}')
-print('The circuit looks like:')
+weights = np.random.random(size=[3, 2])
+print(f"The circuit result is {circuit(weights):.02f}")
 qml.draw_mpl(circuit)(weights)
 
 
 ##############################################################################
 # Classifying the bars and stripes data set
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# The bars and stripes data set is composed of binary black and white images of size :math:`n \times n` pixels, 
+# The bars and stripes data set is composed of binary black and white images of size :math:`n \times n` pixels,
 # where either all pixels in any given column have the same color (bars) or all pixels in any given row have the same color (stripes),
 # as shown in the image below.
 #
@@ -250,34 +244,41 @@ qml.draw_mpl(circuit)(weights)
 
 import matplotlib.pyplot as plt
 
-BAS =[[1,1,0,0],[0,0,1,1],[1,0,1,0],[0,1,0,1]]
-j=1
+BAS = [[1, 1, 0, 0], [0, 0, 1, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
+j = 1
 for i in BAS:
-    plt.subplot(1,4,j)
-    j+=1
-    plt.imshow(np.reshape(i,[2,2]))
+    plt.subplot(1, 4, j)
+    j += 1
+    plt.imshow(np.reshape(i, [2, 2]), cmap="gray")
 
 ##############################################################################
-# Then we define a tree tensor quantum circuit that will learn to label these images
+# Then we define a tree tensor network quantum circuit that will learn to label these images
+
 
 def block(weights, wires):
     qml.CNOT(wires=wires)
-    qml.RX(weights[0],wires=wires[0])
-    qml.RX(weights[1],wires=wires[1])
+    qml.RX(weights[0], wires=wires[0])
+    qml.RX(weights[1], wires=wires[1])
 
-dev= qml.device('default.qubit',wires=4)
-#this circuit gives a label, -1 to +1
-@qml.qnode(dev, expansion_strategy='device')
-def circuit(image,template_weights):
-    qml.BasisStatePreparation(image,wires=range(4))
-    qml.TTN(wires=range(4),n_block_wires=2,block=block,n_params_block=2,template_weights=template_weights)
+
+dev = qml.device("default.qubit", wires=4)
+# this circuit gives a label, -1 to +1
+@qml.qnode(dev, expansion_strategy="device")
+def circuit(image, template_weights):
+    qml.BasisStatePreparation(image, wires=range(4))
+    qml.TTN(
+        wires=range(4),
+        n_block_wires=2,
+        block=block,
+        n_params_block=2,
+        template_weights=template_weights,
+    )
     return qml.expval(qml.PauliZ(wires=3))
 
 
-weights = np.random.random(size=[3,2])
-print(f'The circuit result is {circuit(BAS[0],weights):.02f}')
-print('The circuit looks like:')
-qml.draw_mpl(circuit)(BAS[0],weights)
+weights = np.random.random(size=[3, 2])
+print(f"The circuit result is {circuit(BAS[0],weights):.02f}")
+qml.draw_mpl(circuit)(BAS[0], weights)
 
 ##############################################################################
 # We define a cost function and a gradient descent optimizer to train the circuit.
@@ -285,17 +286,19 @@ qml.draw_mpl(circuit)(BAS[0],weights)
 optimizer = qml.GradientDescentOptimizer(stepsize=0.1)
 def costfunc(params):
     cost = 0
-    cost += circuit(BAS[0],params)
-    cost += circuit(BAS[1],params)
-    cost -= circuit(BAS[2],params)
-    cost -= circuit(BAS[3],params)
+    for i in range(len(BAS)):
+        if i<len(BAS)/2:
+            cost+=circuit(BAS[i],params)
+        else:
+            cost-=circuit(BAS[i],params)
     return cost
+
 
 ##############################################################################
 # We initialize the parameters and then train
 
 np.random.seed(1)
-params=np.random.random(size=[3,2], requires_grad=True)
+params = np.random.random(size=[3, 2], requires_grad=True)
 
 for k in range(100):
     params = optimizer.step(costfunc, params)
@@ -304,7 +307,5 @@ for k in range(100):
 # We print the results of the evaluation
 
 for image in BAS:
-    print(f'result is {circuit(image,params):.02f}')
-    print(qml.draw(circuit,expansion_strategy='device')(image,params))
-
-
+    print(f"result is {circuit(image,params):.02f}")
+    qml.draw_mpl(circuit)(image, params)
