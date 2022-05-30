3669b498b38a06301567981e27b71cbc6ed79e3a
soosub <joost.bus@resident.xanadu.ai>
2022-05-30 14:11:22 -0400

    Stucture of post in introduction

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -13,20 +13,29 @@ Measurement-based quantum computation
 
 ##############################################################################
 #
-# Measurement-based quantum computation
-# --------------
 # Measurement-based quantum computing (MBQC) is a clever approach towards quantum computing that
 # makes use of entanglement as a resource for computation. This method, also referred to as one-way
 # quantum computing, seems very dissimilar from the gate-based model. However, they can be proven to
 # be equivalent and so both are universal. In a one-way quantum computer, we start out with an entangled state, a so-called cluster state, and
 # apply particular single-qubit measurements that correspond to the desired quantum circuit. In
-# MBQC, the measurements are the computation and the entanglement of the cluster state is used as a
+# MBQC, the measurements *are* the computation and the entanglement of the cluster state is used as a
 # resource.
+#
+# The structure of this demo will be as follows. First we introduce the concept of a cluster state, 
+# the substrate for measurement-based quantum computation. Then, we will move on to explain to 
+# implement arbitrary quantum circuits in MBQC. That is,
+# 1. How does **information propagate** through the cluster state?
+# 2. How can **arbitrary qubit rotations** be implemented?
+# 3. How do we implement a **two-qubit gate** in this scheme?
+
 
 ##############################################################################
 #
 # Cluster states
 # --------------
+#
+# Cluster states are the basis of measurement-based quantum computation. They are also dubbed
+# as graph states, because they can be represented as an undirected graph :math:`G = (V,E)` whose vertices . 
 #
 # .. figure:: ../demonstrations/mbqc/mbqc_blueprint.png
 #    :align: center
@@ -36,8 +45,7 @@ Measurement-based quantum computation
 #
 #    Cluster state proposed in [#XanaduBlueprint2021]_
 #
-# To understand how MBQC qubits work, it's good to have an understanding of the cluster state. There
-# is not one cluster state, but rather it’s a name for a class of highly entangled multi-qubit
+# There is not one cluster state, but rather it’s a name for a class of highly entangled multi-qubit
 # states. One example of a cluster state would be
 #
 # .. math::    |\psi\rangle=\Pi_{(i,j)\in E(G)}CZ_{ij}|+⟩^{\otimes n},
