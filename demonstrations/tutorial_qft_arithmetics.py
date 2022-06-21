9e5d73ca8afc596fbac0bbcd809bfddbb6e6b48e
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2022-06-21 11:15:42 -0400

    Update demonstrations/tutorial_qft_arithmetics.py
    Co-authored-by: alvaro-at-xanadu <91897656+alvaro-at-xanadu@users.noreply.github.com>

--- demonstrations/tutorial_qft_arithmetics.py
+++ demonstrations/tutorial_qft_arithmetics.py
@@ -425,7 +425,8 @@ plt.show()
 #
 # Let’s imagine now that we want just the opposite: to factor the
 
-# number 21 as a product of two terms, is this something we could do
+# number 21 as a product of two terms. Is this something we could do
+
 # following this previous reasoning? The answer is yes! We can make use of
 # Grover’s algorithm to amplify the states whose product is the number we
 # are looking for. All we would need is to construct the oracle U, i.e., an
