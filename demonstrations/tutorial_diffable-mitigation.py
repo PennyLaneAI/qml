0acb072a02b7ed932da8298e7d05284a6cf1909c
Qottmann <korbinian.kottmann@gmail.com>
2022-07-06 15:11:47 -0400

    rough sketch of outline for feedback

---
+++ demonstrations/tutorial_diffable-mitigation.py
@@ -0,0 +1,60 @@
+r"""Differentiable error mitigation
+===========================================
+
+.. meta::
+    :property="og:description": Differentiable error mitigation
+    :property="og:image": https://pennylane.ai/qml/_images/QEK_thumbnail.png
+
+.. related::
+
+    tutorial_error_mitigation Error mitigation with Mitiq and PennyLane
+
+*Author: KK,.. Posted: 27 July 2022*
+
+Intro error mitigation
+----------------------
+
+Showing off new functionality with ZNE
+--------------------------------------
+Could be nice to base the tutorial on jax as it the new functions are jitable and super fast?
+Including the example from the fold_global details section, showing the manual fit in ZNE to assure it makes sense (visualization always helps imo)
+
+VQE example using a noisy device with mitigation
+------------------------------------------------
+
+Showing off other interfaces and how to get their gradients
+-----------------------------------------------------------
+
+Discussing differentiation the mitigation scheme itself
+-------------------------------------------------------
+Havent figured out how this works, but could be worth it to potentially ignite some sparks.
+
+
+
+"""
+
+##############################################################################
+# A Heading
+# -------------
+# text
+
+
+# some code
+
+##############################################################################
+# asd
+#
+# References
+# ----------
+#
+# .. [#DiffableTransforms]
+#
+#     Olivia Di Matteo, Josh Izaac, Tom Bromley, Anthony Hayes, Christina Lee, Maria Schuld, Antal Száva, Chase Roberts, Nathan Killoran.
+#     "Quantum computing with differentiable quantum transforms."
+#     `arXiv:2202.13414 <https://arxiv.org/abs/2202.13414>`__, 2021.
+#
+# .. [#VAQEM]
+#
+#     Gokul Subramanian Ravi, Kaitlin N. Smith, Pranav Gokhale, Andrea Mari, Nathan Earnest, Ali Javadi-Abhari, Frederic T. Chong.
+#     "VAQEM: A Variational Approach to Quantum Error Mitigation."
+#     `arXiv:2112.05821 <https://arxiv.org/abs/2112.05821>`__, 2021.
