e533ac839506fddc6ca77ef575364a280c4cac47
soosub <joost.bus@hotmail.com>
2022-07-13 17:07:34 -0400

    try plotly plots interactivity

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -237,6 +237,11 @@ RHG.draw()
 #
 #    Cluster state proposed in [#XanaduBlueprint]_
 
+import plotly.express as px
+df = px.data.iris()
+fig = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
+              color='species')
+fig.show()
 
 ##############################################################################
 # References
