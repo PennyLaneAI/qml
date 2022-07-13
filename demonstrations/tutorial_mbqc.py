3bed8faf5f9fda873b089f929886420b724fd3fa
soosub <joost.bus@hotmail.com>
2022-07-13 17:57:15 -0400

    plotly iframe renderer

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -238,7 +238,7 @@ RHG.draw()
 #    Cluster state proposed in [#XanaduBlueprint]_
 
 import plotly.io as pio
-pio.renderers.default = "sphinx_gallery"
+pio.renderers.default = "iframe"
 
 import plotly.express as px
 df = px.data.iris()
