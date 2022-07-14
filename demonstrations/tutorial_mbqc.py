1f5edae02cd0bf5d6b34b1dc8f97263470f03618
soosub <joost.bus@hotmail.com>
2022-07-14 11:21:55 -0400

    typo

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -28,7 +28,7 @@ Measurement-based quantum computation
 #
 # Throughout this tutorial, we will explain the underlying concepts with the help of some code 
 # snippets using `PennyLane <https://pennylane.readthedocs.io/en/stable/>`_ and Xanadu's quantum 
-# error correction software `FlamingPy <https://flamingpy.readthedocs.io/en/latest/>`_ developped 
+# error correction software `FlamingPy <https://flamingpy.readthedocs.io/en/latest/>`_ developed 
 # by our architecture team [#XanaduPassiveArchitecture]_.
 #
 #
