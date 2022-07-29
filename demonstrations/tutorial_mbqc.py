0853654de0b7bf36c1f402211971d002f248752b
Joost <44366402+soosub@users.noreply.github.com>
2022-07-29 14:23:24 -0400

    Update demonstrations/tutorial_mbqc.py
    Co-authored-by: Maurice Weber <30623391+mauriceweber@users.noreply.github.com>

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -282,7 +282,7 @@ one_bit_teleportation(input_state)
 # The two-qubit gate: CNOT
 # ``````````````````````````
 # The second ingredient for a universal quantum computing scheme is the two-qubit gate. Here, we will
-# show how to do a CNOT in the measurement-based framework. Input state is given on two qubits,
+# show how to perform a CNOT operation in the measurement-based framework. The input state is given on two qubits,
 # control qubit :math:`c` and target qubit :math:`t_\mathrm{in}`. Preparing the cluster state shown in
 # the figure below, and measuring qubits :math:`t_\mathrm{in}` and :math:`a` in the Hadamard basis,
 # we implement the CNOT gate between qubits :math:`c` and :math:`t_\mathrm{out}` up to Pauli corrections.
