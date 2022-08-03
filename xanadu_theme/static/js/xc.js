2eae7c078c2e8ae7a1bde5b5e241b5d40b85d1ee
trbromley <brotho02@gmail.com>
2022-08-03 11:05:18 -0400

    Add

--- xanadu_theme/static/js/xc.js
+++ xanadu_theme/static/js/xc.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-  const demo_name = window.location.pathname.split("/").pop().split(".").reverse().pop();
-  const xc_url = `https://pennylane.xanadu.ai/lab/tree/demo%3A${demo_name}.ipynb`;
+  let url_split = window.location.pathname.split("/");
+  const page_name = url_split.pop();
+  const page_directory = url_split.pop();
 
-  const button = `
-  <button onclick="window.location.href='${xc_url}';">
-    Open demo in Xanadu Cloud
-  </button>
-  `;
+  if (page_directory === "demos") {
+      const demo_name = page_name.split(".").reverse().pop();
+      const xc_url = `https://pennylane.xanadu.ai/lab/tree/demo%3A${demo_name}.ipynb`;
 
-  $( button ).insertAfter($("h1").first());
+      const button = `
+      <a href="${xc_url}" class="button">Open demo in Xanadu Cloud</a>
+      `;
+
+      $( button ).insertAfter($("h1").first());
+  }
 });
