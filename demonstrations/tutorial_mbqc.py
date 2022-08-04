1e9fb86246a44259685f45ee84fcca11050ced38
soosub <joost.bus@hotmail.com>
2022-08-04 17:57:28 -0400

    Changes conclusion

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -578,11 +578,13 @@ RHG = SurfaceCode(code_distance)
 # Conclusion
 # -------------------------------
 #
-# The MBQC framework is a powerful quantum computing approach. It offers several advantages over
+# In this demo, we have learned that a one-way quantum computer capable of cluster state 
+# entanglement together with adaptive arbitrary single-qubit measurements is universal. 
+# The MBQC framework is a powerful quantum computing approach and offers several advantages over
 # the gate-based model and is particularly useful in platforms that allow for many expendable
-# qubits. One major benefit is that it circumvents the need for creating in-line entanglement gates.
-# These gates are often the most noisy operations in gate-based quantum computers based on, for
-# example, trapped-ions or superconducting circuits.
+# qubits. For example, it circumvents the need for applying in-line entangling gates which are 
+# often the most noisy operations in gate-based quantum computers with trapped-ions or 
+# superconducting circuits.
 #
 # Xanadu's approach toward a universal quantum computer involves *continuous-variable* cluster states
 # [#CV-MBQC]_. If you would like to learn more about the architecture, you can read our blueprint
