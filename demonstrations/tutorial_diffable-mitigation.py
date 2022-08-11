cc81a70c59ea045d8c65bf81e8cda24ff758fec3
Qottmann <korbinian.kottmann@gmail.com>
2022-08-11 09:33:53 -0400

    bio image

--- demonstrations/tutorial_diffable-mitigation.py
+++ demonstrations/tutorial_diffable-mitigation.py
@@ -265,6 +265,6 @@ plt.show()
 
 ##############################################################################
 # .. bio:: Korbinian Kottmann
-#    :photo: ../static/qottmann.jpg
+#    :photo: ../static/authors/qottmann.jpg
 #
 #    Korbinian is a summer resident at Xanadu, interested in (quantum) software development, quantum computing and (quantum) machine learning.
