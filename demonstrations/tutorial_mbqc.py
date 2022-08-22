cf59298a2811e6488e4e94bbf9e29f56f8fab47e
Radoica Draškić <33292604+radras@users.noreply.github.com>
2022-08-22 18:24:46 +0200

    Update demonstrations/tutorial_mbqc.py
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -223,7 +223,7 @@ np.allclose(density_matrix, density_matrix_mbqc)
 # ----------------------
 # How do we know if this measurement-based scheme is just as powerful as its gate-based counterpart? We
 # have to prove it! In particular, we want to show that a measurement-based quantum computer is a
-# quantum Turing machine (QTM) also known as a universal quantum computer. To do this, we
+# `quantum Turing machine (QTM) <https://en.wikipedia.org/wiki/Quantum_Turing_machine>`_. To do this, we
 # need to show 4 things [#OneWay2001]_:
 #
 #   1. How **information propagates** through the cluster state.
