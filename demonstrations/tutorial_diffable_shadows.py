8c7773f850351640165ef974bf6a1ecdc9c04494
Qottmann <korbinian.kottmann@gmail.com>
2022-08-23 08:34:41 -0400

    initial structure

---
+++ demonstrations/tutorial_diffable_shadows.py
@@ -0,0 +1,68 @@
+r"""Differentiable classical shadows
+====================================
+
+.. meta::
+    :property="og:description": Differentiable classical shadows
+    :property="og:image": https://pennylane.ai/qml/_images/diffable_mitigation_thumb.png
+
+.. related::
+
+    classical_shadows
+    ml_classical_shadows
+
+*Author: Korbinian Kottmann, Posted: 22 September 2022*
+
+abstract
+
+Classical shadow theory
+-----------------------
+
+classical shadow theory
+
+PennyLane implementation
+------------------------
+"""
+
+import pennylane as qml
+import pennylane.numpy as np
+
+# PennyLane implementation
+
+
+##############################################################################
+# Comparing quantum resources with conventional measurement methods 
+# -----------------------------------------------------------------
+# 
+# Simultaneously measuring qubit-wise-commuting observables
+
+
+
+
+##############################################################################
+#
+# Conclusion
+# ----------
+# 
+# conclusion
+#
+#
+# References
+# ----------
+#
+# .. [#Huang2020]
+#
+#     Hsin-Yuan Huang, Richard Kueng, John Preskill
+#     "Predicting Many Properties of a Quantum System from Very Few Measurements."
+#     `arXiv:2002.08953 <https://arxiv.org/abs/2002.08953>`__, 2020.
+#
+# .. [#Huang2021]
+#
+#     Hsin-Yuan Huang, Richard Kueng, John Preskill
+#     "Efficient estimation of Pauli observables by derandomization."
+#     `arXiv:2103.07510 <https://arxiv.org/abs/2103.07510>`__, 2021.
+
+##############################################################################
+# .. bio:: Korbinian Kottmann
+#    :photo: ../_static/authors/qottmann.jpg
+#
+#    Korbinian is a summer resident at Xanadu, interested in (quantum) software development, quantum computing and (quantum) machine learning.
