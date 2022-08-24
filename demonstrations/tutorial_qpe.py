6494cf2f6edbae8dac9b13e554ca33b5611f1ea0
davidecast <60468944+davidecast@users.noreply.github.com>
2022-08-24 11:02:32 +0200

    Update demonstrations/tutorial_qpe.py
    Co-authored-by: Jen Dodd <85646436+jen-dodd@users.noreply.github.com>

--- demonstrations/tutorial_qpe.py
+++ demonstrations/tutorial_qpe.py
@@ -24,7 +24,7 @@ Finding Ground State Energies via Quantum Phase Estimation
 # You've come to the right place, because in this demo we will:
 #
 # 1. Explain the connection between ground state energies and quantum phases.
-# 2. Recap the quantum phase estimation algorithm, which is great in, well, estimating the quantum phases.
+# 2. Recap the quantum phase estimation algorithm which is good at, well, estimating quantum phases.
 # 3. Show how to make the QPE estimate the phases related to the ground state energies.
 # 4. Mention the possible caveats.
 # 5. Of course, provide the PennyLane implementation!
