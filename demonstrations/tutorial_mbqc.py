9b1cd59e5734ee991673837206feb9a8a4790c75
soosub <joost.bus@hotmail.com>
2022-08-25 18:18:53 -0400

    transported italics

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -205,7 +205,7 @@ np.allclose(density_matrix, density_matrix_mbqc)
 
 ##############################################################################
 #
-# As we can see, :math:`|\psi\rangle`, originally the state of the first qubit, has been *transported* to the second qubit!
+# As we can see, :math:`|\psi\rangle`, originally the state of the first qubit, has been transported to the second qubit!
 #
 # This protocol is one of the main ingredients of one-way quantum computing. Essentially, we
 # propagate the information in one end of our cluster state to the other end by using the
