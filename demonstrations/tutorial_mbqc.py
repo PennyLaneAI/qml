e3d4cdc7e21aa7e91d36915fd10aafcf7ab35b98
Joost <44366402+soosub@users.noreply.github.com>
2022-08-25 17:39:05 -0400

    Update demonstrations/tutorial_mbqc.py
    Co-authored-by: ilan-tz <57886357+ilan-tz@users.noreply.github.com>

--- demonstrations/tutorial_mbqc.py
+++ demonstrations/tutorial_mbqc.py
@@ -530,7 +530,7 @@ np.allclose(CNOT(input_state), CNOT_MBQC(input_state))
 #    qubits are being added to the cluster by entangling them with it. Prior measurement outcomes
 #    determine the basis for future measurements [#OpticalQuantumComputing]_.
 #
-# This feature makes it particularly interesting for photonic-based quantum computers: we can use
+# This feature makes it particularly attractive for photonic quantum computers: we can use
 # expendable qubits that don't have to stick around for the full calculation. If we can find a
 # reliable way to produce qubits and stitch them together through entanglement, we can use it to
 # produce our cluster state resource! Essentially, we need some kind of qubit factory and a
