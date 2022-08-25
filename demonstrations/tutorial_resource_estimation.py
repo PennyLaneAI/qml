bd1f4b7bfba75912ac500c7b9788d57bf11c666f
soranjh <soranjh@yahoo.com>
2022-08-25 14:17:07 -0400

    correct inline equations

--- demonstrations/tutorial_resource_estimation.py
+++ demonstrations/tutorial_resource_estimation.py
@@ -33,16 +33,17 @@ expectation values within a given error using algorithms such as the variational
 
 Quantum Phase Estimation
 ------------------------
-The QPE algorithm can be used to compute the phase of a unitary operator within an error $\epsilon$.
-The unitary operator $U$ can be selected to share eigenvectors $| \Psi \rangle$ with a molecular
-Hamiltonian $H$ by having, for example, $U = e^{-iH}$ to compute the eigenvalues of $H$.
-A QPE conceptual circuit diagram is shown in the following. The circuit contains a set of target
-wires initialized at the eigenstate $| \Psi \rangle$ which encode the unitary operator and a set of
-estimation wires initialized in $| 0 \rangle$ which are measured after applying an inverse quantum
-Fourier transform. The measurement results give a binary string that can be used to estimate the
-phase of the unitary and the ground state energy of the Hamiltonian. The precision in estimating the
-phase depends on the number of estimation wires while the number of gates in the circuit is
-determined by the unitary operator.
+The QPE algorithm can be used to compute the phase of a unitary operator within an error
+:math:`\epsilon`. The unitary operator :math:`U` can be selected to share eigenvectors
+:math:`| \Psi \rangle` with a molecular Hamiltonian :math:`H` by having, for example,
+:math:`U = e^{-iH}` to compute the eigenvalues of :math:`H`. A QPE conceptual circuit diagram is
+shown in the following. The circuit contains a set of target wires initialized at the eigenstate
+:math:`| \Psi \rangle` which encode the unitary operator and a set of estimation wires initialized
+in :math:`| 0 \rangle` which are measured after applying an inverse quantum Fourier transform. The
+measurement results give a binary string that can be used to estimate the phase of the unitary and
+the ground state energy of the Hamiltonian. The precision in estimating the phase depends on the
+number of estimation wires while the number of gates in the circuit is determined by the unitary
+operator.
 
 We are interested to estimate the number of logical qubits and the number of non-Clifford gates,
 which are hard to implement, for a QPE algorithm that implements a second-quantized Hamiltonian
@@ -55,7 +56,7 @@ Second quantization
 *******************
 A `second-quantized <https://en.wikipedia.org/wiki/Second_quantization>`_ molecular Hamiltonian is
 constructed from one- and two-body electron integrals in the basis of molecular orbitals,
-:math:`\phi`,
+ :math:`\phi`,
 
 .. math:: h_{pq} =\int dx \,\phi_p^*(x)\left(-\frac{\nabla^2}{2}-\sum_{i=1}^N\frac{Z_i}{|r-R_i|}\right)\phi_q(x),\\\\
 .. math:: h_{pqrs} = \int dx_1 dx_2\, \frac{\phi_p^*(x_1)\phi_q^*(x_2)\phi_r(x_2)\phi_s(x_1)}{|r_1-r_2|}.
@@ -64,7 +65,7 @@ as
 
 .. math:: H=\sum_{pq} h_{pq}a_p^\dagger a_q +\frac{1}{2}\sum_{pqrs}h_{pqrs}a_p^\dagger a_q^\dagger a_r a_s,
 
-where :math:`a^\dagger` and :math:`a` are the fermionic creation and annihilation operators,
+where  :math:`a^\dagger` and  :math:`a` are the fermionic creation and annihilation operators,
 respectively. This Hamiltonian can then be transformed to the qubit basis and be written as a linear
 combination of unitary operators constructed as tensor products of Pauli and Identity operators
 
@@ -72,12 +73,12 @@ combination of unitary operators constructed as tensor products of Pauli and Ide
 
 Tthe cost of computing the ground state energy of this Hamiltonian using the QPE algorithm depends
 on the complexity of implementing the unitary operator which can be constructed as
-$U = e^{-i \arccos (H / \lambda)}$ and implemented using a quantum walk operator [Cao et al.]. The
-eigenvalues of the quantum walk operator are $e^{-i \arccos (E / \lambda)}$ which give the
-eigenvalues of the Hamiltonian $E$. The parameter $\lambda$, which in the simplest case is a sum
-over the coefficients $c_i$, is needed for normalization. The Toffoli complexity of this
-implementation is proportional to the number of repetitions of the quantum walk which is related to
-$\lambda$ and the QPE error $\epsilon_{QPE}$.
+`U = e^{-i \arccos (H / \lambda)}` and implemented using a quantum walk operator [Cao et al.]. The
+eigenvalues of the quantum walk operator are :math:`e^{-i \arccos (E / \lambda)}` which give the
+eigenvalues of the Hamiltonian :math:`E`. The parameter :math:`\lambda`, which in the simplest case
+is a sum over the coefficients :math:`c_i`, is needed for normalization. The Toffoli complexity of
+this implementation is proportional to the number of repetitions of the quantum walk which is
+related to `\lambda` and the QPE error :math:`\epsilon_{QPE}`.
 
 .. math:: \left \lceil \frac{\pi \lambda}{2 \epsilon_{QPE}} \right \rceil.
 
