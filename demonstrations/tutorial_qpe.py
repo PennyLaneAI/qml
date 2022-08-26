63d260d14c15c14fa598c16cf3dd0121b6e12fee
davidecast <60468944+davidecast@users.noreply.github.com>
2022-08-26 14:30:57 +0200

    Update demonstrations/tutorial_qpe.py
    Co-authored-by: Catalina Albornoz <albornoz.catalina@hotmail.com>

--- demonstrations/tutorial_qpe.py
+++ demonstrations/tutorial_qpe.py
@@ -55,7 +55,7 @@ Finding Ground State Energies via Quantum Phase Estimation
 #
 # What have we just obtained? Well, our derivation shows
 # that as the system evolves, its eigenstates stay the same — up
-# to a global quantum phase. Importantly, the acquired phase is not
+# to a global quantum phase. Importantly the acquired phase is not
 # random, for each eigenstate it is related to its energy in a known way.
 # 
 # Okay, now it is clear what the recipe for finding the ground state
