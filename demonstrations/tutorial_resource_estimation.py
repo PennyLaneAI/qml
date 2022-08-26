b1c6d4267f6642dc05f74b0c58229d65edfc5a06
soranjh <soranjh@yahoo.com>
2022-08-26 13:46:47 -0400

    add measurement

--- demonstrations/tutorial_resource_estimation.py
+++ demonstrations/tutorial_resource_estimation.py
@@ -116,8 +116,8 @@ print(f'Estimated gates : {algo.gates:.2e} \nEstimated qubits: {algo.qubits}')
 #
 # The cost of computing the ground state energy of this Hamiltonian using the QPE algorithm depends
 # on the complexity of implementing the unitary operator for encoding the Hamiltonian, which can be
-# constructed as `U = e^{-i \arccos (H / \lambda)}` and implemented using a quantum walk operator
-# [Cao et al.]. The eigenvalues of the quantum walk operator are
+# constructed as :math:`U = e^{-i \arccos (H / \lambda)}` and implemented using a quantum walk
+# operator [Cao et al.]. The eigenvalues of the quantum walk operator are
 # :math:`e^{-i \arccos (E / \lambda)}` which can be post-processed classically to give the
 # eigenvalues of the Hamiltonian :math:`E`. The parameter :math:`\lambda` is the 1-Norm of the
 # Hamiltonian and plays an important role in determining the cost of implementing the QPE algorithm.
@@ -130,6 +130,41 @@ print(f'1-Norm of the Hamiltonian: {algo.lamb}')
 # first-quantized Hamiltonian it can be computed for a target error in the algorithm with
 
 qml.resource.FirstQuantization.norm(planewaves, electrons, volume, error=0.001)
+
+##############################################################################
+# Measurement Complexity Estimation
+# ---------------------------------
+# In variational quantum algorithms, such as VQE, the expectation value of an observable is
+# typically computed by decomposing the observable to the tensor products of Pauli and Identity
+# operators apply to a limited number of qubits and then measure the expectation value for each of
+# these local terms. The number of qubits required for the measurement is trivially determined by
+# the number of qubits the observable acts on. The number of gates required to implement the
+# variational algorithm is determined by a circuit ansatz that is also known a priori. However,
+# estimating the number of circuit measurements required to achieve a certain error in computing the
+# expectation value is not typically trivial. Let's now use the PennyLane functionality that
+# estimates such measurement requirements for computing the expectation value of the water
+# Hamiltonian as an example.
+#
+# First, we construct the molecular Hamiltonian
+
+H = qml.qchem.molecular_hamiltonian(symbols, geometry)
+
+##############################################################################
+# The number of measurements needed to compute :math:`\left \langle H \right \rangle` within the
+# chemical accuracy, 0.0016 :math:`\text{Ha}`, can be obtained with
+
+m = qml.resource.estimate_samples(H.coeffs, H.ops, error=0.0016)
+print(m)
+
+##############################################################################
+# This number corresponds to the measurement process where each term in the Hamiltonian is measured
+# independently. This number can be significantly reduced by partitioning the Pauli words into
+# groups of commuting terms that can be measured simultaneously.
+
+ops, coeffs = qml.grouping.group_observables(H.ops, H.coeffs)
+
+m = qml.resource.estimate_samples(coeffs, ops, error=0.0016)
+print(m)
 
 ##############################################################################
 #.. bio:: Soran Jahangiri
