4e295522d442c5b379af642475f2b7e9e385f5de
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2022-08-29 14:50:15 -0400

    Update tutorial_learning_few_data.py

--- demonstrations/tutorial_learning_few_data.py
+++ demonstrations/tutorial_learning_few_data.py
@@ -606,4 +606,5 @@ plt.show()
 # .. bio:: Maurice Weber
 #    :photo: ../_static/authors/qottmann.jpeg
 #
-#    Maurice is a summer resident at Xanadu and a PhD student at ETH Zürich. He is interested in the intersection of Machine Learning and Quantum Computing and in developing robustness guarantees for (quantum) machine learning.
+#    Maurice is a summer resident at Xanadu and a PhD student at ETH Zürich. He is interested in the intersection of Machine Learning and Quantum Computing and in developing robustness guarantees for QML.
+#
