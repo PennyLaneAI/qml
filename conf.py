0dbb5f968b6d166b8f9dbe97b06ed650b189892f
Josh Izaac <josh146@gmail.com>
2022-09-13 05:23:29 +0800

    Update conf.py
    Co-authored-by: Mikhail Andrenkov <Mandrenkov@users.noreply.github.com>

--- conf.py
+++ conf.py
@@ -200,7 +200,7 @@ html_theme_options = {
         },
     ],
     "extra_copyrights": [
-        "TensorFlow, the TensorFlow logo, and any related marks are trademarks " "of Google Inc."
+        "TensorFlow, the TensorFlow logo, and any related marks are trademarks of Google Inc."
     ],
     "google_analytics_tracking_id": "UA-130507810-2",
     "border_colour": "#19b37b",
