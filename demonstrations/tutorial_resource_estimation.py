ee627f36b7cab2ddc64a335adedff33c9f6d9f3a
soranjh <soranjh@yahoo.com>
2022-09-20 14:14:24 -0400

    apply review comments

--- demonstrations/tutorial_resource_estimation.py
+++ demonstrations/tutorial_resource_estimation.py
@@ -4,7 +4,8 @@ Quantum Resource Estimation
 ===========================
 
 .. meta::
-    :property="og:description": Learn how to estimate the number of qubits and gates needed to implement quantum algorithms
+    :property="og:description": Learn how to estimate the number of qubits and gates needed to
+     implement quantum algorithms
     :property="og:image": https://pennylane.ai/qml/_images/differentiable_HF.png
 
 .. related::
@@ -15,9 +16,12 @@ Quantum Resource Estimation
 *Author: Soran Jahangiri. Posted: 17 August 2022. Last updated: 17 August 2022*
 
 Quantum algorithms such as quantum phase estimation and the variational quantum eigensolver
-are studied as avenues to tackle problems that are intractable for conventional computers. However, we
-currently do not have quantum computers or simulators capable of implementing large-scale versions of these 
-algorithms. This makes it difficult to properly explore their accuracy and efficiency for problem sizes where the actual advantage of quantum algorithms can potentially occur. Despite these difficulties, it is still possible to estimate the amount of resources required to implement such quantum algorithms .
+are studied as avenues to tackle problems that are intractable for conventional computers. However,
+we currently do not have quantum computers or simulators capable of implementing large-scale
+versions of these algorithms. This makes it difficult to properly explore their accuracy and
+efficiency for problem sizes where the actual advantage of quantum algorithms can potentially occur.
+Despite these difficulties, it is still possible to estimate the amount of resources required to
+implement such quantum algorithms .
 
 In this demo, we describe how to estimate the total number of
 non-Clifford gates and logical qubits required to implement the quantum phase estimation (QPE)
@@ -28,24 +32,34 @@ expectation values using algorithms such as the variational quantum eigensolver
 
 Quantum Phase Estimation
 ------------------------
-The QPE algorithm can be used to compute the phase associated with an eigenstate of a unitary operator. For the purpose of quantum simulation, the unitary operator :math:`U` can be chosen to share eigenvectors
-with a molecular Hamiltonian :math:`H`, for example by setting :math:`U = e^{-iH}`. Estimating the phase of such a unitary then permits recovering the corresponding eigenvalue of the Hamiltonian. A conceptual QPE circuit diagram is
-shown below. The circuit contains target wires, here initialized in the ground state
-:math:`| \psi_0 \rangle`,  and a set of estimation wires, initialized
-in :math:`| 0 \rangle`. The algorithm repeatedly applies powers of `U` controlled on the state of estimation wires, which are measured after applying an inverse quantum Fourier transform. The
-measurement results give a binary string that can be used to estimate the phase of the unitary and
-thus also the ground state energy of the Hamiltonian. The precision in estimating the phase depends on the
-number of estimation wires.
+The QPE algorithm can be used to compute the phase associated with an eigenstate of a unitary
+operator. For the purpose of quantum simulation, the unitary operator :math:`U` can be chosen to
+share eigenvectors with a molecular Hamiltonian :math:`H`, for example by setting
+:math:`U = e^{-iH}`. Estimating the phase of such a unitary then permits recovering the
+corresponding eigenvalue of the Hamiltonian. A conceptual QPE circuit diagram is shown below. The
+circuit contains target wires, here initialized in the ground state :math:`| \psi_0 \rangle`,and a
+set of estimation wires, initialized in :math:`| 0 \rangle`. The algorithm repeatedly applies powers
+of `U` controlled on the state of estimation wires, which are measured after applying an inverse
+quantum Fourier transform. The measurement results give a binary string that can be used to estimate
+the phase of the unitary and thus also the ground state energy of the Hamiltonian. The precision in
+estimating the phase depends on the number of estimation wires.
 
-For most cases of interest, this algorithm requires more qubits and longer circuit depths than what can be implemented on existing hardware. We are instead interested in estimating the number of logical qubits and the number of gates which are needed to implement the algorithm. We focus on non-Clifford gates, which are the most expensive to implement in a fault-tolerant setting. We noe explain how to perform this resource estimation for QPE algorithms based on a second-quantized Hamiltonian
-describing a molecule, and a first-quantized Hamiltonian describing a periodic material.
+For most cases of interest, this algorithm requires more qubits and longer circuit depths than what
+can be implemented on existing hardware. We are instead interested in estimating the number of
+logical qubits and the number of gates which are needed to implement the algorithm. We focus on
+non-Clifford gates, which are the most expensive to implement in a fault-tolerant setting. We now
+explain how to perform this resource estimation for QPE algorithms based on a second-quantized
+Hamiltonian describing a molecule, and a first-quantized Hamiltonian describing a periodic material.
 We assume Gaussian and plane wave basis sets for describing the molecular and periodic systems,
 respectively. The PennyLane functionality in the ``resource`` module allows us to estimate these
 resources by simply defining system specifications and a target error for estimation. Let's see how!
 
 QPE cost for simulating molecules
-******************************************
-We study the double low-rank Hamiltonian factorization algorithm of Ref. [1]. We first need to define the atomic symbols and coordinates for the given molecule. Here we use the water molecule at its equilibrium geometry as an
+*********************************
+We study the double low-rank Hamiltonian factorization algorithm of Ref. [1]. This algorithm
+requires the one- and two-electron integrals as input which can be obtained in different ways. Here,
+we use PennyLane to obtain these integrals. We first need to define the atomic symbols and
+coordinates for the given molecule. Let's use the water molecule at its equilibrium geometry as an
 example
 """
 import pennylane as qml
@@ -59,7 +73,7 @@ geometry = np.array([[0.00000000,  0.00000000,  0.28377432],
 ##############################################################################
 # Then we construct a molecule object by selecting a basis set and compute the one- and two-electron
 # integrals in the molecular orbital basis.
-mol = qml.qchem.Molecule(symbols, geometry, basis_name='sto-3g')
+mol = qml.qchem.Molecule(symbols, geometry, basis_name='6-31g')
 core, one, two = qml.qchem.electron_integrals(mol)()
 
 ##############################################################################
@@ -73,14 +87,25 @@ algo = qml.resource.DoubleFactorization(one, two)
 print(f'Estimated gates : {algo.gates:.2e} \nEstimated qubits: {algo.qubits}')
 
 ##############################################################################
+# This estimation is for a target error that is set to the chemical accuracy, 0.0016
+# :math:`\text{Ha}`, by default. We can change the target error and estimate the number of
+# non-Clifford gates and logical qubits. For instance, setting the target error to a larger value
+# leads to a smaller number of non-Clifford gates and logical qubits
+
+algo = qml.resource.DoubleFactorization(one, two, error=0.016)
+print(f'Estimated gates : {algo.gates:.2e} \nEstimated qubits: {algo.qubits}')
+
+##############################################################################
 # QPE cost for simulating periodic materials
 # ******************************************
-# The cost of implementing the QPE algorithm for periodic materials is estimated following the
-# algorithm of Su et al. for Hamiltonians in first quantization in a
-# plane wave basis. We first need to define the number of plane waves, the number of electrons and
-# the volume of the unit cell that constructs the periodic material. Let's use dilithium iron
-# silicate :math:`\text{Li}_2\text{FeSiO}_4` as an example. Note that atomic units must be used
-# for the cell volume.
+# For periodic materials, we estimate the cost of implementing the QPE algorithm of Su et al.
+# using Hamiltonians in first quantization and in a plane wave basis. We first need to define the
+# number of plane waves, the number of electrons and the volume of the unit cell that constructs
+# the periodic material. Let's use dilithium iron silicate :math:`\text{Li}_2\text{FeSiO}_4` as an
+# example taken from Ref. [battery paper]. For this material, the unit cell contains 156 electrons
+# and has dimensions :math:`9.49 \times 10.20 \times 11.83` in atomic units which amount to a volume
+# of :math:`1145 a_0^3` where :math:`a_0` is the
+# `Bohr radius <https://en.wikipedia.org/wiki/Bohr_radius>`_. We also use 100000 plane waves.
 
 planewaves = 100000
 electrons = 156
@@ -119,8 +144,8 @@ qml.resource.FirstQuantization.norm(planewaves, electrons, volume, error=0.001)
 # In variational quantum algorithms such as VQE, the expectation value of an observable is
 # typically computed by decomposing the observable into a linear combination of Pauli strings,
 # which are tensor products of Pauli and Identity operators. The expectation values is calculated
-# through linearity by measuring the expectation value for each of these terms and combining the results. 
-# The number of qubits required for the measurement is trivially determined by
+# through linearity by measuring the expectation value for each of these terms and combining the
+# results. The number of qubits required for the measurement is trivially determined by
 # the number of qubits the observable acts on. The number of gates required to implement the
 # variational algorithm is determined by a circuit ansatz that is also known a priori. However,
 # estimating the number of circuit measurements required to achieve a certain error in computing the
@@ -133,8 +158,10 @@ qml.resource.FirstQuantization.norm(planewaves, electrons, volume, error=0.001)
 H = qml.qchem.molecular_hamiltonian(symbols, geometry)[0]
 
 ##############################################################################
-# The number of measurements needed to compute :math:`\left \langle H \right \rangle` with
-# chemical accuracy, 0.0016 :math:`\text{Ha}`, can be obtained with
+# The number of measurements needed to compute :math:`\left \langle H \right \rangle` can be
+# obtained with the :func:`~.pennylane.resource.estimate_shots` function which requires the
+# Hamiltonian coefficients and observables as input. The number of measurements required for a
+# target error set to the chemical accuracy, 0.0016 :math:`\text{Ha}`, can be obtained with
 
 # m = qml.resource.estimate_shots(H.coeffs, H.ops, error=0.0016)
 # print(m)
@@ -149,6 +176,10 @@ ops, coeffs = qml.grouping.group_observables(H.ops, H.coeffs)
 # m = qml.resource.estimate_shots(coeffs, ops, error=0.0016)
 # print(m)
 
+# Conclusions
+# -----------
+# This tutorial
+#
 ##############################################################################
 #.. bio:: Soran Jahangiri
 #    :photo: ../_static/Soran.png
