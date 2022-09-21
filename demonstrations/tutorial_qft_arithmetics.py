5414f6c2cf2951e14475707f773682bda6433d38
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2022-09-21 11:45:45 -0400

    Update demonstrations/tutorial_qft_arithmetics.py
    Co-authored-by: Ivana Kurecic <ivanakurecic@gmail.com>

--- demonstrations/tutorial_qft_arithmetics.py
+++ demonstrations/tutorial_qft_arithmetics.py
@@ -67,7 +67,7 @@ represent the numbers from :math:`0` to :math:`2^n-1`.
 
 The first thing we need to know is PennyLane's
 standard for encoding numbers in binary. A binary number can be
-represented as a string of 1s and 0s which we will represent as the multi-qubit state
+represented as a string of 1s and 0s, which we can represent as the multi-qubit state
 
 .. math:: \vert m \rangle = \vert q_0q_1...q_{n-1}\rangle,
 
