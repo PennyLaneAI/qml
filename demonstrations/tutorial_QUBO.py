e929deb1f2cccd06ead5ba34ce4a7c204554ad4c
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2022-10-07 10:38:09 -0400

    first commit

---
+++ demonstrations/tutorial_QUBO.py
@@ -0,0 +1,46 @@
+r""".. _photonics:
+
+Tutorial QUBO
+==============
+
+.. meta::
+    :property="og:description": QUBO es super util
+    :property="og:image": https://pennylane.ai/qml/_images/QUBO_thumbnail.png
+
+.. related::
+   tutorial_qaoa_intro Introduccion al QAOA
+
+*Author: Alberto y Alejandro. Posted: 2023.*
+
+blablabla QUBO
+
+"""
+
+##############################################################################
+#
+# Titulo test
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# bla bla bla texto
+#
+
+import pennylane as qml
+from pennylane import numpy as np
+import matplotlib.pyplot as plt
+
+##############################################################################
+#
+# Pintemos un par de puntos.
+
+plt.plot([0,0], [1,3])
+
+# Los resultados son los esperados.
+#
+# About the author
+# ----------------
+
+##############################################################################
+# .. bio:: Alvaro Ballon
+#    :photo: ../_static/Alvaro.png
+#
+#    Alvaro Ballon is a quantum computing educator at Xanadu. His work involves making the latest developments in quantum computing accessible to the community.
