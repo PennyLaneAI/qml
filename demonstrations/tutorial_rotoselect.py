42cbdf9c5846150b25463d1a5d6f9e49e53b5c01
Ivana Kurecic <ivanakurecic@gmail.com>
2022-10-12 18:49:59 +0200

    Fixed authorship

--- demonstrations/tutorial_rotoselect.py
+++ demonstrations/tutorial_rotoselect.py
@@ -12,13 +12,11 @@ Quantum circuit structure learning
 
 .. related::
 
-   tutorial_vqe Variational quantum eigensolver
+   tutorial_vqe A brief overview of VQE
    tutorial_vqe_qng Accelerating VQEs with quantum natural gradient
    tutorial_rosalin Frugal shot optimization with Rosalin
 
-*Author: Angus Lowe*
-
-*Posted: 16 October 2019. Last updated: 20 Jan 2021.*
+*Author: Angus Lowe — Posted: 16 October 2019. Last updated: 20 Jan 2021.*
 
 """
 ##############################################################################
