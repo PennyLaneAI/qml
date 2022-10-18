eacb8aa07822e32fa0565fe5448217d933bf1b92
RDPEast <Rdp.east@gmail.com>
2022-10-18 18:40:17 +0200

    Update demonstrations/tutorial_equivariant_learning.py
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>

--- demonstrations/tutorial_equivariant_learning.py
+++ demonstrations/tutorial_equivariant_learning.py
@@ -154,7 +154,7 @@ twice and get the second by applying the group action to one of the outputs.
 
 Consider the following figure for 
 example. What we see is a board with a cross in a certain square on the left and some numerical encoding of this
-on the right, where the the 1 is where the X is in the number grid. This is an equivariant map between these two spaces
+on the right, where the 1 is where the X is in the number grid. We present an equivariant mapping between these two spaces
 with respect to a group action that is a rotation or a swap etc (here a :math:`\pi` rotation) either to the original grid
 and then map to the number grid or we could map to the number grid and then apply the group action.
 
