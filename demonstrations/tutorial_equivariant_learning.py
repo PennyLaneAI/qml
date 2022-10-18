66b62cfe2fc3a4c3dc4ddfe204a5c06670972614
RDPEast <Rdp.east@gmail.com>
2022-10-18 18:02:27 +0200

    Update demonstrations/tutorial_equivariant_learning.py
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>

--- demonstrations/tutorial_equivariant_learning.py
+++ demonstrations/tutorial_equivariant_learning.py
@@ -148,7 +148,7 @@ as the same group element represented as a linear map on :math:`W` respectively.
 :math:`f(\varphi_g(v))=\psi_g(f(v))` :math:`\forall g\in G`.
 
 The importance of such a map in machine learning is that if, for example, our neural network layers are
-equivarient maps then two inputs that are related by some intrinsic symmetry (maybe they are reflections etc)
+equivarient maps then two inputs that are related by some intrinsic symmetry (maybe they are reflections)
 preserve this information in the outputs. Which we can see because we could instead input the same input
 twice and get the second by applying the group action to one of the outputs. Consider the following figure for 
 example, what we see is a board with a cross in a certain square on the left and some numerical encoding of this
