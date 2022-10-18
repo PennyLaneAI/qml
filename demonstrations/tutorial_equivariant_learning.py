e5dc37e5573a839c2de27dcedd690c1fe7646551
RDPEast <rdp.east@gmail.com>
2022-10-18 18:30:21 +0200

    addressing comments

--- demonstrations/tutorial_equivariant_learning.py
+++ demonstrations/tutorial_equivariant_learning.py
@@ -45,7 +45,7 @@ that gives us the name *equavariant learning*.
 In classical machine learning, this area is often referred to as geometric deep
 learning (GDL) due to the traditional association of symmetry to the
 world of geometry and the fact that these considerations usually focus on
-deep neural networks (see [#Bronstein2021]_ or [#Nguyen2022]_ for a broad introduction).
+deep neural networks (see [[#Bronstein2021]]_ or [[#Nguyen2022]]_ for a broad introduction).
 We will refer to the quantum computing version of this as quantum geometric machine learning (QGML).
 
 
@@ -179,7 +179,7 @@ unitary representations (and so quantum circuits) we are looking to extend this 
 #
 # Noughts and Crosses
 # ----------------
-# Let's look at the game of noughts and crosses as inspired by [#Meyer2022]_. Two players take
+# Let's look at the game of noughts and crosses as inspired by [[#Meyer2022]]_. Two players take
 # turns to place a O or an X, depending on which player they are, in a 3x3
 # grid. The aim is to get 3 of your symbols in a row, column, or
 # diagonal. As this is not always possible depending
