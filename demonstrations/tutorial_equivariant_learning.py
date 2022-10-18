49288fae223bceea5633f5dffbdb042035e8a0a1
RDPEast <Rdp.east@gmail.com>
2022-10-18 17:27:18 +0200

    Update demonstrations/tutorial_equivariant_learning.py
    Co-authored-by: Nathan Killoran <co9olguy@users.noreply.github.com>

--- demonstrations/tutorial_equivariant_learning.py
+++ demonstrations/tutorial_equivariant_learning.py
@@ -17,7 +17,7 @@ Introduction
 Symmetries are at the heart of physics, indeed in condensed matter and
 particle physics we often define a thing simply by the symmetries it
 adheres to. What does symmetry mean for those in machine learning? In
-this context the ambition is straight forward - it is a means to
+this context the ambition is straightforward — it is a means to
 reduce the parameter space and improve the trained models ability to
 sucessfully label unseen data, i.e., its ability to generalise.
 
