b45cde2ccc2877bb5772b35160ba5522c23c7a88
Matthew Silverman <matthews@xanadu.ai>
2022-11-10 14:01:14 -0500

    fix list format in note

--- demonstrations/qnspsa.py
+++ demonstrations/qnspsa.py
@@ -961,6 +961,7 @@ for i in range(300):
 #
 # .. note::
 #     In order for the remainder of this demo to work, you will need to have done 3 things:
+#
 #     #. Copied the `source_scripts` folder (linked above) to your working directory
 #     #. Authenticated with AWS locally
 #     #. Granted yourself the appropriate permissions as described in this `AWS Braket setup document <https://docs.aws.amazon.com/braket/latest/developerguide/braket-enable-overview.html>`__
