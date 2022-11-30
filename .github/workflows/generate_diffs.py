9de794747a080b0757b92c9753a82b45031a3b08
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2022-11-30 15:21:49 -0500

    revert files out of scope of PR

--- .github/workflows/generate_diffs.py
+++ .github/workflows/generate_diffs.py
@@ -7,7 +7,6 @@ from html_parser import DemoOutputParser
 
 TIMEZONE = pytz.timezone("America/Toronto")
 
-
 def parse_demo_outputs(filename):
     """Parse the outputs produced of a QML repo demonstration from file.
 
@@ -28,17 +27,16 @@ def parse_demo_outputs(filename):
     for d in parser.data:
 
         # We expect to find 'Out' sections as per how Sphinx produces outputs
-        if d != "Out:":
+        if d != 'Out:':
 
-            if "\n" in d:
+            if '\n' in d:
                 # If there are newlines in the string, then extract each line
                 # by splitting and only keep the non-empty ones
-                lines = [line for line in d.split("\n") if line != ""]
+                lines = [line for line in d.split("\n") if line != '']
                 outputs.extend(lines)
             else:
                 outputs.append(d)
     return outputs
-
 
 def write_file_diff(file_obj, qml_version, file_url, outputs, diff_indices):
     """Write the outputs produced by a demonstration from the QML repository to
@@ -55,7 +53,7 @@ def write_file_diff(file_obj, qml_version, file_url, outputs, diff_indices):
         diff_indices (set): the set of indices where a difference was found
     """
     # Write the version name with the associated URL pointing to the demo
-    file_obj.write(f"[{qml_version}]({file_url}):\n\n")
+    file_obj.write(f'[{qml_version}]({file_url}):\n\n')
 
     if len(diff_indices) > 20:
 
@@ -65,26 +63,26 @@ def write_file_diff(file_obj, qml_version, file_url, outputs, diff_indices):
 
         # End html details and code sections, provide a quick summary
         summary_section = "<summary>\n More \n </summary>\n"
-        file_obj.write(f"<details> \n {summary_section} <pre>\n <code>\n")
+        file_obj.write(f'<details> \n {summary_section} <pre>\n <code>\n')
 
         # Dump the outputs
         for idx in diff_indices:
-            file_obj.write(f"{outputs[idx]}\n")
+            file_obj.write(f'{outputs[idx]}\n')
 
         # End html code and details sections
-        file_obj.write(f" </code>\n </pre>\n </details>\n\n")
+        file_obj.write(f' </code>\n </pre>\n </details>\n\n')
 
     else:
 
         # Start a markdown code block
-        file_obj.write(f"```\n")
+        file_obj.write(f'```\n')
 
         # Dump the outputs
         for idx in diff_indices:
-            file_obj.write(f"{outputs[idx]}\n")
+            file_obj.write(f'{outputs[idx]}\n')
 
         # End the markdown code block
-        file_obj.write(f"```\n\n")
+        file_obj.write(f'```\n\n')
 
 
 def main():
@@ -95,29 +93,25 @@ def main():
     master_path = "/tmp/master/home/runner/work/qml/qml/_build/html/demos/"
     dev_path = "/tmp/dev/home/runner/work/qml/qml/_build/html/demos/"
 
-    master_url = "https://pennylane.ai/qml/demos/"
-    dev_url = "http://pennylane.ai-dev.s3-website-us-east-1.amazonaws.com/qml/demos/"
+    master_url = 'https://pennylane.ai/qml/demos/'
+    dev_url = 'http://pennylane.ai-dev.s3-website-us-east-1.amazonaws.com/qml/demos/'
 
     # Get all the filenames
     master_files = os.listdir(master_path)
     dev_files = os.listdir(dev_path)
 
-    master_automatically_run = set(
-        [f for f in master_files if f.startswith("tutorial_")]
-    )
+    master_automatically_run = set([f for f in master_files if f.startswith("tutorial_")])
     dev_automatically_run = set([f for f in dev_files if f.startswith("tutorial_")])
 
     automatically_run = master_automatically_run.union(dev_automatically_run)
 
-    output_file = open("demo_diffs.md", "w")
+    output_file = open('demo_diffs.md','w')
 
     # Write a time update
     update_time = pytz.utc.localize(datetime.utcnow())
     update_time = update_time.astimezone(TIMEZONE)
     update_time_str = update_time.strftime("%Y-%m-%d  %H:%M:%S")
-    output_file.write(
-        f"Last update: {update_time_str} (All times shown in Eastern time)\n"
-    )
+    output_file.write(f"Last update: {update_time_str} (All times shown in Eastern time)\n")
 
     demos_with_diffs = []
     database_of_differences = {}
@@ -129,34 +123,30 @@ def main():
         dev_outputs = parse_demo_outputs(dev_file)
 
         outputs_with_diffs = set()
-        for out_idx, (a, b) in enumerate(zip(master_outputs, dev_outputs)):
+        for out_idx, (a,b) in enumerate(zip(master_outputs, dev_outputs)):
 
-            for i, s in enumerate(difflib.ndiff(a, b)):
+            for i,s in enumerate(difflib.ndiff(a, b)):
 
                 # The output of difflib.ndiff can be one of three cases:
                 # 1. Whitespace (no difference found)
                 # 2-3. The '-' or '+' characters: for character differences
-                if s[0] == " ":
+                if s[0]==' ':
                     continue
 
                 # If any diff found: keep track of the index
-                elif s[0] == "-":
+                elif s[0]=='-':
                     outputs_with_diffs.add(out_idx)
 
-                elif s[0] == "+":
+                elif s[0]=='+':
                     outputs_with_diffs.add(out_idx)
 
         if outputs_with_diffs:
 
             demos_with_diffs.append(filename)
-            database_of_differences[filename] = (
-                master_outputs,
-                dev_outputs,
-                outputs_with_diffs,
-            )
+            database_of_differences[filename] = (master_outputs, dev_outputs, outputs_with_diffs)
 
     if not demos_with_diffs:
-        output_file.write(f"### No differences found between the tutorial outputs. 🎉\n")
+        output_file.write(f'### No differences found between the tutorial outputs. 🎉\n')
     else:
         output_file.write("# List of differences in demonstration outputs\n\n")
 
@@ -164,43 +154,30 @@ def main():
         # contents
         output_file.write("# Table of contents\n\n")
         for i, demo_name in enumerate(demos_with_diffs):
-            output_file.write(f"{i + 1}. [{demo_name}](#demo{i})\n")
+            output_file.write(f'{i + 1}. [{demo_name}](#demo{i})\n')
 
         # 2. Note the number of all demos
-        output_file.write(
-            f"\n\nNumber of demos different/all demos: {len(demos_with_diffs)}/{len(automatically_run)}\n\n"
-        )
+        output_file.write(f"\n\nNumber of demos different/all demos: {len(demos_with_diffs)}/{len(automatically_run)}\n\n")
 
         # 3. Bump the differences
         for i, demo_name in enumerate(demos_with_diffs):
-            master_outputs, dev_outputs, outputs_with_diffs = database_of_differences[
-                demo_name
-            ]
+            master_outputs, dev_outputs, outputs_with_diffs = database_of_differences[demo_name]
 
-            file_html = demo_name.replace(".py", ".html")
+            file_html = demo_name.replace('.py', '.html')
             output_file.write(f'## {i + 1}. {demo_name} <a name="demo{i}"></a>\n\n')
-            output_file.write("---\n\n")
+            output_file.write('---\n\n')
 
             # Write the Master version difference to file
             master_file_url = master_url + file_html
-            write_file_diff(
-                output_file,
-                "Master",
-                master_file_url,
-                master_outputs,
-                outputs_with_diffs,
-            )
+            write_file_diff(output_file, "Master", master_file_url, master_outputs, outputs_with_diffs)
 
             # Write the Dev version difference to file
             dev_file_url = dev_url + file_html
-            write_file_diff(
-                output_file, "Dev", dev_file_url, dev_outputs, outputs_with_diffs
-            )
+            write_file_diff(output_file, "Dev", dev_file_url, dev_outputs, outputs_with_diffs)
 
-            output_file.write("---\n\n")
+            output_file.write('---\n\n')
 
     return 0
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- .github/workflows/html_parser.py
+++ .github/workflows/html_parser.py
@@ -1,32 +1,29 @@
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 
-
 class DemoOutputParser(HTMLParser):
     """An HTML parser class that helps parse the output of demonstrations from
     the QML repository."""
-
+    
     def __init__(self):
 
         self.data = []
-        """Stores the output data of demonstrations extracted from the HTML
-        file of a demonstration."""
+        '''Stores the output data of demonstrations extracted from the HTML
+        file of a demonstration.'''
 
         self.is_demo_output = False
-        """Specifies whether the tag that is being parsed is the output of a cell
-        in the demo."""
+        '''Specifies whether the tag that is being parsed is the output of a cell
+        in the demo.'''
 
         super().__init__()
-
+    
     def handle_starttag(self, tag, attrs):
         """Handles the start tag of a html section looking for Sphinx galery
         output tags."""
 
         for attr in attrs:
             sphinx_script_output_tag = "sphx-glr-script-out"
-            has_output_tag = any(
-                sphinx_script_output_tag in a for a in attr if a is not None
-            )
+            has_output_tag = any(sphinx_script_output_tag in a for a in attr if a is not None)
 
             # The HTML produced by Sphinx has class HTML tags and the
             # sphx-glr-script-out attribute
