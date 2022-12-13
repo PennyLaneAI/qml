760ce669c8045c508946f9fe9bba0b9fdd92093c
rmoyard <rmoyard@gmail.com>
2022-12-13 13:14:00 -0500

    file structure

---
+++ demonstrations/tutorial_zx_calculus.py
@@ -0,0 +1,68 @@
+"""
+ZX calculus
+===========
+
+.. meta::
+    :property="og:description": Investigation of ZX calculus and its applications to quantum computing
+    :property="og:image": https://pennylane.ai/qml/_images/zx.png
+
+*Author: Romain. Posted: January 2023.*
+
+Introduction
+------------
+
+.. figure:: ../demonstrations/zx_calculus/test.png
+    :align: center
+    :width: 70%
+
+    Comment here
+
+"""
+
+import pennylane as qml
+
+
+######################################################################
+# Excitations
+# -----------
+#
+# .. math::
+#
+#    S_s X_i = \left( Z_i Z_a Z_b Z_c \right) X_i = - X_i S_s.
+#
+#
+######################################################################
+# Test include images
+# ^^^^^^^^^^^^^^^^^
+#
+# .. figure:: ../demonstrations/toric_code/types_of_loops.png
+#     :align: center
+#     :width: 50%
+#
+
+
+######################################################################
+# Test
+# ------------------------------
+
+######################################################################
+# Test
+# ^^^^^^^^^^^^^
+#
+
+######################################################################
+# Blabla
+#
+# References
+# ----------
+#
+# .. [#Kitaev2003]
+#
+#    Kitaev, A. Yu. "Fault-tolerant quantum computation by anyons."
+#    `Annals of Physics 303.1 (2003): 2-30. <https://doi.org/10.1016/S0003-4916(02)00018-0>`__.
+#    (`arXiv <https://arxiv.org/pdf/quant-ph/9707021>`__)
+#
+#
+# About the author
+# ----------------
+# .. include:: ../_static/authors/romain_moyard.txt
