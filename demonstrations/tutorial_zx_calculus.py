beaed47b3fdb25ecba5bc71c4797e7378cfb57fd
rmoyard <rmoyard@gmail.com>
2022-12-20 16:20:54 -0500

    draft and images

--- demonstrations/tutorial_zx_calculus.py
+++ demonstrations/tutorial_zx_calculus.py
@@ -11,35 +11,180 @@ ZX calculus
 
 The ZX calculus is a graphical language that can represent any linear maps. Therefore, it can be used to reason
 quantum computations and quantum circuits. Its foundations are based on category theory, which makes it a rigorous
-framework. It was introduced in 2008 by Coecke and Duncan [#Coecke]_ . As it can handle anz linear map, it can be
+framework. It was introduced in 2008 by Coecke and Duncan [#Coecke]_ . As it can handle any linear map, it can be
 considered as a generalization of the circuit representation of quantum computations.
 
 In this tutorial, we first give an overview of the building blocks of the ZX-diagrams and also of the main rewriting
-rules. Then we will explore how to optimize the number of T-gates of a benchmark circuit with PennyLane and PyZX [
-PyZX]_. We also show that simplifying (reducing) a ZX-diagram does not always end up with diagram-like graph, and that
-circuit extraction is a main pain point of the ZX framework. Finally, we give some leads about ZX-calculus advanced
-uses.
+rules, the ZX calculus. Then we will explore how to optimize the number of T-gates of a benchmark circuit with PennyLane
+and PyZX [PyZX]_. We also show that simplifying (reducing) a ZX-diagram does not always end up with diagram-like graph,
+and that circuit extraction is a main pain point of the ZX framework. Finally, we give some leads about ZX-calculus
+advanced uses.
 
-Introduction
-------------
+"""
 
-[#JvdW2020]
+"""
+ZX-diagrams
+-----------
+ZX is graphical depiction of a tensor network
+ZX is a undirected multi graph: you can move vertices
+State
+Tensor
+Composition
 
-.. figure:: ../demonstrations/zx_calculus/zx.png
+[#East2021]_
+[#JvdW2020]_
+
+Symmetric tensors
+The Z spider:
+
+.. figure:: ../demonstrations/zx_calculus/z_spider.png
     :align: center
     :width: 70%
 
-    Comment here
+    The Z spider
+
+The X spider:
+
+.. figure:: ../demonstrations/zx_calculus/x_spider.png
+    :align: center
+    :width: 70%
+
+    The X spider
+    
+All one qubit gates
+Build CNOT
+"""
 
 """
+ZXH-diagrams
+------------
+
+[#East2021]_
+
+[#JvdW2020]_
+
+The H spider:
+
+Hadamard is a blue edge in most papers.
+
+.. figure:: ../demonstrations/zx_calculus/h_box.png
+    :align: center
+    :width: 70%
+
+    The H box
+
+Relationship with X and Z spiders.
+
+Toffoli:
+
+.. figure:: ../demonstrations/zx_calculus/toffoli.png
+    :align: center
+    :width: 70%
+
+    Toffoli
+
+"""
+
+"""
+ZX calculus: rewriting rules
+----------------------------
+dot of same color commute 
+
+(f)use
+
+.. figure:: ../demonstrations/zx_calculus/f_rule.png
+    :align: center
+    :width: 70%
+
+    (f)use
+
+($pi$c)opy
+
+.. figure:: ../demonstrations/zx_calculus/pi_rule.png
+    :align: center
+    :width: 70%
+
+    ($pi$c)opy
+    
+(b)ialgebra
+
+.. figure:: ../demonstrations/zx_calculus/b_rule.png
+    :align: center
+    :width: 70%
+
+    (b)ialgebra
+
+(c)opy
+
+.. figure:: ../demonstrations/zx_calculus/c_rule.png
+    :align: center
+    :width: 70%
+
+    (c)opy
+
+(id)entity
+
+.. figure:: ../demonstrations/zx_calculus/id_rule.png
+    :align: center
+    :width: 70%
+
+    (id)entity
+    
+    
+(ho)pf
+
+.. figure:: ../demonstrations/zx_calculus/hopf_rule.png
+    :align: center
+    :width: 70%
+
+    (ho)pf
+
+[#East2021]_
+
+[#JvdW2020]_
+
+Teleportation example:
+
+.. figure:: ../demonstrations/zx_calculus/teleportation.png
+    :align: center
+    :width: 70%
+
+    Teleportation example 
+
+"""
+
+
+######################################################################
+# ZX-diagrams with PennyLane
+# --------------------------
 
 import pennylane as qml
 import pyzx
 
+dev = qml.device("default.qubit", wires=2)
+
+@qml.transforms.to_zx
+@qml.qnode(device=dev)
+def circuit():
+    qml.PauliX(wires=0),
+    qml.Hadamard(wires=0),
+    qml.CNOT(wires=[0, 1]),
+    return qml.expval(qml.PauliZ(wires=0))
+
+g = circuit()
+pyzx.matplotlib_draw(g)
+
 
 ######################################################################
-# T-count optimization
-# ^^^^^^^^^^^^^^^^^^^^
+# Graph simplification and circuit extraction
+# -------------------------------------------
+# Add the Venne graph for simplification and extraction
+#
+# Full reduce and teleport reduce
+
+######################################################################
+# Example: T-count optimization
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
 # Here we give an example of how to use optimization techniques from ZX calculus to reduce the T count of a quantum
 # circuit and get back a PennyLane circuit.
@@ -156,9 +301,6 @@ def mod_5_4():
         qml.apply(o)
     return qml.expval(qml.PauliZ(wires=0))
 
-######################################################################
-# Graph simplification and circuit extraction
-# -------------------------------------------
 
 ######################################################################
 # Math
@@ -177,10 +319,16 @@ def mod_5_4():
 # References
 # ----------
 #
+# .. [#Duncan2017]
+#
+#    Ross Duncan, Aleks Kissinger, Simon Perdrix, and John van de Wetering. "Graph-theoretic Simplification of Quantum
+#    Circuits with the ZX-calculus"
+#    `Quantum Journal <https://quantum-journal.org/papers/q-2020-06-04-279/pdf/>`__.
+#
 # .. [#Kissinger2021]
 #
 #    Aleks Kissinger and John van de Wetering. "Reducing T-count with the ZX-calculus."
-#    `ArXiv <https://arxiv.org/pdf/1903.10477.pdf__.
+#    `ArXiv <https://arxiv.org/pdf/1903.10477.pdf>`__.
 #
 # .. [#Coecke2011]
 #
@@ -195,7 +343,8 @@ def mod_5_4():
 #
 # .. [#East2021]
 #
-#    Richard D. P. East, John van de Wetering, Nicholas Chancellor and Adolfo G. Grushin. "AKLT-states as ZX-diagrams: diagrammatic reasoning for quantum states."
+#    Richard D. P. East, John van de Wetering, Nicholas Chancellor and Adolfo G. Grushin. "AKLT-states as ZX-diagrams:
+#    diagrammatic reasoning for quantum states."
 #    `ArXiv <https://arxiv.org/pdf/2012.01219.pdf>`__.
 #
 # .. [#PyZX]
