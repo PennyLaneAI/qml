2a947a62d7a7e24744f31455670b7c1284a9cd48
Jack S. Baker <jsbaker1001@gmail.com>
2023-01-19 01:11:15 -0500

    Update demonstrations/tutorial_univariate_qvr.py
    Co-authored-by: Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>

--- demonstrations/tutorial_univariate_qvr.py
+++ demonstrations/tutorial_univariate_qvr.py
@@ -923,7 +923,7 @@ tr_dispatch_id = ct.dispatch(training_workflow)(**training_options)
 
 ######################################################################
 # If you are running the notebook version of this tutorial, if you
-# naviagate to http://localhost:48008/ you can view the workflow on the
+# navigate to http://localhost:48008/ you can view the workflow on the
 # Covalent GUI. It will look like the below, showing nicely how all of the
 # electrons defined above interact with eachother in the workflow. You can
 # also track the progress of the calculation here.
