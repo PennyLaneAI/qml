c25ceca6f85ac4da507251d866b3f2435fa4eb78
AlbertMitjans <a.mitjanscoma@gmail.com>
2023-01-30 10:12:06 +0100

    :memo: docs (Rosalin): Small fix.

--- demonstrations/tutorial_rosalin.py
+++ demonstrations/tutorial_rosalin.py
@@ -238,10 +238,15 @@ for i in range(100):
 
 non_analytic_dev.shots = int(total_shots / len(coeffs))
 
-@qml.qnode(non_analytic_dev)
-def cost(weights):
-    StronglyEntanglingLayers(weights, wires=non_analytic_dev.wires)
-    return qml.expval(qml.Hamiltonian(coeffs, obs))
+def cost(params):
+    result = 0
+    for c, o in zip(coeffs, obs):
+        @qml.qnode(non_analytic_dev)
+        def h(weights):
+            StronglyEntanglingLayers(weights, wires=non_analytic_dev.wires)
+            return qml.expval(c * o)
+        result += h(params)
+    return result
 
 opt = qml.AdamOptimizer(0.05)
 params = init_params
@@ -580,6 +585,7 @@ print(adam_shots_per_step)
 params = init_params
 opt = qml.AdamOptimizer(0.07)
 
+non_analytic_dev.shots = adam_shots_per_eval
 
 @qml.qnode(non_analytic_dev, diff_method="parameter-shift")
 def cost(weights):
