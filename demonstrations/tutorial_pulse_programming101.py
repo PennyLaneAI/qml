6041e867768227fab0b9ee44045c9cb869ddcc83
Korbinian Kottmann <Korbinian.Kottmann@gmail.com>
2023-03-01 19:47:17 +0100

    small fixes

--- demonstrations/tutorial_pulse_programming101.py
+++ demonstrations/tutorial_pulse_programming101.py
@@ -90,10 +90,10 @@ print(Ht((p1, p2), t))        # order of parameters p1, p2 matters
 
 ##############################################################################
 # We can construct general Hamiltonians of the form :math:`\sum_i H_i^d + \sum_i f_i(p, t) H_i`
-# using :func:`qml.dot <pennylane.dot>`. Such a time-dependent Hamiltonian consists time-indepdenent drift terms :math:`H_i^d`
-# and time dependent control terms :math:`f_i(p, t) H_i` with scalar complex-valued function :math:`f_i(p, t)` in time that is 
-# parametrized by some parameters :math:`p`.
-# In the following we are going to construct :math:`\sum_i X_i X_{i+1} + \sum_i f_i(p, t) Z_i`
+# using :func:`qml.dot <pennylane.dot>`. Such a time-dependent Hamiltonian consists of time indepdenent drift terms :math:`H_i^d`
+# and time dependent control terms :math:`f_i(p, t) H_i` with scalar complex-valued functions :math:`f_i(p, t)` in time that may be 
+# parametrized by parameters :math:`p`.
+# In the following we are going to construct :math:`\sum_i X_i X_{i+1} + \sum_i f_i(p, t) Z_i` as an example:
 
 coeffs = [1.0] * 2
 coeffs += [lambda p, t: jnp.sin(p[0] * t) + jnp.sin(p[1] * t) for _ in range(3)]
@@ -104,13 +104,13 @@ Ht = qml.dot(coeffs, ops)
 
 # random coefficients
 key = jax.random.PRNGKey(777)
-subkeys = jax.random.split(key, 3)
+subkeys = jax.random.split(key, 3) # create list of 3 subkeys
 params = [jax.random.uniform(subkeys[i], shape=[2], maxval=5) for i in range(3)]
 print(Ht(params, 0.5))
 
 ##############################################################################
 # We can visualize the Hamiltonian interaction by plotting the time-dependent envelopes. We refer to the drift term as all constant terms in time, i.e. :math:`\sum_i X_i X_{i+1}`,
-# and plot the envelopes :math:`f_i(p, t)` of the time dependent terms :math:`\sum_i f_i(p, t) Z_i`.
+# and plot the envelopes :math:`f_i(p, t)` of the time dependent terms :math:`f_i(p, t) Z_i`.
 
 ts = jnp.linspace(0.0, 5.0, 100)
 fs = Ht.coeffs_parametrized
@@ -120,7 +120,7 @@ fig, axs = plt.subplots(nrows=n_channels, figsize=(5, 2 * n_channels), gridspec_
 for n in range(n_channels):
     ax = axs[n]
     ax.plot(ts, fs[n](params[n], ts))
-    ax.set_ylabel(f"Z_{n}")
+    ax.set_ylabel(f"$f_{n}$")
 axs[0].set_title(f"Drift term: $X_0 X_1 + X_1 X_2$")
 axs[-1].set_xlabel("time t")
 plt.tight_layout()
@@ -149,8 +149,9 @@ print(qnode(params))
 
 ##############################################################################
 # We used the decorator ``jax.jit`` to compile this execution just-in-time. This means the first execution will typically take a little longer with the
-# benefit that all following executions will be significantly faster, see the `jax docs on jitting <https://jax.readthedocs.io/en/latest/jax-101/02-jitting.html>`_. JIT-compiling is optional, and one can remove the decorator when only single executions
-# are of interest.
+# benefit that all following executions will be significantly faster, see the `jax docs on jitting <https://jax.readthedocs.io/en/latest/jax-101/02-jitting.html>`_.
+# Note that when removing the ``jax.jit`` decorator, the numerical solver `odeint <https://github.com/google/jax/blob/main/jax/experimental/ode.py>`_ for the time evolution
+# inside `qml.evolve` is still jit-compiled by default (just that function).
 #
 # Researchers interested in more specific hardware systems can simulate them using the specific Hamiltonian interactions.
 # For example, we will simulate a transmon qubit system in the ctrl-VQE example in the last section of this demo.
