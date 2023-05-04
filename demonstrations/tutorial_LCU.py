30e097bec75a17f62c1e640302a808c666be0bd9
Oriel Kiss <47891476+orielkiss@users.noreply.github.com>
2023-05-04 16:45:56 -0400

    docstring

--- demonstrations/tutorial_LCU.py
+++ demonstrations/tutorial_LCU.py
@@ -1,77 +1,75 @@
-
-
 ######################################################################
 # In this notebook, we will see a practical implementation of a block encoding technique based on
 # linear combination of unitaries (LCU), which can be useful to simulate dynamics of quantum system.
-# 
+#
 
 ######################################################################
 # Linear combination of unitaries
 # -------------------------------
-# 
+#
 # Even if quantum circuit are described by unitary matrices, quantum computers are still able to
 # perform non unitary operations, by using higher dimensional space. In practice, any matrix :math:`H`
 # can be block encoded into a unitary matrix of higher dimension as
-# 
+#
 # :raw-latex:`\begin{equation}
 # V=\begin{pmatrix}H&*\\*&* \end{pmatrix},
 # \end{equation}`
-# 
+#
 # where :math:`*` denote arbitrary numbers such that :math:`V` is unitary.
-# 
+#
 # The key ingredient is to write :math:`H` as a linear combination of :math:`K` unitaries
 # (`LCU <https://arxiv.org/abs/1202.5822>`__):
-# 
+#
 # :raw-latex:`\begin{equation}
 # H = \sum_{k=0}^{K-1} \alpha_k U_k,
 # \end{equation}`
-# 
+#
 # with :math:`\alpha_k \in \mathbb{C}` :math:`\{0\}` and :math:`U_k` unitary. By assuming that
 # :math:`H` is hermitian, whih is the case for the Hamiltonian of any physical system, we can find a
 # simple decomposition by projecting it onto the Pauli basis. We note that, in general, the
 # performance is driven by the 1 norm of the decomposition, and thus it remains a non trivial task to
 # choose a suitable one.
-# 
+#
 # Block encoding
 # --------------
-# 
+#
 # Block encoding relies on two important subroutines:
-# 
+#
 # -  The PREPARE subroutine encodes the coefficients of the Hamiltonian in the amplitudes of the
 #    quantum state as
-# 
+#
 # :raw-latex:`\begin{equation}
 # \text{PREPARE}|\bar{0}\rangle = \sum_{k=0}^{K-1} \sqrt{\frac{\alpha_k}{\|\vec{\alpha}\|_1}} |k\rangle,
 # \end{equation}`
-# 
+#
 # where :math:`|\bar{0}\rangle = |0^{\otimes \lceil \log_2{K}\rceil} \rangle` is the ancillary
 # register. Note that we can always assume that :math:`\alpha_k\in \mathbb{R}^+` by assimilating the
 # phase into the corresponding unitary.
-# 
+#
 # This can be achieved using the
 # ```qml.MottonenStatePreparation`` <https://docs.pennylane.ai/en/stable/code/api/pennylane.MottonenStatePreparation.html>`__
 # function with the vector :math:`\vec{\alpha} = (\alpha_1, \cdots, \alpha_n)`.
-# 
+#
 # -  The SELECT subroutine instead applies the :math:`k`-th unitary :math:`U_k` on
 #    :math:`|\psi\rangle`, when given access to the state :math:`|k\rangle` as follows
-# 
+#
 # :raw-latex:`\begin{equation}
 # \text{SELECT} |k\rangle |\psi\rangle  = |k\rangle U_k|\psi \rangle.
 # \end{equation}`
-# 
+#
 # This can be acchieved using control
 # ```qml.ctrl`` <https://docs.pennylane.ai/en/stable/code/api/pennylane.ctrl.html>`__ operations on
 # the ancila qubits.
-# 
+#
 # -  :math:`H` can then be block encoded using the following operation:
 #    :math:`\|\vec{\alpha}\|_1 \cdot` PREPARE\ :math:`^\dagger` SELECT PREPARE
 #    :math:`|\bar{0}\rangle`.
-# 
+#
 
 ######################################################################
 # Apply the LCU to a quantum state
 # --------------------------------
-# 
+#
 # Let’s focus on the particular example where the LCU is composed of :math:`K=4` terms, and you want
 # to apply :math:`H` to a quantum state :math:`|\psi\rangle`. We can show that
 # :raw-latex:`\begin{equation}
@@ -79,130 +77,146 @@
 # \end{equation}` where :math:`|\Phi\rangle^\perp` is some orthogonal state obtained when the
 # algorithm fails. The desired state, up to the normalisation factor, can then be obtained via post
 # selecting on :math:`|\bar{0}\rangle`. The following circuit summaries the result.
-# 
+#
 # .. container::
-# 
-# 
+#
+#
 
 ######################################################################
 # Let start by setting the problem. We choose a random Hermitian matrix of size
 # :math:`2^n \times 2^n`, with :math:`n=2`.
-# 
+#
 
 import numpy as np
 import pennylane as qml
 
-n = 2 #physical system size 
+n = 2  # physical system size
 
-shape = (2**n,2**n) 
-H     = np.random.uniform(-1, 1, shape) + 1.j * np.random.uniform(-1, 1, shape)  # random matrix
-H     = H + H.conjugate().transpose()  # makes it hermitian
+shape = (2**n, 2**n)
+H = np.random.uniform(-1, 1, shape) + 1.0j * np.random.uniform(-1, 1, shape)  # random matrix
+H = H + H.conjugate().transpose()  # makes it hermitian
 
 
-LCU    = qml.pauli_decompose(H)  #Projecting the Hamiltonian onto the Pauli basis 
-alphas = LCU.terms()[0] 
+LCU = qml.pauli_decompose(H)  # Projecting the Hamiltonian onto the Pauli basis
+alphas = LCU.terms()[0]
 
-print('LCU decomposition: \n',LCU)
+print("LCU decomposition: \n", LCU)
 
 phases = np.angle(alphas)
 coeffs = np.abs(alphas)
 
-coeffs  = np.sqrt(coeffs)
+coeffs = np.sqrt(coeffs)
 coeffs /= np.linalg.norm(coeffs, ord=2)  # normalise the coefficients
 
-unitaries = [qml.matrix(op) for op in LCU.terms()[1] ]
+unitaries = [qml.matrix(op) for op in LCU.terms()[1]]
 
 
-K = len(coeffs) # number of terms in the decomposition 
-a = int(np.ceil(np.log2(K))) # number of ancilla qubits 
-
+K = len(coeffs)  # number of terms in the decomposition
+a = int(np.ceil(np.log2(K)))  # number of ancilla qubits
 
 
 wires_ancilla = np.arange(a)
-wires_physical = np.arange(a,a+n)
+wires_physical = np.arange(a, a + n)
 
-def Block_encoding(coeffs, phases,  unitaries):
-    
+
+def Block_encoding(coeffs, phases, unitaries):
+    """
+    Perform a block encoding of the LCU matrix
+    H = sum_k coeffs[k]*e^{i*phases[k]} *unitaries[k]
+    Args:
+        coeffs: absolute values of the coefficients of the LCU
+        phases: phases of the coefficients of the LCU
+        unitaries: unitaries of the LCU decompositions
+
+    Returns:
+        Unitary matrix containing the LCU as the first (2**nx2**n) block,
+        where n is the system size.
+    """
+
     # Prep
-    qml.MottonenStatePreparation(coeffs , wires = wires_ancilla)
-    
+    qml.MottonenStatePreparation(coeffs, wires=wires_ancilla)
+
     # Select
     for k in range(K):
-        qml.ctrl(qml.QubitUnitary, control = wires_ancilla, 
-                 control_values = np.binary_repr(k,width=a))(
-                np.exp(1.j*phases[k])*unitaries[k],wires = wires_physical)
-  
+        qml.ctrl(
+            qml.QubitUnitary, control=wires_ancilla, control_values=np.binary_repr(k, width=a)
+        )(np.exp(1.0j * phases[k]) * unitaries[k], wires=wires_physical)
+
     # Reverse Prep
-    qml.adjoint(qml.MottonenStatePreparation)(coeffs, wires = wires_ancilla)
-        
+    qml.adjoint(qml.MottonenStatePreparation)(coeffs, wires=wires_ancilla)
+
+
 matrix = qml.matrix(Block_encoding)(coeffs, phases, unitaries)
-block_matrix = np.linalg.norm(alphas,ord=1)*matrix[:2**n,:2**n]
+block_matrix = np.linalg.norm(alphas, ord=1) * matrix[: 2**n, : 2**n]
 
-print("LCU block encoding:\n",np.round(block_matrix,3),'\n')
+print("LCU block encoding:\n", np.round(block_matrix, 3), "\n")
 
-print("Hamiltonian: \n", np.round(H,3),'\n')
+print("Hamiltonian: \n", np.round(H, 3), "\n")
 
-print('Error: ',np.linalg.norm(block_matrix-H),'\n')
+print("Error: ", np.linalg.norm(block_matrix - H), "\n")
 
-print('V is unitary: ', np.allclose(np.eye(matrix.shape[0]),np.dot(matrix.conjugate().transpose(),matrix)))
+print(
+    "V is unitary: ",
+    np.allclose(np.eye(matrix.shape[0]), np.dot(matrix.conjugate().transpose(), matrix)),
+)
 
 
 ######################################################################
 # We see that :math:`H` is exactly block encoded into a larger unitary matrix, which can be run on a
 # quantum computer.
-# 
+#
 
 ######################################################################
 # Application to quantum simulation
 # ---------------------------------
-# 
+#
 # The main problem in quantum chemistry is to be able to extract useful information about quantum
 # state. For instance, the energy of a state can be obtained with the quantum phase estimation
 # algorithm, which relies on performing time evolution. Trotter-Suzuki decompositions are popular
 # techniques to evolve a quantum state, by approximating the time evolution operator using product
 # formulas. For example, the first order product formula for a Hamiltonian of the form
 # :math:`H=\sum_{l=1}^{L} H_l`, where the :math:`H_l` terms are only *hermitian*, is given by
-# 
+#
 # :raw-latex:`\begin{equation}
 # e^{-iHt} \approx \prod_{l=1}^{L}e^{-iH_lt}.
 # \end{equation}`
-# 
+#
 # While these methods already run in polynomial time, better complexity can be achieved by instead
 # expanding the time evolution operator into a `Taylor serie <https://arxiv.org/abs/1412.4687>`__, and
 # truncate it to some order :math:`M`. Hence, we can write
-# 
+#
 # :raw-latex:`\begin{equation}
 # e^{-iHt} = \sum_{k=0}^{\infty} \frac{(-iHt)^k}{k!} \approx \sum_{k=0}^{M} \frac{(-iHt)^k}{k!},
 # \end{equation}`
-# 
+#
 # which can be brought into a LCU form as above, since :math:`H^k` is hermitian.
-# 
+#
 # As a concrete example, let us consider a first order truncation
-# 
+#
 # :raw-latex:`\begin{equation}
 # e^{-iHt} \approx \mathbb{1} -iHt,
 # \end{equation}`
-# 
+#
 # whose simulation can be recast into the problem we just solved!
-# 
+#
 
 ######################################################################
 # Conclusions
 # -----------
-# 
+#
 # We learnt how to decompose a hermitian matrix into a linear combination of Pauli operators, which
 # can be block encoded into a larger unitary matrix. This scheme is useful to perform time evolution
 # via Taylor expansion, which is a recurring sub routine in quantum algorithms.
-# 
+#
 
 ######################################################################
 # References
 # ----------
-# 
+#
 # `[Childs12] <https://arxiv.org/abs/1202.5822>`__ Andrew M. Childs, Nathan Wiebe, *Hamiltonian
 # Simulation Using Linear Combinations of Unitary Operations* (2012)
-# 
+#
 # `[Berry14] <https://arxiv.org/abs/1412.4687>`__ Dominic W. Berry, Andrew M. Childs, Richard Cleve,
 # Robin Kothari and Rolando D. Somma *Simulating Hamiltonian dynamics with a truncated Taylor series*
 # (2014)
-# 
+#
