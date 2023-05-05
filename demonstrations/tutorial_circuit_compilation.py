94ea34cffdaf6b47ad531f8606a6d5608b26104f
BorjaRequena <borja.requena.pozo@gmail.com>
2023-05-05 20:11:11 +0200

    Text improvements with links to docs

--- demonstrations/tutorial_circuit_compilation.py
+++ demonstrations/tutorial_circuit_compilation.py
@@ -1,30 +1,35 @@
 r"""Compilation of quantum circuits
 ===============================
 
-There are many equivalent representations of quantum circuits that use different sets of gates but
-provide the same output. Out of those, we are typically interested in finding the most suitable one
-for our purposes. For example, in the compilation of quantum circuits, we look for the one that will
-incur the least amount of errors in a specific hardware. This usually implies decomposing the
-quantum gates in terms of the native ones of the quantum device, adapting the operations to the
-hardrware’s topology, combining them together to reduce the circuit depth, etc.
+Quantum circuits take many different forms from the moment we design them to the point where they
+are ready to run in a quantum device. These changes happen during the compilation process of the
+quantum circuits, which rely on the fact that there are many equivalent representations of quantum
+circuits that use different sets of gates but provide the same output.
 
-In PennyLane, we can apply
-```transforms`` <https://docs.pennylane.ai/en/stable/code/qml_transforms.html>`__ to our quantum
-functions in order to obtain equivalent ones that may be more convenient for our task. In this
-tutorial, we show how to implement the most fundamental transforms involved in the compilation of
-quantum circuits.
+Out of those representations, we are typically interested in finding the most suitable one for our
+purposes. For example, we usually look for the one that will incur the least amount of errors in the 
+specific hardware for which we are compiling the circuit. This usually implies decomposing the
+quantum gates in terms of the native ones of the quantum device, adapting the operations to the
+hardrware's topology, combining them together to reduce the circuit depth, etc.
+
+A great part of the compilation process consists on repeatedly performing minor circuit modifications.
+In PennyLane, we can apply :mod:`~pennylane.transforms` to our quantum functions in order to obtain
+equivalent ones that may be more convenient for our task. In this tutorial, we introduce the most
+fundamental transforms involved in the compilation of quantum circuits.
 
 Circuit transforms
 ==================
 
 When we implement quantum algorithms, it is typically in our best interest that the resulting
-circuits are as shallow as possible, specially in the current quantum devices. However, they often
-have multiple operations that can be combined together to reduce the circuit complexity, although it
-is not always obvious. Here, we introduce three simple circuit transforms that can be combined
-together to obtain simpler quantum circuits. The transforms are based on very basic circuit
-equivalences:
+circuits are as shallow as possible, especially with the noisy quantum devices available at present.
+However, they often are more complex than needed containing multiple operations that could be
+combined together to reduce the circuit complexity, although it is not always obvious. Here, we
+introduce three simple circuit :mod:`~pennylane.transforms` that can be combined together to obtain
+simpler quantum circuits. The transforms are based on very basic circuit equivalences:
 
-.. image:: qml/demonstrations/circuit_compilation/circuit_transforms.png
+.. figure:: qml/demonstrations/circuit_compilation/circuit_transforms.png
+    :align: center
+    :width: 90%
 
 In order to illustrate their combined effect, let us consider the following circuit.
 """
@@ -59,8 +64,7 @@ qml.draw_mpl(qnode, decimals=1)(angles)
 # Given an arbitrary quantum circuit, it is usually hard to clearly understand what is really
 # happening. To obtain a better picture, we can shuffle the commuting operations to better distinguish
 # between groups of single-qubit and two-qubit gates. We can easily do so by applying the
-# ```commute_controlled`` <https://docs.pennylane.ai/en/stable/code/api/pennylane.transforms.commute_controlled.html>`__
-# transform.
+# :func:`~pennylane.transforms.commute_controlled` transform.
 #
 
 commuted_circuit = qml.transforms.commute_controlled()(circuit)
@@ -71,13 +75,11 @@ qml.draw_mpl(qnode, decimals=1)(angles)
 ######################################################################
 # With this rearrangement, we can clearly identify a few operations that can be merged together.
 # First, the two consecutive CNOT gates from the third to the second qubits will cancel each other
-# out. We can remove these with the
-# ```cancel_inverses`` <https://docs.pennylane.ai/en/stable/code/api/pennylane.transforms.cancel_inverses.html>`__
-# transform, which removes consecutive inverse operations. Then, we can combine the two ``RX``
-# rotations in the first qubit into a single one with the sum of the angles. Finally, the two ``RZ``
+# out. We can remove these with the :func:`~pennylane.transforms.cancel_inverses`
+# transform, which removes consecutive inverse operations. Then, we can combine the two :class:`~.RX`
+# rotations in the first qubit into a single one with the sum of the angles. Finally, the two :class:`~.RZ`
 # rotations in the third qubit will also cancel each other. We can combine these rotations with the
-# ```merge_rotations`` <https://docs.pennylane.ai/en/stable/code/api/pennylane.transforms.merge_rotations.html>`__
-# transform.
+# :func:`~pennylane.transforms.merge_rotations` transform.
 #
 # Let us see the result of applying both transforms to the rearranged circuit. We start with the
 # ``cancel_inverses`` one.
@@ -141,8 +143,7 @@ qml.draw_mpl(q_fun, decimals=1)(angles)
 # representations.
 #
 # We can apply all the transforms introduced above with the
-# ```compile`` <https://docs.pennylane.ai/en/stable/code/api/pennylane.compile.html>`__ function,
-# which yields the same final circuit.
+# :func:`~pennylane.transforms.compile` function, which yields the same final circuit.
 #
 
 compiled_circuit = qml.compile()(circuit)
