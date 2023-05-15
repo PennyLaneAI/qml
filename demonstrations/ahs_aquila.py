7eae1e7a7b6e7871cf862ffcf04b016c38ff8f3a
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2023-05-15 08:59:52 -0400

    Update demonstrations/ahs_aquila.py

--- demonstrations/ahs_aquila.py
+++ demonstrations/ahs_aquila.py
@@ -190,7 +190,7 @@ detail where to find these specifications and where they are relevant as we go t
 
 .. note::
 
-    Those cells of this notebook that contain the real device ``aquila`` will only run when hardware is online. If you want to run it at
+    Those cells of this demo that contain the real device ``aquila`` will only run when hardware is online. If you want to run it at
     other times to experiment with the concepts, the hardware device can be switched out with the Braket
     simulator. When interpreting the section of the demo regarding discretization for hardware, bear in
     mind that the simulator does not discretize the functions before upload, so it will not accurately
