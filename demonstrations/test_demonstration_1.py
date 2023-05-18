9eb90e58694d73aa43f100159652b39aad2b8b34
BM7878 <117289949+BM7878@users.noreply.github.com>
2023-05-18 16:00:16 +0100

    Began adding formatting test demonstration

---
+++ demonstrations/test_demonstration_1.py
@@ -0,0 +1,47 @@
+r"""
+
+Formatting Test Demonstration
+=============================
+
+.. meta::
+    :property="og:description": A demonstration for testing different formatting features.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+"""
+
+##############################################################################
+# .. important::
+#
+#     This is an 'important' block. It should have an orange background.
+#
+#     * List Item 1
+#     * List Item 2
+#     * List Item 3
+#
+#     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+#
+# .. admonition:: Definition
+#     :class: defn
+#
+#     This is a 'definition' block. It should have no background colour, a green title, and padding around it that makes the text narrower than that above and below.
+#
+#     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+#
+# .. tip::
+#
+#     This is a 'tip' block. It should have a blue background.
+#
+#     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+#
+# .. note::
+#
+#     This is a 'note' block. It should have a green background.
+#
+#     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+#
+# .. warning::
+#
+#     This is a 'warning' block. It should have an orange background.
+#
+#     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
