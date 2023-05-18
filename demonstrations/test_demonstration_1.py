647975cb5735fcbd7486827b830646e81711b1a1
BM7878 <117289949+BM7878@users.noreply.github.com>
2023-05-18 19:33:17 +0100

    Added more formatting blocks

--- demonstrations/test_demonstration_1.py
+++ demonstrations/test_demonstration_1.py
@@ -8,6 +8,66 @@ Formatting Test Demonstration
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
+Here we have some *italic text* and some **bold text**.
+
+A bulleted list
+
+* List Item 1
+* List Item 2
+* List Item 3
+
+A numbered list
+
+1. List Item 1
+2. List Item 2
+3. List Item 3
+
+Another numbered list
+
+#. List Item 1
+#. List Item 2
+#. List Item 3
+
+A Heading
+---------
+
+Here we have an inline ``code block``.
+
+Below are some code blocks that are *within* an RST comment, so are not processed as code.
+
+.. code-block:: bash
+
+    pip install amazon-braket-pennylane-plugin
+
+.. code-block:: none
+
+    Execution time on remote device (seconds): 3.5898206680030853
+    Execution time on local device (seconds): 23.50668462700196
+
+Figures
+-------
+
+Centre-aligned
+
+.. figure:: ../_static/remote-multi-job-simulator.png
+    :align: center
+    :scale: 75%
+    :alt: PennyLane can leverage Braket for parallelized gradient calculations
+
+Left-aligned
+
+.. figure:: ../_static/remote-multi-job-simulator.png
+    :align: left
+    :scale: 75%
+    :alt: PennyLane can leverage Braket for parallelized gradient calculations
+
+Right-aligned
+
+.. figure:: ../_static/remote-multi-job-simulator.png
+    :align: right
+    :scale: 75%
+    :alt: PennyLane can leverage Braket for parallelized gradient calculations
+
 """
 
 ##############################################################################
@@ -45,3 +105,9 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 #     This is a 'warning' block. It should have an orange background.
 #
 #     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+#
+# Some 'includes'
+# -----------------
+# .. include:: ../_static/authors/thomas_bromley.txt
+#
+# .. include:: ../_static/authors/maria_schuld.txt
