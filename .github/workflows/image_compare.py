5a7243dcfa5c2952731836070d039d8cd14c9fb3
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-06-12 15:57:40 -0400

    Fixed paths

--- .github/workflows/image_compare.py
+++ .github/workflows/image_compare.py
@@ -6,8 +6,8 @@ def main():
     repository, compares the output of each demo and writes to a file based on the
     differences found.
     """
-    master_path = "/home/runner/work/qml/qml/master/_images"
-    dev_path = "/home/runner/work/qml/qml/dev/_images"
+    master_path = "/home/runner/work/qml/qml/master/_images/"
+    dev_path = "/home/runner/work/qml/qml/dev/_images/"
 
     master_url = 'https://pennylane.ai/qml/_images/'
     dev_url = 'http://pennylane.ai-dev.s3-website-us-east-1.amazonaws.com/qml/_images/'
