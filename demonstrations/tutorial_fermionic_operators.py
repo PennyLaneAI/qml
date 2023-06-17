4776182e23126f73dcf2a4fd0abfb7525f05ec3a
soranjh <soranjh@yahoo.com>
2023-06-17 11:36:15 -0400

    add code review comments

--- demonstrations/tutorial_fermionic_operators.py
+++ demonstrations/tutorial_fermionic_operators.py
@@ -36,24 +36,27 @@ import pennylane as qml
 from pennylane import numpy as np
 from pennylane import FermiC, FermiA
 
-c0 = FermiC(0)
+at0 = FermiC(0)
 a1 = FermiA(1)
 
+print(at0)
+print(a1)
+
 ##############################################################################
-# We used the compact notations :math:`c0` and :math:`a1` to denote a creation operator applied to
+# We used the compact notations :math:`at0` and :math:`a1` to denote a creation operator applied to
 # the 0th orbital and an annihilation operator applied to the 1st one, respectively. Once created,
 # these operators can be multiplied or added to each other to create new operators. A product of
-# fermionic operators is called a *Fermi word*, and linear combination of Fermi words is called a
-# *Fermi sentence*.
+# fermionic operators will be called a *Fermi word* and a linear combination of Fermi words will be
+# called a *Fermi sentence*.
 
-fermi_word = c0 * a1
-fermi_sentence = 1.2 * c0 * a1 + 2.4 * c0 * a1
+fermi_word = at0 * a1
+fermi_sentence = 1.3 * at0 * a1 + 2.4 * at0 * a1
 fermi_sentence
 
 ##############################################################################
 # In this simple example, we first created the operator :math:`a^{\dagger}_0 a_1` and then created
-# the linear combination :math:`1.2 a^{\dagger}_0 a_1 + 2.4 a^{\dagger}_0 a_1`, which is
-# automatically simplified to :math:`3.6 a^{\dagger}_0 a_1`. You can also perform arithmetic
+# the linear combination :math:`1.3 a^{\dagger}_0 a_1 + 2.4 a^{\dagger}_0 a_1`, which is
+# automatically simplified to :math:`3.7 a^{\dagger}_0 a_1`. You can also perform arithmetic
 # operations between Fermi words and Fermi sentences.
 
 fermi_sentence = fermi_sentence * fermi_word + 2.3 * fermi_word
@@ -61,7 +64,7 @@ fermi_sentence
 
 ##############################################################################
 # Beyond multiplication, summation, and subtraction, we can exponentiate fermionic operators in
-# PennyLane to an integer power. For instance, we can create a more complicated operator.
+# PennyLane to an integer power. For instance, we can create a more complicated operator
 #
 # .. math::
 #
@@ -69,7 +72,7 @@ fermi_sentence
 #
 # in the same way that you would write down the operator on a piece of paper.
 
-fermi_sentence = 1.2 * c0 + 0.5 * a1 - 2.3 * (c0 * a1) ** 2
+fermi_sentence = 1.2 * at0 + 0.5 * a1 - 2.3 * (at0 * a1) ** 2
 fermi_sentence
 
 ##############################################################################
@@ -90,7 +93,7 @@ pauli_sentence
 # A toy model
 # ^^^^^^^^^^^
 # Our first example is a toy Hamiltonian inspired by the
-# `Hückel method <https://en.wikipedia.org/wiki/H%C3%BCckel_method>`_, which is a simple method for
+# `Hückel method <https://en.wikipedia.org/wiki/H%C3%BCckel_method>`_, which is a method for
 # describing molecules with alternating single and double bonds. Our toy model is a simplified
 # version of the Hückel Hamiltonian and assumes only two orbitals and a single electron.
 #
@@ -101,26 +104,26 @@ pauli_sentence
 #
 # This Hamiltonian can be constructed with pre-defined values for :math:`\alpha` and :math:`\beta`.
 
-c1 = FermiC(1)
+at1 = FermiC(1)
 a0 = FermiA(0)
 
 alpha = 0.01
 beta = -0.02
-h = alpha * (c0 * a0 + c1 * a1) + beta * (c0 * a1 + c1 * a0)
+h = alpha * (at0 * a0 + at1 * a1) + beta * (at0 * a1 + at1 * a0)
 
 ##############################################################################
 # The fermionic Hamiltonian can be converted to the qubit Hamiltonian with
 
 h = qml.jordan_wigner(h)
-print(h)
 
 ##############################################################################
 # The matrix representation of the qubit Hamiltonian in the computational basis can be diagonalized
 # to get its eigenpairs:
 
 val, vec = np.linalg.eigh(h.sparse_matrix().toarray())
-print(val)
-print(np.real(vec.T))
+print(f"eigenvalues:\n{val}")
+print()
+print(f"eigenvectors:\n{np.real(vec.T)}")
 
 ##############################################################################
 # The eigenvalues of :math:`\alpha + \beta` and :math:`\alpha - \beta` correspond to the states
@@ -133,31 +136,38 @@ print(np.real(vec.T))
 # Hamiltonian is usually constructed as
 #
 # .. math::
-#     H = \sum_{\alpha \in \{\uparrow, \downarrow \} } \sum_{pq} h_{pq} a_{p,\alpha}^{\dagger}
+#     H = \sum_{\alpha \in \{\uparrow, \downarrow \} } \sum_{pq} c_{pq} a_{p,\alpha}^{\dagger}
 #     a_{q, \alpha} + \frac{1}{2} \sum_{\alpha, \beta \in \{\uparrow, \downarrow \} } \sum_{pqrs}
-#     h_{pqrs} a_{p, \alpha}^{\dagger} a_{q, \beta}^{\dagger} a_{r, \beta} a_{s, \alpha},
+#     c_{pqrs} a_{p, \alpha}^{\dagger} a_{q, \beta}^{\dagger} a_{r, \beta} a_{s, \alpha},
 #
-# where :math:`\sigma` denotes the electron spin and the coefficients :math:`h` are integrals over
+# where :math:`\sigma` denotes the electron spin and the coefficients :math:`c` are integrals over
 # molecular orbitals that are obtained from
 # `Hartree-Fock <https://pennylane.ai/qml/demos/tutorial_differentiable_HF#the-hartree-fock-method>`_
 # calculations. These integrals can be
-# computed with PennyLane using the :func:`~.pennylane.qchem.electron_integrals` function. Let's use
-# the hydrogen molecule as an example. We first define the atom types and the atomic coordinates.
+# computed with PennyLane using the :func:`~.pennylane.qchem.electron_integrals` function. Let's
+# build the molecular Hamiltonian for the hydrogen molecule as an example. We first define the atom
+# types and the atomic coordinates.
 
 symbols = ["H", "H"]
 geometry = np.array(
     [[-0.672943567415407, 0.0, 0.0], [0.672943567415407, 0.0, 0.0]], requires_grad=False
 )
 
+##############################################################################
+# Then we compute the one- and two-electron integrals, which are the coefficients :math:`c` in the
+# second quantized molecular Hamiltonian defined above.
+
 mol = qml.qchem.Molecule(symbols, geometry)
 core, one, two = qml.qchem.electron_integrals(mol)()
 
 ##############################################################################
 # These integrals are computed over molecular orbitals and we need to extend them to account for
-# different spins.
+# different electron spins.
 
-one_spin = one.repeat(2, axis=0).repeat(2, axis=1)
-two_spin = two.repeat(2, axis=0).repeat(2, axis=1).repeat(2, axis=2).repeat(2, axis=3) * 0.5
+for i in range(4):
+    if i < 2:
+        one = one.repeat(2, axis=i)
+    two = two.repeat(2, axis=i)
 
 ##############################################################################
 # We can now construct the fermionic Hamiltonian for the hydrogen molecule. We construct the
@@ -166,20 +176,20 @@ two_spin = two.repeat(2, axis=0).repeat(2, axis=1).repeat(2, axis=2).repeat(2, a
 
 import itertools
 
-n = one_spin.shape[0]
+n = one.shape[0]
 
 h = 0.0
 
 for i, j in itertools.product(range(n), repeat=2):
     if i % 2 == j % 2:  # to account for spin-forbidden terms
-        h += FermiC(i) * FermiA(j) * one_spin[i, j]
+        h += one[i, j] * FermiC(i) * FermiA(j)
 
 ##############################################################################
 # The two-body terms can be added with
 
 for p, q, r, s in itertools.product(range(n), repeat=4):
     if p % 2 == s % 2 and q % 2 == r % 2:  # to account for spin-forbidden terms
-        h += FermiC(p) * FermiC(q) * FermiA(r) * FermiA(s) * two_spin[p, q, r, s]
+        h += two[p, q, r, s] / 2 * FermiC(p) * FermiC(q) * FermiA(r) * FermiA(s)
 
 ##############################################################################
 # We then simplify the Hamiltonian to remove terms with negligible coefficients and then map it to
@@ -189,7 +199,7 @@ h.simplify()
 h = qml.jordan_wigner(h)
 
 ##############################################################################
-# We also need to account for the contribution of the nuclear energy.
+# We also need to include the contribution of the nuclear energy.
 
 h += np.sum(core * qml.Identity(0))
 
