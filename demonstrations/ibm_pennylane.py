64a27277432161481c5486bb27cd58577fae4ecd
KaurKristjuhan <31485421+KaurKristjuhan@users.noreply.github.com>
2023-06-19 20:08:42 +0200

    Update demonstrations/ibm_pennylane.py
    Co-authored-by: Alvaro Ballon <91897656+alvaro-at-xanadu@users.noreply.github.com>

--- demonstrations/ibm_pennylane.py
+++ demonstrations/ibm_pennylane.py
@@ -33,7 +33,7 @@ platform. We will learn how to:
 # -----------------
 # IBM offers access to a variety of devices, both classical simulators and real quantum hardware.
 # By default, these devices are not included in PennyLane, but after installing the
-# pennylane-qiskit plugin with the command ``pip install pennylane-qiskit``, they can be used just like any other device offered in Pennylane!
+# pennylane-qiskit plugin with the command ``pip install pennylane-qiskit``, they can be used just like any other device offered in PennyLane!
 # Currently, there are three devices available — Aer, BasicAer and IBMQ — that can be initialized
 # as follows:
 import pennylane as qml
