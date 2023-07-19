fbc15ca9af45d1f92bd4123f3cca7072b8bc2acd
Jay Soni <jbsoni@uwaterloo.ca>
2023-07-19 16:38:45 -0400

    lint demo

--- demonstrations/tutorial_apply_qsvt.py
+++ demonstrations/tutorial_apply_qsvt.py
@@ -11,7 +11,7 @@ How to use QSVT (for matrix inversion)
     tutorial_intro_qsvt Intro to QSVT
     function_fitting_qsp Function Fitting using Quantum Signal Processing
 
-*Authors: Jay Soni, Jarrett Smalley — Posted: <date>, 2023.*
+*Authors: Jay Soni, Jarrett Smalley [Rolls-Royce] — Posted: <date>, 2023.*
 
 The Quantum Singular Value Transformation (QSVT) is a powerful quantum algorithm that
 provides a method to apply arbitrary polynomial transformations to the singular values
@@ -43,8 +43,7 @@ from pennylane import numpy as np
 
 dev = qml.device("default.qubit", wires=[0, 1])
 
-A = np.array([[0.1, 0.2],
-              [0.3, 0.4]])
+A = np.array([[0.1, 0.2], [0.3, 0.4]])
 phase_angles = np.array([0.0, 1.0, 2.0, 3.0])
 
 
@@ -106,7 +105,52 @@ print(my_circuit.tape.expand().draw())
 # of the polynomial approximation. 44 angles were generated from pyqsp which produce a transformation
 # of degree 43.
 
-phi_pyqsp = [0.02833, 0.02642, 0.07694, 0.02781, 0.15714, -0.20679, -0.10759, -0.73309, 0.0006, 0.23101, 1.16472, -2.57156, -0.34539, -0.0479, 0.75539, 0.22588, 1.8765, -2.88892, 2.91344, 0.13498, -2.91905, 2.09895, -1.04265, 0.22254, 0.13498, -0.22815, 0.25268, -1.2651, 0.22588, 0.75539, -0.0479, -0.34539, 0.57003, 1.16472, 0.23101, 0.0006, -0.73309, -0.10759, -0.20679, 0.15714, 0.02781, 0.07694, 0.02642, 1.59912]
+phi_pyqsp = [
+    0.02833,
+    0.02642,
+    0.07694,
+    0.02781,
+    0.15714,
+    -0.20679,
+    -0.10759,
+    -0.73309,
+    0.0006,
+    0.23101,
+    1.16472,
+    -2.57156,
+    -0.34539,
+    -0.0479,
+    0.75539,
+    0.22588,
+    1.8765,
+    -2.88892,
+    2.91344,
+    0.13498,
+    -2.91905,
+    2.09895,
+    -1.04265,
+    0.22254,
+    0.13498,
+    -0.22815,
+    0.25268,
+    -1.2651,
+    0.22588,
+    0.75539,
+    -0.0479,
+    -0.34539,
+    0.57003,
+    1.16472,
+    0.23101,
+    0.0006,
+    -0.73309,
+    -0.10759,
+    -0.20679,
+    0.15714,
+    0.02781,
+    0.07694,
+    0.02642,
+    1.59912,
+]
 
 ###############################################################################
 # .. note::
@@ -126,11 +170,13 @@ phi_pyqsp = [0.02833, 0.02642, 0.07694, 0.02781, 0.15714, -0.20679, -0.10759, -0
 
 kappa = 4
 x_vals = np.linspace(0, 1, 50)
-target_y_vals = [1/(kappa * x) for x in np.linspace(1/kappa, 1, 50)]
+target_y_vals = [1 / (kappa * x) for x in np.linspace(1 / kappa, 1, 50)]
 
 qsvt_y_vals = []
 for a in x_vals:
-    poly_a = qml.matrix(qml.qsvt)(a, phi_pyqsp, wires=[0], convention="Wx")  # specify angles convention: `Wx`
+    poly_a = qml.matrix(qml.qsvt)(
+        a, phi_pyqsp, wires=[0], convention="Wx"
+    )  # specify angles convention: `Wx`
     qsvt_y_vals.append(np.real(poly_a[0, 0]))
 
 ###############################################################################
@@ -141,9 +187,9 @@ for a in x_vals:
 import matplotlib.pyplot as plt
 
 plt.plot(x_vals, np.array(qsvt_y_vals), label="qsvt")
-plt.plot(np.linspace(1/kappa, 1, 50), target_y_vals, label="target")
+plt.plot(np.linspace(1 / kappa, 1, 50), target_y_vals, label="target")
 
-plt.vlines(1/kappa, -1.0, 1.0, linestyle="--", color="grey", label="1/kappa")
+plt.vlines(1 / kappa, -1.0, 1.0, linestyle="--", color="grey", label="1/kappa")
 plt.vlines(0.0, -1.0, 1.0, color="black")
 plt.hlines(0.0, -0.1, 1.0, color="black")
 
@@ -174,15 +220,21 @@ plt.show()
 # Finally, the ancilla qubit is reset. The top left block of this circuit corresponds
 # to the sum of the even and odd polynomial transformations.
 
+
 def sum_even_odd_circ(a, phi, ancilla_wire, wires):
-    phi1, phi2 = phi[:len(phi) // 2], phi[len(phi) // 2:]
+    phi1, phi2 = phi[: len(phi) // 2], phi[len(phi) // 2 :]
 
     qml.Hadamard(wires=ancilla_wire)
 
-    qml.ctrl(qml.qsvt, control=(ancilla_wire,), control_values=(0,))(a, phi1, wires=wires)
-    qml.ctrl(qml.qsvt, control=(ancilla_wire,), control_values=(1,))(a, phi2, wires=wires)
+    qml.ctrl(qml.qsvt, control=(ancilla_wire,), control_values=(0,))(
+        a, phi1, wires=wires
+    )
+    qml.ctrl(qml.qsvt, control=(ancilla_wire,), control_values=(1,))(
+        a, phi2, wires=wires
+    )
 
     qml.Hadamard(wires=ancilla_wire)
+
 
 ###############################################################################
 # A random set of 101 phase angles is initialized, this implies that the transform we train
@@ -206,16 +258,21 @@ phi = np.random.rand(101)
 kappa = 5
 samples_a = np.linspace(1 / kappa, 1, 70)
 
+
 def target_func(x):
     return 1 / (kappa * x)
+
 
 def loss_func(phi):
     sum_square_error = 0
     for s in samples_a:
-        qsvt_val = qml.matrix(sum_even_odd_circ)(s, phi, ancilla_wire="ancilla", wires=[0])[0, 0]
+        qsvt_val = qml.matrix(sum_even_odd_circ)(
+            s, phi, ancilla_wire="ancilla", wires=[0]
+        )[0, 0]
         sum_square_error += (np.real(qsvt_val) - target_func(s)) ** 2
 
     return sum_square_error / len(samples_a)
+
 
 ###############################################################################
 # Thanks to PennyLane's fully differentiable workflow, we can execute the optimization
@@ -226,32 +283,35 @@ cost = 1
 iter = 0
 opt = qml.AdagradOptimizer(0.1)
 
-while cost > 0.01:
+while cost > 0.001:
     iter += 1
     phi, cost = opt.step_and_cost(loss_func, phi)
 
-    if iter % 10 == 0:
+    if iter % 10 == 0 or iter == 1:
         print(f"iter: {iter}, cost: {cost}")
 
-    if iter > 200:
+    if iter > 100:
         print("Iteration limit reached!")
         break
 
-print(f"Completed Optimization!")
+print(f"Completed Optimization! (final cost: {cost})")
 
 ###############################################################################
 # Now we plot the results:
 
-samples_inv = np.linspace(1/kappa, 1, 50)
+samples_inv = np.linspace(1 / kappa, 1, 50)
 inv_x = [target_func(a) for a in samples_inv]
 
 samples_a = np.linspace(0, 1, 100)
-qsvt_y_vals = [np.real(qml.matrix(qml.qsvt)(x, phi, wires=[0])[0, 0]) for x in samples_a]
+qsvt_y_vals = [
+    np.real(qml.matrix(sum_even_odd_circ)(a, phi, "ancilla", wires=[0])[0, 0])
+    for a in samples_a
+]
 
 plt.plot(samples_a, qsvt_y_vals, label="qsvt")
 plt.plot(samples_inv, inv_x, label="target")
 
-plt.vlines(1/kappa, -1.0, 1.0, linestyle="--", color="grey", label="1/kappa")
+plt.vlines(1 / kappa, -1.0, 1.0, linestyle="--", color="grey", label="1/kappa")
 plt.vlines(0.0, -1.0, 1.0, color="black")
 plt.hlines(0.0, -0.1, 1.0, color="black")
 
@@ -261,62 +321,106 @@ plt.show()
 ###############################################################################
 # Awesome, we successfully optimized the phase angles!
 #
-# Using a higher degree polynomial and training for longer would result in a
-# closer fit for both. While we used a standard loss function and optimizer,
-# users have the freedom to explore any optimizer, loss function, and sampling
-# scheme when training the phase angles for QSVT. Let's take everything we have
-# learned and apply it to solve a linear system of equations!
+# While we used a standard loss function and optimizer, users have the freedom to explore any
+# optimizer, loss function, and sampling scheme when training the phase angles for QSVT. Let's
+# take everything we have learned and apply it to solve a linear system of equations!
 #
 # Solving a Linear System with QSVT
 # ---------------------------------
-# Our goal is to solve the equation :math:`A \cdot \vec{x} = \vec{b}` for a valid :math:`\vec{x}`.
-# Let's begin by defining the specific :math:`A` and :math:`\vec{b}` quantities. Our target
-# value for :math:`\vec{x}` will be the result of :math:`A^{-1} \cdot \vec{b}`.
+# Our goal is to solve the equation :math:`A \cdot \vec{x} = \vec{b}`. Let's begin by defining the
+# specific :math:`A` and :math:`\vec{b}` quantities:
 #
 
 A = np.array(
-    [[ 0.65713691, -0.05349524,  0.08024556, -0.07242864],
-     [-0.05349524,  0.65713691, -0.07242864,  0.08024556],
-     [ 0.08024556, -0.07242864,  0.65713691, -0.05349524],
-     [-0.07242864,  0.08024556, -0.05349524,  0.65713691]]
+    [
+        [0.65713691, -0.05349524, 0.08024556, -0.07242864],
+        [-0.05349524, 0.65713691, -0.07242864, 0.08024556],
+        [0.08024556, -0.07242864, 0.65713691, -0.05349524],
+        [-0.07242864, 0.08024556, -0.05349524, 0.65713691],
+    ]
 )
 
 b = np.array([1, 2, 3, 4])
-target_x = np.linalg.inv(A) @ b  # the solution to the system!
+target_x = np.linalg.inv(A) @ b  # true solution
 
 # Normalize states:
 norm_b = np.linalg.norm(b)
 normalized_b = b / norm_b
 
 norm_x = np.linalg.norm(target_x)
-normalized_x = target_x/norm_x
+normalized_x = target_x / norm_x
 
 ###############################################################################
-# To solve this expression, we construct a quantum circuit that first prepares
-# the normalized vector :math:`\vec{b}`. Next we apply the QSVT operation using
-# the normalized matrix :math:`A`. This is equivalent to applying :math:`A^{-1}`
-# to the prepared state. Finally, we return the state at the end of the circuit.
-# The subset of qubits which prepared the :math:`\vec{b}` vector should now be
-# transformed to represent our target state (excluding scale factors).
+# The :code:`sum_even_odd_circ()` function produces the operation :math:`hat{U}_{p}` with matrix
+# representation:
+#
+# .. math::
+#
+#    \hat{U}_{p}(\vec{\phi}) = \ \ \begin{bmatrix}
+#                                       P(A) & \cdot \\
+#                                       \cdot & \cdot
+#                                      \end{bmatrix}
+#
+# Using the optimized phase angles :math:`\vec{\phi_{opt}}` from above, we have
+# :math:`Re[P(A)] \approx \frac{1}{\kappa \cdot x}`. Ultimately, we want to apply the real part of
+# this transformation while ignoring the imaginary component. Note that we can express the real part as
+# :math:`Re(P(A)) = \frac{1}{2}(P(a) + P^{*}(a))`. Similarly, the operator is given by:
+#
+# .. math::
+#
+#    \hat{U}_{real}(\vec{\phi}) = \frac{1}{2} \ ( \hat{U}_{p}(\vec{\phi}) + \hat{U}^{\dagger}_{p}(\vec{\phi}) )
+#
+# Here we use a two term LCU to define the quantum function for this operator:
 
 
-@qml.qnode(qml.device('default.qubit', wires=[0, 1, 2]))
+def real_p(A, phi):
+    qml.Hadamard(wires="ancilla1")
+
+    qml.ctrl(sum_even_odd_circ, control=("ancilla1",), control_values=(0,))(
+        A, phi, "ancilla2", [0, 1, 2]
+    )
+    qml.ctrl(
+        qml.adjoint(sum_even_odd_circ), control=("ancilla1",), control_values=(1,)
+    )(A, phi, "ancilla2", [0, 1, 2])
+
+    qml.Hadamard(wires="ancilla1")
+
+
+###############################################################################
+# To solve the linear system we construct a quantum circuit that first prepares the normalized vector
+# :math:`\vec{b}` in the working qubit register. Next we call :code:`real_p(A, phi)` function. This
+# is equivalent to applying :math:`\frac{1}{\kappa} \cdot A^{-1}` to the prepared state. Finally,
+# we return the state at the end of the circuit.
+#
+# The subset of qubits which prepared the :math:`\vec{b}` vector should be transformed to represent
+# :math:`\vec{x}` (up to scale factors):
+
+
+@qml.qnode(qml.device("default.qubit", wires=["ancilla1", "ancilla2", 0, 1, 2]))
 def linear_system_solver_circuit(phi):
     qml.QubitStateVector(normalized_b, wires=[1, 2])
-    qml.qsvt(A, phi, wires=[0, 1, 2])
+    real_p(A, phi)
     return qml.state()
 
-trained_phi = stored_phi[-1][0]
 
-transformed_state = linear_system_solver_circuit(trained_phi)[:4]  # first 4 entries of the state vector
+transformed_state = linear_system_solver_circuit(phi)[
+    :4
+]  # first 4 entries of the state vector
 rescaled_computed_x = transformed_state * kappa * norm_b
 normalized_computed_x = rescaled_computed_x / np.linalg.norm(rescaled_computed_x)
 
 print("target x:", np.round(normalized_x, 3))
 print("computed x:", np.round(normalized_computed_x, 3))
 
-A_kappa_inv = qml.matrix(qml.qsvt(A, trained_phi, wires=[0, 1, 2]))[:4, :4]  # top left block
-print("\nA @ A^(-1):\n", np.round(A * kappa @ A_kappa_inv, 1))
+###############################################################################
+# To further verify that we did in fact invert the matrix, we compute the product of :math:`A`
+# with the inverse we generated. We compute the matrix representation of the :code:`real_p()`
+# circuit using :code:`qml.matrix()`. Then we extract the top left block and re-scale it by
+# :math:`kappa` to get :math:`A^{-1}`:
+
+U_real_matrix = qml.matrix(real_p, wire_order=["ancilla1", "ancilla2", 0, 1, 2])(A, phi)
+A_inv = kappa * U_real_matrix[:4, :4]  # top left block
+print("\nA @ A^(-1):\n", np.round(A @ A_inv, 1))
 
 ###############################################################################
 # We have solved the linear system 🎉!
@@ -334,15 +438,15 @@ print("\nA @ A^(-1):\n", np.round(A * kappa @ A_kappa_inv, 1))
 #
 # Conclusion
 # -------------------------------
-# In this demo, we showcased the :code:`qml.qsvt()` functionality in PennyLane,
-# specifically to solve the problem of matrix inversion. We showed how to use
-# phase angles computed with external packages and how to use PennyLane
-# to optimize the phase angles directly.  We finally described how to apply qsvt to an example linear system.
+# In this demo, we showcased the :code:`qml.qsvt()` functionality in PennyLane, specifically to
+# solve the problem of matrix inversion. We showed how to use phase angles computed with external
+# packages and how to use PennyLane to optimize the phase angles directly.  We finally described
+# how to apply qsvt to an example linear system.
 #
-# While the matrix we inverted was a simple example, the general problem of matrix
-# inversion is often the bottleneck in many applications from regression analysis in
-# financial modelling to simulating fluid dynamics for jet engine design. We hope that PennyLane can help you
-# along the way to your next big discovery in quantum algorithms.
+# While the matrix we inverted was a simple example, the general problem of matrix inversion is often
+# the bottleneck in many applications from regression analysis in financial modelling to simulating
+# fluid dynamics for jet engine design. We hope that PennyLane can help youalong the way to your next
+# big discovery in quantum algorithms.
 #
 # References
 # ----------
@@ -387,5 +491,3 @@ print("\nA @ A^(-1):\n", np.round(A * kappa @ A_kappa_inv, 1))
 # .. include:: ../_static/authors/jay_soni.txt
 #
 # .. include:: ../_static/authors/jarrett_smalley.txt
-
-
