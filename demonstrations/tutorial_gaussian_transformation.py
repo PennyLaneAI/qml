b075e11f669f66f780085b74b63bb278d7815a58
BM7878 <117289949+BM7878@users.noreply.github.com>
2023-08-16 19:40:19 +0100

    Image file renaming

--- demonstrations/tutorial_gaussian_transformation.py
+++ demonstrations/tutorial_gaussian_transformation.py
@@ -27,7 +27,7 @@ The quantum circuit
 For this basic tutorial, we will consider a special subset of CV operations:
 the *Gaussian transformations*. We work with the following simple Gaussian circuit:
 
-.. figure:: ../demonstrations/gaussian_transformation/gaussian_transformation.svg
+.. figure:: ../demonstrations/gaussian_transformation/gaussian_transformation_circuit.svg
     :align: center
     :width: 40%
     :target: javascript:void(0);
