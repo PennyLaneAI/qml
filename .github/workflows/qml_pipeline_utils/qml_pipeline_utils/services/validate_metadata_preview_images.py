86a85fb002dd894e9d34d07b91fcb3a89233b4eb
Rashid N H M <95639609+rashidnhm@users.noreply.github.com>
2023-09-01 12:26:19 -0400

    Address PR Comments

--- .github/workflows/qml_pipeline_utils/qml_pipeline_utils/services/validate_metadata_preview_images.py
+++ .github/workflows/qml_pipeline_utils/qml_pipeline_utils/services/validate_metadata_preview_images.py
@@ -1,20 +1,18 @@
 import os
 import json
 from pathlib import Path
-from typing import List, Union, TYPE_CHECKING
+from typing import List
 
 from tqdm import tqdm
-
-if TYPE_CHECKING:
-    from pathlib import Path
 
 
 def validate_metadata_preview_images(metadata_files: List[str], sphinx_build_directory: Path):
     working_dir = Path(os.getcwd())
 
-    sphinx_build_directory = working_dir / sphinx_build_directory \
-        if not sphinx_build_directory.is_absolute() \
-        else sphinx_build_directory
+    if sphinx_build_directory.is_absolute():
+        sphinx_build_directory = sphinx_build_directory
+    else:
+        sphinx_build_directory = working_dir / sphinx_build_directory
 
     for metadata_file in metadata_files:
         if not metadata_file.startswith("/"):
@@ -41,8 +39,3 @@ def validate_metadata_preview_images(metadata_files: List[str], sphinx_build_dir
             uri_path = sphinx_build_directory / image_uri[1:]
 
             assert uri_path.exists(), f"Could not find previewImage {image_uri}"
-
-    return None
-
-
-
