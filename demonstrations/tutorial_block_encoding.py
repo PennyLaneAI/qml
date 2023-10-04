faef859f6a415a74f1c8da65731dd402b425c57d
soranjh <soran.jahangiri@gmail.com>
2023-10-04 18:40:17 -0400

    edit text

--- demonstrations/tutorial_block_encoding.py
+++ demonstrations/tutorial_block_encoding.py
@@ -1,7 +1,7 @@
 r"""
 
-Block Encoding
-==============
+Block encoding with matrix query oracles
+========================================
 
 .. meta::
     :property="og:description": Learn how to perform block encoding
@@ -10,44 +10,46 @@ Block Encoding
 .. related::
      tutorial_intro_qsvt Intro to QSVT
 
-*Author: Diego Guala, Soran Jahangiri, Jay Soni — Posted: September 29, 2023.*
+*Author: Diego Guala, Jay Soni, Soran Jahangiri — Posted: September 29, 2023.*
 
 Prominent quantum algorithms such as Quantum Phase Estimation and Quantum Singular Value
 Transformation require implementing a non-unitary operator in a quantum circuit. This is problematic
 because quantum computers can only perform unitary evolutions. Block encoding is a general technique
 that solves this problem by embedding the non-unitary operator in a unitary matrix that can be
-implemented in a quantum circuit containing a set of ancilla qubits. In this [link] demo, we
-learned how to block encode a matrix by simply embedding it in a larger unitary matrix using the
-:class:`~pennylane.BlockEncode` operation. We also learned [link] a powerful method for block encoding a
+implemented in a quantum circuit using a set of ancilla qubits. In
+`this <https://pennylane.ai/qml/demos/tutorial_intro_qsvt#transforming-matrices-encoded-in-matrices>`_
+demo, we learned how to block encode a matrix by simply embedding it in a larger unitary matrix
+using the :class:`~pennylane.BlockEncode` operation. We also
+`learned <https://github.com/PennyLaneAI/qml/pull/888>`_ a powerful method for block encoding a
 matrix by decomposing it into a linear combination of unitaries (LCU) and then block encode the LCU.
-In this tutorial we explore a general block encoding method that can be efficiently implemented for
-sparse and structured matrices. We first explain the method and then apply it to specific examples
-that can be efficiently block-encoded.
+In this tutorial we explore another general block encoding method that can be particularly
+efficient for sparse and structured matrices. We first explain the method and then apply it to
+some selected examples.
 
-A general circuit for block encoding
-------------------------------------
-An arbitrary matrix :math:`A`, can be block encoded by relying on oracle access to the entries in
-the matrix (see [#fable]_, [#sparse]_). A general circuit for block encoding :math:`A` can be
-constructed from such oracles as illustrated in the following.
+Matrix query oracles
+--------------------
+An arbitrary matrix :math:`A`, can be block encoded by relying on oracle access to its entries
+(see [#fable]_, [#sparse]_). A general circuit for block encoding :math:`A` can be constructed from
+such oracles.
 
 .. figure:: ../demonstrations/block_encoding/general_circuit.png
     :width: 50%
     :align: center
 
-Finding the optimal sequence of the quantum gates that implement the :math:`\hat{O}_{A}` and
-:math:`\hat{O}_{C}` oracles is not straightforward for random matrices. In the general case,
-:math:`\hat{O}_{C}` can be represented by a set of SWAP gates and :math:`\hat{O}_{A}` can be
-constructed from a sequence of uniformly controlled rotation gates:
+Finding the optimal sequence of the quantum gates that implement the :math:`O_A` and :math:`O_C`
+oracles is not straightforward for random matrices. In the general case, :math:`O_A` can be
+constructed from a sequence of uniformly controlled rotation gates and :math:`O_C` can be
+represented by a set of SWAP gates.
 
 .. figure:: ../demonstrations/block_encoding/fable_circuit.png
     :width: 50%
     :align: center
 
-The rotation angles are computed from the matrix elements of the block encoded matrix as
-:math:`\theta = arcsin(a_{ij}`. The gate complexity of this circuit is O(N4) which makes its
-implementation highly inefficent. We now explain two approaches that provide alternative
-constructions of :math:`\hat{O}_{A}` and :math:`\hat{O}_{C}` that can be very efficient for matrices
-with specific sparsity and structure.
+The rotation angles are computed from the elements of the block encoded matrix as
+:math:`\theta = \text{arccos}(a_{ij}`. The gate complexity of this circuit is :math:`O(N^4)` which
+makes its implementation highly inefficient. We now explain two approaches that provide alternative
+constructions of :math:`O_A` and :math:`O_C` that can be very efficient specially for matrices with
+specific sparsity and structure.
 
 Block encoding with FABLE
 -------------------------
@@ -56,22 +58,34 @@ for block encoding dense and sparse matrices. The level of approximation in FABL
 to compress and sparsify the resulting circuit. For matrices with specific structures, FABLE
 provides an efficient circuit without sacrificing accuracy. The general circuit is constructed
 from a set of rotation and C-NOT gates where the rotation angles are obtained from a transformation
-of the elements of the block encoded matrix. Let's construct a FABLE circuit for a matrix that is
-structured.
+of the elements of the block encoded matrix. The rotation angles,
+:math:`\Theta = (\theta_1, ..., \theta_n)`, are obtained with
+
+.. math:: \left ( H^{\otimes 2n} P \right ) \Theta = C,
+
+where :math:`P` is a permutation that transforms a binary ordering to the Gray code ordering,
+:math:`C = (\text{arccos}(A_00), ..., \text{arccos}(A_nn))` is obtained from the matrix elements of
+the matrix :math:`A` and :math:`H` is defined as
+
+.. math:: H = \begin{pmatrix} 1 & 1\\
+                             1 & -1
+               \end{pmatrix}.
+
+Let's now construct the block encoding circuit for a structured matrix.
 """
 
 import pennylane as qml
 from pennylane.templates.state_preparations.mottonen import compute_theta, gray_code
 from pennylane import numpy as qnp
 
-n = 2
-N = 2**n
-
 A = qnp.array([[-0.51192128, -0.51192128,  0.6237114 ,  0.6237114 ],
                [ 0.97041007,  0.97041007,  0.99999329,  0.99999329],
                [ 0.82429855,  0.82429855,  0.98175843,  0.98175843],
                [ 0.99675093,  0.99675093,  0.83514837,  0.83514837]])
 
+
+##############################################################################
+# We now normalize the matrix and compute the rotation angles.
 
 Avec = qnp.ravel(A)
 alpha = max(1,qnp.linalg.norm(Avec,qnp.inf))
@@ -82,13 +96,17 @@ alphas = 2*qnp.arccos(Avec)
 thetas = compute_theta(alphas)
 
 
-code = gray_code(N)
+code = gray_code(len(A))
 num_selections=len(code)
 
 control_wires = control_indices = [
     int(qnp.log2(int(code[i], 2) ^ int(code[(i + 1) % num_selections], 2)))
     for i in range(num_selections)
 ]
+
+##############################################################################
+# We construct the :math:`O_A` and :math:`O_C` oracles as well as the Ds operator, which is a tensor
+# product of Hadamard gates.
 
 def OA():
     for idx in range(len(thetas)):
@@ -103,6 +121,9 @@ def Ds():
     qml.Hadamard(wires=2)
     qml.Hadamard(wires=3)
 
+##############################################################################
+# We construct the circuit using these oracles and draw it.
+
 dev = qml.device('default.qubit', wires=5)
 @qml.qnode(dev)
 def circuit():
@@ -115,29 +136,17 @@ def circuit():
 print(qml.draw(circuit)())
 
 ##############################################################################
-# The rotation angles, :math:`\Theta = (\theta_1, ..., \theta_n)`, are obtained with
-#
-# .. math:: \left ( H^{\otimes 2n} P \right ) \Theta = C,
-#
-# where P is a permutation that transforms a binary ordering to the Gray code ordering,
-# :math:`C = (arccos(A_00), ..., arccos(A_nn))` is obtained from the matrix elements of the matrix A
-# and H is defined as
-
-# .. math:: H = \begin{pmatrix} 1 & 1\\
-#                               1 & -1
-#               \end{pmatrix}.
-#
-#
-# We now compute the matrix representation of the circuit
+# We compute the matrix representation of the circuit and print its top-left block to compare it
+# with the original matrix
 
 print(A)
-print(alpha*N*qml.matrix(circuit,wire_order=[0,1,2,3,4][::-1])()[0:N,0:N])
+print(alpha*len(A)*qml.matrix(circuit,wire_order=[0,1,2,3,4][::-1])()[0:len(A),0:len(A)])
 
 ##############################################################################
-# You can see that the matrix is a block encoding of the original matrix A.
+# You can easily confirm that the circuit block encodes the original matrix defined above.
 #
-# The interesting thing about the Fable method is that one can eliminate those rotation gates that
-# have an angle smaller than a pre-defined threshold. This leaves a sequence of C-NOT gates that in
+# The interesting thing about the FABLE method is that one can eliminate those rotation gates that
+# have an angle smaller than a selected threshold. This leaves a sequence of C-NOT gates that in
 # most cases cancel each other out.
 
 tolerance= 0.01
@@ -151,9 +160,9 @@ def OA():
 print(qml.draw(circuit)())
 
 ##############################################################################
-# You can confirm that two C-NOT gates applied to the same wires
-# cancel each other. Compressing the circuit in this way is an approximation. Let's see how good
-# this approximation is in the case of our example.
+# You can confirm that two C-NOT gates applied to the same wires cancel each other. Compressing the
+# circuit in this way is an approximation. Let's see how good this approximation is in the case of
+# our example.
 
 def OA():
     nots=[]
@@ -172,25 +181,31 @@ def OA():
 print(qml.draw(circuit)())
 
 print(A)
-print(alpha*N*qml.matrix(circuit,wire_order=[0,1,2,3,4][::-1])()[0:N,0:N])
+print(alpha*len(A)*qml.matrix(circuit,wire_order=[0,1,2,3,4][::-1])()[0:len(A),0:len(A)])
+
+##############################################################################
+# You can see that the compressed circuit is equivalent to the original circuit simply because our
+# matrix is highly structured and many of the rotation angles are zero. This is not the case for any
+# given matrix. Can you construct a matrix that also allows a significant compression of the block
+# encoding circuit without affecting the accuracy?
 
 ##############################################################################
 # Block-encoding sparse matrices
 # ------------------------------
 # The quantum circuit for the oracle :math:`\hat{O}_{A}`, presented above, accesses every entry of 
-# :math:`A` and thus requires on the order of :math:`~ O(N^{2})` gates to implement ([#fable]_). 
+# :math:`A` and thus requires on the order of :math:`~ O(N^2)` gates to implement ([#fable]_).
 # In the case where  :math:`A` is a structured sparse matrix, we can generate a more efficient quantum 
-# circuit representation for both oracles. Let's see how we can implment these oracles for a given 
+# circuit representation for both oracles. Let's see how we can implement these oracles for a given
 # structured sparse matrix.  
 #
 # Consider the s-sparse matrix given by:
 #
 # .. math:: A = \begin{bmatrix}
-#       \alpha & \gamma & 0 & \hdots & beta\\
-#       \beta & \alpha & \ddots \ddots & 0 \\
-#       0 & \beta & \ddots & \gamma & \vdots\\
-#       \vdots & \ddots & \ddots & \alpha & \gamma\\
-#       \gamma & 0 & \hdots & \beta & \alpha \\
+#       \alpha & \gamma & 0 & \dots & \beta\\
+#       \beta & \alpha & \gamma & \ddots & 0 \\
+#       0 & \beta & \alpha & \gamma \ddots & 0\\
+#       0 & \ddots & \beta & \alpha & \gamma\\
+#       \gamma & 0 & \dots & \beta & \alpha \\
 #       \end{bmatrix}
 #
 # The following code block prepares the matrix representation for an :math:`8x8` sparse matrix:
@@ -297,6 +312,6 @@ print(mat, "\n")
 # ----------------
 # .. include:: ../_static/authors/diego_guala.txt
 #
-# .. include:: ../_static/authors/soran_jahangiri.txt
-#
 # .. include:: ../_static/authors/jay_soni.txt
+#
+# .. include:: ../_static/authors/soran_jahangiri.txt
