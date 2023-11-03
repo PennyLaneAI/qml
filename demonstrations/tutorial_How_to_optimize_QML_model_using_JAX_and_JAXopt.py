9d657616d7423f3d2d02efd65002ff1b7b059497
Josh Izaac <josh146@gmail.com>
2023-11-02 21:08:12 -0400

    updates

--- demonstrations/tutorial_How_to_optimize_QML_model_using_JAX_and_JAXopt.py
+++ demonstrations/tutorial_How_to_optimize_QML_model_using_JAX_and_JAXopt.py
@@ -32,7 +32,9 @@ import pennylane as qml
 import jax
 from jax import numpy as jnp
 import jaxopt
+
 jax.config.update("jax_platform_name", "cpu")
+
 n_wires = 5
 data = jnp.sin(jnp.mgrid[-2:2:0.2].reshape(n_wires, -1)) ** 3
 targets = jnp.array([-0.2, 0.4, 0.35, 0.2])
