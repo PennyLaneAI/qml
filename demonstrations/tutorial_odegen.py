146a7258dbba36cca646938bc56b54a603f16bb1
Qottmann <korbinian.kottmann@gmail.com>
2023-11-22 12:46:51 +0100

    big update

--- demonstrations/tutorial_odegen.py
+++ demonstrations/tutorial_odegen.py
@@ -22,9 +22,9 @@ Introduction
 Many contemporary quantum computers are operated by steering the qubit state through an
 electromagnetic pulse. This can be modeled by means of a time-dependent Hamiltonian 
 
-.. math:: H(\theta, t) = \sum_q f_q(\theta, t) H_q
+.. math:: H(\theta, t) = H_\text{drift} + \sum_{j=1}^{N_g} f_j(\theta, t) H_j
 
-with time-dependent, parametrized pulse envelopes :math:`f_q(\theta, t)`. A prominent example
+with time-dependent, parametrized pulse envelopes :math:`f_qj(\theta, t)` and constant drift term :math:`H_\text{drift}`. A prominent example
 is superconducting qubit platforms as described in :the demo on differentiable pulse programming <tutorial_pulse_programming101>`_
 or :the demo about OQC's Lucy <oqc_pulse>`_. Such a drive then induces a unitary evolution :math:`U(\theta)` according
 to the time-dependent Schrödinger equation.
@@ -47,7 +47,7 @@ We are interested in cost functions of the form
 
 .. math:: \mathcal{L}(\theta) = \langle 0 | U(\theta)^\dagger H_\text{obj} U(\theta) | 0 \rangle
 
-where we compute the expectation value of some objective Hamiltonian :math:`H_\text{obj}` (think molecular Hamiltonian to estimate the ground state energy of a molecule).
+where we compute the expectation value of some objective Hamiltonian :math:`H_\text{obj}` (think quantum many-body Hamiltonian whose ground state energy we want to estimate).
 For simplicity, we assume a sole pulse gate :math:`U(\theta)` (we will discuss the case of multiple gates later). Further, let us assume the so-called pulse generators
 :math:`H_q` in :math:`H(\theta, t)` to be Pauli words, which will make SPS rule below a bit more digestible (the general case is described in [#Kottmann]).
 
@@ -64,22 +64,65 @@ In practice, the integral is approximated via Monte Carlo integration
 
 .. math:: \frac{\partial}{\partial \theta_j} \mathcal{L}(\theta) \approx \frac{1}{N_s} \sum_{\tau \in \mathcal{U}([0, T])} \sum_q \frac{\partial f_q(\theta, \tau)}{\partial \theta_j} \left(\tilde{\mathcal{L}}^+_q(\tau) - \tilde{\mathcal{L}}^-_q(\tau) \right)
 
-where :math:`N_s` is the number of Monte Carlo samples for the integration. The larger the number of samples, the better the approximation.
+where :math:`N_s` is the number of Monte Carlo samples for the integration. The larger the number of samples, the better the approximation. This comes at the cost of more quantum resources :math:`\mathcal{R}`
+in form of the number of distinct expectation values executed on the quantum device:
+
+.. math:: \mathcal{R} = 2 N_s N_g.
 
 ODEgen
 ------
 
 In contrast, the recently introduced ODEgen method [#Kottmann] has the advantage that it circumvents the need for Monte Carlo sampling by off-loading the complexity
-introduced by the time-dynamics to a differentiable ODE solver. Let me walk you throug hthe basic steps.
+introduced by the time-dynamics to a ordinary differential equation (ODE) solver. Let me walk you through the basic steps.
+
+The first step of ODEgen is writing the derivative of a pulse unitary :math:`U(\theta)` as
+
+.. math:: \frac{\partial}{\partial \theta_j} U(\theta) = -i U(\theta) \mathcal{H}_j.
+
+with a so-called effective generator :math:`\mathcal{H}_j` for each of the parameters :math:`\theta_j`.
+We can obtain :math:`\mathcal{H}_j` classically by computing both :math:`\frac{\partial}{\partial \theta_j} U(\theta)` and :math:`U(\theta)` with a
+differentiable ODE solver. We already use such a solver in PennyLane for simulating pulses in :class:`ParametrizedEvolution`.
+Here, we use it to generate parameter rules that can be executed on hardware.
+
+The next step is to decompose each effective generator into a basis the quantum computer can understand, and, in particular, can execute.
+We choose the typical Pauli basis and write
+
+.. math:: \mathcal{H}_j = \sum_\ell \omega_\ell^{(j)} P_\ell
+
+for Pauli words :math:`P_\ell` (e.g. :math:`X_0 Y_1`). With this decomposition, we can write the gradient of the cost function in terms of parameter-shift rules
+that can be executed on a quantum computer:
+
+.. math:: 
+    \frac{\partial \mathcal{L}}{\partial \theta_j} = \langle 0 | \left[U(\theta)^\dagger H_\text{obj} U(\theta), -i\mathcal{H}_j \right] | 0 \rangle \\
+    = \sum_\ell 2 \omega_\ell^{(j)} \langle 0 | \left[U(\theta)^\dagger H_\text{obj} U(\theta), -\frac{i}{2} P_\ell \right] |0\rangle \\
+    = \sum_\ell 2 \omega_\ell^{(j)} \frac{d}{dx} \left[ \langle 0 | U(\theta)^\dagger e^{\frac{i}{2} P_\ell} H_\text{obj} e^{-\frac{i}{2} P_\ell} U(\theta)|0\rangle \right]_{x=0}.
+
+In particular, we can identify
+
+.. math:: L_\ell(x) = \langle 0 | U(\theta)^\dagger e^{\frac{i}{2} P_\ell} H_\text{obj} e^{-\frac{i}{2} P_\ell} U(\theta)|0\rangle
+
+as an expectation value shifted by the dummy variable :math:`x`, whose derivative is given by the standard two-term parameter-shift rule (see e.g. `this derivation <https://pennylane.ai/qml/glossary/parameter_shift/>`_).
+Overall, we have 
+
+.. math:: \frac{\partial \mathcal{L}}{\partial \theta_j} = \sum_\ell \omega_\ell^{(j)} \left(L_\ell(\frac{\pi}{2}) - L_\ell(-\frac{\pi}{2}) \right).
+
+The quantum resources for ODEgen are :math:`2` executions for each overall non-zero Pauli term :math:`\omega_\ell^{(j)} P_\ell` of the decomposition.
+This number is at most :math:`4^n-1` for :math:`n` qubits. A better upper bound is given by the dimension of the dynamical Lie algebra of the pulse Hamiltonian. That is, the number of linearly independent operators
+that can be generated from nested commutators of the pulse generators and the drift term. Hence, ODEgen is well-suited for pulse Hamiltonians that act effectively on few qubits or yield a small dynamical Lie algebra 
+- as is the case for superconducting qubit and ion trap qubit architectures.
 
 
 
-Variational Quantum Algorithms
-==============================
+Numerical experiment
+====================
 
 We want to put ODEgen and SPS head to head in a variational quantum algorithm with the same available quantum resources.
-For this, we are going to perform the variational quantum eigensolver (VQE) on a ``HeH+`` molecular Hamiltonian taken from
-the PennyLane `quantum dataset <datasets>`.
+For this, we are going to perform the variational quantum eigensolver (VQE) on the Heisenberg model Hamiltonian
+
+.. math:: H_\text{obj} = X_0 X_1 + Y_0 Y_1 + Z_0 Z_1
+
+for two qubits. The ground state of this Hamiltonian is the maximally entangled singlet state
+:math:`|\phi^- \rangle = (|01\rangle - |10\rangle)/\sqrt{2}` with ground state energy :math:`-3`.
 
 """
 from copy import copy
@@ -95,11 +138,9 @@ jax.config.update("jax_enable_x64", True)
 
 import matplotlib.pyplot as plt
 
-bond_distance = 1.78
-data = qml.data.load("qchem", molname="HeH+", basis="STO-3G", bondlength=bond_distance)[0]
-H_obj = data.tapered_hamiltonian
-H_obj = qml.dot(jnp.array(H_obj.coeffs), H_obj.ops)
-E_exact = data.fci_energy
+H_obj = qml.sum(qml.PauliX(0)@qml.PauliX(1), qml.PauliY(0)@qml.PauliY(1), qml.PauliZ(0) @ qml.PauliZ(1))
+E_exact = -3.
+wires = H_obj.wires
 
 ##############################################################################
 # We are going to consider a system of transmon qubits
@@ -114,27 +155,13 @@ E_exact = data.fci_energy
 # :math:`\mathcal{C} = ((0, 1))`.
 # The coupling is necessary to generate entanglement, which is achieved with cross-resonant driving in fixed-coupling 
 # transmon systems, as is the case here.
-#
-
-
-H_obj = qml.sum(qml.PauliX(0)@qml.PauliX(1), qml.PauliY(0)@qml.PauliY(1), qml.PauliZ(0) @ qml.PauliZ(1))
-E_exact = -3.
-wires = H_obj.wires
-
-
-##############################################################################
-# We are going to take the parametrization from [#Leng]_ 
 # 
-# .. math:: f_q(\theta, t) = \sum_{j=0}^d \theta_j \mathcal{P}\left( \frac{2t}{T} - 1 \right)
-# 
-# in terms of legendre polynomials :math:`\mathcal{P}` up to degree :math:`d=4`.
-# For this we take their 
-# `coefficients <https://en.wikipedia.org/wiki/Legendre_polynomials#Rodrigues'_formula_and_other_explicit_formulas>`_
-# and create callables with trainable :math:`d+1` complex coefficients for each qubit, separated in :math:`n2(d+1)` real numbers.
-#
-
-atol = 1e-8           # accuracy of ODE integration
-tbins = 10            # number of time bins per pulse
+# We will use realistic parameters for the transmons, taken from the `coaxmon design benchmark <https://arxiv.org/pdf/1905.05670.pdf>`_ [#Patterson]_.
+# In order to prepare the singlet ground state, we will perform two pulses: one on resonance for rotating the qubit 
+# (see our recent :doc:`single qubit drive demo <oqc_pulse>`_) and one on cross resonance, i.e. driving one qubit at its coupled neighbor's 
+# frequency for entanglement generation (see [#Patterson]_ or [#Krantz]_).
+# Because cross-resonant driving is significantly slower than on-resonance driving we choose realistic gate times of 
+# :math:`10 \text{ ns}` and :math:`100 \text{ ns}`, respectively.
 
 T_single = 10.        # gate time for single qubit drive (on resonance)
 T_CR = 100.            # gate time for two qubit drive (cross resonance)
@@ -148,61 +175,57 @@ def drive_field(T, wdrive):
         # The first len(p)-1 values of the trainable params p characterize the pwc function
         amp = qml.pulse.pwc(T)(p[:len(p)//2], t)
         phi = qml.pulse.pwc(T)(p[len(p)//2:], t)
-        wd = wdrive # + qml.pulse.pwc(T)(p[-tomegas:], t)
-        #amp = max_amp*normalize(amp)
-        return amp * jnp.sin(wd * t + phi)
+        return amp * jnp.sin(wdrive * t + phi)
 
     return wrapped
 
-H0 = qml.dot(-0.5*qubit_freq, [qml.PauliZ(i) for i in wires])
-H0 += 2 * np.pi * 0.0123 * (qml.PauliX(wires[0]) @ qml.PauliX(wires[1]) + qml.PauliY(wires[0]) @ qml.PauliY(wires[1]))
+H_pulse = qml.dot(-0.5*qubit_freq, [qml.PauliZ(i) for i in wires])
+H_pulse += 2 * np.pi * 0.0123 * (qml.PauliX(wires[0]) @ qml.PauliX(wires[1]) + qml.PauliY(wires[0]) @ qml.PauliY(wires[1]))
 
-H_on = H0 + drive_field(T_single, qubit_freq[1]) * qml.PauliY(wires[1])
-
-H_off = H0 + drive_field(T_CR, qubit_freq[0]) * qml.PauliY(wires[1])
+H_pulse += drive_field(T_CR, qubit_freq[0]) * qml.PauliY(wires[0]) # on-resonance driving of qubit 0
+H_pulse += drive_field(T_CR, qubit_freq[0]) * qml.PauliY(wires[1]) # off-resonance driving of qubit 1
 
 ##############################################################################
 # We can now define the cost function that computes the expectation value of 
-# the molecular Hamiltonian after evolving the state with the parametrized pulse Hamiltonian.
-# As is standard in quantum chemistry, we initialize the Hartree Fock state by flipping both qubits.
+# the Heisenberg Hamiltonian after evolving the state with the parametrized pulse Hamiltonian.
 # We then define the two separate qnodes with ODEgen and SPS as their differentiation methods, respectively.
 
-
-n_params = 2          # number of pulse gates in Ansatz
-
 def qnode0(params):
-    qml.evolve(H_on)(params[0], t=T_single, atol=atol)
-    qml.evolve(H_off)(params[1], t=T_CR, atol=atol)
-    return qml.expval(H_obj) # H_obj
+    qml.evolve(H_pulse)((params[0], params[1]), t=T_CR)
+    return qml.expval(H_obj)
 
-dev_jax = qml.device("default.qubit", wires=range(2))
+dev = qml.device("default.qubit", wires=range(2))
 
-qnode_jax = jax.jit(qml.QNode(qnode0, dev_jax, interface="jax"))
+qnode_jax = qml.QNode(qnode0, dev, interface="jax")
 value_and_grad_jax = jax.jit(jax.value_and_grad(qnode_jax))
 
 num_split_times = 8
-qnode_sps = qml.QNode(qnode0, dev_jax, interface="jax", diff_method=qml.gradients.stoch_pulse_grad, use_broadcasting=True, num_split_times=num_split_times)
+qnode_sps = qml.QNode(qnode0, dev, interface="jax", diff_method=qml.gradients.stoch_pulse_grad, use_broadcasting=True, num_split_times=num_split_times)
 value_and_grad_sps = jax.value_and_grad(qnode_sps)
 
-qnode_odegen = qml.QNode(qnode0, dev_jax, interface="jax", diff_method=qml.gradients.pulse_odegen)
+qnode_odegen = qml.QNode(qnode0, dev, interface="jax", diff_method=qml.gradients.pulse_odegen)
 value_and_grad_odegen = jax.value_and_grad(qnode_odegen)
 
 ##############################################################################
-# We note that for as long as we are in simulation, there is no difference between the gradients obtained
+# We note that for as long as we are in simulation, there is naturally no difference between the gradients obtained
 # from direct backpropagation and using ODEgen.
 
-x = jnp.ones(n(_params, 1, tbins * 2))
+tbins = 10            # number of time bins per pulse
+n_param_batch = 2     # number of parameter batches
+
+x = jnp.ones((n_param_batch, 1, tbins * 2))
 
 res0, grad0 = value_and_grad_jax(x)
 res1, grad1 = value_and_grad_odegen(x)
 np.allclose(res0, res1), np.allclose(grad0, grad1, atol=1e-2)
 
 ##############################################################################
-# This allows us to use direct backpropagation here, which is always faster in simulation.
+# This allows us to use direct backpropagation in this demo, which is always faster in simulation.
 # We now have all ingredients to run VQE with ODEgen and SPS. We define the following standard
-# optimization loop using `optax`. We start the optimization from the same random initial values.
+# optimization loop using `optax` and perform the optimization from the same random initial values
+# with ODEgen and SPS gradients.
 
-def run_opt(value_and_grad, theta, n_epochs=100, lr=0.1, b1=0.9, b2=0.999, E_exact=0., verbose=True):
+def run_opt(value_and_grad, theta, n_epochs=150, lr=0.1, b1=0.9, b2=0.999):
 
     optimizer = optax.adam(learning_rate=lr, b1=b1, b2=b2)
     opt_state = optimizer.init(theta)
@@ -213,49 +236,54 @@ def run_opt(value_and_grad, theta, n_epochs=100, lr=0.1, b1=0.9, b2=0.999, E_exa
 
     @jax.jit
     def partial_step(grad_circuit, opt_state, theta):
+        # SPS gradients dont allow for full jitting of the update step
         updates, opt_state = optimizer.update(grad_circuit, opt_state)
         theta = optax.apply_updates(theta, updates)
 
         return opt_state, theta
 
-
-    t0 = datetime.now()
     ## Optimization loop
     for n in range(n_epochs):
-        # val, theta, grad_circuit, opt_state = step(theta, opt_state)
         val, grad_circuit = value_and_grad(theta)
         opt_state, theta = partial_step(grad_circuit, opt_state, theta)
 
         energy[n] = val
-        gradients.append(
-            grad_circuit
-        )
-        thetas.append(
-            theta
-        )
-    t1 = datetime.now()
-    if verbose:
-        print(f"final loss: {val - E_exact}; min loss: {np.min(energy) - E_exact}; after {t1 - t0}")
-    
+        gradients.append(grad_circuit)
+        thetas.append(theta)
+
     return thetas, energy
 
-seed, lr, n_epochs = 0, 0.1, 150
-key = jax.random.PRNGKey(seed)
-theta0 = jax.random.normal(key, shape=(n_params, tbins * 2))
+key = jax.random.PRNGKey(0)
+theta0 = jax.random.normal(key, shape=(n_param_batch, tbins * 2))
 
-thetaf_odegen, energy_odegen = run_opt(value_and_grad_jax, theta0, lr=lr, verbose=1, n_epochs=n_epochs, E_exact=E_exact)
-thetaf_sps, energy_sps = run_opt(value_and_grad_sps, theta0, lr=lr, verbose=1, n_epochs=n_epochs, E_exact=E_exact)
+thetaf_odegen, energy_odegen = run_opt(value_and_grad_jax, theta0)
+thetaf_sps, energy_sps = run_opt(value_and_grad_sps, theta0)
 
 plt.plot(np.array(energy_sps) - E_exact)
 plt.plot(np.array(energy_odegen) - E_exact)
 plt.yscale("log")
+plt.ylabel("$E(\\theta) - E_{{FCI}}$")
+plt.xlabel("epochs")
 plt.show()
 
 
 ##############################################################################
+# We see that the analytic gradients with ODEgen reach the ground state energy within 100 epochs, whereas the stochastic gradients from SPS do not converge to the minimum.
+# This picture solidifies when repeating this procedure for multiple runs from different random initializations, as was demonstrated in [#Kottmann]_.
+#
+# We also want to make sure that this is a fair comparison in terms of quantum resources. In the case of ODEgen we maximally have :math:`2 (4^n - 1) = 30` expectation values
+# for the two-qubit gate and :math:`6` for the single qubit (resonant) drive.
+# For SPS we have :math:`N_s 4 = 32` in both cases (due to :math:`N_s=8` time samples per gradient that we chose in ``num_split_times`` above). Thus, overall, we require fewer 
+# quantum resources for ODEgen gradients while achieving better performance.
+#
 # Conclusion
 # ==========
-# Text. #
+# We introduced ODEgen for computing analytic gradients of pulse gates and showcased its advantages in simulation for a VQE example.
+# The method is particularly well-suited for quantum computing architectures that build complexity from few-qubit gates, as is the case
+# for superconducting qubit architectures.
+# We invite you to play with ODEgen yourself. Note that this feature is amenable to hardware and you can compute gradients on OQC's Lucy via PennyLane.
+# We show you how to connect to Lucy and run pulse gates in our :doc:`recent demo <oqc_pulse>`_.
+# Running VQE using ODEgen on hardware has recently been demonstrated in [#Kottmann]_ and you can directly find `the code here <https://github.com/XanaduAI/Analytic_Pulse_Gradients/tree/main/VQE_OQC>`_.
 
 
 
@@ -295,6 +323,12 @@ plt.show()
 #     Jiaqi Leng, Yuxiang Peng, Yi-Ling Qiao, Ming Lin, Xiaodi Wu
 #     "Differentiable Analog Quantum Computing for Optimization and Control"
 #     `arXiv:2210.15812 <https://arxiv.org/abs/2210.15812>`__, 2022
+#
+# ..  [#Patterson]
+#
+#     A. D. Patterson, J. Rahamim, T. Tsunoda, P. Spring, S. Jebari, K. Ratter, M. Mergenthaler, G. Tancredi, B. Vlastakis, M. Esposito, P. J. Leek
+#     "Calibration of the cross-resonance two-qubit gate between directly-coupled transmons"
+#     `arXiv:1905.05670 <https://arxiv.org/abs/1905.05670>`__, 2019
 #
 #
 ##############################################################################
