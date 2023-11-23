f435dd95fdc9cc2759e6cfba124bd1a4c3321fcd
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2023-11-22 19:26:02 -0500

    Update tutorial_quantum_phase_estimation.py

--- demonstrations/tutorial_quantum_phase_estimation.py
+++ demonstrations/tutorial_quantum_phase_estimation.py
@@ -332,3 +332,7 @@ plt.show()
 #
 #     Gumaro Rendon, Taku Izubuchi, Yuta Kikuchi,  `"Effects of Cosine Tapering Window on Quantum Phase Estimation" <https://arxiv.org/abs/2110.09590>`__.
 #
+#
+# About the author
+# ----------------
+#
