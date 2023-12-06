fa22efd92078abd7c6bb0f98ea8483fcd78f2ab7
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2023-12-06 14:14:22 -0500

    Clarifications

--- demonstrations/tutorial_iterative_quantum_phase_estimation.py
+++ demonstrations/tutorial_iterative_quantum_phase_estimation.py
@@ -1,26 +1,22 @@
 r"""The power of Iterative Quantum Phase Estimation
 =============================================================
 
-Halfway between the current NISQ era  with few noisy qubits, and fault-tolerant computers with many noiseless qubits,
-we find what is called the ISQ era. This term refers to the situation in which we find ourselves with few qubits but
-we can execute deep circuits since we have surpassed the noise barrier: goodbye to the "N".
-
-One of the most important challenges in this ISQ era will be to adapt algorithms that depend on many qubits to others
-that can be executed on these devices. From this need, the *Iterative Quantum Phase Estimation* algorithm was born,
-which seeks to reduce the number of qubits of the *Quantum Phase Estimation* algorithm (QPE). In this demo you will see the potential of this approach!
+One of the most important challenges in the ISQ era will be to adapt algorithms that depend on many qubits to others
+that can be executed on smaller devices. From this need, the *Iterative Quantum Phase Estimation* algorithm was born,
+which seeks to reduce the number of qubits of the *Quantum Phase Estimation* algorithm (QPE). In this demo you will
+learn how to save qubits with this approach!
 
 Quantum Phase Estimation
 -----------------------------------------
 
-QPE is an algorithm that is responsible for estimating the eigenvalue of an eigenvector associated with an operator.
-We are not going to delve into this algorithm since we have already worked with it in this other :doc:`demo <tutorial_quantum_phase_estimation>` but it is worth
-remembering that if :math:`U` is our operator and :math:`|\phi\rangle` our eigenstate, we have that:
+QPE is an algorithm we have talked in this :doc:`demo <tutorial_quantum_phase_estimation>` but it is worth
+remembering the problem it solves. Given a unitary :math:`U`,
+and one of its eigenvectors :math:`|\psi \rangle`, QPE is able to find the :math:`\theta` such that:
 
 .. math::
-    U|\phi\rangle = e^{2\pi i\theta}|\phi\rangle,
+     U |\psi \rangle = e^{2 \pi i \theta} |\psi \rangle.
 
-where :math:`e^{2\pi i\theta}` is the eigenvalue and :math:`\theta` is what we will call the phase.
-Said value :math:`\theta` can be represented in binary as
+As we will see later, for convenience we can represent this value :math:`\theta` in binary as:
 
 .. math::
     \theta = \overline{0.\theta_0\theta_1\theta_2}...
@@ -28,11 +24,11 @@ Said value :math:`\theta` can be represented in binary as
 so each of these :math:`\theta_i` can take the value :math:`0` or :math:`1`.
 
 .. note::
-    We will use the horizontal line to refer to the fact that it is a binary representation and
+    We will use the horizontal line to refer to the fact that it is a digit representation and
     we are not multiplying the values of :math:`\theta_i`.
 
-Well, with Quantum Phase Estimation we can obtain the precision of as many decimal bits as `estimation_wires`,
-This is because we store each different binary bit representation in each qubit. For example , if we take :math:`3`
+With Quantum Phase Estimation we can obtain the precision of as many decimal digits as estimation wires we choose.
+This is because we store each different binary digit in each qubit. For example , if we take :math:`3`
 estimation wires:
 
 .. math::
@@ -46,14 +42,12 @@ import pennylane as qml
 import numpy as np
 import matplotlib.pyplot as plt
 
-# We build a simple operator
 def U(wires):
   return qml.PhaseShift(2 * np.pi * 0.375, wires = wires)
 
 estimation_wires = [1,2,3]
 
 dev = qml.device("default.qubit", wires = 4, shots = 1)
-
 
 @qml.qnode(dev)
 def circuit_qpe():
@@ -87,10 +81,9 @@ print(f"The estimated phase is: 0.{''.join([str(r) for r in results])}")
 # What Iterative Quantum Phase Estimation achieves is to reduce that number to a single auxiliary qubit,
 # in which we will perform different measurements throughout the circuit.
 #
-# Although it may seem surprising, this is something that can be achieved with a very simple tool: Phase KickBack.
-# In this :doc:`demo <tutorial_phase_kickback>` you can find the details of Phase Kickback but to give the general idea, suppose that we have
-# an operator :math:`U` and an eigenvector :math:`|\psi\rangle` with eigenvalue :math:`\lambda` that can take the values :math:`1` or :math:`-1`.
-# Phase KickBack provides us with a circuit capable of telling us which of the two cases we are in with the help of an auxiliary qubit:
+# Although it may seem surprising, this is something that can be achieved with a very simple tool: :doc:`Phase KickBack <tutorial_phase_kickback>`.
+# So suppose that we have an operator :math:`U` and an eigenvector :math:`|\psi\rangle` with eigenvalue :math:`\lambda \in \{1,-1\}`,
+# Phase KickBack provides us with a circuit capable of telling us which of the two possible values :math:`\lambda` takes:
 #
 # .. figure:: ../demonstrations/iterative_quantum_phase_estimation/phase_kick_back.jpeg
 #   :align: center
@@ -117,12 +110,12 @@ def circuit():
 qml.draw_mpl(circuit, wire_order=[0,1], style="pennylane")()
 plt.show()
 
-print(f"The output in the ancilla wire is: |{circuit()}>")
+print(f"The output in the ancilla wire is: |{circuit()}⟩")
 
 
 ##############################################################################
 # In this case we have obtained :math:`1` because the eigenvalue is :math:`\lambda = -1`. In the case of starting in the state
-# :math:`|0\rangle` you can verify that the output will be 0 since the associated eigenvalue is :math:`\lambda =1`. However,
+# :math:`|0\rangle` you can verify that the output will be :math:`0` since the associated eigenvalue is :math:`\lambda =1`. However,
 # although talking about :math:`1` and :math:`-1` is intuitively simple, there is an equivalent notation that will be more
 # comfortable for us:
 #
@@ -137,10 +130,11 @@ print(f"The output in the ancilla wire is: |{circuit()}>")
 # .. math::
 #   \lambda = e^{2\pi i \overline{0.\theta_0}},
 #
-# we will be obtaining the value of :math:`\theta_0` in the auxiliary qubit.
+# after apply Phase KickBack we will be obtaining the value of :math:`\theta_0` in the auxiliary qubit.
+#
 # Although it seems that this already solves our problem, we still have to be careful. If we apply this technique
 # directly to our original eigenvalue :math:`\theta = \overline{0.\theta_0\theta_1\theta_2}` we will not obtain the value
-# :math:`\theta_0` since the values of :math:`\theta_1` and :math:`\theta_2` do not have to be :math:`0` and this means that the previous
+# :math:`\theta_0` since the values of :math:`\theta_1` and :math:`\theta_2` could not be :math:`0` and this means that the previous
 # situation does not directly apply.
 # This is not a limitation and there is a very nice trick: instead of first approximating :math:`\theta_0`, we can first
 # approximate :math:`\theta_2` by applying KickBack phase to :math:`U^4` instead of :math:`U`.
@@ -168,20 +162,20 @@ print(f"The value of θ2 is: {theta_2}")
 
 
 ##############################################################################
-# Why this works may seems difficult at first but as you will see it is not complicated:
+# The reasoning for why it works is as follows:
 #
 # .. math::
 #   \left( e^{2\pi i \overline{0.\theta_0\theta_1\theta_2}} \right)^4 = e^{2\pi i \overline{\theta_0\theta_1.\theta_2}} = e^{2\pi i \overline{\theta_0\theta_1} + 2\pi i \overline{0.\theta_2}} = e^{2\pi i \overline{0.\theta_2}}.
 #
-# Basically we are first multiplying by :math:`4` and it is equivalent to moving the decimal point two units to the right.
-# After simplifying the expression we can see that we arrive at the particular case of Phase KickBack so we will obtain
+# Basically we are first multiplying by :math:`4` and, in binary, this is equivalent to moving the decimal point two units to the right.
+# After simplifying the expression we can see that we arrive at the particular case of Phase KickBack, so we will obtain
 # exactly :math:`\theta_2`.
 #
 # At this point, the next step will be to obtain :math:`\theta_1`, and by the same reasoning we would have to apply :math:`U^2`.
 # In this way, you would know that the eigenvalue we are looking for is of the form :math:`e^{2 \pi i \overline{0.\theta_1\theta_2}}`.
 # In this case, it is still not equivalent, but since we already know :math:`\theta_2`, we can subtract that value from the
 # expression to ensure that our final eigenvalue is :math:`e^{2 \pi i \overline{0.\theta_10}}`.
-# This is something we can do with a :math:`R_{\phi}(2 \pi \cdot 0.375)` gate:
+# This is something we can do with a :math:`R_{\phi}` gate:
 
 
 @qml.qnode(dev)
@@ -194,7 +188,9 @@ def circuit():
   qml.Hadamard(wires = 0)
   qml.ctrl(qml.pow(U(wires = 1), z = 2), control = 0)
 
-  qml.PhaseShift(- 2 * np.pi * theta_2 * (1 / 4), wires = 0) # 1/4 = 0.01 en binario
+  # We subtract the θ2 value
+  if theta_2 == 1:
+    qml.PhaseShift(- 2 * np.pi * (1 / 4), wires = 0) # 1/4 = 0.01 en binario
 
   qml.Hadamard(wires = 0)
 
@@ -221,8 +217,12 @@ def circuit():
   qml.Hadamard(wires = 0)
   qml.ctrl(U(wires = 1), control = 0)
 
-  qml.PhaseShift(- 2 * np.pi * theta_1 * (1 / 4), wires = 0) # 1/4 = 0.01 en binario
-  qml.PhaseShift(- 2 * np.pi * theta_2 * (1 / 8), wires = 0) # 1/8 = 0.001 en binario
+  # We subtract the θ1 and θ2 values
+  if theta_1 == 1:
+    qml.PhaseShift(- 2 * np.pi * (1 / 4), wires = 0) # 1/4 = 0.01 en binario
+
+  if theta_2 == 1:
+    qml.PhaseShift(- 2 * np.pi * (1 / 8), wires = 0) # 1/8 = 0.001 en binario
 
   qml.Hadamard(wires = 0)
 
