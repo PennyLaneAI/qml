af52e2bb3d93870f1bdcd8fc7528027b8cd7d4fb
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-01-04 18:31:34 +0100

    Update tutorial_qpe.py

--- demonstrations/tutorial_qpe.py
+++ demonstrations/tutorial_qpe.py
@@ -11,9 +11,7 @@ QPE and how to implement it in PennyLane.
 
 .. figure:: ../_static/demonstration_assets/qpe/socialthumbnail_large_Quantum_Phase_Estimation_2023-11-27.png
     :align: center
-    :width: 30%
-    :target: javascript:void(0)
-
+    :width: 50%
 
 Quantum phase estimation
 ------------------------
@@ -24,9 +22,9 @@ eigenstate exactly on a quantum computer (we'll relax that assumption later). Th
 so we can write:
 
 .. math::
-    U |\psi \rangle = e^{i \theta} |\psi \rangle,
+    U |\psi \rangle = e^{i \phi} |\psi \rangle,
 
-where :math:`\theta` is the *phase* of the eigenvalue. The goal is to estimate :math:`\theta`
+where :math:`\phi` is the *phase* of the eigenvalue. The goal is to estimate :math:`\phi`
 (hence the name phase estimation). Our challenge is to design a quantum algorithm to solve this problem.
 How would that work? 🧠
 
@@ -37,17 +35,17 @@ Part 1: Encoding the phase
 We could solve the problem by finding a quantum circuit that performs the transformation
 
 .. math::
-    |\psi \rangle |0\rangle \rightarrow  |\psi \rangle |\theta\rangle.
+    |\psi \rangle |0\rangle \rightarrow  |\psi \rangle |\phi\rangle.
 
-By measuring the second register we obtain :math:`\theta` directly. We refer to the first register as the **system**,
+By measuring the second register we obtain :math:`\phi` directly. We refer to the first register as the **target**,
 and the second one is the **estimation** register.
 
 But let's be more careful. Because the
 complex exponential has period :math:`2\pi`, technically the phase is not unique. Instead, we
-define :math:`\theta = 2\pi \phi` so that :math:`\phi` is a number between 0 and 1; this forces :math:`\theta`
-to be between 0 and :math:`2\pi`. We'll refer to :math:`\phi` as the phase from now on.
+define :math:`\phi = 2\pi \theta` so that :math:`\theta` is a number between 0 and 1; this forces :math:`\phi`
+to be between 0 and :math:`2\pi`. We'll refer to :math:`\theta` as the phase from now on.
 
-How can we encode :math:`\phi` on a quantum computer? The answer is the first clever part of the algorithm: we represent
+How can we represent :math:`\phi` on a quantum computer? The answer is the first clever part of the algorithm: we represent
 :math:`\phi` in binary. 🧠
 
 Since you probably don't use binary fractions on a daily basis, (or do you?) it's worth stopping for a moment
@@ -60,12 +58,12 @@ to make sure we're on the same page.
     10:
 
     .. math::
-        0.15625 = 1*10^{-1} + 5*10^{-2} + 6*10^{-3} + 2*10^{-4} +  5*10^{-5}.
+        0.15625 = 1 \times 10^{-1} + 5 \times 10^{-2} + 6 \times 10^{-3} + 2 \times 10^{-4} +  5 \times 10^{-5}.
 
     But nothing is stopping us from using 2 instead of 10. In binary, the same number is
 
     .. math::
-        0.00101 = 0*2^{-1} + 0*2^{-2} + 1*2^{-3} + 0*2^{-4} +  1*2^{-5}.
+        0.00101 = 0 \times 2^{-1} + 0 \times 2^{-2} + 1 \times 2^{-3} + 0 \times 2^{-4} +  1 \times 2^{-5}.
 
     (You can confirm this by computing 1/8 + 1/32 on a calculator). Similarly, 0.5 in decimal is 0.1 in binary,
     and 0.3125 is 0.0101.
@@ -74,7 +72,8 @@ Ok, now back to quantum. A binary representation is useful because we can encode
 qubits, e.g., :math:`|110010\rangle` for :math:`\phi=0.110010`. The phase is retrieved by measuring the qubits.
 The **precision** of the estimate is determined by the number of qubits. We've used examples of fractions that can be
 conveniently expressed exactly with just a few binary points, but this won't
-always be possible. From now on, we'll use :math:`n` for the number of estimation qubits.
+always be possible. For example, the binary expansion of :math:`0.8` is :math:`0.11001100...` which does not terminate.
+From now on, we'll use :math:`n` for the number of estimation qubits.
 
 Part 2: Quantum Fourier Transform
 ---------------------------------
@@ -83,25 +82,31 @@ The second clever part of the algorithm is to follow an advice given to many phy
 "When in doubt, take the Fourier transform"; or in our case, "When in doubt, take the quantum Fourier transform (QFT)".
 
 .. math::
-   \text{QFT}|\phi\rangle = \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{i\phi k} |k\rangle.
+   \text{QFT}|\theta\rangle = \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{2 \pi i\theta k} |k\rangle.
 
-If we can prepare the state on the right, then applying the *inverse* QFT would gives
-:math:`|\phi\rangle` in the estimation register.
-This looks more promising, especially if we notice the appearance of the eigenvalues :math:`e^{i\phi}`,
-although with an extra factor of :math`k`. We can obtain these by applying the unitary :math`k` times:
-
-.. math::
-   U^k|\psi\rangle = \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{i2\pi\phi k} |k\rangle.
-
-The sum is just an equal superposition over the estimation qubits, except taht each state carries a coefficient
- :math:`e^{i2\pi\phi k}`. So if we could find a circuit that performs the transformation
+Note that it is the equiprobable superposition where each state has an additional phase.
+If we can prepare that state, then applying the *inverse* QFT would gives
+:math:`|\theta\rangle` in the estimation register.
+This looks more promising, especially if we notice the appearance of the eigenvalues :math:`e^{2 \pi i\theta}`,
+although with an extra factor of :math`k`. We can obtain this factor by applying the unitary :math`k` times to the state :math:`|\psi\rangle`:
 
 .. math::
-   |\psi\rangle |k\rangle=  U^k |\psi\rangle |k\rangle,
+   U^k|\psi\rangle =  e^{2\pi i \theta k} |\psi\rangle.
 
-we could (i) apply a Hadamrd gate on all estimation qubits to create an equal superposition state,
- (ii) apply the circuit in the equation above, (iii) perform the inverse QFT, and
- (iv) measure the estimation qubits to retrieve the phase.
+Therefore, we will use :math:`|\psi\rangle` and :math:`U` to generate the factors that are of interest to us in each of the basic states.
+If we pay attention carefully we can see that it would be enough to create an operator such that:
+
+.. math::
+   |\psi\rangle |k\rangle \rightarrow  U^k |\psi\rangle |k\rangle.
+
+
+In this way, if we apply this operator to the equiprobable superposition we would have that:
+
+.. math::
+   |\psi\rangle \frac{1}{\sqrt{2^n}}\sum_{k=0}|k\rangle \rightarrow |\psi\rangle \frac{1}{\sqrt{2^n}}\sum_{k=0}U^k|k\rangle \rightarrow  |\psi\rangle \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{2 \pi i\theta k} |k\rangle
+
+This is exactly what we want!
+In PennyLane we refer to this operator as a :class:`~.ControlledSequence` operation. Let's see how to build it.
 
 Part 3: Controlled sequence
 ---------------------------
@@ -113,56 +118,55 @@ This means just one estimation qubit. After applying a Hadamard, the circuit we 
    \frac{1}{\sqrt{2}} |\psi\rangle |0\rangle + \frac{1}{\sqrt{2}} |\psi\rangle |1\rangle \rightarrow
    \frac{1}{\sqrt{2}} |\psi\rangle |0\rangle + \frac{1}{\sqrt{2}} U|\psi\rangle |1\rangle.
 
-We know how to do this: it's just a controlled-U operation. What about two qubits? Ommiting normalization factors,
+We know how to do this: it's just a controlled-U operation. What about two qubits? Omitting normalization factors,
 we need
 
 .. math::
-   \psi\rangle |00\rangle + |\psi\rangle |01\rangle + |\psi\rangle |10\rangle+  |\psi\rangle |11\rangle\rightarrow
-   \psi\rangle |00\rangle + |\psi\rangle U|01\rangle + |\psi\rangle U^2|10\rangle+ |\psi\rangle U^3|11\rangle\rightarrow.
+   |\psi\rangle |00\rangle + |\psi\rangle |01\rangle + |\psi\rangle |10\rangle+  |\psi\rangle |11\rangle\rightarrow
+   |\psi\rangle |00\rangle + U |\psi\rangle |01\rangle + U^2 |\psi\rangle |10\rangle+ U^3 |\psi\rangle |11\rangle.
 
-Notice something? This is just applying :math:`\psi\rangle |k\rangle \rightarrow \psi U^k\rangle |k\rangle` with
+Notice something? This is just applying :math:`|\psi\rangle |k\rangle \rightarrow U^k |\psi \rangle |k\rangle` with
 :math:`k` expressed in binary. It can be implemented by applying :math:`U` controlled on the first qubit, and
 :math:`U^2` controlled on the second qubit. We can extend this idea to any number of qubits.
 
 With six qubits, an example would be
 
 .. math::
-   \psi\rangle |010111\rangle \rightarrow U^1U^2U^4U^{16}\psi\rangle |010110\rangle = U^{23}\psi\rangle |010111\rangle.
+   |\psi\rangle |010111\rangle \rightarrow U^1U^2U^4U^{16}|\psi\rangle |010111\rangle = U^{23}|\psi\rangle |010111\rangle.
 
 (Note that 010111 is 23 in binary.)
 
-So we have the answer: apply :math:`U^{2^m}` controlled on the `m`-th estimation qubit. In PennyLane we refer to this
-circuit as a :class:`~.ControlledSequence` operation.
+So we have the answer: apply :math:`U^{2^m}` controlled on the `m`-th estimation qubit.
 
 Bringing it all together, here is the quantum phase estimation algorithm in all its glory:
 
 The QPE algorithm
 -----------------
 
-1. Start with the state :math:`|\psi \rangle |0\rangle`. Apply a Hadmard gate to all estimation qubits to implement the
+1. Start with the state :math:`|\psi \rangle |0\rangle`. Apply a Hadamard gate to all estimation qubits to implement the
    transformation
+
    .. math::
-       |\psi \rangle |0\rangle \rightarrow \frac{1}{\sqrt{2^n}}\sum_{k=0} |\psi\rangle|k\rangle.
+
+       |\psi \rangle |0\rangle \rightarrow |\psi\rangle \frac{1}{\sqrt{2^n}}\sum_{k=0} |k\rangle.
 
 2. Apply a :class:`~.ControlledSequence` operation, i.e., :math:`U^{2^m}` controlled on the `m`-th estimation qubit.
    This gives
+
    .. math::
-       \frac{1}{\sqrt{2^n}}\sum_{k=0} |\psi\rangle|k\rangle \rightarrow  \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{i2\pi \phi k}|\psi\rangle|k\rangle.
+
+       \frac{1}{\sqrt{2^n}}\sum_{k=0} |\psi\rangle|k\rangle \rightarrow  \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{2\pi i \theta k}|\psi\rangle|k\rangle.
 
 3. Apply the inverse quantum Fourier transform to the estimation qubits
-    .. math::
-       \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{i2\pi \phi k}|\psi\rangle|k\rangle \rightarrow |\psi\rangle|\phi\rangle.
 
-4. Measure the estimation qubits to recover :math:`\phi`.
+    .. math::
+
+       \frac{1}{\sqrt{2^n}}\sum_{k=0} e^{2 \pi i \theta k}|\psi\rangle|k\rangle \rightarrow |\psi\rangle|\theta\rangle.
+
+4. Measure the estimation qubits to recover :math:`\theta`.
 
 Most of the heavy lifting is done by the controlled sequence step. Control-U operations are the heart of the algorithm,
 coupled with a clever use of Fourier transforms.
-
-Overall, we recognize three main ingredients: (i) creating a
-superposition state over the estimation qubits, (ii) a controlled sequence operation, and (iii) the inverse
-quantum Fourier transform. PennyLane
-implements QPE by treating each of these separately, allowing you to experiment with multiple
-possibilities.
 
 QPE is doing something incredible: it can calculate eigenvalues **without ever diagonalizing
 a matrix**. Wow. This is true even if we relax the assumption that the input is an eigenstate. By linearity, for an arbitray
@@ -171,9 +175,10 @@ state expanded in the eigenbasis of :math:`U` as
 .. math::
    \Psi\rangle = \sum_i c_i |\psi_i\rangle,
 
-QPE outputs the eigenphase :math:`\phi_i` with probability :math:`|c_i|^2.
+QPE outputs the eigenphase :math:`\theta_i` with probability :math:`|c_i|^2`.
 This feature is crucial for quantum chemistry applications,
-where preparing good initial states is essential [link to demo and paper].
+where preparing good initial states is essential [#initial_state]_.
+If you want to learn more about this check out our :doc:`demo <tutorial_initial_state_preparation>`.
 
 One more point of importance. Generally it is not possible to represent a given phase exactly using a limited number of
 estimation qubits. Thus, there is typically a distribution of possible
@@ -183,15 +188,13 @@ The error *decreases* exponentially with the number of estimation qubits, but th
 with error :math:`\varepsilon` is poportional to :math:`1/\varepsilon`.
 
 
+Time to code!
+-------------
+We already know the three building blocks of QPE; it is time to put them to practice.
+We use a single-qubit :class:`~pennylane.PhaseShift` operator :math:`U = R_{\phi}(2 \pi / 5)`
+and its eigenstate :math:`|1\rangle`` with corresponding phase :math:`\theta=0.2`.
+
 """
-
-#
-# Time to code!
-# -------------
-# We already know the three building blocks of QPE; it is time to put them to practice.
-# We use a single-qubit rotation operator :math:`U = R_{\phi}(2 \pi / 5)` [JM: explain what R_phi is]
-# and its eigenstate :math:`|1\rangle`` with corresponding phase :math:`\phi=0.2`.
-
 import pennylane as qml
 import numpy as np
 
@@ -241,42 +244,6 @@ plt.show()
 # distribution of possible outcomes. [JM: rewrite this to express as binary fraction]. The peak occurs
 # at :math:`\phi` is :math:`0.19921875`, which is very close to the exact value of :math:`0.2` 🎊.
 #
-# Cleaning the signal
-# -------------------
-# The plot obtained above is noisy and has long tails, called leaks. A
-# common technique in signal processing is the use of windows: functions applied prior to the
-# Fourier transform. We use a similar method here
-# and apply a cosine window [#Gumaro]_ instead of the uniform window induced by the Hadamard gates.
-# In PennyLane, this is done using the :class:`~.CosineWindow` operator, which can be efficiently implemented on a
-# quantum computer.
-#
-
-
-@qml.qnode(dev)
-def circuit_qpe():
-    # we initialize the eigenvalue |1>
-    qml.PauliX(wires=0)
-
-    # We apply the window
-    qml.CosineWindow(wires = estimation_wires)
-
-    # We apply the function f to all values
-    qml.ControlledSequence(U(wires = 0), control=estimation_wires)
-
-    # We apply the inverse QFT to obtain the frequency
-    qml.adjoint(qml.QFT)(wires=estimation_wires)
-
-    return qml.probs(wires=estimation_wires)
-
-
-results = circuit_qpe()
-plt.bar(range(len(results)), results)
-plt.xlabel("frequency")
-plt.show()
-
-##############################################################################
-# Goodbye leaks!
-#
 # Conclusion
 # ----------
 # This demo presented the "textbook" version of QPE. There are multiple variations, notably iterative QPE that
@@ -294,11 +261,12 @@ plt.show()
 # References
 # ---------------
 #
-# .. [#Gumaro]
+# .. [#initial_state]
 #
-#     Gumaro Rendon, Taku Izubuchi, Yuta Kikuchi,  `"Effects of Cosine Tapering Window on Quantum Phase Estimation" <https://arxiv.org/abs/2110.09590>`__.
+#    Stepan Fomichev et al. "Initial state preparation for quantum chemistry on quantum computers",
+#    `Arxiv <https://arxiv.org/pdf/2310.18410.pdf/>`__, 2023
 #
 #
-# About the author
-# ----------------
+# About the authors
+# -----------------
 #
