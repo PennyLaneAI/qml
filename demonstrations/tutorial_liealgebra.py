409b48510624ec3456e92823d369d2fe0ffd0970
qottmann <korbinian.kottmann@gmail.com>
2024-01-18 16:50:21 +0100

    change title, wip

--- demonstrations/tutorial_liealgebra.py
+++ demonstrations/tutorial_liealgebra.py
@@ -1,5 +1,5 @@
-r"""A quantum scientist's introduction to (Dynamical) Lie Algebras in quantum computing
-=======================================================================================
+r"""A quantum engineer's guide to (Dynamical) Lie Algebras
+==========================================================
 
 
 We are going to introduce the basics of (dynamical) Lie algebras with a focus on quantum computing.
