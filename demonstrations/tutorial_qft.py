a3d7c089bd43f33388f2bb67691b16306cdc17cd
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-03-05 18:09:18 -0500

    Update tutorial_qft.py

--- demonstrations/tutorial_qft.py
+++ demonstrations/tutorial_qft.py
@@ -1,4 +1,4 @@
-r"""Intro to Quantum Phase Estimation
+r"""Intro to Quantum Fourier Transform
 =============================================================
 
 intro QFT
@@ -21,16 +21,16 @@ The nice thing about this representation is that it shows that there is no entan
 
 .. math::
 
-    U_k |x\rangle = |x0 \dots x_{k-1}\rangle \otimes \left (|0\rangle + \exp \left (\frac{2\pi i 2^k}{2^n} x  \right) |1\rangle \right ) \otimes |x_{k+1} \dots x_{n-1}\rangle,
+    U_k |x\rangle = |x_0 \dots x_{k-1}\rangle \otimes \left (|0\rangle + \exp \left (\frac{2\pi i 2^k}{2^n} x  \right) |1\rangle \right ) \otimes |x_{k+1} \dots x_{n-1}\rangle,
 
-where :math:`|x\rangle = |x0 \dots x_k\rangle \otimes |x_{k+1} \dots x_{n-1}\rangle`.
-We can build :math:`U_k` with one Hadamard gate and controlled Phase Shift gates which add a phase only to :math:`|1\rangle`. We will represent these control gates with a box in which we will include inside it the phase they add. Below we show an animation in which this operator can be visualized for the particular case of :math:`n = 4` and :math:`k = 1`.
+where :math:`|x\rangle = |x0 \dots x_{n-1}\rangle`.
+We can build :math:`U_k` with one Hadamard gate and controlled Phase Shift gates, which add a phase only to :math:`|1\rangle`. We will represent these control gates with a box in which we will include inside it the phase they add. Below we show an animation in which this operator can be visualized for the particular case of :math:`n = 4` and :math:`k = 1`.
 
 .. figure:: ../_static/demonstration_assets/qft/qft.gif
     :align: center
     :width: 80%
 
-As can be seen, each qubit adds a phase to the k-th qubit, proportional to its position in the binary representation. Note that qubits over the :math:`k`-th one don't contribute to the k-th qubit since they would make complete turns and it would not affect their value.
+Each qubit adds a phase to the k-th qubit, proportional to its position in the binary representation. Note that qubits over the :math:`k`-th one don't contribute to the k-th qubit since they would make complete turns and it would not affect their value.
 With this operator we can now prepare the QFT by applying them sequentially:
 
 .. math::
@@ -44,7 +44,7 @@ Therefore, the QFT for the case of 3 qubits would be:
     ../_static/demonstration_assets/qft/qft3.jpeg
 
 One last detail to consider, is that in the QFT formula we have given, the values of the k's goes in reversed order from :math:`n-1` to :math:`0`.
-That is why we should make a last step and change the order of all the qubits. That is why it is common to find some swap operators at the end of the template.
+That is why we should make a last step and change the order of all the qubits. For this reason, it is common to find some swap operators at the end of the template.
 """
 
 import pennylane as qml
