beb660cd54d0187ad1fd9638b6c048447206d967
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-03-05 19:34:58 -0500

    Update tutorial_qft.py

--- demonstrations/tutorial_qft.py
+++ demonstrations/tutorial_qft.py
@@ -17,20 +17,22 @@ Although we have already shown a formula for the QFT above, there is another equ
 
     \text{QFT}|x\rangle = \bigotimes_{k = n-1}^{0} \left (|0\rangle + \exp \left (\frac{2\pi i 2^k}{2^n} x  \right) |1\rangle \right ).
 
-The nice thing about this representation is that it shows that there is no entanglement between qubits and we could somehow prepare each qubit independently. We will call :math:`U_k` the operator that is able to prepare the k-th qubit. This operator is defined as:
+The nice thing about this representation is that it gives us an independent expression for each qubit and we could somehow prepare them independently. We will call :math:`U_k` the operator that is able to prepare the k-th qubit. This operator is defined as:
 
 .. math::
 
     U_k |x\rangle = |x_0 \dots x_{k-1}\rangle \otimes \left (|0\rangle + \exp \left (\frac{2\pi i 2^k}{2^n} x  \right) |1\rangle \right ) \otimes |x_{k+1} \dots x_{n-1}\rangle,
 
-where :math:`|x\rangle = |x0 \dots x_{n-1}\rangle`.
-We can build :math:`U_k` with one Hadamard gate and controlled Phase Shift gates, which add a phase only to :math:`|1\rangle`. We will represent these control gates with a box in which we will include inside it the phase they add. Below we show an animation in which this operator can be visualized for the particular case of :math:`n = 4` and :math:`k = 1`.
+where :math:`|x\rangle = |x_0 \dots x_{n-1}\rangle`.
+We can build :math:`U_k` with one Hadamard gate and controlled Phase Shift gates, which add a phase only to :math:`|1\rangle`. Below we show an animation in which this operator can be visualized for the particular case of :math:`n = 4` and :math:`k = 1`.
+We will represent these control gates with a box in which we will include inside it the phase they add.
 
 .. figure:: ../_static/demonstration_assets/qft/qft.gif
     :align: center
     :width: 80%
 
-Each qubit adds a phase to the k-th qubit, proportional to its position in the binary representation. Note that qubits over the :math:`k`-th one don't contribute to the k-th qubit since they would make complete turns and it would not affect their value.
+
+Each qubit adds a phase to the k-th qubit, proportional to its position in the binary representation. Note that qubits over the :math:`k`-th one don't contribute to the phase since they would make complete turns and it would not affect its value.
 With this operator we can now prepare the QFT by applying them sequentially:
 
 .. math::
@@ -43,7 +45,7 @@ Therefore, the QFT for the case of 3 qubits would be:
 .. figure::
     ../_static/demonstration_assets/qft/qft3.jpeg
 
-One last detail to consider, is that in the QFT formula we have given, the values of the k's goes in reversed order from :math:`n-1` to :math:`0`.
+One last detail to consider, is that in the QFT formula we have given, the index :math:`k` goes in reversed order from :math:`n-1` to :math:`0`.
 That is why we should make a last step and change the order of all the qubits. For this reason, it is common to find some swap operators at the end of the template.
 """
 
