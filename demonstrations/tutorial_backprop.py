4783bc0d24a423790ee787fc0a16629814393398
Josh Izaac <josh146@gmail.com>
2024-03-07 22:20:13 -0500

    Update tutorial_backprop.py

--- demonstrations/tutorial_backprop.py
+++ demonstrations/tutorial_backprop.py
@@ -147,7 +147,7 @@ print(grad_function(params)[0])
 # Alternatively, we can directly compute quantum gradients of QNodes using
 # PennyLane's built in :mod:`qml.gradients <pennylane.gradients>` module:
 
-print(qml.gradients.param_shift(circuit)(params))
+print(np.stack(qml.gradients.param_shift(circuit)(params)))
 
 ##############################################################################
 # If you count the number of quantum evaluations, you will notice that we had to evaluate the circuit
