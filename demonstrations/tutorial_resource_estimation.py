4eff8f05beca0259d08f6e97d094fe0c24a8d2e3
soranjh <40344468+soranjh@users.noreply.github.com>
2024-03-12 08:54:35 -0400

    Apply suggestions from code review
    Co-authored-by: Utkarsh <utkarshazad98@gmail.com>

--- demonstrations/tutorial_resource_estimation.py
+++ demonstrations/tutorial_resource_estimation.py
@@ -302,7 +302,7 @@ fig.tight_layout()
 #     Modjtaba Shokrian Zini, Alain Delgado, Roberto dos Reis, Pablo A. M. Casares,
 #     Jonathan E. Mueller, Arne-Christian Voigt, Juan Miguel Arrazola,
 #     "Quantum simulation of battery materials using ionic pseudopotentials".
-#     `arXiv:2302.07981 (2023) <https://arxiv.org/abs/2302.07981>`__
+#     `Quantum 7, 1049 (2023) <https://doi.org/10.22331/q-2023-07-10-1049>`__
 #
 # .. [#delgado2022]
 #
