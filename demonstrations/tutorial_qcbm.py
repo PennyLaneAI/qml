580978f08aafa1f66018123851636187c97d862e
Gopal Ramesh Dahale <49199003+Gopal-Dahale@users.noreply.github.com>
2024-04-12 22:29:31 +0530

    Grammar fix. Add comma.
    Co-authored-by: Alvaro Ballon <91897656+alvaro-at-xanadu@users.noreply.github.com>

--- demonstrations/tutorial_qcbm.py
+++ demonstrations/tutorial_qcbm.py
@@ -119,7 +119,7 @@ class QCBM:
 # -----------------------------------------------
 #
 # We train the QCBM on the bars and stripes dataset. The dataset has binary black and white images of
-# size :math:`n \times n` pixels. We consider :math:`n=3` for this tutorial which will give 14 valid
+# size :math:`n \times n` pixels. We consider :math:`n=3` for this tutorial, which will give 14 valid
 # configurations. The dataset is represented by flattened bitstrings. The quantum circuit will use 9
 # qubits in total.
 #
