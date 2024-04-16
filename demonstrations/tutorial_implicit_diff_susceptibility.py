140343de03d04013fd654b7916717cee70e3e868
qottmann <korbinian.kottmann@gmail.com>
2024-04-16 10:38:11 +0200

    simplify and fix susceptibility demo

--- demonstrations/tutorial_implicit_diff_susceptibility.py
+++ demonstrations/tutorial_implicit_diff_susceptibility.py
@@ -322,10 +322,8 @@ def build_H0(N, J, gamma):
     return H
 
 H0 = build_H0(N, J, gamma)
-H0_coeffs, H0_ops = H0.terms()
 H0_matrix = qml.matrix(H0)
 A = reduce(add, ((1 / N) * qml.PauliZ(i) for i in range(N)))
-A_coeffs, A_ops = A.terms()
 A_matrix = qml.matrix(A)
 
 ###############################################################################
@@ -451,11 +449,8 @@ def energy(z, a):
         float: The expectation value (energy).
     """
     variational_ansatz(*z, wires=range(N))
-    # here we compute the Hamiltonian coefficients and operations
-    # 'by hand' because the qml.Hamiltonian class does not support
-    # operator arithmetic with JAX device arrays.
-    coeffs = jnp.concatenate([H0_coeffs, a * jnp.array(A_coeffs)])
-    return qml.expval(qml.Hamiltonian(coeffs, H0_ops + A_ops))
+
+    return qml.expval(H0 + a * A)
 
 
 z_init = [jnp.array(2 * np.pi * np.random.random(s)) for s in weights_shape]
