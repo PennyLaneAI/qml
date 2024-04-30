f5b4983fb905666acfa322870709607516604973
minhtriet <minhtriet09@gmail.com>
2024-04-30 19:56:49 +0100

    fix math

--- demonstrations/tutorial_vqe_vqd.py
+++ demonstrations/tutorial_vqe_vqd.py
@@ -119,7 +119,6 @@ def circuit(param):
 # paper <https://www.nature.com/articles/s41524-023-00965-1>`_
 #
 # .. math::  C_0(\theta) = \left\langle {\Psi(\theta)\left| {\hat H} \right|{\Psi}(\theta)} \right\rangle
-#
 # .. math:: C_1(\theta) = \left\langle\Psi(\theta)|\hat H |\Psi (\theta) \right\rangle + \beta | \left\langle \Psi (\theta)| \Psi_0 \right\rangle|^2
 #
 # We can then define a lost function using the VQE and VQD methods
