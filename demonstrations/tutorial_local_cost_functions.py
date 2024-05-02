761ca88d7dbd0116c7f5af3128e440e1b712d81e
Astral Cai <astral.cai@xanadu.ai>
2024-05-02 10:19:25 -0400

    bugfix bugfix

--- demonstrations/tutorial_local_cost_functions.py
+++ demonstrations/tutorial_local_cost_functions.py
@@ -344,7 +344,7 @@ cost_global(params_local)
 #
 
 _dev = qml.device("lightning.qubit", wires=wires, shots=None)
-global_circuit = qml.QNode(global_cost_simple, dev, interface="autograd")
+global_circuit = qml.QNode(global_cost_simple, _dev, interface="autograd")
 print(
     "Current cost: "
     + str(cost_global(params_local))
