3453ad75f7be0615b086be0e8f8f883b115da6dc
dwierichs <david.wierichs@xanadu.ai>
2024-05-06 11:35:37 +0200

    prepare file

--- demonstrations/tutorial_mcm_introduction.py
+++ demonstrations/tutorial_mcm_introduction.py
@@ -1,1 +1,17 @@
+r"""Introduction to mid-circuit measurements
+============================================
+
+
+.. figure:: ../_static/demonstration_assets/mcm_introduction/socialthumbnail_mcm_introduction.png
+    :align: center
+    :width: 50%
+
+"""
+
+######################################################################
+#
+
+######################################################################
+# About the author
+# ----------------
 #
