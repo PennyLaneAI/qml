7db83abcfeb4b31ff8d2ac78572f30b95dc4fe0a
EmilianoG-byte <emilianogodinez0518@gmail.com>
2024-05-08 15:58:29 -0400

    add initial draft for qcbm demo

---
+++ demonstrations/tutorial_qcbm.py
@@ -0,0 +1,18 @@
+"""
+.. qcbm:
+
+QCBM with tensor networks ansatz
+===============
+
+.. meta::
+    :property="og:description": Implementing the Quantum Circuit Born Machine (QCBM) using a tensor network ansatz
+    :property="og:image": https://pennylane.ai/qml/_static/demonstration_assets//qaoa_maxcut_partition.png
+
+.. related::
+   tutorial_qaoa_intro Intro to QAOA
+
+*Author: Ahmed Darwish and Emiliano Godinez — Posted: 08 May 2024. Last updated: 08 May 2024.*
+
+"""
+
+##############################################################################
