4ae5326e1ca06bf2c6b56b9dcc42c97cc600e3e6
WrathfulSpatula <stranoj@gmail.com>
2024-06-05 14:00:09 -0400

    tutorial_qrack.py

---
+++ demonstrations/tutorial_qrack.py
@@ -0,0 +1,140 @@
+r"""The Qrack device back end (with Catalyst)
+=============================================================
+
+.. meta::
+    :property="og:description": Using the Qrack device for PennyLane and Catalyst, with GPU-acceleration and novel optimization.
+    :property="og:image": https://pennylane.ai/qml/_static/demonstration_assets/qrack/qrack_logo.jpg
+
+*Author: Dan Strano — Posted: 5 July 2024.*
+
+`Qrack <https://github.com/unitaryfund/qrack`__ is a GPU-accelerated quantum computer simulator with many "novel" optimizations, and `PyQrack <https://github.com/unitaryfund/pyqrack`__ is its Python wrapper, written in pure (`ctypes`) Python language standard.
+
+When simulating quantum subroutines of varying qubit widths, Qrack will "transparently," automatically, and dynamically transition between GPU-based and CPU-based simulation techniques for maximum execution speed, when qubit registers might be too narrow to benefit from the large parallel processing element count of a GPU, up to maybe roughly 20 qubits, depending upon the classical hardware platform. Qrack also offers "hybrid" stabilizer simulation (with fallback to universal simulation) and near-Clifford simulation with greatly reduced memory footprint on Clifford gate sets with the inclusion of the `RZ` variational Pauli Z-axis rotation gate. Particularly for systems that don't rely on GPU-acceleration, Qrack offers a "quantum binary decision diagram" ("QBDD") simulation algorithm option that might significantly reduce memory footprint or execution complexity for circuits with "low entanglement," judged by the complexity of a QBDD to represent the state. Qrack also offers approximation options aimed at trading off minimal fidelity reduction for maximum reduction in simulation complexity (as opposed to models of physical noise) such as "Schmidt decomposition rounding parameter" ("SDRP") and "near-Clifford rounding parameter" ("NCRP").
+
+As you might guess from the last paragraph, the Qrack simulator doesn't fit neatly into a single canonical category of quantum computer simulation algorithm: it optionally and by default leverages elements of state vector simulation, tensor network simulation, stabilizer and near-Clifford simulation, and QBDD simulation, often all at once, while it introduces some novel algorithmic "tricks" for Schmidt decomposition of state vectors in a manner similar to "matrix product state" ("MPS") simulation.
+
+In this tutorial you will learn how to use the Qrack device back end for PennyLane and Catalyst, and you'll learn certain suggested cases of use where Qrack might particularly excel at delivering lightning-fast performance or minimizing required memory resources (though Qrack is a "general-purpose" simulator, and users might employ it for all their applications and still see parity with or improvement over available device back ends).
+
+.. figure:: ../_static/demonstration_assets/qrack/qrack_logo.jpg
+    :align: center
+    :width: 60%
+    :target: javascript:void(0)
+
+Demonstrating Qrack with the Quantum Fourier Transform
+------------------------------------------------------
+
+You can learn about the quantum Fourier transform (QFT) in `other PennyLane tutorials <https://pennylane.ai/qml/demos/tutorial_qft/>`__. It is a building block subroutine of many other quantum algorithms. Qrack exhibits unique capability on many cases of the QFT algorithm, and worst-case performance is competitive with other popular quantum computer simulators (as `reported <https://arxiv.org/abs/2304.14969>`__ in 2023 at IEEE QCE).
+
+In the case of a "trivial" computation basis eigenstate input, Qrack can simulate basically as wide of a QFT as any for which you'd ask. Below, we pick a random eigenstate initialization and perform the QFT across a width of 60 qubits.
+"""
+
+import pennylane as qml
+from pennylane import numpy as np
+from catalyst import qjit
+
+import matplotlib.pyplot as plt
+
+import random
+
+dev = qml.device("qrack.simulator", 60, shots=8)
+@qml.qnode(dev)
+def circuit():
+    for i in range(60):
+        if random.uniform(0, 1) < 0.5:
+            qml.X(wires=[i])
+    qml.QFT(wires=range(60))
+    return qml.counts(wires=range(60))
+
+def counts_to_int_keys(counts):
+    counts_fixed = {}
+    for k, v in counts.items():
+        counts_fixed[str(int(k, 2))] = int(v)
+    return counts_fixed
+
+counts = counts_to_int_keys(circuit())
+
+plt.bar(counts.keys(), counts.values())
+plt.xlabel("|x⟩")
+plt.ylabel("counts")
+plt.show()
+
+#############################################
+# In this image we have represented only the first 8 qubits so we can visualize it easily.
+#
+# This becomes harder is we request a "non-trivial" initialization. In general, Qrack will use Schmidt decomposition techniques to try to break up circuits in separable subsystems of qubits to simulate semi-independently, combining them just-in-time with Kronecker products when they need to interact, according the user's circuit definition.
+#
+# The circuit becomes much harder for Qrack if we randomly initialize the input qubits with Haar-random `U3 gates <https://docs.pennylane.ai/en/stable/code/api/pennylane.U3.html>`__, but the performance is still significantly better than the worst case (of `GHZ state <https://en.wikipedia.org/wiki/Greenberger%E2%80%93Horne%E2%80%93Zeilinger_state>`__ initialization).
+
+
+dev = qml.device("qrack.simulator", 24, shots=8)
+@qml.qnode(dev)
+def circuit():
+    for i in range(24):
+        th = random.uniform(0, np.pi)
+        ph = random.uniform(0, np.pi)
+        dl = random.uniform(0, np.pi)
+        qml.U3(th, ph, dl, wires=[i])
+    qml.QFT(wires=range(24))
+    return qml.counts(wires=range(24))
+
+counts = counts_to_int_keys(circuit())
+
+plt.bar(counts.keys(), counts.values())
+plt.xlabel("|x⟩")
+plt.ylabel("counts")
+plt.show()
+
+#############################################
+# Alternate Simulation Algorithms (QBDD and Near-Clifford)
+# --------------------------------------------------------
+# By default, Qrack relies on a combination of state vector simulation, "hybrid" stabilizer and near-Clifford simulation, and Schmidt decomposition optimization techniques. Alternatively, we could use pure stabilizer simulation or QBDD simulation if the circuit is at all amenable to optimization this way.
+#
+# To demonstrate this, we prepare a 60 qubit GHZ state, which would commonly be intractable with state vector simulation.
+
+dev = qml.device("qrack.simulator", 60, shots=8, isBinaryDecisionTree=False, isStabilizerHybrid=True, isSchmidtDecompose=False)
+@qml.qnode(dev)
+def circuit():
+    qml.Hadamard(0)
+    for i in range(1, 60):
+        qml.CNOT(wires=[i - 1, i])
+    return qml.counts(wires=range(60))
+
+counts = counts_to_int_keys(circuit())
+
+plt.bar(counts.keys(), counts.values())
+plt.xlabel("|x⟩")
+plt.ylabel("counts")
+plt.show()
+
+#############################################
+# As you can see, Qrack was able to construct the 60-qubit GHZ state (without exceeding memory limitations), and the probability is peaked at bit strings of all 0 and all 1.
+#
+# It's trivial for Qrack to perform large GHZ state preparations with "hybrid" stabilizer or near-Clifford simulation, if Schmidt decomposition is deactivated.
+# QBDD cannot be accelerated by GPU, so its application might be limited, but it is parallel over CPU processing elements, hence it might be particularly well-suited for systems with no GPU at all, though Qrack default simulation methods will likely still outperform on it "BQP-complete" problems like random circuit sampling or quantum volume certification.
+#
+
+dev = qml.device("qrack.simulator", 24, shots=8, isBinaryDecisionTree=True, isStabilizerHybrid=False)
+@qml.qnode(dev)
+def circuit():
+    qml.Hadamard(0)
+    for i in range(1, 24):
+        qml.CNOT(wires=[i - 1, i])
+    return qml.counts(wires=range(24))
+
+counts = counts_to_int_keys(circuit())
+
+plt.bar(counts.keys(), counts.values())
+plt.xlabel("|x⟩")
+plt.ylabel("counts")
+plt.show()
+
+#############################################
+# If the user's gate set is restricted to Clifford with general RZ gates (being mindful of the fact that compilers like Catalyst might optimize such a gate set basis into different gates), the time complexity for measurement samples becomes "doubly-exponential" with near-Clifford simulation, but the space complexity almost exactly that of stabilizer simulation for the logical qubits plus an ancillary qubit per (non-optimized) RZ gate, scaling like the square of the logical plus ancillary qubit count.
+#
+# Conclusion
+# ----------
+# In this tutorial, we've demonstrated the basics of using the Qrack simulator back end, as well showed readers examples of special cases on which Qrack's "novel" optimizations can lead to huge increases in performance or maximum achievable qubit widths. Remember the Qrack device back end for PennyLane if you'd like to leverage GPU acceleration but don't want to complicate your choice of devices or device initialization, to handle a mixture of wide and narrow qubit registers in your subroutines.
+#
+# About the authors
+# -----------------
+# Dan Strano is a technical staff member at `Unitary Fund <https://unitary.fund>`__. Additionally, he has been the lead author of the Qrack simulator framework since 2017.
