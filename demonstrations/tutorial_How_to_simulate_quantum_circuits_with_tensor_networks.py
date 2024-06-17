7f1adf4c9a60aa29daa0d9f759ee9bf23b151515
PietropaoloFrisoni <pietropaolo.frisoni@xanadu.ai>
2024-06-17 18:46:04 -0400

    First bone structure (still incomplete, many things to discuss)

--- demonstrations/tutorial_How_to_simulate_quantum_circuits_with_tensor_networks.py
+++ demonstrations/tutorial_How_to_simulate_quantum_circuits_with_tensor_networks.py
@@ -16,6 +16,154 @@ TODO: Insert figure
 """
 
 ######################################################################
+# Choosing the method to simulate quantum circuits
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# The ``default.tensor`` device can simulate quantum circuits using two different computational methods.
+# We only need to specify the ``method`` keyword argument when instantiating the device.
+# The first one is the Matrix Product State (MPS) representation, the second is the Tensor Network (TN) approach.
+# The MPS representation can be seen as a special case of the TN method, where the tensor network has a one-dimensional structure.
+#
+
+######################################################################
+# Simulating a quantum circuit with the MPS method
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Let's start by simulating a quantum circuit using the Matrix Product State (MPS) method.
+# If the number of wires is not specified when instantiating the device, it is inferred from the circuit at runtime.
+# The MPS method is used by default when the ``method`` argument is not specified.
+#
+# We choose the maximum bond dimension to be ``None``, which means that the bond dimension is not restricted.
+# The ``cutoff`` parameter is set to the machine epsilon of the ``numpy.complex128`` data type.
+# This value is the threshold below which to discard the singular coefficients of the Singular Value Decomposition (SVD) in the MPS method.
+# Finally, we set the ``contract`` parameter to ``auto-mps``.
+#
+
+import pennylane as qml
+import numpy as np
+
+# Define the keyword arguments for the MPS method
+kwargs_mps = {
+    "max_bond_dim": None,
+    "cutoff": np.finfo(np.complex128).eps,
+    "contract": "auto-mps",
+}
+
+# The parameters of the quantum circuit
+theta = 0.5
+phi = 0.1
+
+# Instantiate the device with the MPS method and the specified kwargs
+dev = qml.device("default.tensor", method="mps", **kwargs_mps)
+
+
+# Define the quantum circuit
+@qml.qnode(dev)
+def circuit(theta, phi, num_qubits):
+    for qubit in range(num_qubits - 4):
+        qml.RX(theta, wires=qubit + 1)
+        qml.CNOT(wires=[qubit, qubit + 1])
+        qml.RY(phi, wires=qubit + 1)
+        qml.DoubleExcitation(theta, wires=[qubit, qubit + 1, qubit + 3, qubit + 4])
+        qml.Toffoli(wires=[qubit + 1, qubit + 3, qubit + 4])
+    return qml.expval(
+        qml.X(num_qubits - 1) @ qml.Y(num_qubits - 2) @ qml.Z(num_qubits - 3)
+    )
+
+
+######################################################################
+# We can now simulate the quantum circuit for different numbers of qubits.
+# The execution time will generally increase as the number of qubits grows.
+#
+
+import time
+
+# Simulate the circuit for different numbers of qubits
+for num_qubits in range(50, 201, 50):
+    print(f"Number of qubits: {num_qubits}")
+    start_time = time.time()
+    result = circuit(theta, phi, num_qubits)
+    end_time = time.time()
+    print(f"Result: {result}")
+    print(f"Execution time: {end_time - start_time:.4f} seconds")
+
+
+######################################################################
+# We can also visualize the tensor network representation of the quantum circuit with the ``draw`` method.
+# This method generates a graphical representation of the tensor network using the ``quimb``'s plotting functionalities.
+#
+# Since we did not specify the number of qubits when instantiating the device,
+# the number of tensors in the tensor network is inferred from the last execution of the quantum circuit.
+# Let's visualize the MPS representation of the quantum circuit with 12 qubits.
+#
+
+circuit(theta, phi, num_qubits=12)
+dev.draw(color="auto", show_inds=True, figsize=(10, 7))
+
+######################################################################
+# Simulating a quantum circuit with the TN method
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# The Tensor Network (TN) method is a more general approach than the Matrix Product State (MPS) method.
+# This method can be more efficient for simulating quantum circuits when the MPS method
+# may require a large bond dimension to accurately represent the state, leading to increased computational and memory costs.
+# For example, this method can be useful for simulating circuits with a higher degree of entanglement.
+#
+
+import pennylane as qml
+import numpy as np
+
+# Define the keyword arguments for the MPS method
+kwargs_tn = {
+    "contract": "auto-split-gate",
+    "local_simplify": "ADCRS",
+    "contraction_optimizer": "auto-hq",
+}
+
+# The parameters of the quantum circuit
+theta = 0.5
+phi = 0.1
+depth = 10
+
+# Instantiate the device with the TN method and the specified kwargs
+dev = qml.device("default.tensor", method="tn", **kwargs_tn)
+
+
+@qml.qnode(dev)
+def circuit(theta, depth, num_qubits):
+    for i in range(num_qubits):
+        qml.X(wires=i)
+    for _ in range(1, depth - 1):
+        for i in range(0, num_qubits, 2):
+            qml.CNOT(wires=[i, i + 1])
+        for i in range(num_qubits % 5):
+            qml.RZ(theta, wires=i)
+        for i in range(1, num_qubits - 1, 2):
+            qml.CZ(wires=[i, i + 1])
+    for i in range(num_qubits):
+        qml.CNOT(wires=[i, (i + 1)])
+    return qml.var(qml.X(num_qubits - 1))
+
+
+# Simulate the circuit for different numbers of qubits
+for num_qubits in range(25, 101, 25):
+    print(f"Number of qubits: {num_qubits}")
+    start_time = time.time()
+    result = circuit(theta, depth, num_qubits)
+    end_time = time.time()
+    print(f"Result: {result}")
+    print(f"Execution time: {end_time - start_time:.4f} seconds")
+
+######################################################################
+# As before, let's visualize the tensor network representation of the circuit with 15 qubits.
+# We can see that the Tensor Network (TN) method generates a more complex tensor network than the Matrix Product State (MPS) method.
+# In this case, the representation depends on the structure of the quantum circuit and the specified keyword arguments.
+#
+
+circuit(theta, depth, num_qubits=15)
+dev.draw(color="auto", show_inds=True, figsize=(7, 7))
+
+######################################################################
 # ...
 #
 # About the authors
