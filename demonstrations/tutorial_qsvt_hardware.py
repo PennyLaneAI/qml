09ca08be6868fda1dd12b3ff55961cae3be03d1e
KetpuntoG <65235481+KetpuntoG@users.noreply.github.com>
2024-07-09 14:20:13 -0400

    Update tutorial_qsvt_hardware.py

--- demonstrations/tutorial_qsvt_hardware.py
+++ demonstrations/tutorial_qsvt_hardware.py
@@ -2,9 +2,9 @@ r"""How to implement QSVT on hardware
 =======================================
 
 Performing polynomial transformations on matrices is a task of great interest,
-which we have previously addressed in our introductory demos on QSVT and its practical applications.
+which we have previously addressed in our introductory demos on :doc:`QSVT </demos/tutorial_intro_qsvt>` and its practical :doc:`applications </demos/tutorial_apply_qsvt>`.
 However, until now, we had focused more on the algebraic aspect rather than on the implementation in real hardware.
-In this how-to guide, you will see how we can implement the QSVT subroutine in a hardware-compatible manner,
+In this how-to guide, you will see how we can implement the QSVT subroutine in a hardware-compatible way,
 taking your applications to the next level.
 
 Conventions and Angle Calculation 
@@ -57,8 +57,8 @@ print(angles)
 # -----------------
 #
 # The :class:`~.qml.QSVT` template expects two inputs. The first one is the block encoding operator (:class:`~.qml.PrepSelPrep`)
-# and the next one is the projection operators (:class:`~.qml.PCPhase`) that encodes the angles properly. To do so,
-# let's define a Hamiltonian and the operators:
+# and the next one are the projection operators (:class:`~.qml.PCPhase`) that encodes the angles properly. Let's define
+# a Hamiltonian and the operators:
 
 import pennylane as qml
 
@@ -76,7 +76,7 @@ block_encode = qml.PrepSelPrep(H, control = control_wires)
 projectors = [qml.PCPhase(angles[i], dim=2**len(H.wires), wires= control_wires + H.wires) for i in range(len(angles))]
 
 ######################################################################
-# With this we now have all the pieces to run the algorithm. Let's put it together and see the resulting matrix:
+# We now have all the pieces to run the algorithm. Let's put it together and see the resulting matrix:
 
 dev = qml.device("default.qubit")
 
@@ -100,12 +100,38 @@ H_poly = -H_mat + 0.5 * np.linalg.matrix_power(H_mat, 3) + 0.5* np.linalg.matrix
 print(np.round(H_poly,4).real)
 
 ######################################################################
-# As we can see, the matrix obtained from the QSVT subroutine is the same as the one obtained by applying the polynomial.
-# The great advantage of this approach is that the :class:`~.qml.BlockEncode` operator has not been used,
+# As we can see, the matrix obtained from the QSVT subroutine is the same as the one obtained by applying the polynomial
+# directly to the Hamiltonian. The great advantage of this approach is that the :class:`~.qml.BlockEncode` operator has not been used,
 # making it possible to decompose the algorithm into basic gates easily.
 #
+# Extracting the real part
+# ------------------------
 #
-##############################################################################
+# In the previous example we have manually extracted the real part of the matrix to check that it actually worked.
+# However, this is not something we can do in hardware. For this reason we present here a way to extract the real
+# part with the help of an auxiliary qubit:
+
+@qml.qnode(dev)
+def circuit():
+
+    qml.Hadamard(0)
+    qml.ctrl(qml.QSVT, control=0, control_values=[1])(block_encode, projectors)
+    qml.ctrl(qml.adjoint(qml.QSVT), control=0, control_values=[0])(block_encode, projectors)
+    qml.Hadamard(0)
+
+    return qml.state()
+
+matrix = qml.matrix(circuit, wire_order = [0] + control_wires + H.wires)()[: 2**len(H.wires), :2**len(H.wires)]
+print(np.round(matrix,4))
+
+######################################################################
+# As we can see, the real part has been correctly encoded into the circuit.
+# The idea behind this algorithm is based on the fact that the sum of a complex number and its conjugate gives us
+# its real part. I recommend :doc:`this demo </demos/tutorial_lcu_blockencoding>` to learn more about the structure
+# of the circuit.
+#
+# I hope this how-to will serve as a guide to run your own workflows and experiment with more advanced Hamiltonians and functions.
+#
 # About the author
 # ----------------
 # .. include:: ../_static/authors/guillermo_alonso.txt
