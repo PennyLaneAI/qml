7fee55ffa185a40122a5513a55561db367d2d165
Diego <diego_guala@hotmail.com>
2024-07-12 20:34:01 -0400

    fix rendering

--- demonstrations/tutorial_learning_dynamics_incoherently.py
+++ demonstrations/tutorial_learning_dynamics_incoherently.py
@@ -1,12 +1,15 @@
-r"""If we have an unknown quantum process ❓ that takes a quantum state as input and outputs
+r"""How to learn quantum dynamics incoherently
+==========================================
+
+If we have an unknown quantum process ❓ that takes a quantum state as input and outputs
 another state, how can we recreate it or simulate it in a quantum circuit? How do we create a
 model circuit that reproduces the target quantum process? One approach is to learn the
 [dynamics of this process incoherently](https://arxiv.org/abs/2303.12834). In simple terms, this
 consists of two steps:
 1. Measure the output of the unknown process for many different inputs
-2. Adjust a variational quantum circuit until it produces the same input-output 
-   combinations as the unknown process. 
-  
+2. Adjust a variational quantum circuit until it produces the same input-output combinations as
+the unknown process.
+
 For step 1, we measure classical shadows of the target process output.
 For step 2, we simulate the model circuit to get its final state. To know how similar the simulated
 state is to the target process output, we use the classical shadows to estimate the overlap of the
@@ -26,24 +29,24 @@ In this tutorial, we will use PennyLane to do the following:
 4. Create a model variational circuit to learn the quantum process.
 5. Train the variational circuit.
 6. Repeat the procedure using the target quantum process and hardware measurements used in
-   [The power and limitations of learning quantum dynamics incoherently]
-   (https://arxiv.org/abs/2303.12834)
+[The power and limitations of learning quantum dynamics incoherently]
+(https://arxiv.org/abs/2303.12834)
 """
 
 ######################################################################
-# How to learn quantum dynamics incoherently
-# -----------------------------------------------
-#
-######################################################################
 # Creating the unknown target quantum process
-# -------------------------------------------
+# --------------------------------------------
 # 
 # We can perform a well known quantum process, imaginary time evolution of a Hamiltonian:
+# 
 # $$U(H, t) = e^{-i H t / \hbar}$$
+#
 # Specifically, we will use the same Hamiltonian as in the paper,
 # a transverse-field Ising Hamiltonian:
-# :math:`H = \sum _{i=0}^{n-1} Z_iZ_{i+1} + \sum_{i=0}^{n}\alpha_iX_i`,
-# where $n$ is the number of qubits and $\alpha$ are randomly generated weights.
+#
+# $$H = \sum _{i=0}^{n-1} Z_iZ_{i+1} + \sum_{i=0}^{n}\alpha_iX_i$$,
+# 
+# where :math:`n` is the number of qubits and $\alpha$ are randomly generated weights.
 # 
 # We use a Trotterized version of this Hamiltonian through :class:`~pennylane.TrotterProduct`.
 # 
@@ -59,11 +62,11 @@ np.random.seed(0)
 alphas = np.random.normal(0, 0.5, size=n_qubits)
 hamiltonian = qml.sum(
     *[qml.PauliZ(wires=i) @ qml.PauliZ(wires=i + 1) for i in range(n_qubits - 1)]
-) + qml.sum(*[alphas[i] * qml.PauliX(wires=i) for i in range(n_qubits)])
+)+ qml.sum(*[alphas[i] * qml.PauliX(wires=i) for i in range(n_qubits)])
 
 ######################################################################
 # Create random initial states
-# -----------------------------
+# ------------------------------
 #
 # The next step in our procedure is to prepare several initial states. We will then apply the
 # "unknown" quantum process to each of these states to create input-output pairs. That is, for each
@@ -80,7 +83,7 @@ hamiltonian = qml.sum(
 #
 # .. note ::
 #
-#    Using this method to produce random Haar states starts to become computationally expensive 
+#    Using this method to produce random Haar states becomes computationally expensive 
 #    around 10 qubits.
 
 from numpy.linalg import qr
@@ -111,11 +114,11 @@ random_states = [random_unitary[:,0]]
 
 ######################################################################
 # Time evolution and classical shadow measurements
-# ------------------------------------------------
+# --------------------------------------------------
 #
 # Now we can evolve the initial states using a Trotterized version of the Hamiltonian above. This
 # will approximate the time evolution of the corresponding transverse-field Ising system.
-# Similarly to the paper, we will use a Trotterization with time `0.1`, order 1, and 1 step. We then
+# Similarly to the paper, we will use a Trotterization with time 0.1, order 1, and 1 step. We then
 # measure classical shadows on this system using :func:`~pennylane.classical_shadow`.
 #
 
@@ -145,7 +148,7 @@ shadow = qml.ClassicalShadow(bits, recipes)
 
 ######################################################################
 # Create model circuit that will learn the target process
-# -------------------------------------------------------
+# ---------------------------------------------------------
 #
 # Now that we have the classical shadow measurements, we need to create a `model_circuit` that
 # learns to produce the same output as the target circuit. We will then use the classical shadow
@@ -191,7 +194,7 @@ print(qml.draw(model_circuit)(initial_params, random_states[0]))
 
 ######################################################################
 # Train the model circuit using the classical shadows in a cost function
-# ----------------------------------------------------------------------
+# ------------------------------------------------------------------------
 #
 # We now have to find the optimal parameters for `model_circuit` to mirror the `target_circuit`. 
 # We can estimate the similarity between the circuits according to the cost function provided in 
@@ -211,23 +214,21 @@ def cost(params):
 print("Initial cost:", cost(initial_params))
 
 params = initial_params
-observable_mat = model_circuit(params)
-observable_pauli = qml.pauli_decompose(observable_mat)
 
 optimizer = qml.GradientDescentOptimizer(stepsize=0.1)
-steps = 1000
+steps = 100
 for i in range(steps):
     params, final_cost = optimizer.step_and_cost(cost, params)
 
 print("Final cost:", final_cost)
 
-print(model_circuit(params))
+print(model_circuit(params, random_states[0]))
 
-print(target_circuit())
+print(np.mean(shadow.local_snapshots(),axis=0))
 
 ######################################################################
 # Now we repeat using the learning dynamics incoherently dataset
-# --------------------------------------------------------------
+# ----------------------------------------------------------------
 #
 
 [ds] = qml.data.load("other", name="learning-dynamics-incoherently")
@@ -240,6 +241,7 @@ ds.attr_info["hamiltonian"]["doc"]
 n_shadows = 10
 shadow_ds = qml.ClassicalShadow(ds.shadow_meas[0][:n_shadows], ds.shadow_bases[0][:n_shadows])
 
+dev = qml.device('default.qubit')
 
 @qml.qnode(dev)
 def placeholder_trotter_circuit():
@@ -270,7 +272,7 @@ def cost_dataset(params):
     cost = 0.0
     observable_mats = model_circuit(params, ds.training_states[0])
     observable_pauli = [qml.pauli_decompose(observable_mat, wire_order=[qubit]) for qubit, observable_mat in enumerate(observable_mats)]
-    cost = cost - qml.math.sum(shadow.expval(observable_pauli))
+    cost = cost - qml.math.sum(shadow_ds.expval(observable_pauli))
     return cost
 
 
