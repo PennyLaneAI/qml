95a24e8f22984a2cb36b4fa944e8eab30eefb4f1
qottmann <korbinian.kottmann@gmail.com>
2024-08-02 13:44:37 +0200

    messed up metadata

--- demonstrations/tutorial_shadow_hamiltonian_simulation.py
+++ demonstrations/tutorial_shadow_hamiltonian_simulation.py
@@ -84,7 +84,7 @@ res
 ##############################################################################
 #
 # We evolved a :math:`2^n = 2` dimensional quantum state and performed :math:`3` independent measurements (non-commuting).
-# In shadow Hamiltonian simulation, we encode :math:`3` expectation values in a :math:`2^2 = 4` dimensional
+# In shadow Hamiltonian simulation, we encode :math:`4` expectation values in a :math:`2^2 = 4` dimensional
 # quantum state, i.e. :math:`n_S = 2`.
 # (TODO comment on whether or not that is practical. At first it seems
 # redundant, but this technique is allowing new things like sampling from :math:`p_m = \langle O_m \rangle`
