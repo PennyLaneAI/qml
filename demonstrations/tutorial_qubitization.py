ce462612974936d7fb6e67861c1091bace811ec4
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2024-08-21 11:43:17 -0400

    Apply suggestions from code review
    some suggestions from review [skip-ci]
    
    Co-authored-by: ixfoduap <40441298+ixfoduap@users.noreply.github.com>

--- demonstrations/tutorial_qubitization.py
+++ demonstrations/tutorial_qubitization.py
@@ -1,7 +1,7 @@
 r"""Intro to Qubitization
 =========================
 
-Encoding a Hamiltonian into a quantum computer is a fundamental task for many applications, but the way to do it is not unique. One method that has gained special status is known as **Qubitization**. In this demo, we will introduce the Qubitization operator and explain how to view it as a rotation operator. Understanding Qubitization as a rotation simplifies the analysis and implementation of quantum algorithms, particularly when combined with Quantum Phase Estimation. We will demonstrate this through an example application, with code provided using the :class:`~.pennylane.Qubitization` template in PennyLane.
+Encoding a Hamiltonian into a quantum computer is a fundamental task for many applications, but the way to do it is not unique. One method that has gained special status is known as **qubitization**. In this demo, we will introduce the qubitization operator and explain how to view it as a rotation operator. This perspective is useful when combining qubitization with quantum phase estimation (QPE). We will demonstrate this by implementing qubitization-based quantum phase estimation, with code provided using the :class:`~.pennylane.Qubitization` template in PennyLane.
 
 .. figure:: ../_static/demonstration_assets/qubitization/OGthumbnail_large_Qubitization.png
     :align: center
@@ -18,13 +18,14 @@ For a Hamiltonian :math:`\mathcal{H}` given in its `LCU representation <https://
     Q =  \text{PSP}_{\mathcal{H}}\cdot (2|0\rangle\langle 0| - I),
 
 where :math:`\text{PSP}_{\mathcal{H}}` refers to the block encoding :math:`\text{Prep}_{\mathcal{H}}^{\dagger} \text{Sel}_{\mathcal{H}} \text{Prep}_{\mathcal{H}}`, as explained in `this demo <https://pennylane.ai/qml/demos/tutorial_lcu_blockencoding/>`_.
+
 The operator :math:`Q` is also a block encoding operator with a key property: its eigenvalues encode the eigenvalues of the Hamiltonian.
 As we will soon explain in detail, if :math:`E` is an eigenvalue of :math:`\mathcal{H}`, then :math:`e^{i\arccos(E/\lambda)}` is an
 eigenvalue of :math:`Q`, where :math:`\lambda` is a known normalization factor. This property is very useful because it means that we can use the `Quantum Phase Estimation algorithm <https://pennylane.ai/qml/demos/tutorial_qpe/>`_  to estimate the eigenvalues of  :math:`Q`, and then use them to retrieve the eigenvalues of the encoded Hamiltonian.
 
 This is the essence of why Qubitization is attractive for applications: it provides a method to exactly encode eigenvalues of a Hamiltonian into a unitary operator that can be used inside the Quantum Phase Estimation algorithm to sample Hamiltonian eigenvalues. But where does this decomposition come from? Why are the eigenvalues encoded in this way? 🤔 We explain these concepts below.
 
-Block Encoding
+Block Encodings
 ----------------
 
 First, we introduce some useful concepts about block encodings.
@@ -34,7 +35,7 @@ inside the matrix associated to the circuit (up to a normalization factor):
 .. math::
     \text{Block Encoding}_\mathcal{H} \rightarrow \begin{bmatrix} \mathcal{H} / \lambda & \cdot \\ \cdot & \cdot \end{bmatrix}.
 
-Given an eigenvector of :math:`\mathcal{H}` such that :math:`\mathcal{H}| \phi \rangle = E | \phi \rangle`, we deduce that any :math:`\text{Block Encoding}_\mathcal{H}`
+Given an eigenvector of :math:`\mathcal{H}` such that :math:`\mathcal{H}| \phi \rangle = E | \phi \rangle`, any :math:`\text{Block Encoding}_\mathcal{H}`
 generates a state:
 
 .. math::
@@ -54,30 +55,30 @@ Qubitization as a rotation
 
 Any block encoding operator manages to transform the state :math:`|0\rangle |\phi\rangle` into the state :math:`|\Psi\rangle`.
 The Qubitization operator does this by applying a rotation in that two-dimensional subspace by an angle of :math:`\theta=\arccos {\frac{E}{\lambda}}`.
-The advantage of a rotation operator is that the angle :math:`\theta` appears directly in its eigenvalues as the phases  :math:`e^{\pm i\theta}`. Therefore, if the rotation angle encodes useful information, it can be retrieved by estimating the phase of the rotation operator, for example using QPE.
+The advantage of a rotation operator is that the angle :math:`\theta` appears directly in the eigenvalues, which are simply :math:`e^{\pm i\theta}`. Therefore, if the rotation angle encodes useful information, it can be retrieved by estimating the phase of the rotation operator, for example using QPE.
 
 We now show that the Qubitization operator is a rotation by using the same idea of Amplitude Amplification:
 two reflections are equivalent to one rotation. These reflections correspond to :math:`(2|0\rangle\langle 0| - I)` and :math:`\text{PSP}_{\mathcal{H}}`, which together define our Qubitization operator.
 
 .. note::
 
-    To verify that an operator is indeed a reflection, one must square the operator and check if the result is the identity operator.
+    An operator :math:`U` is a reflection if :math:`U^2=I`.
 
-As an example, let’s take :math:`|\Psi\rangle` as the initial state to visualize the rotation. The first reflection, given by :math:`(2|0\rangle\langle 0| - I)`, mirrors the state around the axis defined by :math:`|0\rangle |\phi\rangle`. This occurs because the operator flips the sign of any component not aligned with :math:`|0\rangle` in the first register.
+As an example, let’s take :math:`|\Psi\rangle` as the initial state to visualize the rotation. The first reflection, given by :math:`(2|0\rangle\langle 0| - I)`, mirrors the state around the axis defined by :math:`|0\rangle |\phi\rangle`. This occurs because the operator flips the sign of any component not aligned with :math:`|0\rangle` in the first register. The result is to prepare state :math:`|\Psi_1\rangle`, as illustrated below:
 
 .. figure:: ../_static/demonstration_assets/qubitization/qubitization2.jpeg
     :align: center
     :width: 50%
     :target: javascript:void(0)
 
-Given the effect that :math:`\text{PSP}_{\mathcal{H}}` has on the state :math:`|0\rangle|\phi\rangle`, it can be verified that the second reflection occurs along the line that bisects the angle between the state :math:`|\Psi\rangle` and :math:`|0\rangle |\phi\rangle`. Let's now examine the effect this reflection has on the previous state:
+The block encoding operator :math:`\text{PSP}_{\mathcal{H}}` performs a second reflection along the line that bisects the angle between :math:`|\Psi\rangle` and :math:`|0\rangle |\phi\rangle`. Let's now examine the effect this reflection has on the previous state :math:`|\Psi_1\rangle`:
 
 .. figure:: ../_static/demonstration_assets/qubitization/qubitization3.jpeg
     :align: center
     :width: 60%
     :target: javascript:void(0)
 
-After applying the reflection, the new state is rotated by :math:`\theta` degrees relative to the initial state :math:`|\Psi\rangle`. This shows that the Qubitization operator successfully creates a rotation of :math:`\theta` degrees within the subspace.
+After applying the reflection, the new state :math:`|\Psi_2\rangle` is rotated by :math:`\theta` degrees relative to the initial state :math:`|\Psi\rangle`. This shows that the Qubitization operator successfully creates a rotation of :math:`\theta` degrees within the subspace.
 
 The term "qubitization" comes from the fact that this process occurs within a two-dimensional subspace, which can be viewed as a qubit. For each eigenstate of the Hamiltonian, the Qubitization operator acts within this two-dimensional space, effectively treating it as a qubit. This is why we say the system has been "qubitized".
 
@@ -86,7 +87,7 @@ Qubitization in PennyLane
 
 We now describe how to implement qubitization-based `Quantum Phase Estimation (QPE) <https://pennylane.ai/qml/demos/tutorial_qpe/>`_  to sample eigenvalues of a Hamiltonian.
 
-First, let's define a Hamiltonian to use as an example:
+First, let's define a simple Hamiltonian to use as an example:
 """
 
 import pennylane as qml
@@ -97,11 +98,11 @@ print(qml.matrix(H))
 
 ##############################################################################
 # We have chosen an operator that is diagonal in the computational basis because it is easy to identify
-# eigenvalues and eigenvectors, but this would work with any Hamiltonian. We are going to take
+# eigenvalues and eigenvectors, but the algorithm works with any Hamiltonian. We are going to take
 # the eigenstate  :math:`|\phi\rangle = |11\rangle` as input and try to estimate its eigenvalue :math:`E = 0.5` using
 # this technique.
 #
-# In PennyLane, the Qubitization operator can be easily constructed using the built-in :class:`~.pennylane.Qubitization` operation. You simply need to provide the Hamiltonian and the control qubits that define the block encoding. The number of control wires is :math:`⌈\log_2 k⌉`, where :math:`k` is the number of terms in the `LCU representation <https://pennylane.ai/qml/demos/tutorial_lcu_blockencoding/>`_ of the Hamiltonian.
+# In PennyLane, the Qubitization operator can be easily constructed using the built-in class:class:`~.pennylane.Qubitization`. You simply need to provide the Hamiltonian and the control qubits that define the block encoding. The number of control wires is :math:`⌈\log_2 k⌉`, where :math:`k` is the number of terms in the `LCU representation <https://pennylane.ai/qml/demos/tutorial_lcu_blockencoding/>`_ of the Hamiltonian.
 #
 # .. note::
 #
@@ -160,7 +161,7 @@ lambda_ = sum([abs(coeff) for coeff in H.terms()[0]])
 print("E = ", lambda_ * np.cos(2 * np.pi * np.argmax(results) / 2 ** (len(estimation_wires))))
 
 ##############################################################################
-# Great! We successfully approximated the real value of :math:`E = 0.5`. 🚀
+# Great! We successfully approximated the correct eigenvalue of :math:`E = 0.5`. 🚀
 #
 # Conclusion
 # ----------------
