b44aabab95060287dc031c1370cb975edadc5fac
Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
2024-08-28 16:18:01 -0400

    Apply suggestions from code review
    Co-authored-by: soranjh <40344468+soranjh@users.noreply.github.com>

--- demonstrations/tutorial_qsvt_hardware.py
+++ demonstrations/tutorial_qsvt_hardware.py
@@ -10,14 +10,14 @@ taking your applications to the next level.
 Angles calculation
 ------------------
 
-Our goal is to apply a polynomial transformation to a given Hamiltonian (i.e., :math:`p(\mathcal{H})`). To achieve this, we must consider the two
+Our goal is to apply a polynomial transformation to a given Hamiltonian, i.e., :math:`p(\mathcal{H})`. To achieve this, we must consider the two
 fundamental components of the QSVT algorithm:
 
 - **Projection angles**: A list of angles that will determine the coefficients of the polynomial to be applied.
-- **Block Encoding**: The strategy used to introduce the Hamiltonian into the quantum computer. We will use :class:`~.qml.PrepSelPrep`.
+- **Block Encoding**: The strategy used to encode the Hamiltonian. We will use :class:`~.qml.PrepSelPrep`.
 
-Calculating angles is not a trivial task but there are some frameworks like ``pyqsp`` that do that job for us.
-To find the angles to apply the polynomial :math:`p(x) = -x + \frac{x^3}{2}+ \frac{x^5}{2}`, we run this script:
+Calculating angles is not a trivial task but there are tools such as ``pyqsp`` that do the job for us.
+For instance, to find the angles to apply the polynomial :math:`p(x) = -x + \frac{x^3}{2}+ \frac{x^5}{2}`, we can run this code:
 
 .. code-block:: python
 
@@ -36,7 +36,7 @@ The angles obtained after execution are as follows:
 ang_seq = [-1.5115007723754004, 0.6300762184670975, 0.8813995564082947, -2.2601930971815003, 3.7716688720568885, 0.059295554419495855]
 
 ######################################################################
-# We are going to use this angles to apply the polynomial transformation.
+# We use this angles to apply the polynomial transformation.
 # However, we are not finished yet: these angles have been calculated following the "Wx"
 # convention, while :class:`~.qml.PrepSelPrep` follows a different one. Moreover, the angles obtained in the
 # context of QSP (the ones given by ``pyqsp``) are not the same as the ones we have to use in QSVT. That is why
@@ -63,8 +63,8 @@ print(angles)
 # QSVT on Hardware
 # -----------------
 #
-# The :class:`~.qml.QSVT` template expects two inputs. The first one is the block encoding operator (:class:`~.qml.PrepSelPrep`)
-# and the next one are the projection operators (:class:`~.qml.PCPhase`) that encodes the angles properly.
+# The :class:`~.qml.QSVT` template expects two inputs. The first one is the block encoding operator, :class:`~.qml.PrepSelPrep`, 
+# and the second one is a set of projection operators, :class:`~.qml.PCPhase`, that encode the angles properly.
 # We will see how to apply them later, but first let's define
 # a Hamiltonian and manually apply the polynomial of interest:
 
@@ -86,8 +86,8 @@ H_poly = -H_mat + 0.5 * mpow(H_mat, 3) + 0.5* mpow(H_mat, 5)
 print(np.round(H_poly,4))
 
 ######################################################################
-# Great, we already know what the goal is, let's see how to do this with a quantum circuit.
-# We start by defining the operators and then apply them to the :class:`~.qml.QSVT` template.
+# Great, we already know what the target result is. Let's now see how apply the polynomial with a quantum circuit.
+# We start by defining the proper input operators for the :class:`~.qml.QSVT` template.
 
 # We need |log2(len(coeffs))| = 2 control wires to encode the Hamiltonian
 control_wires = [1,2]
@@ -110,22 +110,22 @@ matrix = qml.matrix(circuit, wire_order = [0] + control_wires + H.wires)()
 print(np.round(matrix[: 2**len(H.wires), :2**len(H.wires)],4))
 
 ######################################################################
-# The matrix obtained from the QSVT subroutine is the same as the one obtained by applying the polynomial
-# directly to the Hamiltonian! That means that the circuit is encoding :math:`p(\mathcal{H})` correctly.
-# The great advantage of this approach is that all the templates used can be
+# The matrix obtained using QSVT is the same as the one obtained by applying the polynomial
+# directly to the Hamiltonian! That means the circuit is encoding :math:`p(\mathcal{H})` correctly.
+# The great advantage of this approach is that all the building blocks used in the circuit can be
 # decomposed into basic gates easily.
 #
-# The idea behind this circuit is that QSVT encodes the desired polynomial :math:`p(\mathcal{H})` but also
-# a polynomial :math:`i q(\mathcal{H})`. To isolate :math:`p(\mathcal{H})` we have used an auxiliary qubit and the property that
+# Please also note that QSVT encodes the desired polynomial :math:`p(\mathcal{H})` as well as
+# a polynomial :math:`i q(\mathcal{H})`. To isolate :math:`p(\mathcal{H})`, we have used an auxiliary qubit and considered the that
 # the sum of a complex number and its conjugate gives us twice its real part. We
 # recommend :doc:`this demo </demos/tutorial_apply_qsvt>` to learn more about the structure
 # of the circuit.
 #
 # Conclusion
 # ----------
-# In this brief how-to we have seen how to apply QSVT on a Hamiltonian. Note that the algorithm is sensitive to
-# the encoding used so make sure that the angles are converted to the proper format.
-# We hope this how-to will serve as a guide to running your own workflows and experimenting with more advanced Hamiltonians and functions.
+# In this brief how-to we demonstrated applying QSVT on a sample Hamiltonian. Note that the algorithm is sensitive to
+# the block-encoding method, so please make sure that the projection angles are converted to the proper format.
+# This how-to serves as a guide for running your own workflows and experimenting with more advanced Hamiltonians and polynomial functions.
 #
 # About the author
 # ----------------
