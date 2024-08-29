PennyLaneAI/qml

commit 1714f2a4d322adee4fdf24ef3ebd241616188362

Merge master branch into dev (#1199)
Co-authored-by: Mudit Pandey <mudit.pandey@xanadu.ai>

Makefile
@@ -77,7 +77,7 @@ environment:
 			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane-cirq.git#egg=pennylane-cirq;\
 			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane-qiskit.git#egg=pennylane-qiskit;\
 			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane-qulacs.git#egg=pennylane-qulacs;\
-			$$PYTHON_VENV_PATH/bin/python -m pip install -i https://test.pypi.org/simple/ PennyLane-Lightning --pre --upgrade;\
-			$$PYTHON_VENV_PATH/bin/python -m pip install -i https://test.pypi.org/simple/ PennyLane-Catalyst --pre --upgrade;\
+			$$PYTHON_VENV_PATH/bin/python -m pip install --extra-index-url https://test.pypi.org/simple/ PennyLane-Lightning --pre --upgrade;\
+			$$PYTHON_VENV_PATH/bin/python -m pip install --extra-index-url https://test.pypi.org/simple/ PennyLane-Catalyst --pre --upgrade;\
 		fi;\
 	fi

demonstrations/tutorial_quantum_circuit_cutting.py
@@ -713,7 +713,7 @@ fig.set_size_inches(12, 6)
 # simulator. Luckily, PennyLane has just what we need:
 #
 
-device = qml.device("default.mixed", wires=tape.wires)
+device = qml.device("default.mixed", wires=tape.wires, shots=n_shots)
 
 ######################################################################
 # We only need a single run for each of the two generated tapes, ``tape0`` and
@@ -738,11 +738,12 @@ print(f"Channel 1: {channel_shots[1]} times.")
 # Time to run the simulator!
 #
 
-device.shots = channel_shots[0].item()
+tape0 = QuantumTape(ops=ops_0, measurements=tape.measurements, shots=channel_shots[0].item())
+tape1 = QuantumTape(ops=ops_1, measurements=tape.measurements, shots=channel_shots[1].item())
+
 (shots0,) = qml.execute([tape0], device=device, cache=False, gradient_fn=None)
 samples[choices == 0] = shots0
 
-device.shots = channel_shots[1].item()
 (shots1,) = qml.execute([tape1], device=device, cache=False, gradient_fn=None)
 samples[choices == 1] = shots1
 

commit cdcaa5310789c3247114362eccd04b83930a3a0a

Update failing demos (#1202)
As name says. Lots of demos are failing after #1199 was merged. This PR
aims to fix all those failures.

Makefile
@@ -73,11 +73,11 @@ environment:
 		if [ '$(UPGRADE_PL)' = 'true' ]; then\
 			echo "Updating PennyLane and plugins to latest ... ";\
 			PYTHON_VENV_PATH=`$(POETRY_BIN) env info --path`;\
-			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane.git@v0.38.0-rc0#egg=pennylane;\
 			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane-cirq.git#egg=pennylane-cirq;\
 			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane-qiskit.git#egg=pennylane-qiskit;\
 			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane-qulacs.git#egg=pennylane-qulacs;\
 			$$PYTHON_VENV_PATH/bin/python -m pip install --extra-index-url https://test.pypi.org/simple/ PennyLane-Lightning --pre --upgrade;\
 			$$PYTHON_VENV_PATH/bin/python -m pip install --extra-index-url https://test.pypi.org/simple/ PennyLane-Catalyst --pre --upgrade;\
+			$$PYTHON_VENV_PATH/bin/python -m pip install --upgrade git+https://github.com/PennyLaneAI/pennylane.git@v0.38.0-rc0#egg=pennylane;\
 		fi;\
 	fi

demonstrations/tutorial_quantum_circuit_cutting.py
@@ -713,7 +713,7 @@ fig.set_size_inches(12, 6)
 # simulator. Luckily, PennyLane has just what we need:
 #
 
-device = qml.device("default.mixed", wires=tape.wires, shots=n_shots)
+device = qml.device("default.mixed", wires=tape.wires)
 
 ######################################################################
 # We only need a single run for each of the two generated tapes, ``tape0`` and