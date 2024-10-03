15c46907b5fe2b47b15160ded65cb3e3df6c2ab5
dwierichs <david.wierichs@xanadu.ai>
2024-10-03 09:02:46 +0200

    add dummy files

---
+++ demonstrations/tutorial_kak_theorem.py
@@ -0,0 +1,25 @@
+r"""The KAK theorem
+-------------------
+
+.. figure:: ../_static/demo_thumbnails/opengraph_demo_thumbnails/OGthumbnail_kak_theorem.png
+    :align: center
+    :width: 60%
+    :target: javascript:void(0)
+"""
+
+import pennylane as qml
+
+######################################################################
+######################################################################
+#
+# References
+# ----------
+#
+# .. [#ref1]
+#
+#     Author1, Author2
+#     "Title"
+#     `arXiv:quant-ph/9807006 <https://arxiv.org/abs/quant-ph/9807006>`__, 1998.
+#
+# About the author
+# ----------------
