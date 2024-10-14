7b21c93114d86436f61dbe4303fcccb3c62d79a5
dwierichs <david.wierichs@xanadu.ai>
2024-10-14 14:10:23 +0200

    outline

--- demonstrations/tutorial_kak_theorem.py
+++ demonstrations/tutorial_kak_theorem.py
@@ -35,75 +35,100 @@ hands-on examples throughout.
     :doc:`introduction to (dynamical) Lie algebras </demos/tutorial_liealgebra/>`.
 
 
-The stage
----------
+Introduction
+------------
 
-Intro
+Basic mathematical objects
+--------------------------
 
-
-
-
-The actors
-----------
+Introduce the mathematical objects that will play together to yield
+the KAK theorem.
 
 (Semi-)simple Lie algebras
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
- Brief!
 
-Subalgebras
+- Introduce the notion of a Lie algebra very briefly, refer to existing demo(s).
+- Focus on vector space notion being clear.
+- [optional] Briefly say what a simple/semisimple Lie algebra is.
+- [optional] In particular mention that the adjoint representation is faithful for semisimple algebras.
+
+Group and algebra interaction
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Exponential map
+- adjoint action of group on algebra
+- adjoint action of algebra on algebra -> adjoint representation
+- adjoint identity (-> g-sim demo)
+
+Subalgebras and Cartan pairs
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Introduce the notion of a subalgebra.
+- Explain that there can be vector subspaces that are not subalgebras.
+- Define Cartan pairs via commutation relations
+
+Cartan subalgebras
+~~~~~~~~~~~~~~~~~~
+
+- Define Cartan subalgebras of :math:`m`.
+- Dimension of Cartan subalgebras
+- Transition between Cartan subalgebras via :math:`K`
+
+Involutions
 ~~~~~~~~~~~
 
-Cartan involution
-~~~~~~~~~~~~~~~~~
+- Explain linear maps on (matrix) algebras (-> homomorphism)
+- Define involutions.
+- Involutions define Cartan pairs (:math:`k = +1 | m = -1` eigenspaces)
+- Cartan pairs define involutions :math:`\theta = \Pi_{\mathfrak{k}} - \Pi_{\mathfrak{m}}`
 
-Symmetric space
-~~~~~~~~~~~~~~~
+KAK theorem
+~~~~~~~~~~~
 
-
-
-
-The props
----------
-
-Exponential map
-~~~~~~~~~~~~~~~
-
-Adjoint representation
-~~~~~~~~~~~~~~~~~~~~~~
+- KP decomposition
+- KAK decomposition
+- [optional] implication: KaK on algebra level
 
 
+Two-qubit KAK decomposition
+---------------------------
 
+- Algebra/subalgebra :math:`\mathfrak{g} =\mathfrak{su}(4) | \mathfrak{k} =\mathfrak{su}(2) \oplus \mathfrak{su}(2)`
+- Involution: EvenOdd
+- CSA: :math:`\mathfrak{a} = \langle\{XX, YY, ZZ\}\rangle_{i\mathbb{R}}`
+- KAK decomposition :math:`U= (A\otimes B) \exp(i(\eta_x XX+\eta_y YY +\eta_z ZZ)) (C\otimes D)`.
+- [optional] Mention Cartan coordinates
 
-The plot
---------
+Khaneja-Glaser decomposition
+----------------------------
 
-Cartan involutions create subalgebras
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Subalgebras create Cartan involutions
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-What happens in horizontal space...
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-So many Cartan subalgebras
-~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Finale: KAK theorem
-~~~~~~~~~~~~~~~~~~~
-
-
-The sequel, but quantum
------------------------
+- Important first recursive decomposition showing universality of single- and two-qubit operations
+- Used for practical decompositions, replaced by other, similar decompositions by now
 
 A recursive decomposition
 ~~~~~~~~~~~~~~~~~~~~~~~~~
 
+- Show recursion on qubit count
+- display resulting decomposition structure
+- Mention that a two-qubit interaction is enough to get the CSA elements
+- Universality
+
 The recursion step in detail
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Local unitaries are universal
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Two substeps in each recursion step:
+    - Algebra/subalgebra :math:`\mathfrak{g}=\mathfrak{su}(2^n) | \mathfrak{k} = \mathfrak{su}(2^{n-1}) \oplus \mathfrak{su}(2^{n-1})`
+    - Involution TBD
+    - CSA TBD
+    - Algebra/subalgebra :math:`\mathfrak{g}=\mathfrak{su}(2^{n-1}) \oplus \mathfrak{su}(2^{n-1}) | \mathfrak{k} = \mathfrak{su}(2^{n-1})`
+    - Involution TBD
+    - CSA TBD
+
+Overview of resulting decomposition
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+- Count blocks
+- [optional] CNOT count
 
 
 Conclusion
@@ -115,19 +140,26 @@ A famous immediate application of this result is the circuit decomposition, or
 parametrization, for arbitrary qubit numbers by Khaneja and Glaser. It also allowed
 us to prove universality of single and two-qubit unitaries for quantum computation.
 
-If you are interested in other applications of Lie theory in the field of 
+If you are interested in other applications of Lie theory in the field of
 quantum computing, you are in luck! It has been a handy tool throughout the last
-decades, e.g., for the simulation and compression of quantum circuits, 
+decades, e.g., for the simulation and compression of quantum circuits, # TODO: REFS
 in quantum optimal control, and for trainability analyses. For Lie algebraic
 classical simulation of quantum circuits, check the
 :doc:`g-sim </demos/tutorial_liesim/>` and
 :doc:`(g+P)-sim </demos/tutorial_liesim_extension/>` demos, and stay posted for
 a brand new demo on compiling Hamiltonian simulation circuits with the KAK theorem!
+
+
+The props
+---------
+
+Adjoint representation
+~~~~~~~~~~~~~~~~~~~~~~
+
 """
 
 import pennylane as qml
 
-######################################################################
 ######################################################################
 #
 # References
