2e4fb3a88c886641edefb03d126840e9aaf946b7
qottmann <korbinian.kottmann@gmail.com>
2024-11-21 18:07:07 +0100

    main body

--- demonstrations/tutorial_fdhs.py
+++ demonstrations/tutorial_fdhs.py
@@ -19,10 +19,22 @@ are left- and right-unitary with :math:`\mu = \min(m, n)`.
 In the case of the KAK decomposition, :math:`\mathcal{A}` is an Abelian subgroup such that all its elements are commuting,
 just as is the case for diagonal matrices.
 
+Goal
+----
+
 Unitary gates in quantum computing are described by the special orthogonal Lie group :math:`SU(2^n)`, so we can use the KAK
 theorem to decompose quantum gates into :math:`U = K_1 A K_2`. While the mathematical statement is rather straight-forward,
 actually finding this decomposition is not. We are going to follow the recipe prescribed in 
 `Fixed Depth Hamiltonian Simulation via Cartan Decomposition <https://arxiv.org/abs/2104.00728>`__ [#Kökcü]_.
+
+In particular, we are going to consider the problem of time-evolving a Hermitian operator :math:`H` the generates the time-evolution unitary :math:`U = e^{-i t H}`.
+We are going to perform a special case of KAK decomposition, a KhK decomposition if you will, on the algebra level of H in terms of
+
+.. math:: H = K^\dagger h_0 K.
+
+This then induces the KAK decomposition on the group level as
+
+.. math:: e^{-i t H} = K^\dagger e^{-i t h_0} K.
 
 Let us walk through an explicit example, doing theory and a concrete example side-by-side.
 For that we are going to use the Heisenberg model generators and Hamiltonian for :math:`n=4` qubits.
@@ -57,9 +69,6 @@ g = [op.pauli_rep for op in g]
 # 
 # Cartan decomposition
 # --------------------
-# 
-# First let us recap the necessary ingredients to mathematically obtain the KAK decomposition.
-# 
 # 
 # A Cartan decomposition is a bipartition :math:`\mathfrak{g} = \mathcal{k} \oplus \mathcal{m}` into a vertical subspace
 # :math:`\mathfrak{k}` and an orthogonal horizontal subspace :math:`\mathfrak{m}`. In practice, it can be induced by an
@@ -113,23 +122,247 @@ def cartan_decomposition(g, involution):
     return k, m
 
 k, m = cartan_decomposition(g, even_odd_involution)
-len(k), len(m)
+len(g), len(k), len(m)
 
 
 ##############################################################################
+# We have successfully decomposed the :math:`60`-dimensional Lie algebra 
+# into a :math:`24`-dimensional vertical subspace and a :math:`36`-dimensional subspace.
+#
+# Note that not every bipartition of a Lie algebra constitutes a Cartan decomposition.
+# For that, the subspaces need to fulfil the following three commutation relations
+#
+# .. math::
+#     \begin{align}
+#     [\mathfrak{k}, \mathfrak{k}] \subseteq \mathfrak{k} & \text{ (subalgebra)}\\
+#     [\mathfrak{k}, \mathfrak{m}] \subseteq \mathfrak{m} & \text{ (reductive property)}\\
+#     [\mathfrak{m}, \mathfrak{m}] \subseteq \mathfrak{k} & \text{ (symmetric property)}\\
+#
+# In particular, :math:`\mathfrak{k}` is a subalgebra of :math:`\mathfrak{g}`, whereas :math:`\mathfrak{m}` is not.
+# This also has the consequence that the associated Lie group :math:`\mathcal{K} := e^{i \mathfrak{k}}` is a subgroup
+# of the associated Lie group :math:`\mathcal{G} := e^{i \mathfrak{g}}`.
+#
+# Cartan subalgebra
+# -----------------
 # 
+# With this we have identified the first subgroup (:math:`\mathcal{K}`) of the KAK decomposition. The other subgroup
+# is induced by the so-called (horizontal) Cartan subalgebra :math:`\mathfrak{a}`. This is a maximal Abelian subalgebra of :math:`\mathfrak{m}`, i.e. it is not unique.
+# For the case of Pauli words we can simply pick any element in :math:`\mathfrak{m}` and collect all other operators in :math:`\mathfrak{m}`
+# that commute with it.
+#
+# We then obtain a further split of the vector space :math:`\mathfrak{m} = \tilde{\mathfrak{m}} \oplus \mathfrak{h}`,
+# where :math:`\tilde{\mathfrak{m}}` is just the remainder of :math:`\mathfrak{m}`.
+
+def _commutes_with_all(candidate, ops):
+    """Check if ``candidate commutes with all ``ops``"""
+    for op in ops:
+        com = candidate.commutator(op)
+        com.simplify()
+        
+        if not len(com) == 0:
+            return False
+    return True
+
+def cartan_subalgebra(m, which=0):
+    """Compute the Cartan subalgebra from the odd parity space :math:`\mathfrak{m}` of the Cartan decomposition
+
+    This implementation is specific for cases of bases of m with pure Pauli words as detailed in Appendix C in `2104.00728 <https://arxiv.org/abs/2104.00728>`__.
+    
+    Args:
+        m (List[PauliSentence]): the odd parity subspace :math:`\Theta(\mathfrak{m}) = \mathfrak{m}
+        which (int): Choice for initial element of m from which to construct the maximal Abelian subalgebra
+    
+    Returns:
+        mtilde (List): remaining elements of :math:`\mathfrak{m}` s.t. :math:`\mathfrak{m} = \tilde{\mathfrak{m}} \oplus \mathfrak{h}`.
+        a (List): Cartan subalgebra
+
+    """
+
+    h = [m[which]] # first candidate
+    mtilde = m.copy()
+
+    for m_i in m:
+        if _commutes_with_all(m_i, h):
+            if m_i not in h:
+                h.append(m_i)
+    
+    for h_i in h:
+        mtilde.remove(h_i)
+    
+    return mtilde, h
+
+mtilde, h = cartan_subalgebra(m)
+len(g), len(k), len(mtilde), len(h)
+
+##############################################################################
+# We now have the Cartan decomposition :math:`\mathfrak{g} = \mathfrak{k} \oplus \tilde{\mathfrak{m}} \oplus \mathfrak{h}``
+# and with that all the necessary ingredients for the KAK decomposition.
+# 
+# Variational KhK
+# ---------------
+#
+# Obtaining the actual decomposition is highly non-trivial and there is no canonical way to go about computing it in terms of linear algebra sub-routines.
+# In [#Kökcü]_, the authors propose to find the extrema of the cost function
+# 
+# .. math:: f(\theta) = \langle K(\theta) v K(\theta)^\dagger, H\rangle
+# 
+# where :math:`\langle \cdot, \cdot \rangle` is some inner product (in our case the trace inner product).
+# This construction uses the operator :math:`v = \sum_j \pi^j h_j \in \mathcal{h}`
+# that is such that :math:`e^{i t v}` is dense in :math:`e^{i \mathcal{h}}`.
+# The latter means that for any point in :math:`e^{i \mathcal{h}}` there is a :math:`t` such that :math:`e^{i t v}` reaches it.
+# Let us construct it.
+
+gammas = [np.pi**i for i in range(len(h))]
+
+v = qml.dot(gammas, h)
+v_m = qml.matrix(v, wire_order=range(n_wires))
+v_m = jnp.array(v_m)
 
 ##############################################################################
 # 
+# This procedure has the advantage that we can use an already decomposed ansatz
+# 
+# .. math:: K(\theta) = \prod_j e^{-i \theta_j k_j}
+# 
+# for the vertical unitary.
+# 
+# Now we just have to define the cost function and find an extremum.
+# In this case we are going to just use gradient descent to minimize the cost function to a minimum.
+# We are going to use ``jax`` and ``optax`` and write some boilerplate for the optimization procedure.
+
+def run_opt(
+    value_and_grad,
+    theta,
+    n_epochs=500,
+    lr=0.1,
+):
+    """Boilerplate jax optimization"""
+    optimizer = optax.adam(learning_rate=lr)
+    opt_state = optimizer.init(theta)
+
+    energy = []
+    gradients = []
+    thetas = []
+
+    @jax.jit
+    def step(opt_state, theta):
+        val, grad_circuit = value_and_grad(theta)
+        updates, opt_state = optimizer.update(grad_circuit, opt_state)
+        theta = optax.apply_updates(theta, updates)
+
+        return opt_state, theta, val
+
+    t0 = datetime.now()
+    ## Optimization loop
+    for _ in range(n_epochs):
+        opt_state, theta, val = step(opt_state, theta)
+
+        energy.append(val)
+        thetas.append(theta)
+        
+    t1 = datetime.now()
+    print(f"final loss: {val}; min loss: {np.min(energy)}; after {t1 - t0}")
+
+    return thetas, energy, gradients
+
 
 ##############################################################################
-# 
+# We can now implement the cost function and find a minimum via gradient descent.
+
+H_m = qml.matrix(H, wire_order=range(n_wires))
+H_m = jnp.array(H_m)
+
+def K(theta, k):
+    for th, k_j in zip(theta, k):
+        qml.exp(-1j * th * k_j.operation())
+
+@jax.jit
+@jax.value_and_grad
+def loss(theta):
+    K_m = qml.matrix(K, wire_order=range(n_wires))(theta, k)
+    A = K_m @ v_m @ K_m.conj().T
+    return jnp.trace(A.conj().T @ H_m).real
+
+theta0 = jnp.ones(len(k), dtype=float)
+
+thetas, energy, _ = run_opt(loss, theta0, n_epochs=500)
+plt.plot(energy)
+plt.xlabel("epochs")
+plt.ylabel("cost")
 
 ##############################################################################
-# 
+# This gives us the optimal values of the parameters :math:`\theta_\text{opt}` of :math:`K(\theta_\text{opt}) =: K_c`.
+
+theta_opt = thetas[-1]
+Kc_m = qml.matrix(K, wire_order=range(n_wires))(theta_opt, k)
 
 ##############################################################################
+# The special element :math:`h_0` from the Cartan subalgebra :math:`\mathfrak{h}` is given by
+# rotating the Hamiltonian by the critical :math:`K_c`.
 # 
+# .. math:: h_0 = K_c H K_c^\dagger.
+
+h_0_m = Kc_m @ H_m @ Kc_m.conj().T
+h_0 = qml.pauli_decompose(h_0_m)
+a_vspace = qml.pauli.PauliVSpace(h)
+not a_vspace.is_independent(h_0.pauli_rep)
+
+##############################################################################
+#
+# This (trivially) gives us the KhK decomposition of :math:`H`,
+# 
+# .. math:: H = K_c^\dagger h_0 K_c
+# 
+# This trivially reproduces the original Hamiltonian.
+#
+
+H_re = Kc_m.conj().T @ h_0_m @ Kc_m
+np.allclose(H_re, H_m)
+
+##############################################################################
+# We can now check if the Hamiltonian evolution is reproduced correctly.
+#
+
+t = 1.
+U_exact = qml.exp(-1j * t * H)
+U_exact_m = qml.matrix(U_exact, wire_order=range(n_wires))
+
+def U_kak(theta_opt, t):
+    K(theta_opt, k)
+    qml.exp(-1j * t * h_0)
+    qml.adjoint(K)(theta_opt, k)
+
+U_kak_m = qml.matrix(U_kak, wire_order=range(n_wires))(theta_opt, t)
+
+def trace_distance(A, B):
+    return 1 - np.abs(np.trace(A.conj().T @ B))/len(A)
+
+trace_distance(U_exact_m, U_kak_m)
+
+
+
+
+
+
+##############################################################################
+# Indeed we find that the KAK decomposition that we found reproduces the unitary evolution operator.
+# Note that this is valid for arbitrary :math:`t`, such that in that sense the Hamiltonian simulation operator has a fixed depth.
+
+##############################################################################
+# Comparison with Trotter products
+# --------------------------------
+# 
+# We can compare this with the standard method for computing time evolution operators, Suzuki-Trotter products (see :class:`TrotterProduct`).
+#
+
+
+##############################################################################
+#
+#
+
+
+##############################################################################
+#
 #
 
 
@@ -138,11 +371,7 @@ len(k), len(m)
 # Conclusion
 # ----------
 #
-# With this introduction, we hope to clarify some terminology, introduce the basic concepts of Lie theory and motivate their relevance in quantum physics by touching on universality and symmetries.
-# While Lie theory and symmetries are playing a central role in established fields such as quantum phase transitions (see note above) and `high energy physics <https://en.wikipedia.org/wiki/Standard_Model>`_,
-# they have recently also emerged in quantum machine learning with the onset of geometric quantum machine learning [#Meyer]_ [#Nguyen]_
-# (see our recent :doc:`introduction to geometric quantum machine learning <tutorial_geometric_qml>`).
-# Further, DLAs have recently become instrumental in classifying criteria for barren plateaus [#Fontana]_ [#Ragone]_ and designing simulators based on them [#Goh]_.
+# many good
 #
 
 
