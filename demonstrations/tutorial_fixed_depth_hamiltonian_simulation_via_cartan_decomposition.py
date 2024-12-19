0241befa5da17649e79cc32787c08e9fd8960d02
qottmann <korbinian.kottmann@gmail.com>
2024-12-19 13:42:41 +0100

    intro mention fast-forwarding

--- demonstrations/tutorial_fixed_depth_hamiltonian_simulation_via_cartan_decomposition.py
+++ demonstrations/tutorial_fixed_depth_hamiltonian_simulation_via_cartan_decomposition.py
@@ -6,9 +6,11 @@ that lets you perform time-evolution by arbitrary times with fixed depth, :math:
 In particular, we follow the approach in [#Kökcü]_ that directly provides us with a (fixed-depth) circuit
 decomposition of the unitaries :math:`K` and :math:`e^{-i t h}.`
 
-Sounds too good to be true? There are of course caveats, mostly of practical nature.
-One is that the relevant Lie algebra quickly becomes too large to handle. Yet, this is still an extremely
-powerful mathematical result integral for quantum compilation, circuit optimization, and Hamiltonian simulation.
+Sounds too good to be true? There are of course caveats as this constitutes a fast-forwarding.
+In this algebraic setting, fast-forwarding is prohibited whenever the relevant :doc:`dynamical Lie algebra </demos/tutorial_liealgebra>` (DLA)
+becomes too large to handle, which is often the case as most systems yield an exponentially large DLA.
+Yet, this is still an extremely powerful mathematical result integral for quantum compilation,
+circuit optimization, and Hamiltonian simulation.
 
 .. figure:: ../_static/demo_thumbnails/opengraph_demo_thumbnails/OGthumbnail_fixed_depth_hamiltonian_simulation_via_cartan_decomposition.png
     :align: center
