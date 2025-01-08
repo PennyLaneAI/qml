f967f1b6f7d001c354dc2c878d07cdb66bc05ef2
JerryChen97 <chenys13@outlook.com>
2025-01-08 12:15:41 -0500

    try fixing warning

--- demonstrations/qnspsa.py
+++ demonstrations/qnspsa.py
@@ -916,11 +916,11 @@ for i in range(300):
 # QN-SPSA in Fig. 1(b) of reference [#Gacon2021]_ with the following job. 
 #
 # .. warning:: 
-# The following code is incomplete and might require additional attention. 
-# You can find a more detailed version of the example in this
-# `notebook <https://github.com/aws/amazon-braket-examples/blob/main/examples/hybrid_jobs/6_QNSPSA_optimizer_with_embedded_simulator/qnspsa_with_embedded_simulator.ipynb>`__,
-# with its dependencies in the `source_scripts`
-# `folder <https://github.com/aws/amazon-braket-examples/blob/main/examples/hybrid_jobs/6_QNSPSA_optimizer_with_embedded_simulator/source_scripts/>`__.
+#   The following code is incomplete and might require additional attention. 
+#   You can find a more detailed version of the example in this
+#   `notebook <https://github.com/aws/amazon-braket-examples/blob/main/examples/hybrid_jobs/6_QNSPSA_optimizer_with_embedded_simulator/qnspsa_with_embedded_simulator.ipynb>`__,
+#   with its dependencies in the `source_scripts`
+#   `folder <https://github.com/aws/amazon-braket-examples/blob/main/examples/hybrid_jobs/6_QNSPSA_optimizer_with_embedded_simulator/source_scripts/>`__.
 #
 # .. note::
 #     In order for the remainder of this demo to work, you will need to have done 3 things:
