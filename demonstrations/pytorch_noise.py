10b0e5806328f2a443355f5ba34dfe4363fb98af
Pietropaolo Frisoni <pietropaolo.frisoni@xanadu.ai>
2025-01-10 10:34:58 -0500

    QA demo process for PL 0.40 during feature freeze (#1291)
    As the title says.
    
    ---------
    
    Co-authored-by: Isaac De Vlugt <isaacdevlugt@gmail.com>
    Co-authored-by: JerryChen97 <chenys13@outlook.com>
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>
    Co-authored-by: lillian542 <38584660+lillian542@users.noreply.github.com>
    Co-authored-by: Alex Preciado <alex.preciado@xanadu.ai>

--- demonstrations/pytorch_noise.py
+++ demonstrations/pytorch_noise.py
@@ -15,6 +15,10 @@ PyTorch and noisy devices
 
 *Author: Josh Izaac — Posted: 11 October 2019. Last updated: 9 November 2022.*
 
+.. warning::
+    This demo is only compatible with PennyLane v0.40 or below and Braket v1.31.0. To use Rigetti hardware with newer versions of PennyLane please use the `PennyLane-Braket plugin <https://amazon-braket-pennylane-plugin-python.readthedocs.io/en/stable/index.html>`__ instead.
+    
+
 Let's revisit the original :ref:`qubit rotation <qubit_rotation>` tutorial, but instead of
 using the default NumPy/autograd QNode interface, we'll use the :doc:`introduction/interfaces/torch`.
 We'll also replace the ``default.qubit`` device with a noisy ``rigetti.qvm``
