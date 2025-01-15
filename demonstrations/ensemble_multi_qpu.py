5f3c0efe692cc871f100dfff93686e41c64bdba2
Pietropaolo Frisoni <pietropaolo.frisoni@xanadu.ai>
2025-01-14 19:06:21 -0500

    Merge `dev` into `master` (#1302)
    Co-authored-by: GitHub Nightly Merge Action <actions@github.com>
    Co-authored-by: Andrija Paurevic <46359773+andrijapau@users.noreply.github.com>
    Co-authored-by: Yushao Chen (Jerry) <chenys13@outlook.com>
    Co-authored-by: Soran Jahangiri <40344468+soranjh@users.noreply.github.com>
    Co-authored-by: Guillermo Alonso-Linaje <65235481+KetpuntoG@users.noreply.github.com>
    Co-authored-by: Austin Huang <65315367+austingmhuang@users.noreply.github.com>
    Co-authored-by: Mudit Pandey <mudit.pandey@xanadu.ai>
    Co-authored-by: Mikhail Andrenkov <mikhail@xanadu.ai>
    Co-authored-by: David Wierichs <davidwierichs@gmail.com>
    Co-authored-by: Korbinian Kottmann <43949391+Qottmann@users.noreply.github.com>
    Co-authored-by: Ivana Kurečić <ivana@xanadu.ai>
    Co-authored-by: Paul Finlay <50180049+doctorperceptron@users.noreply.github.com>
    Co-authored-by: Jack Brown <jack.brown486@gmail.com>
    Co-authored-by: bellekaplan <167111433+bellekaplan@users.noreply.github.com>
    Co-authored-by: soranjh <soran.jahangiri@gmail.com>
    Co-authored-by: obliviateandsurrender <utkarshazad98@gmail.com>
    Co-authored-by: Isaac De Vlugt <isaacdevlugt@gmail.com>
    Co-authored-by: Isaac De Vlugt <34751083+isaacdevlugt@users.noreply.github.com>
    Co-authored-by: lillian542 <38584660+lillian542@users.noreply.github.com>
    Co-authored-by: Alex Preciado <alex.preciado@xanadu.ai>
    Co-authored-by: Christina Lee <chrissie.c.l@gmail.com>
    Co-authored-by: Astral Cai <astral.cai@xanadu.ai>

--- demonstrations/ensemble_multi_qpu.py
+++ demonstrations/ensemble_multi_qpu.py
@@ -18,10 +18,10 @@ This tutorial outlines how two QPUs can be combined in parallel to help solve a 
 classification problem.
 
 .. warning::
-    This demo does not work with the latest version of Qiskit or the Pennylane-Qiskit plugin.
-    It is compatible with ``qiskit==0.46`` and ``pennylane-qiskit==0.35.1``. Older versions of
-    Qiskit and the Pennylane-Qiskit plugin should not be installed in environments with an
-    existing installation of Qiskit 1.0 or above.
+    This demo does not work with the latest version of Qiskit or the Pennylane-Qiskit plugin and
+    is compatible with Python versions 3.10 and lower. It is compatible with ``qiskit==0.46`` and 
+    ``pennylane-qiskit==0.35.1``. Older versions of Qiskit and the Pennylane-Qiskit plugin should 
+    not be installed in environments with an existing installation of Qiskit 1.0 or above.
 
 We use the ``rigetti.qvm`` device to simulate one QPU and the ``qiskit.aer`` device to
 simulate another. Each QPU makes an independent prediction, and an ensemble model is
@@ -198,9 +198,9 @@ devs = [dev0, dev1]
 ##############################################################################
 # .. note::
 #    If you have access to Rigetti hardware, you can swap out ``rigetti.qvm`` for ``rigetti.qpu``
-#    and specify the hardware device to run on. Users with access to the IBM Q Experience can
-#    swap ``qiskit.aer`` for ``qiskit.ibmq`` and specify their chosen backend (see `here
-#    <https://docs.pennylane.ai/projects/qiskit/en/latest/devices/ibmq.html>`__).
+#    and specify the hardware device to run on. Users with access to the IBM hardware can
+#    swap ``qiskit.aer`` for ``qiskit.remote`` and specify their chosen backend (see `here
+#    <https://docs.pennylane.ai/projects/qiskit/en/latest/devices/remote.html>`__).
 #
 #
 # The circuits for both QPUs are shown in the figure below:
