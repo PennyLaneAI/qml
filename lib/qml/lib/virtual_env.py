c51c68efe2ed2515d6e6c098a8e945a338b1c006
Jack Brown <jack@xanadu.ai>
2025-01-16 13:00:04 -0500

    docs

--- lib/qml/lib/virtual_env.py
+++ lib/qml/lib/virtual_env.py
@@ -4,17 +4,27 @@ import sys
 
 
 class Virtualenv:
+    """Interface to a Python virtual environment."""
+
     def __init__(self, path: Path):
+        """
+        Get python virtual env, creating it if it does not exist.
+        Args:
+            path: Path to virtual env directory. Will be initialized
+                if it does not exist.
+        """
         self.path = path.resolve()
 
         if not self.python.exists():
-            self.init()
+            self._init()
 
     @property
     def python(self) -> Path:
+        """Path to the python executable in this virtual env."""
         return self.path / "bin" / "python"
 
-    def init(self):
+    def _init(self):
+        """Initialize a virtual environment."""
         self.path.parent.mkdir(exist_ok=True)
 
         subprocess.run([sys.executable, "-m", "venv", self.path]).check_returncode()
