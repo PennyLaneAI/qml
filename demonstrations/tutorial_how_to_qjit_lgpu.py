fec90401ad077950f9c9590e30140f7a2b8ab82d
Ali Asadi <10773383+maliasadi@users.noreply.github.com>
2025-02-19 13:30:00 -0500

    Update authors

--- demonstrations/tutorial_how_to_qjit_lgpu.py
+++ demonstrations/tutorial_how_to_qjit_lgpu.py
@@ -2,19 +2,19 @@ r"""
 How to use Catalyst with Lightning-GPU
 =======================================
 
-Catalyst is a quantum just-in-time (QJIT) compiler framework that optimizes
-and compiles quantum programs for efficient execution.
-Lightning-GPU is a CUDA-backed state-vector simulator that enables
-fast quantum circuit simulations on NVIDIA GPUs.
+`Catalyst <https://docs.pennylane.ai/projects/catalyst>`__
+is a quantum just-in-time (QJIT) compiler framework that optimizes
+and compiles hybrid (quantum and classical) PennyLane's programs.
+`Lightning-GPU <https://docs.pennylane.ai/projects/lightning/en/stable/lightning_gpu/device.html>`__
+is a CUDA-backed state-vector simulator that enables fast quantum circuit simulations on NVIDIA GPUs.
+
+Using Lightning-GPU is particularly beneficial when dealing with large quantum circuits or
+when running simulations that require significant computational resources.
+By offloading the heavy linear algebra operations to GPUs,
+we can achieve substantial performance improvements.
+
 Here, we will demonstrate how to leverage the power of Catalyst
 and Lightning-GPU to accelerate quantum simulations.
-
-For more information on Catalyst, refer to the
-`Catalyst documentation <https://docs.pennylane.ai/projects/catalyst>`__.
-
-To learn more about Lightning-GPU, visit the
-`Lightning-GPU documentation <https://docs.pennylane.ai/projects/lightning/en/stable/lightning_gpu/device.html>`__.
-
 
 .. figure:: ../_static/demo_thumbnails/large_demo_thumbnails/thumbnail_large_Lightning_GPU_Catalyst.png
      :align: center
@@ -22,10 +22,6 @@ To learn more about Lightning-GPU, visit the
      :target: javascript:void(0)
      :alt: How to use Catalyst with Lightning-GPU
 
-Using ``lightning.gpu`` is particularly beneficial when dealing with large quantum circuits or
-when running simulations that require significant computational resources.
-By offloading the heavy linear algebra operations to GPUs,
-we can achieve substantial performance improvements.
 
 Let's get started with setting up the environment and running a simple example
 to highlight Catalyst with Lightning-GPU features.
@@ -33,11 +29,11 @@ to highlight Catalyst with Lightning-GPU features.
 Setting up your environment
 ----------------------------
 
-Lightning-GPU can be used on systems equipped with NVIDIA GPUs that support CUDA,
+Lightning-GPU can be used on systems equipped with NVIDIA GPUs of generation SM 7.0 (Volta) and greater,
 specifically, it is optimized for Linux X86-64 or ARM64 machines with CUDA-12 installed.
 
 To bring the power of Lightning-GPU to Catalyst, you need to install PennyLane,
-the :func:`~.qjit` compiler framework, and the ``lightning.gpu`` plugin.
+the :func:`~.qjit` compiler framework, and the ``lightning.gpu`` device.
 These packages can be installed from the Python Package Index (PyPI) using the following command:
 
 .. code-block:: bash
@@ -59,7 +55,8 @@ We then define a quantum circuit that applies layers of :func:`RZ` and :func:`RY
 rotations to each wire, and returns the expectation value as the measurement result.
 
 To compile the circuit with Catalyst, we use the :func:`~.qjit` decorator.
-We also use the ``autograph=True`` option to compile the circuit including the for-loop,
+The `autograph=True <https://docs.pennylane.ai/projects/catalyst/en/stable/dev/autograph.html>`__
+compilation option compiles the circuit with for-loops,
 this allows Catalyst to capture control-flow operations without unrolling,
 reduces the compilation time and generates a more efficient program.
 
@@ -99,21 +96,18 @@ reduces the compilation time and generates a more efficient program.
 
     1.7712995142661776
 
-As quantum simulation is most often expressed in the language
-of linear algebra to apply gates to the state-vector memory buffer,
-extract measurement results, or calculate the circuit differentiation,
-we can leverage GPUs to scale the heavy-lifting parts of the simulation.
-
-``lightning.gpu`` has feature parity with both ``lightning.qubit`` and ``lightning.kokkos``,
+Lightning-GPU has feature parity with
+`other Lightning state-vector simulators <https://docs.pennylane.ai/projects/lightning/en/stable>`__,
 providing native support for many PennyLane's operations and measurement processes with
 a fast adjoint-Jacobian differentiation implementation.
+
+In the next section, we demonstrate how to compute the gradient of your quantum programs
+using the native adjoint-Jacobian method on this device.
+
 
 
 Computing gradients
 --------------------
-
-Let's demonstrate how to compute the gradient of your quantum programs
-using the native adjoint-Jacobian method in Lightning-GPU.
 
 We use :func:`~.grad` to compute the gradient of the ``circuit`` with respect to ``weights``.
 This method is particularly useful for variational quantum algorithms
@@ -143,7 +137,7 @@ documentation.
 
 If you haven't used Catalyst, we hope this demonstration has encouraged you to check out the
 `Catalyst Quick Start <https://docs.pennylane.ai/projects/catalyst/en/stable/dev/quick_start.html>`__ guide
-and use it to compile and optimize your quantum-classical programs.
+and use it with our fast GPU-enabled state-vector simulator to compile, optimize and execute your hybrid programs.
 
 About the authors
 -----------------
