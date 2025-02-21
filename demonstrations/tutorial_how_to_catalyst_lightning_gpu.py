3b1281f38216506420d7d661ca49afaca9cf8c5e
Ali Asadi <10773383+maliasadi@users.noreply.github.com>
2025-02-20 22:18:16 -0500

    Update Precis

--- demonstrations/tutorial_how_to_catalyst_lightning_gpu.py
+++ demonstrations/tutorial_how_to_catalyst_lightning_gpu.py
@@ -42,7 +42,7 @@ To use Catalyst with Lightning-GPU, you need to install PennyLane,
 the :func:`~pennylane.qjit` compiler framework, and the Lightning-GPU plugin (``lightning.gpu``).
 These packages can be installed from the Python Package Index (PyPI) using the following command:
 
-.. code-block:: bash
+.. code-block:: none
 
     pip install pennylane pennylane-catalyst pennylane-lightning-gpu
 
@@ -52,14 +52,14 @@ Detailed instructions for source installation can be found
 in the respective `documentation <https://pennylane.ai/install>`__ of each package.
 
 
-A simple circuit
------------------
+Compile and run a circuit
+--------------------------
 
 Here, we use a simple PennyLane circuit to demonstrate
 how to compile and run a quantum program on Lightning-GPU with Catalyst.
 
 First, we create a ``lightning.gpu`` device with 28 qubits using the :func:`~pennylane.device` function.
-We then define a quantum circuit that applies layers of :func:`~.RZ` and :func:`~.RY`
+We then define a quantum circuit that applies layers of :func:`~pennylane.RZ` and :func:`~pennylane.RY`
 rotations to each wire and returns the expectation value as the measurement result.
 
 To compile the circuit with Catalyst, we use the :func:`~pennylane.qjit` decorator.
@@ -100,7 +100,7 @@ reduce the compilation time, and generate a more efficient program.
 
     >>> circuit(weights)
 
-.. code-block:: bash
+.. code-block:: none
 
     1.7712995142661776
 
@@ -116,7 +116,7 @@ using the native adjoint-Jacobian method on this device.
 Compute gradients
 ------------------
 
-We use :func:`~.grad` to compute the gradient of the ``circuit`` with respect to
+We use :func:`~pennylane.grad` to compute the gradient of the ``circuit`` with respect to
 a set of trainable arguments.
 This method is particularly useful for variational quantum algorithms
 where the gradient of the cost function with respect to the circuit parameters is required.
@@ -132,32 +132,18 @@ for more information on compiling and optimizing Variational Quantum Eigensolver
 
     >>> workflow(weights)
 
-.. code-block:: bash
+.. code-block:: none
 
-    [ 8.8817842e-16 -1.0920765e-01  6.6613381e-16  6.6613381e-16
-      2.2204460e-16  2.2204460e-16  2.2204460e-16  5.5511151e-16
-      1.1102230e-16  6.6613381e-16 -4.4408921e-16 -2.2204460e-16
-      2.2204460e-16  0.0000000e+00 -1.1102230e-16  5.5511151e-16
-      3.3306691e-16 -2.2204460e-16 -1.1102230e-16 -2.2204460e-16
-      1.1102230e-16  0.0000000e+00 -3.3306691e-16  4.4408921e-16
-      -3.3306691e-16  1.1102230e-16 -1.1102230e-16  0.0000000e+00
-      -1.1102230e-16  2.2204460e-16 -1.1102230e-16 -2.2204460e-16
-      5.5511151e-16  0.0000000e+00 -3.3306691e-16  0.0000000e+00
-      2.2204460e-16  0.0000000e+00 -1.1102230e-16  0.0000000e+00
-      0.0000000e+00 -4.4408921e-16  0.0000000e+00 -1.1102230e-16
-      3.3306691e-16 -1.1102230e-16  1.1102230e-16  1.1102230e-16
-      2.2204460e-16  3.3306691e-16  2.2204460e-16  1.1102230e-16
-      -1.1102230e-16  1.1102230e-16  1.1102230e-16  3.3306691e-16
-      0.0000000e+00 -1.1102230e-16 -6.2915415e-01  0.0000000e+00]
+    [ 8.8817842e-16 ... -6.2915415e-01  0.0000000e+00]
+
+Optimize parameters
+--------------------
 
 Note that in the above example, we didn't use ``method="adjoint"``.
 The adjoint-Jacobian is the default gradient method when you compile
 and run your program on Lightning-GPU.
-You can learn more about the different gradient support by checking the
-`Catalyst Quantum Gradients <https://docs.pennylane.ai/projects/catalyst/en/stable/dev/quick_start.html#calculating-quantum-gradients>`__
-documentation.
 
-Now that we have seen how to use the device gradient method,
+Now that we have seen how to leverage the device differentiation capabilities,
 we can move on to further optimizing the circuit parameters using Catalyst.
 We follow the same steps as in
 `the VQE demo <https://pennylane.ai/qml/demos/tutorial_how_to_quantum_just_in_time_compile_vqe_catalyst>`__,
@@ -183,10 +169,9 @@ to QJIT-compile the entire optimization workflow.
         (params, opt_state) = qml.for_loop(0, 10, 1)(update_step)(params, opt_state)
         return params
 
-    >>> final_params = optimization(weights)
-    >>> print(f"Final angle parameters: {final_params}")
+    >>> optimization(weights)
 
-.. code-block:: bash
+.. code-block:: none
 
     Step = 0
     Step = 1
@@ -198,21 +183,7 @@ to QJIT-compile the entire optimization workflow.
     Step = 7
     Step = 8
     Step = 9
-    Final angle parameters: [1.24479175e-01 2.22040820e+00 2.36939192e-01 9.45755959e-01
-      1.59472704e-01 8.45545650e-01 1.32652521e-02 3.71306539e-01
-      3.42241764e-01 6.25356674e-01 7.08195210e-01 1.62913680e-01
-      2.94586539e-01 1.94847584e-03 5.49157500e-01 8.47560406e-01
-      9.25927997e-01 5.43066740e-01 4.00609255e-01 8.86821628e-01
-      9.50046659e-01 5.18365145e-01 6.38097048e-01 6.67127371e-02
-      6.42662406e-01 3.17808628e-01 2.13315248e-01 2.62881398e-01
-      2.57341623e-01 7.23513365e-01 6.26209974e-02 1.64659262e-01
-      2.68713236e-02 1.52902603e-01 2.55607367e-01 3.54435444e-02
-      3.17132115e-01 3.33583951e-01 9.48547006e-01 9.31932330e-01
-      7.38109469e-01 2.07342863e-01 1.35567427e-01 5.76237440e-01
-      5.13184071e-02 8.20117712e-01 5.33855081e-01 8.53034496e-01
-      9.65461254e-01 9.17515278e-01 3.34429860e-01 9.56996560e-01
-      9.54037666e-01 9.20464039e-01 5.59616089e-01 4.27830935e-01
-      9.16242123e-01 4.42039609e-01 3.07716966e+00 4.64060426e-01]
+    [1.24479175e-01 ... 9.45755959e-01 4.64060426e-01]
 
 We used `Optax <https://github.com/google-deepmind/optax>`__,
 a library designed for optimization using JAX,
@@ -229,8 +200,8 @@ We have compiled a simple quantum circuit with Catalyst,
 computed the gradient of the circuit using the adjoint-Jacobian method,
 and optimized the circuit parameters using Catalyst and Optax.
 
-To learn more about Catalyst and all the integrated devices,
-check out the :doc:`Catalyst Quick Start <catalyst:dev/quick_start>` guide.
+To learn more about all the integrated devices,
+check out the :doc:`Catalyst Devices <catalyst:dev/devices>` guide.
 
 About the authors
 -----------------
