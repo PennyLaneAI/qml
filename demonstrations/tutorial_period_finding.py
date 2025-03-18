f73714af15f3d43aa3bf6c190ce7c8acbc7bb4b0
Maria Schuld <mariaschuld@gmail.com>
2025-03-18 12:10:18 +0200

    some more fixes

--- demonstrations/tutorial_period_finding.py
+++ demonstrations/tutorial_period_finding.py
@@ -10,7 +10,7 @@ While trends may have moved on, the idea of extracting group structure from the 
     :align: center
     :width: 75%
 
-However, group theory is a huge hurdle for even some of the more seasoned quantum enthusiasts. This demo wants to give a glimpse of what this "Abelian structure" is all about. Luckily, the fruit-fly example of a hidden subgroup problem is just the task of finding the period of a integer-valued function - something one can appreciate without much group jargon. A fantastic resource to read up on the basics is the review of hidden subgroup problems by Childs & Dam (2010) [#Childs2010]_.
+However, group theory is a huge hurdle for even some of the more seasoned quantum enthusiasts. This demo wants to give a glimpse of what this "Abelian structure" is all about. Luckily, the fruit-fly example of a hidden subgroup problem is just the task of finding the period of a integer-valued function - something one can appreciate without much group jargon. A fantastic resource to read up on the basics is the review of hidden subgroup problems by Childs & van Dam (2010) [#Childs2010]_.
 """
 
 #####################################################################
@@ -25,11 +25,11 @@ However, group theory is a huge hurdle for even some of the more seasoned quantu
 #
 # .. figure:: ../_static/demonstration_assets/period_finding/periodic_function.png
 #    :align: center
-#    :width: 90%
+#    :width: 70%
 #
 # Importantly, we assume that we have *black-box access* to that function. For a python coder this 
 # abstract technical term is actually quite intuitive: imagine some part of your code returns a python function ``f`` 
-# that you can call by using integers :math:`x \in \{\0,..,11\}` as arguments, like ``f(2)``. However, 
+# that you can call by using integers :math:`x \in \{0,..,11\}` as arguments, like ``f(2)``. However, 
 # you have no knowledge of the definition ``def f(x)`` --- your only way to
 # learn about the function is to evaluate it at every point. We're ultimately interested in cases
 # where there are too many x-values to evaluate each function value and recover the period in a
@@ -38,19 +38,19 @@ However, group theory is a huge hurdle for even some of the more seasoned quantu
 # What does period finding have to do with groups? Well, in the language of group theory, the
 # integers from 0 to 11 (together with an appropriate operation, like addition modulo 12) form a
 # so-called *cyclic group*, which is an example of *Abelian* groups that Aaronson referred to above. The
-# values :math:`\{0,4,8\}` form a *subgroup* that is "generated" by the period 4. Finding the period
+# values {0,4,8} form a *subgroup* that is "generated" by the period 4. Finding the period
 # means to find the subgroup. The function is said to "hide" the subgroup, since it has the same
 # value on all its elements, effectively labeling them. It is also constant on the *cosets* of
-# the subgroups, which are shifted sets of the form :math:`\{0+s, 4+s, 8+s\}` (where :math:`s` is an
+# the subgroups, which are shifted sets of the form {0+s, 4+s, 8+s} (where s is an
 # element of the group, or a number between 0 and 11). 
 #
 # .. figure:: ../_static/demonstration_assets/period_finding/periodic_function_groups.png
 #    :align: center
-#    :width: 90%
+#    :width: 70%
 #
 # This jargon hopefully does not scare you, but should illustrate that
 # period finding --- and the algorithm we will have a look at here --- can be generalised to other groups and
-# subgroups. It can even be used for some instances of non-Abelian groups.
+# subgroups. It can even be efficiently run for some instances of non-Abelian groups.
 #
 # The quantum algorithm to find the period of the function above is really simple. Encode the
 # function into a quantum state of the form :math:`\sum_x |x \rangle |f(x) \rangle`, apply the quantum
@@ -58,12 +58,12 @@ However, group theory is a huge hurdle for even some of the more seasoned quantu
 # processing: The state we measure, written as an integer, is a multiple of the number of periods
 # that "fit" into the x-domain. The period is then the number of integers divided by the number
 # of periods. In the example above, the quantum algorithm would return random bistrings that can be
-# interpreted as integers :math:`\{0, 3, 6, 9\}`. By sampling only two distinct values, say 6 and
+# interpreted as integers {0, 3, 6, 9}. By sampling only two distinct values, say 6 and
 # 9, we can determine the common denominator as 3, which is the number of periods fitting
 # into 12. From there we can recover 12/3 = 4.
 #
 # We'll now implement this recipe, but use a more convenient cyclic group of 16 elements
-# :math:`\{0,..,15\}`, which can be exactly represented by 4 qubits.
+# {0,..,15}, which can be exactly represented by 4 qubits.
 
 
 #####################################################################
@@ -109,7 +109,7 @@ plt.show()
 # nonzero uniform amplitude at :math:`| 0010 \rangle | 11 \rangle`, but zero amplitudes at the states
 # :math:`| 0010 \rangle | 00 \rangle`, :math:`| 0010 \rangle | 01 \rangle`, :math:`| 0010 \rangle | 10 \rangle`.
 #
-# Since we move between integers and computational basis states, we need these utility conversion functions.
+# Since we move between integers and computational basis states, we need two utility conversion functions.
 
 
 def to_int(binary):
@@ -124,7 +124,7 @@ def to_binary(integer, n):
 # Now we need an oracle that implements the function ``f`` in "quantum parallel". Applied to a
 # superposition of inputs, :math:`\sum_x |x \rangle |0 \rangle`, this unitary prepares the state
 # :math:`\sum_x |x \rangle |f(x) \rangle`. There are many such unitaries and here we'll somewhat hack it 
-# together by defining a unitary matrix that does the job. Of course, in a real application this 
+# together by defining a matrix that does the job. Of course, in a real application this 
 # would be a quantum circuit defined by a sequence of gates.
 
 
@@ -170,7 +170,7 @@ def Oracle(f):
 # measures in the "Fourier basis", which is where all the magic happens.
 #
 # We only have to get two unique samples, from which we can compute the period of the function. For this 
-# reason we define a device with 2 shots. We also add some snapshots to the circuit that we will look at below.
+# reason we define a device with 2 shots. We also add some snapshots to the circuit that we will look at later.
 
 
 dev = qml.device("default.qubit", wires=7, shots=2)
@@ -200,8 +200,8 @@ def circuit():
 samples = circuit()
 
 #####################################################################
-# The classical postprocessing for period finding is relatively simple, while 
-# for more general hidden subgroup problems it requires more algebra. 
+# The classical post-processing is relatively simple for period finding (whereas 
+# for general hidden subgroup problems it requires some less trivial algebra). 
 # If you are curious about the details, you'll find them in
 # Childs & Dam (2013) [#Childs2010]_, Section IVa.
 
@@ -225,9 +225,7 @@ print(f"Hidden period: {result}")
 # Yay, we've found the hidden period! Now, of course this is only impressive if we increase the 
 # number of qubits. We can find the hidden period of a function defined on :math:`2^n` values in time
 # :math:`O(n \mathrm{log}(n))`.
-
-
-#####################################################################
+#
 # A peep at the states involved
 ###############################
 #
@@ -287,9 +285,7 @@ plt.show()
 # Furthermore, the state representing the Fourier spectrum looks actually quite interesting. But
 # the important feature of this state is also clearly visible: Amplitudes concentrate in :math:`x`
 # values that are multiples of 2. And 2 was exactly the amount of periods of 8 that fit into 16.
-
-
-#####################################################################
+#
 # What is the "Abelian structure" exploited by the quantum Fourier transform?
 #############################################################################
 #
