97389c913adb89f2fded3362619533c2e201a00a
JerryChen97 <chenys13@outlook.com>
2025-03-21 11:18:33 -0400

    Add the missing blank line after .. warning::

--- demonstrations_v2/tutorial_qnn_module_tf/demo.py
+++ demonstrations_v2/tutorial_qnn_module_tf/demo.py
@@ -3,6 +3,7 @@ Turning quantum nodes into Keras Layers
 =======================================
 
 .. warning::
+
     This demo is only compatible with PennyLane version 0.40 or below.
     For usage with a later version of PennyLane, please consider using
     :doc:`PyTorch <tutorial_qnn_module_torch>` or :doc:`JAX <tutorial_How_to_optimize_QML_model_using_JAX_and_Optax>`__.
