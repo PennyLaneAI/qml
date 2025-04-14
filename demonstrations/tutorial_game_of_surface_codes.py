070f4135e742988bd9728e058e35dad5e5008ce4
qottmann <korbinian.kottmann@gmail.com>
2025-04-14 13:01:01 +0200

    A game of surface codes demo

---
+++ demonstrations/tutorial_game_of_surface_codes.py
@@ -0,0 +1,84 @@
+r"""A Game of Surface Codes: Large-Scale Quantum Computing with Lattice Surgery
+===============================================================================
+
+asd
+
+.. figure:: ../_static/demo_thumbnails/opengraph_demo_thumbnails/OGthumbnail_fixed_depth_hamiltonian_simulation_via_cartan_decomposition.png
+    :align: center
+    :width: 70%
+    :target: javascript:void(0)
+
+Introduction
+------------
+
+
+Rules of the game
+-----------------
+
+
+Data blocks design
+------------------
+
+Compact data blocks
+^^^^^^^^^^^^^^^^^^^
+
+Intermediate data blocks
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+Fast data blocks
+^^^^^^^^^^^^^^^^
+
+
+"""
+import numpy as np
+import pennylane as qml
+from pennylane import X, Y, Z, I
+
+import matplotlib.pyplot as plt
+
+
+##############################################################################
+#
+
+##############################################################################
+#
+
+##############################################################################
+#
+
+##############################################################################
+#
+
+##############################################################################
+#
+
+##############################################################################
+#
+
+
+
+
+##############################################################################
+#
+# Conclusion
+# ----------
+#
+# asd
+
+
+##############################################################################
+#
+# References
+# ----------
+#
+# .. [#Litinski]
+#
+#     Daniel Litinski
+#     "A Game of Surface Codes: Large-Scale Quantum Computing with Lattice Surgery"
+#     `arXiv:1808.02892 <https://arxiv.org/abs/1808.02892v3>`__, 2018.
+#
+#
+
+##############################################################################
+# About the author
+# ----------------
