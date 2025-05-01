908b82ee12af3f3330053d304755e36d5edcf8bf
soranjh <soran.jahangiri@gmail.com>
2025-05-01 10:28:07 -0400

    disable code

--- demonstrations/tutorial_dmet_embedding.py
+++ demonstrations/tutorial_dmet_embedding.py
@@ -81,41 +81,45 @@ DMET calculations to construct a Hamiltonian that can be used in a quantum algor
 # a ``Cell`` object with three unit cell each containing two Hydrogen atoms at a bond distance of
 # 0.75 Å. Then, we construct a ``Lattice`` object from the libDMET library, associating it with
 # the defined cell.
-
-import numpy as np
-from pyscf.pbc import gto, df, scf, tools
-from libdmet.system import lattice
-
-cell = gto.Cell()
-cell.a = ''' 10.0    0.0     0.0                                                                                                                                                                                      
-             0.0     10.0    0.0                                                                                                                                                                                      
-             0.0     0.0     1.5 ''' # lattice vectors for unit cell
-cell.atom = ''' H 0.0      0.0      0.0                                                                                                                                                                               
-                H 0.0      0.0      0.75 ''' # coordinates of atoms in unit cell
-cell.basis = '321g'
-cell.build(unit='Angstrom')
-
-kmesh = [1, 1, 3] # number of k-points in xyz direction
-
-lat = lattice.Lattice(cell, kmesh)
-filling = cell.nelectron / (lat.nscsites * 2.0)
-kpts = lat.kpts
-
+#
+# .. code-block:: python
+#
+#    import numpy as np
+#    from pyscf.pbc import gto, df, scf, tools
+#    from libdmet.system import lattice
+#
+#    cell = gto.Cell()
+#    cell.a = ''' 10.0    0.0     0.0
+#                 0.0     10.0    0.0
+#                 0.0     0.0     1.5 ''' # lattice vectors for unit cell
+#    cell.atom = ''' H 0.0      0.0      0.0
+#                    H 0.0      0.0      0.75 ''' # coordinates of atoms in unit cell
+#    cell.basis = '321g'
+#    cell.build(unit='Angstrom')
+#
+#    kmesh = [1, 1, 3] # number of k-points in xyz direction
+#
+#    lat = lattice.Lattice(cell, kmesh)
+#    filling = cell.nelectron / (lat.nscsites * 2.0)
+#    kpts = lat.kpts
+#
 ######################################################################
 # Performing mean-field calculations
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # We can now perform a mean-field calculation on the whole system through Hartree-Fock with density
 # fitted integrals using PySCF.
-
-gdf = df.GDF(cell, kpts)
-gdf._cderi_to_save = 'gdf_ints.h5' # output file for density fitted integral tensor
-gdf.build() # compute the density fitted integrals
-
-kmf = scf.KRHF(cell, kpts, exxdiv=None).density_fit()
-kmf.with_df = gdf # use density-fitted integrals
-kmf.with_df._cderi = 'gdf_ints.h5' # input file for density fitted integrals
-kmf.kernel() # run Hartree-Fock
-
+#
+# .. code-block:: python
+#
+#    gdf = df.GDF(cell, kpts)
+#    gdf._cderi_to_save = 'gdf_ints.h5' # output file for density fitted integral tensor
+#    gdf.build() # compute the density fitted integrals
+#
+#    kmf = scf.KRHF(cell, kpts, exxdiv=None).density_fit()
+#    kmf.with_df = gdf # use density-fitted integrals
+#    kmf.with_df._cderi = 'gdf_ints.h5' # input file for density fitted integrals
+#    kmf.kernel() # run Hartree-Fock
+#
 ######################################################################
 # Partitioning the orbital space
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -125,30 +129,34 @@ kmf.kernel() # run Hartree-Fock
 # localized basis provides a convenient way to understand the contribution of each atom to
 # properties of the full system. Here, we rotate the one-electron and two-electron integrals into
 # IAO basis.
-
-from libdmet.basis_transform import make_basis
-
-c_ao_iao, _, _, lo_labels = make_basis.get_C_ao_lo_iao(lat, kmf, minao="MINAO", full_return=True, return_labels=True)
-c_ao_lo = lat.symmetrize_lo(c_ao_iao)
-lat.set_Ham(kmf, gdf, c_ao_lo, eri_symmetry=4) # rotate integral tensors to IAO basis
-
+#
+# .. code-block:: python
+#
+#    from libdmet.basis_transform import make_basis
+#
+#    c_ao_iao, _, _, lo_labels = make_basis.get_C_ao_lo_iao(lat, kmf, minao="MINAO", full_return=True, return_labels=True)
+#    c_ao_lo = lat.symmetrize_lo(c_ao_iao)
+#    lat.set_Ham(kmf, gdf, c_ao_lo, eri_symmetry=4) # rotate integral tensors to IAO basis
+#
 ######################################################################
 # We now obtain the orbital labels for each atom in the unit cell and define the impurity and bath
 # by looking at the labels. In this example, we choose to keep the :math:`1s` orbitals in the unit
 # cell in the impurity, while the bath contains the :math:`2s` orbitals, and the orbitals belonging
 # to the rest of the supercell become part of the unentangled environment. These can be separated by
 # getting the valence and virtual labels from get_labels function.
-
-from libdmet.lo.iao import get_labels
-
-aoind = cell.aoslice_by_atom()
-labels, val_labels, virt_labels = get_labels(cell, minao="MINAO")
-ncore = 0
-lat.set_val_virt_core(len(val_labels), len(virt_labels), ncore)
-
-print("Valence orbitals: ", val_labels)
-print("Virtual orbitals: ", virt_labels)
-
+#
+# .. code-block:: python
+#
+#    from libdmet.lo.iao import get_labels
+#
+#    aoind = cell.aoslice_by_atom()
+#    labels, val_labels, virt_labels = get_labels(cell, minao="MINAO")
+#    ncore = 0
+#    lat.set_val_virt_core(len(val_labels), len(virt_labels), ncore)
+#
+#    print("Valence orbitals: ", val_labels)
+#    print("Virtual orbitals: ", virt_labels)
+#
 ######################################################################
 # Self-Consistent DMET
 # ^^^^^^^^^^^^^^^^^^^^
@@ -158,47 +166,53 @@ print("Virtual orbitals: ", virt_labels)
 # referred to as single-shot DMET.
 #
 # We first need to construct the impurity Hamiltonian.
-
-def construct_impurity_hamiltonian(lat, v_cor, filling, mu, last_dmu, int_bath=True):
-
-    rho, mu, scf_result = dmet.HartreeFock(lat, v_cor, filling, mu,
-                                    ires=True, labels=lo_labels)
-    imp_ham, _, basis = dmet.ConstructImpHam(lat, rho, v_cor, int_bath=int_bath)
-    imp_ham = dmet.apply_dmu(lat, imp_ham, basis, last_dmu)
-
-    return rho, mu, scf_result, imp_ham, basis
-
+#
+# .. code-block:: python
+#
+#    def construct_impurity_hamiltonian(lat, v_cor, filling, mu, last_dmu, int_bath=True):
+#
+#        rho, mu, scf_result = dmet.HartreeFock(lat, v_cor, filling, mu,
+#                                        ires=True, labels=lo_labels)
+#        imp_ham, _, basis = dmet.ConstructImpHam(lat, rho, v_cor, int_bath=int_bath)
+#        imp_ham = dmet.apply_dmu(lat, imp_ham, basis, last_dmu)
+#
+#        return rho, mu, scf_result, imp_ham, basis
+#
 ######################################################################
 # Next, we solve this impurity Hamiltonian with a high-level method. The following function defines
 # the electronic structure solver for the impurity, provides an initial point for the calculation
 # and passes the ``Lattice`` information to the solver. The solver then calculates the energy and
 # density matrix for the impurity.
-
-def solve_impurity_hamiltonian(lat, cell, basis, imp_ham, last_dmu, scf_result):
-
-    solver = dmet.impurity_solver.FCI(restricted=True, tol=1e-13)
-    basis_k = lat.R2k_basis(basis) #basis in k-space
-
-    solver_args = {"nelec": min((lat.ncore+lat.nval)*2, lat.nkpts*cell.nelectron), \
-               "dm0": dmet.foldRho_k(scf_result["rho_k"], basis_k)}
-    
-    rho_emb, energy_emb, imp_ham, dmu = dmet.SolveImpHam_with_fitting(lat, filling, 
-        imp_ham, basis, solver, solver_args=solver_args)
-
-    last_dmu += dmu
-    return rho_emb, energy_emb, imp_ham, last_dmu, [solver, solver_args]
-
+#
+# .. code-block:: python
+#
+#    def solve_impurity_hamiltonian(lat, cell, basis, imp_ham, last_dmu, scf_result):
+#
+#        solver = dmet.impurity_solver.FCI(restricted=True, tol=1e-13)
+#        basis_k = lat.R2k_basis(basis) #basis in k-space
+#
+#        solver_args = {"nelec": min((lat.ncore+lat.nval)*2, lat.nkpts*cell.nelectron), \
+#                   "dm0": dmet.foldRho_k(scf_result["rho_k"], basis_k)}
+#
+#        rho_emb, energy_emb, imp_ham, dmu = dmet.SolveImpHam_with_fitting(lat, filling,
+#            imp_ham, basis, solver, solver_args=solver_args)
+#
+#        last_dmu += dmu
+#        return rho_emb, energy_emb, imp_ham, last_dmu, [solver, solver_args]
+#
 ######################################################################
 # The final step is to include the effect of the environment in the expectation value. So we define
 # a function which returns the density matrix and energy for the whole system.
-
-def solve_full_system(lat, rho_emb, energy_emb, basis, imp_ham, last_dmu, solver_info, lo_labels):
-    rho_full, energy_full, nelec_full = \
-            dmet.transformResults(rho_emb, energy_emb, basis, imp_ham, \
-            lattice=lat, last_dmu=last_dmu, int_bath=True, \
-                                  solver=solver_info[0], solver_args=solver_info[1], labels=lo_labels)
-    energy_full *= lat.nscsites
-    return rho_full, energy_full
+#
+# .. code-block:: python
+#
+#    def solve_full_system(lat, rho_emb, energy_emb, basis, imp_ham, last_dmu, solver_info, lo_labels):
+#        rho_full, energy_full, nelec_full = \
+#                dmet.transformResults(rho_emb, energy_emb, basis, imp_ham, \
+#                lattice=lat, last_dmu=last_dmu, int_bath=True, \
+#                                      solver=solver_info[0], solver_args=solver_info[1], labels=lo_labels)
+#        energy_full *= lat.nscsites
+#        return rho_full, energy_full
 
 ######################################################################
 # Note here that the effect of environment included in this step is at the meanfield level. So if we
@@ -210,57 +224,63 @@ def solve_full_system(lat, rho_emb, energy_emb, basis, imp_ham, last_dmu, solver
 # correlation potential and optimize it by minimizing the difference between density matrices
 # obtained from the mean-field Hamiltonian and the impurity Hamiltonian. Let's initialize the
 # correlation potential and define a function to optimize it.
-
-def initialize_vcor(lat):
-    v_cor = dmet.VcorLocal(restricted=True, bogoliubov=False, nscsites=lat.nscsites)
-    v_cor.assign(np.zeros((2, lat.nscsites, lat.nscsites)))
-    return v_cor
-
-def fit_correlation_potential(rho_emb, lat, basis, v_cor):
-    vcor_new, err = dmet.FitVcor(rho_emb, lat, basis, \
-                v_cor, beta=np.inf, filling=filling, MaxIter1=300, MaxIter2=0)
-
-    dVcor_per_ele = np.max(np.abs(vcor_new.param - v_cor.param))
-    v_cor.update(vcor_new.param)
-    return v_cor, dVcor_per_ele
-
+#
+# .. code-block:: python
+#
+#    def initialize_vcor(lat):
+#        v_cor = dmet.VcorLocal(restricted=True, bogoliubov=False, nscsites=lat.nscsites)
+#        v_cor.assign(np.zeros((2, lat.nscsites, lat.nscsites)))
+#        return v_cor
+#
+#    def fit_correlation_potential(rho_emb, lat, basis, v_cor):
+#        vcor_new, err = dmet.FitVcor(rho_emb, lat, basis, \
+#                    v_cor, beta=np.inf, filling=filling, MaxIter1=300, MaxIter2=0)
+#
+#        dVcor_per_ele = np.max(np.abs(vcor_new.param - v_cor.param))
+#        v_cor.update(vcor_new.param)
+#        return v_cor, dVcor_per_ele
+#
 ######################################################################
 # Now, we have defined all the ingredients of DMET. We can set up the self-consistency loop to get
 # the full execution. We set up this loop by defining the maximum number of iterations and a
 # convergence criteria. We use both energy and correlation potential as our convergence parameters,
 # so we define the initial values and convergence tolerance for both.
-
-import libdmet.dmet.Hubbard as dmet
-
-max_iter = 10 # maximum number of iterations
-e_old = 0.0 # initial value of energy
-v_cor = initialize_vcor(lat) # initial value of correlation potential
-dVcor_per_ele = None # initial value of correlation potential per electron
-vcor_tol = 1.0e-5 # tolerance for correlation potential convergence
-energy_tol = 1.0e-5 # tolerance for energy convergence
-mu = 0 # initial chemical potential
-last_dmu = 0.0 # change in chemical potential
-
+#
+# .. code-block:: python
+#
+#    import libdmet.dmet.Hubbard as dmet
+#
+#    max_iter = 10 # maximum number of iterations
+#    e_old = 0.0 # initial value of energy
+#    v_cor = initialize_vcor(lat) # initial value of correlation potential
+#    dVcor_per_ele = None # initial value of correlation potential per electron
+#    vcor_tol = 1.0e-5 # tolerance for correlation potential convergence
+#    energy_tol = 1.0e-5 # tolerance for energy convergence
+#    mu = 0 # initial chemical potential
+#    last_dmu = 0.0 # change in chemical potential
+#
 ######################################################################
 # Now we set up the iterations in a loop.
-
-for i in range(max_iter):
-    rho, mu, scf_result, imp_ham, basis = construct_impurity_hamiltonian(lat,
-                                     v_cor, filling, mu, last_dmu) # construct impurity Hamiltonian
-    rho_emb, energy_emb, imp_ham, last_dmu, solver_info = solve_impurity_hamiltonian(lat, cell,
-                                     basis, imp_ham, last_dmu, scf_result) # solve impurity Hamiltonian
-    rho_full, energy_full = solve_full_system(lat, rho_emb, energy_emb, basis, imp_ham,
-                                     last_dmu, solver_info, lo_labels) # include the environment interactions
-    v_cor, dVcor_per_ele = fit_correlation_potential(rho_emb,
-                                     lat, basis, v_cor) # fit correlation potential
-
-    dE = energy_full - e_old
-    e_old = energy_full
-    if dVcor_per_ele < vcor_tol and abs(dE) < energy_tol:
-        print("DMET Converged")
-        print("DMET Energy per cell: ", energy_full)
-        break
-
+#
+# .. code-block:: python
+#
+#    for i in range(max_iter):
+#        rho, mu, scf_result, imp_ham, basis = construct_impurity_hamiltonian(lat,
+#                                         v_cor, filling, mu, last_dmu) # construct impurity Hamiltonian
+#        rho_emb, energy_emb, imp_ham, last_dmu, solver_info = solve_impurity_hamiltonian(lat, cell,
+#                                         basis, imp_ham, last_dmu, scf_result) # solve impurity Hamiltonian
+#        rho_full, energy_full = solve_full_system(lat, rho_emb, energy_emb, basis, imp_ham,
+#                                         last_dmu, solver_info, lo_labels) # include the environment interactions
+#        v_cor, dVcor_per_ele = fit_correlation_potential(rho_emb,
+#                                         lat, basis, v_cor) # fit correlation potential
+#
+#        dE = energy_full - e_old
+#        e_old = energy_full
+#        if dVcor_per_ele < vcor_tol and abs(dE) < energy_tol:
+#            print("DMET Converged")
+#            print("DMET Energy per cell: ", energy_full)
+#            break
+#
 ######################################################################
 # This concludes the DMET procedure.
 #
@@ -273,32 +293,36 @@ for i in range(max_iter):
 # to pave the path for using it with quantum algorithms. The hamiltonian object generated above
 # provides us with one-body and two-body integrals along with the nuclear repulsion energy which can
 # be accessed as follows:
-
-from pyscf import ao2mo
-norb = imp_ham.norb
-h1 = imp_ham.H1["cd"]
-h2 = imp_ham.H2["ccdd"][0]
-h2 = ao2mo.restore(1, h2, norb) # Get the correct shape based on permutation symmetry
-
+#
+# .. code-block:: python
+#
+#    from pyscf import ao2mo
+#    norb = imp_ham.norb
+#    h1 = imp_ham.H1["cd"]
+#    h2 = imp_ham.H2["ccdd"][0]
+#    h2 = ao2mo.restore(1, h2, norb) # Get the correct shape based on permutation symmetry
+#
 ######################################################################
 # These one-body and two-body integrals can then be used to generate the qubit Hamiltonian in
 # PennyLane. We then diagonaliz it to get the eigenvalues and show that the lowest eigenvalue
 # matches the energy we obtained for the embedded system above.
-
-import pennylane as qml
-from pennylane.qchem import one_particle, two_particle, observable
-
-t = one_particle(h1[0])
-v = two_particle(np.swapaxes(h2, 1, 3)) # Swap to physicist's notation
-qubit_op = observable([t,v], mapping="jordan_wigner")
-eigval_qubit = qml.eigvals(qml.SparseHamiltonian(qubit_op.sparse_matrix(), wires = qubit_op.wires))
-print("eigenvalue from PennyLane: ", eigval_qubit)
-print("embedding energy: ", energy_emb)
-
+#
+# .. code-block:: python
+#
+#    import pennylane as qml
+#    from pennylane.qchem import one_particle, two_particle, observable
+#
+#    t = one_particle(h1[0])
+#    v = two_particle(np.swapaxes(h2, 1, 3)) # Swap to physicist's notation
+#    qubit_op = observable([t,v], mapping="jordan_wigner")
+#    eigval_qubit = qml.eigvals(qml.SparseHamiltonian(qubit_op.sparse_matrix(), wires = qubit_op.wires))
+#    print("eigenvalue from PennyLane: ", eigval_qubit)
+#    print("embedding energy: ", energy_emb)
+#
 ######################################################################
 # We can also get ground state energy for the system from this value by solving for the full system
 # as done above in the self-consistency loop using solve_full_system function.
-
+#
 ######################################################################
 # Conclusion
 # ^^^^^^^^^^^^^^
