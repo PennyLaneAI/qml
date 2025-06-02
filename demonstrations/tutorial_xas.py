b0cea86fce63aa0a6a0df593f9cba703f9e39a02
Colin J. Dale <79223094+colinjdale@users.noreply.github.com>
2025-06-02 15:49:56 -0400

    Formatting changes

--- demonstrations/tutorial_xas.py
+++ demonstrations/tutorial_xas.py
@@ -11,149 +11,145 @@ is a large amount of correlations between these molecular states. This is the sw
 having to require too many qubits, but having enough correlations that make the calculation
 intractible on a quantum computer. The ground state is computable, but the highly correlated
 high-energy states are very difficult to compute in a quantum chemistry classical computation
+
+In this demo we will implement a simplified version of the algorithm as it appears in the paper  we
+will not be applying all of the optimizations present. We will show how to determine and prepare a
+group state, how determine the states that result from acting on the group state by the dipole
+operator (representing the electromagnetic field from the X-ray) and how to code the time-domain
+analysis circuit which can determine the spectrum.
+
+We will be using concepts that were introduced in other PennyLane demos, such as utilizing pyscf
+with PennyLane, initial state preparation of molecules, and building compressed double-factorized
+Hamiltonians. If you havent checked out those demos yet, it might be best to do so and then come
+back here.
+
+Why simulate X-ray absorption spectroscopy?
+-------------------------------------------
+
+- method for studying local electronic structure
+- can directly probe local structure by exciting tightly bound core electrons
+- determining battery degredation relevant mechanisms, such as oxidation states, from an observed
+  spectrum is difficult without simulations
+- the way one could do this is through spectral fingerprinting
+- each oxidized state would have some spectrum, that one would determine through simulation
+- this could be repeated for all possible oxidized states
+- the observed spectrum could then be matched to combinations of these single cluster spectra
+- this *finger printing* method could allow one to determine the composition of oxidized states in
+  the material
+
+**insert figure demonstrating fingerprinting** observed spectrum, decomposition into single cluster
+spectra, which come from simulations from a quantum computer
+
+This method is particularly difficult for classical computers when strongly-correlated transition
+metals are present, such as those typically in lithium-excess materials which are battery cathod
+candidates.
+
+.. figure:: ../_static/demonstration_assets/xas/fingerprinting.png
+   :width: 100%
+   :align: center
+   :alt: alt text
+
+
+Algorithm
+---------
+
+Absorption cross-section
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+In XAS experiments, the spectrum observed is a measure of the absorption cross section as a function
+of the frequency of the incident X-rays :math:`\sigma_A(\omega)`. For our situation, the electrons
+in the molecular cluster start in a ground molecular state :math:`|I\rangle` with energy
+:math:`E_I`, and will be coupled to an excited state :math:`|F\rangle` with energy :math:`E_F`
+through the action of the dipole operator :math:`\hat m_\rho`, which represents the effect of the
+radiative field, where :math:`\rho` is any of the Cartesian directions :math:`\{x,y,z\}`.
+
+Using Fermi's golden rule, the absorption cross-section is given by
+
+.. math::  \sigma_A(\omega) = \frac{4 \pi}{3 \hbar c} \omega \sum_{F \neq I}\sum_{\rho=x,y,z} 
+            \frac{|\langle F|\hat m_\rho|I \rangle|^2 \eta}{((E_F - E_I)-\omega)^2 + \eta^2}\,, 
+
+where :math:`c` is the speed of light, :math:`\hbar` is Plank's constant, and :math:`\eta` is the
+line broadening which represents the experimental resolution of the spectroscopy, and is typically
+around :math:`1` eV.
+
+Core-valence separation approximation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+We can restrict the range of frequencies, and consequently the range of final states, by only
+considering final states for which *core* electrons are excited, i.e. there exists a hole in the
+core orbitals. These are known as core-excited states, and lie significally above the
+valance-excited states in energy. Typically the frequency range is focused on a target atom in a
+molecular cluster, and also near a transition energy, such as targetting core :math:`1s` electrons.
+We will also neglect relativistic corrections, and focus on frequencies for which the dipole
+approximation is valid, which is the assumption that the wavelength of the radiation is large
+compared to the extent of the electronic wavefunction.
+
+Atomic species and oxidations states will determine the energy difference between states with
+different principle quantum numbers, and this difference will show as a peak in spectroscopy, known
+as the *absorption edge*. Focusing spctroscopy near this edge for :math:`1s` to :math:`2p` is called
+the :math:`K`-edge, and in general X-ray absorption near-edge spectroscopy is known as XANES. We
+will focus on simulating spectroscopy in this XANES regime.
+
+**insert figure which is a single particle energy diagram** should show XANES excitation from core
+to excited compared to UV/vis light excitations of valence electrons to excited
+
+.. figure:: ../_static/demonstration_assets/xas/core-valence.png
+    :width: 50%
+    :align: center
+    :alt: alt text
+
+
+Time-domain determination of the cross section
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+We will use a mathematical trick of a frequency-domain Green's function to link the absorption cross
+section we want to estimate to the time-propagation of our initial state through a Fourier
+transform. The expectation value of our initial state :math:`m_\rho|I\rangle` propagated various
+amounts in time by the molecular hamiltonian :math:`H` will give us a time-domain Green's function,
+the Fourier transform of which is the frequency-domain Green's function, and that is directly
+related to the absorption cross section.
+
+**Insert intuition for using a Green's function**
+
+**Insert derivation of relation between cross section and Green's function, i.e. expectation value
+of time evolution operator for various times**
+
+Below is a step-by-step process of the algorithm: - determine our initial state :math:`|I\rangle` -
+determine how the dipole operator acts on that state to obtain :math:`\hat m_\rho|I\rangle` -
+determine how best to efficiently time propagate that state with the molecular Hamiltonian - run the
+time propagation for various times, and from the expectation value determine the time-domain Green's
+function - Fourier transform (classically) the Green's function to obtain it in the freuency domain,
+which gives us the absorption cross section as a function of frequency
+
+**insert figure that is a block-diagram outline of this procedure** follows the steps above,
+includes some loops showing repeated shots and various time selections, classical computer computing
+the Fourier transform, final spectrum
+
+.. figure:: ../_static/demonstration_assets/xas/block_diagram_of_algorithm.png
+   :width: 100%
+   :align: center
+   :alt: alt text
+
+
+Implementation
+--------------
+
+Let's look at how to implement these steps in PennyLane. We will make extensive use of the
+``qml.qchem`` module, as well as modules from ``pyscf``.
+
+For this demo, we are going to use the very simple :math:`H_2` molecule.
+
+Initial state preparation
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+If you haven't, check out the demo "Initial state preparation for quantum chemistry".
+
+We start by creating our molecule object using the Gaussian type orbitals module (``pyscf.gto``),
+and obtaining the reduced Hartree-Fock molecular orbitals with the self-consistent field methods
+(``pyscf.scf``).
+
 """
 
-######################################################################
-# In this demo we will implement a simplified version of the algorithm as it appears in the paper  we
-# will not be applying all of the optimizations present. We will show how to determine and prepare a
-# group state, how determine the states that result from acting on the group state by the dipole
-# operator (representing the electromagnetic field from the X-ray) and how to code the time-domain
-# analysis circuit which can determine the spectrum.
-# 
-# We will be using concepts that were introduced in other PennyLane demos, such as utilizing pyscf
-# with PennyLane, initial state preparation of molecules, and building compressed double-factorized
-# Hamiltonians. If you havent checked out those demos yet, it might be best to do so and then come
-# back here.
-# 
-######################################################################
-# Why simulate X-ray absorption spectroscopy?
-# -------------------------------------------
-# 
-# - method for studying local electronic structure
-# - can directly probe local structure by exciting tightly bound core electrons
-# - determining battery degredation relevant mechanisms, such as oxidation states, from an observed
-#   spectrum is difficult without simulations
-# - the way one could do this is through spectral fingerprinting
-# - each oxidized state would have some spectrum, that one would determine through simulation
-# - this could be repeated for all possible oxidized states
-# - the observed spectrum could then be matched to combinations of these single cluster spectra
-# - this *finger printing* method could allow one to determine the composition of oxidized states in
-#   the material
-# 
-# **insert figure demonstrating fingerprinting** observed spectrum, decomposition into single cluster
-# spectra, which come from simulations from a quantum computer
-# 
-# This method is particularly difficult for classical computers when strongly-correlated transition
-# metals are present, such as those typically in lithium-excess materials which are battery cathod
-# candidates.
-# 
-# .. figure:: ../_static/demonstration_assets/xas/fingerprinting.png
-#    :width: 100%
-#    :align: center
-#    :alt: alt text
-# 
-# 
-######################################################################
-# Algorithm
-# ---------
-# 
-# Absorption cross-section
-# ~~~~~~~~~~~~~~~~~~~~~~~~
-# 
-# In XAS experiments, the spectrum observed is a measure of the absorption cross section as a function
-# of the frequency of the incident X-rays :math:`\sigma_A(\omega)`. For our situation, the electrons
-# in the molecular cluster start in a ground molecular state :math:`|I\rangle` with energy
-# :math:`E_I`, and will be coupled to an excited state :math:`|F\rangle` with energy :math:`E_F`
-# through the action of the dipole operator :math:`\hat m_\rho`, which represents the effect of the
-# radiative field, where :math:`\rho` is any of the Cartesian directions :math:`\{x,y,z\}`.
-# 
-# Using Fermi's golden rule, the absorption cross-section is given by
-# 
-# .. math::  \sigma_A(\omega) = \frac{4 \pi}{3 \hbar c} \omega \sum_{F \neq I}\sum_{\rho=x,y,z} \frac{|\langle F|\hat m_\rho|I \rangle|^2 \eta}{((E_F - E_I)-\omega)^2 + \eta^2}\,, 
-# 
-# where :math:`c` is the speed of light, :math:`\hbar` is Plank's constant, and :math:`\eta` is the
-# line broadening which represents the experimental resolution of the spectroscopy, and is typically
-# around :math:`1` eV.
-# 
-######################################################################
-# Core-valence separation approximation
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 
-# We can restrict the range of frequencies, and consequently the range of final states, by only
-# considering final states for which *core* electrons are excited, i.e. there exists a hole in the
-# core orbitals. These are known as core-excited states, and lie significally above the
-# valance-excited states in energy. Typically the frequency range is focused on a target atom in a
-# molecular cluster, and also near a transition energy, such as targetting core :math:`1s` electrons.
-# We will also neglect relativistic corrections, and focus on frequencies for which the dipole
-# approximation is valid, which is the assumption that the wavelength of the radiation is large
-# compared to the extent of the electronic wavefunction.
-# 
-# Atomic species and oxidations states will determine the energy difference between states with
-# different principle quantum numbers, and this difference will show as a peak in spectroscopy, known
-# as the *absorption edge*. Focusing spctroscopy near this edge for :math:`1s` to :math:`2p` is called
-# the :math:`K`-edge, and in general X-ray absorption near-edge spectroscopy is known as XANES. We
-# will focus on simulating spectroscopy in this XANES regime.
-# 
-# **insert figure which is a single particle energy diagram** should show XANES excitation from core
-# to excited compared to UV/vis light excitations of valence electrons to excited
-# 
-# .. figure:: ../_static/demonstration_assets/xas/core-valence.png
-#     :width: 100%
-#     :align: center
-#     :alt: alt text
-# 
-# 
-######################################################################
-# Time-domain determination of the cross section
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 
-# We will use a mathematical trick of a frequency-domain Green's function to link the absorption cross
-# section we want to estimate to the time-propagation of our initial state through a Fourier
-# transform. The expectation value of our initial state :math:`m_\rho|I\rangle` propagated various
-# amounts in time by the molecular hamiltonian :math:`H` will give us a time-domain Green's function,
-# the Fourier transform of which is the frequency-domain Green's function, and that is directly
-# related to the absorption cross section.
-# 
-# **Insert intuition for using a Green's function**
-# 
-# **Insert derivation of relation between cross section and Green's function, i.e. expectation value
-# of time evolution operator for various times**
-# 
-# Below is a step-by-step process of the algorithm: - determine our initial state :math:`|I\rangle` -
-# determine how the dipole operator acts on that state to obtain :math:`\hat m_\rho|I\rangle` -
-# determine how best to efficiently time propagate that state with the molecular Hamiltonian - run the
-# time propagation for various times, and from the expectation value determine the time-domain Green's
-# function - Fourier transform (classically) the Green's function to obtain it in the freuency domain,
-# which gives us the absorption cross section as a function of frequency
-# 
-# **insert figure that is a block-diagram outline of this procedure** follows the steps above,
-# includes some loops showing repeated shots and various time selections, classical computer computing
-# the Fourier transform, final spectrum
-# 
-# .. figure:: ../_static/demonstration_assets/xas/block_diagram_of_algorithm.png
-#    :width: 100%
-#    :align: center
-#    :alt: alt text
-# 
-# 
-######################################################################
-# Implementation
-# --------------
-# 
-# Let's look at how to implement these steps in PennyLane. We will make extensive use of the
-# ``qml.qchem`` module, as well as modules from ``pyscf``.
-# 
-# For this demo, we are going to use the very simple :math:`H_2` molecule.
-# 
-# Initial state preparation
-# ~~~~~~~~~~~~~~~~~~~~~~~~~
-# 
-# If you haven't, check out the demo "Initial state preparation for quantum chemistry".
-# 
-# We start by creating our molecule object using the Gaussian type orbitals module (``pyscf.gto``),
-# and obtaining the reduced Hartree-Fock molecular orbitals with the self-consistent field methods
-# (``pyscf.scf``).
-# 
 
 from pyscf import gto, scf, mcscf
 import numpy as np
@@ -326,7 +322,6 @@ Z0 = np.diag(eigenvals)
 # **Explain simplification using Thouless' theorem (here or in earlier section)** Specifically how to
 # go form what looks like two U rotations to just one when using the ``qml.BasisRotation``.
 # 
-######################################################################
 # Time-propagation circuit
 # ~~~~~~~~~~~~~~~~~~~~~~~~
 # 
