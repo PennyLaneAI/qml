d497621064eae2a2440402f70a5188aa3b72679f
soranjh <soran.jahangiri@gmail.com>
2025-06-10 15:55:34 -0400

    add hamiltonians

--- demonstrations/tutorial_how_to_build_vibrational_hamiltonians.py
+++ demonstrations/tutorial_how_to_build_vibrational_hamiltonians.py
@@ -25,34 +25,32 @@ molecule.
 # Vibrational Hamiltonian
 # -----------------------
 # A molecular vibrational Hamiltonian can be defined in terms of the kinetic energy operator of the
-# nuclei, :math:`T`, and the potential energy operator, :math:`V`, that describes the interaction  between
-# the nuclei as:
+# nuclei, :math:`T`, and the potential energy operator, :math:`V`, that describes the interaction
+# between the nuclei as:
 #
 # .. math::
 #
 #     H = T + V.
 #
-# The kinetic energy operator can be written in terms of position and momentum operations or bosonic
-# creation and annihilation operations. The potential energy operator is usually obtained by
-# expanding the molecular potential energy surface over vibrational normal coordinates :math:`Q`.
+# The kinetic and potential energy operators can be written in terms of momentum and position
+# operators, respectively. There are several ways to construct the potential energy operator which
+# lead to different representation of the vibrational Hamiltonian. Here we explain some of these
+# representations and provide PennyLane codes for constructing them.
+#
+# Christiansen representation
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# The Christiansen representation of the vibrational Hamiltonian relies on the n-mode expansion of
+# the potential energy surface :math:`V` over vibrational normal coordinates :math:`Q`.
 #
 # .. math::
 #
 #     V({Q}) = \sum_i V_1(Q_i) + \sum_{ij} V_2(Q_i,Q_j) + ....
 #
-# This is typically done by performing single-point energy calculations at small distances along the
-# normal mode coordinates. Computing the energies for each mode separately provides the term
-# :math:`V_1` while displacing atoms along two different modes simultaneously gives the term
-# :math:`V_2` and so on.
+# This provides a general representation of the potential energy surface where the terms :math:`V_n`
+# depend on :math:`n` vibrational mode at most.
 #
-# There are several ways to represent a vibrational Hamiltonian. Here we explain some of these
-# representations and provide PennyLane codes for constructing them.
-#
-# Christiansen representation
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# The Christiansen representation of the vibrational Hamiltonian is a second-quantization form for
-# defining the Hamiltonian in term os bosonic creation :math:`b^{\dagger}` and annihilation
-# :math:`b` operations:
+# The Christiansen Hamiltonian is then constructed in second-quantization based on this potential
+# energy surface and bosonic creation :math:`b^{\dagger}` and annihilation :math:`b` operations:
 #
 # .. math::
 #
@@ -61,25 +59,115 @@ molecule.
 #         b_{k_i}^{\dagger} b_{k_j}^{\dagger} b_{l_i} b_{l_j},
 #
 # where :math:`M` represents the number of normal modes and :math:`N` is the number of modals. The
-# coefficients :math:`C` represent the one-mode and two-mode integrals defined here.
+# coefficients :math:`C` represent n-mode integrals which depend on the :math:`n`-mode contribution
+# of the potential energy, :math:`V_n`, defined above.
 #
 # PennyLane provides a set of functions to construct the Christiansen Hamiltonian, either directly
 # in one step or by building the Hamiltonian from its building blocks step by step. An important
 # step in both methods is to construct the potential energy operator which is done based on
 # single-point energy calculations along the normal modes of the molecule. The
-# :func:`~.pennylane.qchem.vibrational_pes` function in PennyLane provides an convenient way to
-# perform the potential energy scan by just providing minimal input.
-
+# :func:`~.pennylane.qchem.vibrational_pes` function in PennyLane provides a convenient way to
+# perform the potential energy calculations for a given molecule. The calculations are typically
+# done by performing single-point energy calculations at small distances along the
+# normal mode coordinates. Computing the energies for each mode separately provides the term
+# :math:`V_1` while displacing atoms along two different modes simultaneously gives the term
+# :math:`V_2` and so on.
+#
+# Let's look at an example for the HF molecule.
 
 import numpy as np
 import pennylane as qml
 
-symbols  = ['H', 'F']                                    # define atomic symbols
-geometry = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])  # define atom positions
-mol = qml.qchem.Molecule(symbols, geometry)              # construct the molecule
+symbols  = ['H', 'F']
+geometry = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+mol = qml.qchem.Molecule(symbols, geometry)
 pes = qml.qchem.vibrational_pes(mol)
 
+######################################################################
+# The :func:`~.pennylane.qchem.vibrational_pes` function creates a
+# :class:`~.pennylane.qchem.VibrationalPES` object that stores the potential energy surface
+# and vibrational information. This object is the input for several functions that we learn about
+# here. For instance, the :func:`~.pennylane.qchem.christiansen_integrals` function accepts this
+# object to compute the integrals needed to construct the bosonic form of the Christiansen
+# Hamiltonian defined above.
+
+integrals = qml.qchem.vibrational.christiansen_integrals(pes,n_states=4)
+h_bosonic = qml.qchem.christiansen_bosonic(integrals[0])
+print(h_bosonic)
+
+######################################################################
+# The bosonic Hamiltonian constructed with :func:`~.pennylane.qchem.christiansen_bosonic` can be
+# mapped to its qubit form by using the proper mapping equations defined in the
+# :func:`~.pennylane.qchem.christiansen_mapping` function.
+
+h_qubit = qml.bose.christiansen_mapping(h_bosonic)
+h_qubit
+
+######################################################################
+# This provides a vibrational Hamiltonian in the qubit basis that can be used in any desired quantum
+# algorithm in PennyLane.
 #
+# Note that PennyLane also provides the :func:`~.pennylane.qchem.christiansen_hamiltonian` function
+# that uses the :class:`~.pennylane.qchem.VibrationalPES` object directly and builds the qubit
+# Christiansen Hamiltonian.
+
+h_qubit2 = qml.qchem.vibrational.christiansen_hamiltonian(pes,n_states=4)
+
+######################################################################
+# You can verify that the two Hamiltonians are identical.
+#
+# Taylor representation
+# ^^^^^^^^^^^^^^^^^^^^^
+# The Taylor representation of the vibrational Hamiltonian relies on a Taylor expansion of
+# the potential energy surface :math:`V` in terms of the vibrational mass-weighted normal
+# coordinate operators :math:`q`.
+#
+# .. math::
+#
+#     V = V_0 + \sum_i F_i q_i + \sum_{ij} F_{ij} q_i,q_j + ....
+#
+# Note that the force constants :math:`F` are derivatives of the potential energy surface.
+#
+# The Taylor Hamiltonian can then be constructed by defining the kinetic and potential energy
+# components in terms of the momentum and coordinate operators.
+#
+# .. math::
+#
+#     H = \sum_{i \ge j} K_{ij} p_i p_j + \sum_{i\ge j} \Phi_{ij}^{(2)} q_i q_j +
+#          \sum_{i \ge j \ge k} \Phi_{ijk}^{(3)} q_i q_j q_k + ...,
+#
+# where the coefficients :math:`\Phi` are obtained from the force constants :math:`F` after
+# rearranging the terms in the potential energy operator according to the number of different modes.
+#
+# The :func:`~.pennylane.qchem.taylor_coeffs` function computes the coefficients :math:`\Phi` from
+# a :class:`~.pennylane.qchem.VibrationalPES` object.
+
+one, two = qml.qchem.taylor_coeffs(pes)
+
+######################################################################
+# We can then use these coefficients to construct a bosonic form of the Taylor Hamiltonian.
+
+h_bosonic = qml.qchem.taylor_bosonic(coeffs=[one, two], freqs=pes.freqs, uloc=pes.uloc)
+print(h_bosonic)
+
+######################################################################
+# This bosonic Hamiltonian can be mapped to the qubit basis using mapping schemes of
+# :func:`~.pennylane.bose.binary_mapping` or :func:`~.pennylane.bose.unary_mapping` functions.
+
+h_qubit = qml.binary_mapping(h_bosonic, n_states=4)
+h_qubit
+
+######################################################################
+# This Hamiltonian can be used in any desired quantum algorithm in PennyLane.
+#
+# Note that PennyLane also provides the :func:`~.pennylane.qchem.taylor_hamiltonian` function
+# that uses the :class:`~.pennylane.qchem.VibrationalPES` object directly and builds the qubit
+# Taylor Hamiltonian.
+
+h_qubit2 = qml.qchem.vibrational.taylor_hamiltonian(pes, n_states=4)
+
+######################################################################
+# You can verify that the two Hamiltonians are identical.
 #
 # Conclusion
 # ----------
