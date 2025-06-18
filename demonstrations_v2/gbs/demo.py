b716a74ecc45d1df8cea424d06289ea6ac42c520
Alan Martin <alan.martin@xanadu.ai>
2025-06-18 10:52:40 -0400

    another test

--- demonstrations_v2/gbs/demo.py
+++ demonstrations_v2/gbs/demo.py
@@ -2,8 +2,8 @@ r"""
 .. role:: html(raw)
    :format: html
 
-Quantum advantage with Gaussian Boson Sampling Test
-===================================================
+Quantum advantage with Gaussian Boson Sampling Test More
+========================================================
 
 .. meta::
     :property="og:description": Using light to perform tasks beyond the reach of classical computers.
