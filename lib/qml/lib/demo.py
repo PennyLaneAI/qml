65f3b7e77e5a1fbe5d7a2e9f6dd211eeec25147c
doctorperceptron <paul@xanadu.ai>
2025-07-04 08:27:53 -0400

    Fix module import error

--- lib/qml/lib/demo.py
+++ lib/qml/lib/demo.py
@@ -162,7 +162,7 @@ def build(
 
     build_venv = Virtualenv(ctx.build_venv_path)
     cmds.pip_install(
-        build_venv.python, requirements=ctx.build_requirements_file, use_uv=False
+        build_venv.python, requirements=ctx.build_requirements_file, use_uv=False, quiet=False
     )
 
     for demo in demos:
@@ -245,6 +245,7 @@ def generate_requirements(
         output_file,
         *requirements_in,
         constraints_files=constraints,
+        quiet=False,
         prerelease=dev,
     )
 
@@ -268,7 +269,7 @@ def _build_demo(
             build_venv.python,
             "--upgrade",
             requirements=out_dir / "requirements.txt",
-            quiet=True,
+            quiet=False,
             pre=dev,
         )
 
