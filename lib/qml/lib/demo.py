8719d5751dfe25531c928540e53869b299d4d462
doctorperceptron <paul@xanadu.ai>
2025-07-08 16:01:58 -0400

    Use rc branch

--- lib/qml/lib/demo.py
+++ lib/qml/lib/demo.py
@@ -326,7 +326,7 @@ def _build_demo(
         cmds.pip_install(
             build_venv.python,
             "--upgrade",
-            "git+https://github.com/PennyLaneAI/pennylane.git#egg=pennylane",
+            "git+https://github.com/PennyLaneAI/pennylane.git@v0.42.0-rc0#egg=pennylane",
             use_uv=False,
             quiet=False,
         )
