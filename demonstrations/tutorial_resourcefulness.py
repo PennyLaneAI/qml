64ae004e74af45bc749bd4fee7d7ba2047dc594c
mariaschuld <mariaschuld@gmail.com>
2025-07-23 10:24:57 +0200

    add empty demo template

---
+++ demonstrations/tutorial_resourcefulness.py
@@ -0,0 +1,53 @@
+r"""
+Analysing quantum resource with the generalized Fourier transform 
+=================================================================
+
+
+
+.. figure:: ../_static/demo_thumbnails/opengraph_demo_thumbnails/OGthumbnail_resourcefulness_figure1.png
+    :align: center
+    :width: 70%
+    :alt: DESCRIPTION.
+    :target: javascript:void(0)
+
+Here is a reference [#Bermejo_Braccia]_.
+
+
+Section Header
+------------------
+
+:math:`\mathcal{G}` 
+
+.. figure:: ../_static/demonstration_assets/unitary_synthesis_kak/KAK_generic.png
+   :align: center
+   :width: 70%
+   :alt: Quantum circuit of a generic KAK decomposition.
+
+.. math::
+
+    latex here
+"""
+
+import numpy as np
+
+######################################################################
+# Text here!
+#
+
+
+
+#
+# References
+# ----------
+#
+# .. [#Bermejo_Braccia]
+#
+#     Bermejo, Pablo, Paolo Braccia, Antonio Anna Mele, Nahuel L. Diaz, Andrew E. Deneris, Martin Larocca, and M. Cerezo. "Characterizing quantum resourcefulness via group-Fourier decompositions." arXiv preprint arXiv:2506.19696 (2025).
+#
+
+
+######################################################################
+#
+# About the author
+# ----------------
+#
