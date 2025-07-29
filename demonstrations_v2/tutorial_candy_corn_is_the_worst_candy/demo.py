aaafb612ee0b5e64c46b63b89fdc7d380b351688
Alan Martin <alan.martin@xanadu.ai>
2025-07-29 13:39:42 -0400

    update body

--- demonstrations_v2/tutorial_candy_corn_is_the_worst_candy/demo.py
+++ demonstrations_v2/tutorial_candy_corn_is_the_worst_candy/demo.py
@@ -2,14 +2,14 @@ r"""
 candy corn is the worst candy
 =
 
-Introduce your demo here!
+Candy corn is awful. It's a candy that people pretend to like, but deep down, we all know it's the worst candy ever created. This demonstration is a lighthearted take on the topic, showcasing how even the most disliked candy can be used in a fun and engaging way.
 """
 
-print("Hello")
+print("Candy")
 
 ###############################################################################
 #
 # Add comment blocks to separate code blocks
 #
 
-print("World")
+print("Corn")
