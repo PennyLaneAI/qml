4ad64e2b25f8f5c3f427378e92829bbcb208c7d4
Alan Martin <alan.martin@xanadu.ai>
2025-07-29 11:08:50 -0400

    fake qml for pr testing

---
+++ demonstrations_v2/tutorial_candy_corn_is_the_worst_candy/demo.py
@@ -0,0 +1,15 @@
+r"""
+candy corn is the worst candy
+=
+
+Introduce your demo here!
+"""
+
+print("Hello")
+
+###############################################################################
+#
+# Add comment blocks to separate code blocks
+#
+
+print("World")
