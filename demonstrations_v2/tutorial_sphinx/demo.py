452cca8197ad730202d6affe75e763c38bbb2016
Alan Martin <alan.martin@xanadu.ai>
2025-07-31 09:13:41 -0400

    wow

---
+++ demonstrations_v2/tutorial_sphinx/demo.py
@@ -0,0 +1,15 @@
+r"""
+I like sphinx cats
+=
+
+Introduce your demo here!
+"""
+
+print("Hello")
+
+###############################################################################
+#
+# Add comment blocks to separate code blocks
+#
+
+print("World")
