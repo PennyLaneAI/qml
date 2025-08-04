2f8a79520f6447e7f70a7dca5fc4db766a387591
Alan Martin <alan.martin@xanadu.ai>
2025-08-04 10:29:39 -0400

    try image

---
+++ demonstrations_v2/a/demo.py
@@ -0,0 +1,15 @@
+r"""
+a
+=
+
+Introduce your demo here!
+"""
+
+print("Hello")
+
+###############################################################################
+#
+# Add comment blocks to separate code blocks
+#
+
+print("World")
