28f70f58e7eb8170b7d6dfba7678b4860b23c1b6
Alan Martin <alan.martin@xanadu.ai>
2025-08-05 13:34:32 -0400

    fixes

---
+++ demonstrations_v2/testing_cli_new_command/demo.py
@@ -0,0 +1,15 @@
+r"""
+testing cli new command
+=
+
+Introduce your demo here!
+"""
+
+print("Hello")
+
+###############################################################################
+#
+# Add comment blocks to separate code blocks
+#
+
+print("World")
