8dc08291b101b611c729cd84ac4cc8f29740d382
Diego <diego_guala@hotmail.com>
2025-08-18 12:42:25 -0400

    initial draft

---
+++ demonstrations_v2/tutorial_qksd_qualtran/demo.py
@@ -0,0 +1,58 @@
+r"""Title TBD (something about QKSD)
+======================================
+
+A catchy hook sentence and basic explanation.
+
+.. figure:: ../_static/demo_thumbnails/opengraph_demo_thumbnails/pennylane-demo-qualtran-qksd.png
+    :align: center
+    :width: 70%
+    :target: javascript:void(0)
+
+What we'll do in this demo, maybe some bullet points.
+"""
+
+######################################################################
+# Heading 1
+# -------------------------
+#
+# Introduction to the method and initial steps to set it up
+
+import pennylane as qml
+
+# do some stuff
+
+######################################################################
+# Explanation about code
+
+import numpy as np
+# additional code
+
+######################################################################
+# Explanation about code
+#
+# potential subsection
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Subsection explanation and preparation for code
+
+from qualtran.bloqs import something
+
+######################################################################
+
+
+######################################################################
+# 
+#
+# Second Section
+# ------------------------------------------
+#
+# 
+
+######################################################################
+# Conclusion
+# ----------
+# In this demo, we did something.
+#
+# About the author
+# ----------------
+#
