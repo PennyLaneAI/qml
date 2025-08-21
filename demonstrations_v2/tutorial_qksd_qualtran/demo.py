fc44385962d56dab814e5f39c387ad7e2ad089a2
Diego <diego_guala@hotmail.com>
2025-08-21 09:48:32 -0400

    remove decoy integrals, remove extra parameters

--- demonstrations_v2/tutorial_qksd_qualtran/demo.py
+++ demonstrations_v2/tutorial_qksd_qualtran/demo.py
@@ -43,7 +43,7 @@ symbols = ["H", "O", "H"]
 coordinates = np.array([[-0.0399, -0.0038, 0.0], [1.5780, 0.8540, 0.0], [2.7909, -0.5159, 0.0]])
 
 molecule = qml.qchem.Molecule(symbols, coordinates)
-H, qubits = qml.qchem.molecular_hamiltonian(molecule)
+H, qubits = qml.qchem.molecular_hamiltonian(symbols,coordinates)
 print(H)
 
 ######################################################################
@@ -74,7 +74,7 @@ import pennylane as qml
 from scipy.stats import ortho_group
 from openfermion.ops import InteractionOperator
 from openfermion.transforms import get_fermion_operator, jordan_wigner
-from pennylane.qchem.openfermion_obs import observable
+from pennylane.qchem import observable
 
 
 class QubitizedWalkOperator:
@@ -212,32 +212,7 @@ def shift_and_transform_of_qubit_op_to_PL_lcu(qubit_op, ancilla_shift):
     return transform_into_proper_lcu(list(shifted_qubit_operator.values()), PL_ops_list)
 
 
-class ChebychevQubitizedOperator(QubitizedWalkOperator):
-    def __init__(
-        self, lcu_coeffs, lcu_paulis, lambda_lcu, prep_wires, reflection_wires, order
-    ) -> None:
-        super().__init__(
-            lcu_coeffs, lcu_paulis, lambda_lcu, prep_wires, reflection_wires
-        )
-        self.order = order
-
-    def chebychev_operator_circuit(self):
-        for _ in range(self.order):
-            self.qubitized_walk_operator()
-
-
-def generate_decoy_electron_integrals(n):
-    hpq = np.random.normal(size=(n, n))
-    hpq = 0.5 * (hpq + hpq.T)
-    ut = np.array([ortho_group.rvs(n) for _ in range(n * (n - 1) // 2)])
-    zt = [np.random.normal(size=(n,)) for _ in range(len(ut))]
-    zt = np.array([np.abs(np.random.normal(size=1)) * np.outer(x, x) for x in zt])
-    hpqrs = np.einsum("tpk,tqk,tkl,trl,tsl->pqrs", ut, ut, zt, ut, ut)
-    hpqrs = np.transpose(hpqrs, (0, 2, 1, 3))
-    return hpq, hpqrs
-
-
-class QSPKrylovState(ChebychevQubitizedOperator):
+class QSPKrylovState(QubitizedWalkOperator):
     def __init__(
         self,
         lcu_coeffs,
@@ -247,14 +222,13 @@ class QSPKrylovState(ChebychevQubitizedOperator):
         reflection_wires,
         rot_wires,
         control_wires,
-        order,
         angles_odd,
         angles_conj_odd,
         angles_even,
         angles_conj_even,
     ) -> None:
         super().__init__(
-            lcu_coeffs, lcu_paulis, lambda_lcu, prep_wires, reflection_wires, order
+            lcu_coeffs, lcu_paulis, lambda_lcu, prep_wires, reflection_wires
         )
 
         self.angles_odd = angles_odd
@@ -299,7 +273,7 @@ hpq = np.array([[-2.50986452e00, 1.05657350e-16, -2.22121281e-01, -2.86068006e-0
 hpqrs = np.array([[[[6.96056671e-01, 8.64123979e-17, 7.51963671e-02, 9.51662480e-03],[1.71207496e-16, 4.07411355e-02, -1.45820198e-17, -3.87762611e-18],[7.51963671e-02, -1.67452401e-17, 3.05202191e-02, 5.34230841e-03],[9.51662480e-03, 2.61368866e-17, 5.34230841e-03, 1.67384743e-02],],[[1.71207496e-16, 4.07411355e-02, -1.45820198e-17, -3.87762611e-18],[6.50690725e-01, 1.23045003e-17, 7.46798397e-02, 1.01652275e-02],[-2.47658344e-16, 2.43546325e-03, -4.51712874e-17, 2.48872553e-17],[2.25988600e-16, 1.23962208e-03, 6.53416581e-17, -4.61937088e-17],],[[7.51963671e-02, -1.67452401e-17, 3.05202191e-02, 5.34230841e-03],[-2.47658344e-16, 2.43546325e-03, -4.51712874e-17, 2.48872553e-17],[3.54579407e-01, -3.60619099e-17, 5.55938827e-03, -2.76946416e-03],[1.02943160e-02, 2.41224973e-17, -5.75780811e-03, -2.10651109e-02],],[[9.51662480e-03, 2.61368866e-17, 5.34230841e-03, 1.67384743e-02],[2.25988600e-16, 1.23962208e-03, 6.53416581e-17, -4.61937088e-17],[1.02943160e-02, 2.41224973e-17, -5.75780811e-03, -2.10651109e-02],[3.41425718e-01, -2.31491583e-17, 3.85389582e-03, 7.06935786e-03],],],[[[8.64123979e-17, 6.50690725e-01, -2.78377815e-16, 2.49516124e-16],[4.07411355e-02, 6.27787581e-18, 2.43546325e-03, 1.23962208e-03],[-1.67452401e-17, 7.46798397e-02, -5.60235836e-17, 6.70915608e-17],[2.61368866e-17, 1.01652275e-02, 2.78665353e-17, -5.04507066e-17],],[[4.07411355e-02, 6.27787581e-18, 2.43546325e-03, 1.23962208e-03],[1.23045003e-17, 7.62241410e-01, -3.05861696e-16, 3.55270558e-16],[2.43546325e-03, -3.26993657e-16, 1.24617768e-02, -3.33498976e-06],[1.23962208e-03, 3.18178705e-16, -3.33498976e-06, 7.25183601e-03],],[[-1.67452401e-17, 7.46798397e-02, -5.60235836e-17, 6.70915608e-17],[2.43546325e-03, -3.26993657e-16, 1.24617768e-02, -3.33498976e-06],[-3.60619099e-17, 3.59082675e-01, -5.69105903e-17, 9.60811644e-18],[2.41224973e-17, 5.08519862e-03, -9.99415338e-17, 1.14310026e-16],],[[2.61368866e-17, 1.01652275e-02, 2.78665353e-17, -5.04507066e-17],[1.23962208e-03, 3.18178705e-16, -3.33498976e-06, 7.25183601e-03],[2.41224973e-17, 5.08519862e-03, -9.99415338e-17, 1.14310026e-16],[-2.31491583e-17, 3.44431171e-01, -2.81970002e-17, -2.33497921e-17],],],[[[7.51963671e-02, -2.78377815e-16, 3.54579407e-01, 1.02943160e-02],[-1.45820198e-17, 2.43546325e-03, 5.56527542e-17, 6.35026535e-17],[3.05202191e-02, -5.60235836e-17, 5.55938827e-03, -5.75780811e-03],[5.34230841e-03, 2.78665353e-17, -2.76946416e-03, -2.10651109e-02],],[[-1.45820198e-17, 2.43546325e-03, 5.56527542e-17, 6.35026535e-17],[7.46798397e-02, -3.05861696e-16, 3.59082675e-01, 5.08519862e-03],[-4.51712874e-17, 1.24617768e-02, -5.24368327e-17, -1.09398900e-16],[6.53416581e-17, -3.33498976e-06, 3.62832929e-17, 9.42506062e-17],],[[3.05202191e-02, -5.60235836e-17, 5.55938827e-03, -5.75780811e-03],[-4.51712874e-17, 1.24617768e-02, -5.24368327e-17, -1.09398900e-16],[5.55938827e-03, -5.69105903e-17, 3.12395481e-01, 1.60872665e-02],[-5.75780811e-03, -9.99415338e-17, 1.60872665e-02, 6.97375883e-02],],[[5.34230841e-03, 2.78665353e-17, -2.76946416e-03, -2.10651109e-02],[6.53416581e-17, -3.33498976e-06, 3.62832929e-17, 9.42506062e-17],[-5.75780811e-03, -9.99415338e-17, 1.60872665e-02, 6.97375883e-02],[3.85389582e-03, -2.81970002e-17, 2.94109098e-01, -1.19378613e-02],],],[[[9.51662480e-03, 2.49516124e-16, 1.02943160e-02, 3.41425718e-01],[-3.87762611e-18, 1.23962208e-03, 6.35026535e-17, -4.14362821e-17],[5.34230841e-03, 6.70915608e-17, -5.75780811e-03, 3.85389582e-03],[1.67384743e-02, -5.04507066e-17, -2.10651109e-02, 7.06935786e-03],],[[-3.87762611e-18, 1.23962208e-03, 6.35026535e-17, -4.14362821e-17],[1.01652275e-02, 3.55270558e-16, 5.08519862e-03, 3.44431171e-01],[2.48872553e-17, -3.33498976e-06, -1.09398900e-16, -2.46748276e-18],[-4.61937088e-17, 7.25183601e-03, 9.42506062e-17, -4.91354497e-18],],[[5.34230841e-03, 6.70915608e-17, -5.75780811e-03, 3.85389582e-03],[2.48872553e-17, -3.33498976e-06, -1.09398900e-16, -2.46748276e-18],[-2.76946416e-03, 9.60811644e-18, 1.60872665e-02, 2.94109098e-01],[-2.10651109e-02, 1.14310026e-16, 6.97375883e-02, -1.19378613e-02],],[[1.67384743e-02, -5.04507066e-17, -2.10651109e-02, 7.06935786e-03],[-4.61937088e-17, 7.25183601e-03, 9.42506062e-17, -4.91354497e-18],[-2.10651109e-02, 1.14310026e-16, 6.97375883e-02, -1.19378613e-02],[7.06935786e-03, -2.33497921e-17, -1.19378613e-02, 3.06238461e-01],],],])
 system_size = 4
 def build_circuit(
-    poly_coeffs, angles_even, angles_conj_even, angles_odd, angles_conj_odd, poly_degree
+    angles_even, angles_conj_even, angles_odd, angles_conj_odd
 ):
     interaction_op = InteractionOperator(
         constant=0.0, one_body_tensor=hpq, two_body_tensor=hpqrs
@@ -331,7 +305,7 @@ def build_circuit(
     coeffs, pls, lambda_value = shift_and_transform_of_qubit_op_to_PL_lcu(
         qubit_op, ancilla_shift
     )
-    lambda_value = float(lambda_value)
+    lambda_value = float(lambda_value.real)
 
     assert np.isclose(s, lambda_value, atol=0.0, rtol=1e-8)
 
@@ -343,7 +317,6 @@ def build_circuit(
         reflection_wires=range(2, ancilla_shift),
         rot_wires=range(2, ancilla_shift),
         control_wires=range(2),
-        order=poly_degree,
         angles_conj_even=angles_conj_even,
         angles_conj_odd=angles_conj_odd,
         angles_even=angles_even,
@@ -352,64 +325,18 @@ def build_circuit(
     # Final circuit
     qsp_circuit_of_poly_with_parity = generic_poly.qsp_generic_polynomial
 
-    if system_size < 4:
-        # For sanity checks
-        hamiltonian = qml.matrix(hamiltonian)
-        e, u = np.linalg.eigh(hamiltonian)
-        e /= lambda_value
-        P_even = (
-            lambda ord: u
-            @ np.diag(
-                [
-                    sum(
-                        [
-                            poly_coeffs[i] * np.cos(i * np.arccos(x))
-                            for i in range(0, ord + 1, 2)
-                        ]
-                    )
-                    for x in e
-                ]
-            )
-            @ u.T
-        )
-        P_odd = (
-            lambda ord: u
-            @ np.diag(
-                [
-                    sum(
-                        [
-                            poly_coeffs[i] * np.cos(i * np.arccos(x))
-                            for i in range(1, ord + 1, 2)
-                        ]
-                    )
-                    for x in e
-                ]
-            )
-            @ u.T
-        )
-
-        ref = lambda_value * (P_even(poly_degree) + P_odd(poly_degree))
-
-        be_mtx = 2 * lambda_value * qml.matrix(qsp_circuit_of_poly_with_parity)()
-        print(np.max(np.abs(be_mtx[: 2**system_size, : 2**system_size] - ref)))
-        assert np.allclose(np.real(be_mtx[: 2**system_size, : 2**system_size]), ref)
-
     return qsp_circuit_of_poly_with_parity
 
-poly_coeffs = np.array([0.04839504, -0.11122954, -0.11047445, 0.19321127, 0.10155112, -0.07727596, 0.12920728, 0.03954071, -0.25404897, -0.03059887, -0.00703527, -0.28554963, 0.14565572, -0.1226918, 0.02780524])
 angles_even = np.array([3.11277458, 2.99152757, 3.15307452, 3.40611024, 3.00166196, 3.03597059, 3.25931224, 3.04073693, 3.25931224, 3.03597059, 3.00166196, 3.40611024, 3.15307452, 2.99152757, -40.86952257])
 angles_conj_even = np.array([[3.17041073, 3.29165774, 3.13011078, 2.87707507, 3.28152334, 3.24721472, 3.02387307, 3.24244837, 3.02387307, 3.24721472, 3.28152334, 2.87707507, 3.13011078, 3.29165774, -47.09507173]])
 angles_odd = np.array([3.26938242, 3.43658284, 3.17041296, 3.10158929, 3.22189574, 2.93731798, 3.25959312, 3.25959312, 2.93731798, 3.22189574, 3.10158929, 3.17041296, 3.43658284, -37.57132208])
 angles_conj_odd = [3.01380289, 2.84660247, 3.11277234, 3.18159601, 3.06128956, 3.34586733, 3.02359219, 3.02359219, 3.34586733, 3.06128956, 3.18159601, 3.11277234, 2.84660247, -44.11008691]
-poly_degree = len(poly_coeffs) - 1
 
 cir = build_circuit(
-    poly_coeffs,
     angles_even,
     angles_conj_even,
     angles_odd,
-    angles_conj_odd,
-    poly_degree,
+    angles_conj_odd
 )
 
 ######################################################################
