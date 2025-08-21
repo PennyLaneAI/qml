20d28d170e57af72760c00fecb862b4a1bc921b9
Daniela Angulo <42325731+daniela-angulo@users.noreply.github.com>
2025-08-21 16:27:43 -0400

    Update demo.py

--- demonstrations_v2/tutorial_dqi/demo.py
+++ demonstrations_v2/tutorial_dqi/demo.py
@@ -71,7 +71,7 @@ def objective_function(x):
     return f
 
 
-# Random samplingxs
+# Random sampling
 f_x_array_random = [objective_function(pnp.random.randint(0, 2, size=n)) for i in range(n_samples)]
 
 plt.hist(f_x_array_random, bins=30, density=True)
