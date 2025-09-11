a3d8ea8d427cfbaeee067009a2646c737c42ac33
doctorperceptron <paul@xanadu.ai>
2025-09-11 14:13:56 -0400

    Update filter

--- lib/filter_figures.py
+++ lib/filter_figures.py
@@ -8,9 +8,14 @@ from pandocfilters import toJSONFilter, Image
 
 def filter_images(key, value, format, _):
     if key == 'Image':
-        specs, url, alt = value
+        specs, [alt], src = value
+
+        try:
+            alt["c"] = ""
+        except KeyError:
+            alt = { "t": "Str", "c": "" }
         
-        return Image([specs, url, alt])
+        return Image(specs, [alt], src)
 
 if __name__ == '__main__':
     toJSONFilter(filter_images)
