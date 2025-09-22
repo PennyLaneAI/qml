5c1ed1b4cc96585e6d9c470581a84194b58f43d7
vinayak19th <vinayak1998th@Gmail.com>
2025-09-22 00:45:42 -0700

    Added demo

---
+++ demonstrations_v2/keras_3_training/demo.py
@@ -0,0 +1,15 @@
+r"""
+Keras 3 Training
+=
+
+Introduce your demo here!
+"""
+
+print("Hello")
+
+###############################################################################
+#
+# Add comment blocks to separate code blocks
+#
+
+print("World")
