9e512bc16dfd01ca3623353ec6b8559a0dc1c82b
doctorperceptron <paul@xanadu.ai>
2025-09-22 16:02:15 -0400

    Fix language warning

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ except ModuleNotFoundError:
 
 project = "PennyLane"
 
-copyright = "2022, Xanadu Quantum Technologies, Inc."
+copyright = "2025, Xanadu Quantum Technologies, Inc."
 
 author = "Xanadu Inc."
 
@@ -43,7 +43,6 @@ author = "Xanadu Inc."
 version = ""
 # The full version, including alpha/beta/rc tags
 release = ""
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -148,7 +147,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
