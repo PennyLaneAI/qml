9ccabdbadfdc679bf55035dee10a2295c95f56db
qottmann <korbinian.kottmann@gmail.com>
2025-10-20 11:38:31 +0200

    Lattice Surgery demo

---
+++ demonstrations_v2/tutorial_lattice_surgery/demo.py
@@ -0,0 +1,64 @@
+r"""
+
+Lattice Surgery
+===============
+
+.. figure:: ../_static/demonstration_assets/block_encoding/thumbnail_Block_Encodings_Matrix_Oracle.png
+    :align: center
+    :width: 50%
+    :target: javascript:void(0)
+
+abstract
+
+Sec 1
+-----------------------------------
+blah
+"""
+
+import pennylane as qml
+from pennylane.templates.state_preparations.mottonen import compute_theta, gray_code
+import numpy as np
+import matplotlib.pyplot as plt
+
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+#
+##############################################################################
+# text
+#
+# Conclusion
+# -----------------------
+# text
+#
+# References
+# ----------
+#
+# .. [#fable]
+#
+#     Daan Camps, Roel Van Beeumen,
+#     "FABLE: fast approximate quantum circuits for block-encodings",
+#     `arXiv:2205.00081 <https://arxiv.org/abs/2205.00081>`__, 2022
+#
+#
+# .. [#sparse]
+#
+#     Daan Camps, Lin Lin, Roel Van Beeumen, Chao Yang,
+#     "Explicit quantum circuits for block encodings of certain sparse matrices",
+#     `arXiv:2203.10236 <https://arxiv.org/abs/2203.10236>`__, 2022
+#
+#
