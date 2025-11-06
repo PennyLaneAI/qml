c99c4230578cb3e90e1c53424b17077434d7d42e
doctorperceptron <paul@xanadu.ai>
2025-11-06 17:04:29 -0500

    Process rst-class

--- lib/filter_directives.py
+++ lib/filter_directives.py
@@ -6,15 +6,28 @@ Also convert reference links to links to the online demo's references section.
 Code, link URLs, etc. are not affected.
 """
 import os
-from pandocfilters import toJSONFilter, BlockQuote, Link
+from pandocfilters import toJSONFilter, BlockQuote, Link, Table, CodeBlock
 
 DEMOS_URL = "https://pennylane.ai/qml/demos/"
+
+def parse_rst_class(body):
+    """Parse the body of an rst-class directive. This could either be a table or a sphinx-glr-script-out block."""
+    [metadata, content] = body
+    class_type = metadata.get("c")[0].get("c")
+    if class_type == "sphx-glr-script-out":
+        return CodeBlock(content)
+    elif class_type == "docstable":
+        return Table(content)
+    else:
+        return
 
 def filter_directives(key, value, format, _):
     if key == 'Div':
         [[_, classes, _], body] = value
         if "related" in classes or "meta" in classes:
             return []
+        elif "rst-class" in classes:
+            return parse_rst_class(body)
         else:
             return BlockQuote(body)
 
