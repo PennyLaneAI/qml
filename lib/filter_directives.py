c71070a57eaefe7cadfe8f9af231652f56b7bd69
doctorperceptron <paul@xanadu.ai>
2025-11-07 11:42:07 -0500

    Filter sphinx gallery output

--- lib/filter_directives.py
+++ lib/filter_directives.py
@@ -10,16 +10,6 @@ from pandocfilters import toJSONFilter, BlockQuote, Link, Table, CodeBlock
 
 DEMOS_URL = "https://pennylane.ai/qml/demos/"
 
-def parse_rst_class(body):
-    """Parse the body of an rst-class directive. This could either be a table or a sphinx-glr-script-out block."""
-    [metadata, content] = body
-    class_type = metadata.get("c")[0].get("c")
-    if class_type == "sphx-glr-script-out":
-        return CodeBlock(content)
-    elif class_type == "docstable":
-        return Table(content)
-    else:
-        return
 
 def filter_directives(key, value, format, _):
     if key == 'Div':
@@ -27,7 +17,12 @@ def filter_directives(key, value, format, _):
         if "related" in classes or "meta" in classes:
             return []
         elif "rst-class" in classes:
-            return parse_rst_class(body)
+            [metadata, content] = body
+            rst_class_type = metadata.get("c")[0].get("c")
+            if rst_class_type == "sphx-glr-script-out":
+                return content
+            else:
+                return
         else:
             return BlockQuote(body)
 
