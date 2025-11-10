05ad77768e80d7a9db958376e14a6d750bbaf7bd
doctorperceptron <paul@xanadu.ai>
2025-11-10 10:54:44 -0500

    Refactor

--- lib/filter_helpers.py
+++ lib/filter_helpers.py
@@ -1,5 +1,5 @@
 import os
-
+from pandocfilters import stringify
 
 ASSETS_DIR = "https://blog-assets.cloud.pennylane.ai/demos/"
 
@@ -8,3 +8,23 @@ def parse_img_source(src: str) -> str:
         return src.replace("../_", ASSETS_DIR + os.getenv("CURRENT_DEMO") + "/main/_assets/")
     else:
         return src
+
+def make_markdown_figure(figure: list) -> str:
+    """Make a markdown figure from a figure block.
+    Returns a markdown text string for the figure.
+    The format for the figure block is a list containing three elements:
+    [metadata, [url_dict], [url, alt_text]]
+    """
+    [_, [url_dict], _] = figure
+    return f"![]({url_dict.get('c', '')})"
+
+def make_markdown_link(link: list) -> str:
+    """Make a markdown link from a link block.
+    Returns a markdown text string for the link.
+    The format for the link block is a list containing three elements:
+    [metadata, link_text, [link_url, caption]]
+    """
+    [_, link_text, link_url] = link
+    text = stringify(link_text)
+    url = link_url[0]
+    return f"[{text}]({url})"
--- lib/filter_tables.py
+++ lib/filter_tables.py
@@ -3,7 +3,9 @@
 """
 Pandoc filter to convert grid tables to simple markdowntables.
 """
-from pandocfilters import toJSONFilter
+from pandocfilters import toJSONFilter, walk
+from filter_helpers import make_markdown_figure, make_markdown_link
+
 
 def process_header(header_cells):
     """Process the header cells of a table.
@@ -26,10 +28,39 @@ def process_rows(rows):
         row_content = row[1]
         for cell in row_content:
             [_, _, _, _, block] = cell
-            cell_content_buffer.append(block)
+            cell_content_buffer.append(process_block(block))
         row_content_buffer.append(cell_content_buffer)
         cell_content_buffer = []
     return row_content_buffer
+
+def process_block(block):
+    """Process a block of a table.
+    Return a markdown text string for the content in the block.
+    """
+    result = []
+    def parse_block(key, val, format, meta):
+        if key in ['Str', 'MetaString']:
+            result.append(val)
+        elif key == 'Code':
+            result.append(val[1])
+        elif key == 'Math':
+            result.append(val[1])
+        elif key == 'LineBreak':
+            result.append(" ")
+        elif key == 'SoftBreak':
+            result.append(" ")
+        elif key == 'Space':
+            result.append(" ")
+        elif key == 'Image':
+            result.append(make_markdown_figure(val))
+            # Don't process the rest of the block
+            return []
+        elif key == 'Link':
+            result.append(make_markdown_link(val))
+            # Don't process the rest of the block
+            return []
+    walk(block, parse_block, "", {})
+    return ''.join(result)
 
 def make_markdown_table(table_content):
     # Parse the table into headers and cells.
