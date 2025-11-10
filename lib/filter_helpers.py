977a7b497ab6b2d12dfc1e040576cb6acdf49ae8
doctorperceptron <paul@xanadu.ai>
2025-11-10 14:46:24 -0500

    Fix image urls

--- lib/filter_helpers.py
+++ lib/filter_helpers.py
@@ -4,8 +4,9 @@ from pandocfilters import stringify, walk
 ASSETS_DIR = "https://blog-assets.cloud.pennylane.ai/demos/"
 
 def parse_img_source(src: str) -> str:
-    if "../_static/" in src:
-        return src.replace("../_", ASSETS_DIR + os.getenv("CURRENT_DEMO") + "/main/_assets/")
+    if "../_static/demonstration_assets/" in src:
+        img = src.split("/")[-1]
+        return ASSETS_DIR + os.getenv("CURRENT_DEMO") + "/main/_assets/images/" + img
     else:
         return src
 
