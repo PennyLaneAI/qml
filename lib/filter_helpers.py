46fe57076cb335950fba09057c15df389923fd98
doctorperceptron <paul@xanadu.ai>
2025-12-18 15:30:02 -0500

    Process thumbnails

--- lib/filter_helpers.py
+++ lib/filter_helpers.py
@@ -4,7 +4,7 @@ from pandocfilters import stringify, walk
 ASSETS_DIR = "https://blog-assets.cloud.pennylane.ai/demos/"
 
 def parse_img_source(src: str) -> str:
-    if "/_static/demonstration_assets/" in src:
+    if "/_static/demonstration_assets/" in src or "/_static/demo_thumbnails/" in src:
         img = src.split("/")[-1]
         return ASSETS_DIR + os.getenv("CURRENT_DEMO") + "/main/_assets/images/" + img
     else:
