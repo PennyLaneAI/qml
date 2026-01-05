7b7775eebf2b2c152074018013dcb8f491f53014
Diksha Dhawan <ddhawan@umich.edu>
2026-01-05 15:32:12 -0500

    remove border

--- demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
+++ demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
@@ -46,15 +46,13 @@ In this demo, we show how to get concrete costs using logical resource estimatio
 # rely on QROM to access Hamiltonian coefficients and rotation angles respectively. We can use
 # the select-swap variant of the QROM, which allows us to trade the depth of the circuit for width as shown in the circuit diagrams.
 #
-# .. list-table::
-#    :widths: 50 50
-#    :header-rows: 0
-#    :class: borderless
+# .. |img1| image:: ../_static/demonstration_assets/qrom/select_swap.jpeg
+#    :width: 45%
 #
-#    * - .. image:: ../_static/demonstration_assets/qrom/select_swap.jpeg
-#           :width: 100%
-#    * - .. image:: ../_static/demonstration_assets/qrom/select_swap_4.jpeg
-#           :width: 100%
+# .. |img2| image:: ../_static/demonstration_assets/qrom/select_swap_4.jpeg
+#    :width: 45%
+#
+# |img1| |img2|
 #
 # Standard resource estimates often treat these oracles as fixed "black boxes", yielding a single cost number.
 # However, this demo is more than just a static cost report. We demonstrate how PennyLane exposes these
