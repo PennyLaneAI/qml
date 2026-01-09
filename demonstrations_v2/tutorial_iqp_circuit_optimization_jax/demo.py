61833abb9705bb3a4cfb58282efd83f1d828acb9
Marcus Edwards <marcusstephenedwards@gmail.com>
2026-01-09 15:13:09 -0500

    Link to docs

--- demonstrations_v2/tutorial_iqp_circuit_optimization_jax/demo.py
+++ demonstrations_v2/tutorial_iqp_circuit_optimization_jax/demo.py
@@ -16,8 +16,8 @@ estimating expectation values of certain observables is easy.
 
 .. note::
     The IQP circuit and expectation values estimator are directly implemented and
-    available in PennyLane via the :class:`~.IQP` template
-    and :func:`~.pennylane.qnn.iqp_expval`, respectively.
+    available in PennyLane via the `~.IQP <https://docs.pennylane.ai/en/stable/code/api/pennylane.IQP.html>`__ template
+    and `~.pennylane.qnn.iqp_expval <https://docs.pennylane.ai/en/stable/code/api/pennylane.qnn.iqp_expval.html>`__, respectively.
 
 Parameterized IQP circuits
 --------------------------
