d0857e759e0ff276d23337f46be7ee03d64452e6
Mudit Pandey <mudit.pandey@xanadu.ai>
2026-01-09 12:10:04 -0500

    Add warning to the getting-started-with-hybrid-jobs demo

--- demonstrations_v2/getting_started_with_hybrid_jobs/demo.py
+++ demonstrations_v2/getting_started_with_hybrid_jobs/demo.py
@@ -52,7 +52,8 @@ Let’s setup an algorithm that makes use of both classical and quantum resource
 
 .. warning::
 
-    The following demo is only compatible with Python version 3.10.
+    The following demo is only compatible with Python version 3.10. Consequently, the latest supported PennyLane
+    version is ``pennylane==0.42``.
 
 """
 
@@ -63,7 +64,6 @@ Let’s setup an algorithm that makes use of both classical and quantum resource
 
 import pennylane as qml
 from pennylane import numpy as np
-
 
 device = qml.device("braket.local.qubit", wires=1)
 
@@ -272,8 +272,8 @@ while len(job.metrics()) == 0:
     pass
 
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 from braket.jobs.metrics_data.definitions import MetricType
 
 df = pd.DataFrame(job.metrics(metric_type=MetricType.ITERATION_NUMBER))
@@ -345,7 +345,8 @@ def qpu_qubit_rotation_hybrid_job(num_steps=10, stepsize=0.5):
     device = qml.device(
         "braket.aws.qubit",
         device_arn=device_arn.value,  # Make sure the device ARN matches the hybrid job device ARN
-        wires=2)
+        wires=2,
+    )
 
     @qml.set_shots(1_000)
     @qml.qnode(device)
