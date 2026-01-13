a9451aeca51927528f3bbe5feb07460e6137e557
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2026-01-13 16:25:21 -0500

    Update demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
    Co-authored-by: ixfoduap <40441298+ixfoduap@users.noreply.github.com>

--- demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
+++ demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
@@ -111,7 +111,7 @@ femoco = qre.THCHamiltonian(num_orbitals=76, tensor_rank=450, one_norm=1201.5)
 # Estimating Qubitized QPE Cost
 # -----------------------------
 # With these parameters in hand, we can esimate the total resources. The full algorithm consists of the Walk Operator,
-# constructed via :class:`~.pennylane.templates.templates.QubitizeTHC` running within a QPE routine.
+# constructed via :class:`~.pennylane.templates.templates.QubitizeTHC`, running within a QPE routine.
 #
 # We must note that :class:`~.pennylane.estimator.templates.SelectTHC` oracle implementation is based on the description in
 # von Burg et al. [#vonburg]_. This work uses the phase gradient technique to implement Givens rotations, and thus requires an
