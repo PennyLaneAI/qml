adb86a02e44c4143e4d1934ffe28157092dadffc
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2026-01-12 22:21:32 -0500

    Update demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
    Co-authored-by: ixfoduap <40441298+ixfoduap@users.noreply.github.com>

--- demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
+++ demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
@@ -2,7 +2,7 @@ r"""Qubit and gate trade-offs in Qubitized Quantum Phase Estimation
 ======================================================================
 
 From designing better battery materials to discovering
-new drugs, unlocking the full potential of quantum computing for chemistry requires simulating the quantum dynamics of molecular systems.
+new drugs, unlocking the full potential of quantum computing requires simulating the properties of molecular systems.
 To achieve this, we rely on `Quantum Phase Estimation (QPE) <https://pennylane.ai/qml/demos/tutorial_qpe>`_,
 which allows us to estimate the energy eigenstates of a Hamiltonian with high precision.
 QPE, however, relies on unitary evolution, and chemical Hamiltonians :math:`\hat{H}` are Hermitian.
