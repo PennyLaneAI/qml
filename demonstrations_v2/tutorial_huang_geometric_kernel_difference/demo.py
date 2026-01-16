d6f00f875fa31f92bb7a5e48e2af136a9e93d4e4
Daniela Angulo <42325731+daniela-angulo@users.noreply.github.com>
2026-01-16 10:51:33 -0500

    Add utils file for auxiliar function

--- demonstrations_v2/tutorial_huang_geometric_kernel_difference/demo.py
+++ demonstrations_v2/tutorial_huang_geometric_kernel_difference/demo.py
@@ -205,34 +205,13 @@ def embedding_E2(features):
     qml.IQPEmbedding(features, wires=range(n_features))
 
 
-def draw_circuits_side_by_side(circuits, titles, figsize=(14, 4)):
-    """Display multiple PennyLane circuits side by side."""
-    from io import BytesIO
-    import matplotlib.image as mpimg
-
-    images = []
-    for circuit in circuits:
-        fig, _ = qml.draw_mpl(circuit)(np.zeros(n_qubits))
-        buf = BytesIO()
-        fig.savefig(buf, format='png', bbox_inches='tight', dpi=100)
-        buf.seek(0)
-        images.append(mpimg.imread(buf))
-        plt.close(fig)
-
-    fig, axes = plt.subplots(1, len(circuits), figsize=figsize)
-    for ax, img, title in zip(axes, images, titles):
-        ax.imshow(img)
-        ax.axis('off')
-        ax.set_title(title, fontsize=12)
-    plt.tight_layout()
-    plt.show()
+from utils import draw_circuits_side_by_side
 
 
 draw_circuits_side_by_side(
     [embedding_E1, embedding_E2],
     ["E1 Embedding Circuit", "E2 Embedding Circuit"]
 )
-
 
 
 ######################################################################
---
+++ demonstrations_v2/tutorial_huang_geometric_kernel_difference/utils.py
@@ -0,0 +1,21 @@
+def draw_circuits_side_by_side(circuits, titles, figsize=(14, 4)):
+    """Display multiple PennyLane circuits side by side."""
+    from io import BytesIO
+    import matplotlib.image as mpimg
+
+    images = []
+    for circuit in circuits:
+        fig, _ = qml.draw_mpl(circuit)(np.zeros(n_qubits))
+        buf = BytesIO()
+        fig.savefig(buf, format='png', bbox_inches='tight', dpi=100)
+        buf.seek(0)
+        images.append(mpimg.imread(buf))
+        plt.close(fig)
+
+    fig, axes = plt.subplots(1, len(circuits), figsize=figsize)
+    for ax, img, title in zip(axes, images, titles):
+        ax.imshow(img)
+        ax.axis('off')
+        ax.set_title(title, fontsize=12)
+    plt.tight_layout()
+    plt.show()
