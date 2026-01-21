d55e4784e93a6058cd1f2ceeebc284875954029e
Diksha Dhawan <ddhawan@umich.edu>
2026-01-21 15:19:05 -0500

    check image

--- demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
+++ demonstrations_v2/tutorial_re_for_qubitizedQPE/demo.py
@@ -41,7 +41,7 @@ a few hundred logical qubits. In particular, we show how to implement **QPE for 
 #
 # .. figure:: ../_static/demonstration_assets/qubitization_re/pennylane-demo-image-circuit-batching-fig.png
 #    :align: center
-#    :width: 100%
+#    :width: 120%
 #    :target: javascript:void(0)
 #
 # In the left panel, we load all angles at once using a single call to QROM (pink), but this requires four ancilla registers.
@@ -281,7 +281,7 @@ for depth in swap_depths:
 #    :align: center
 #
 #    * - Configuration
-#      - Naive Baseline
+#      - Baseline
 #      - Optimized
 #    * - Logical Qubits
 #      - 2200
