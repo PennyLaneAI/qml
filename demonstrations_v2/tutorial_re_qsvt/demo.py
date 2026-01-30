d84f591b4bb8d2b95341ef2cd06e60f9ef01f965
Jay Soni <jbsoni@uwaterloo.ca>
2026-01-30 12:54:56 -0500

    demo links, lint, and fix image

--- demonstrations_v2/tutorial_re_qsvt/demo.py
+++ demonstrations_v2/tutorial_re_qsvt/demo.py
@@ -14,7 +14,7 @@ problems including unstructured search, phase estimation, and Hamiltonian simula
 problem of practical importance is **matrix inversion** because of its application to computational fluid dynamics
 (CFD) simulations [#lapworth2022]_. For more information on how to use PennyLane's :func:`~.pennylane.qsvt`
 functionality to run matrix inversion on a quantum computer see our demo on `QSVT in Practice
-<https://pennylane.ai/qml/demos/tutorial_apply_qsvt>`_.
+<tutorial_apply_qsvt>`_.
 
 Simulations can only take us so far, and industrially relevant system sizes are often too large to
 meaningfully simulate. Let's see how we can use PennyLane to gather insights for the problem in this regime.
@@ -40,25 +40,28 @@ polynomial transformation to the matrix :math:`A`:
 for :math:`a = 0.25` and :math:`b = 0.75`. This particular matrix can be expressed as a linear combination of
 unitaries (LCU) :math:`A = 0.25 \cdot \hat{Z}_{1} + 0.75 \cdot \hat{X}_{0}\hat{X}_{1}`. We begin by building the
 **block encoding** operator using the standard method of LCUs. For a recap on this technique, see our demo on
-`linear combination of unitaries and block encodings <https://pennylane.ai/qml/demos/tutorial_lcu_blockencoding>`_.
+`linear combination of unitaries and block encodings <tutorial_lcu_blockencoding>`_.
 """
+
 import pennylane.estimator as qre
 
-lcu_A = qre.PauliHamiltonian(  
-    num_qubits = 2,
-    pauli_terms = {"Z": 1, "XX": 1},
+lcu_A = qre.PauliHamiltonian(
+    num_qubits=2,
+    pauli_terms={"Z": 1, "XX": 1},
 )  # represents A = 0.25 * Z(1) + 0.75 * X(0)X(1)
 
+
 def Standard_BE(prep, sel):
-    """Standard block-encoding of the Hamiltonian """
+    """Standard block-encoding of the Hamiltonian"""
     return qre.ChangeOpBasis(prep, sel)
+
 
 # Preparing a qubit in the state (√0.25)|0> + (√0.75)|1>
 Prep = qre.RY(wires=["prep_1"])
 
 Select = qre.SelectPauli(
     lcu_A,
-    wires = ["prep_1", 0, 1],
+    wires=["prep_1", 0, 1],
 )  # Select over the operators in the LCU
 
 resources = qre.estimate(Standard_BE(Prep, Select))
@@ -75,9 +78,9 @@ print(resources)
 # can then be obtained using this block encoding:
 
 qsvt_op = qre.QSVT(
-    block_encoding = Standard_BE(Prep, Select),
-    encoding_dims = (4, 4),  # The shape of matrix A
-    poly_deg = 5,  # quintic
+    block_encoding=Standard_BE(Prep, Select),
+    encoding_dims=(4, 4),  # The shape of matrix A
+    poly_deg=5,  # quintic
 )
 
 resources = qre.estimate(qsvt_op)
@@ -96,7 +99,7 @@ print(resources)
 # approximation will be in general.
 #
 # This creates a delicate balance, we want to reduce the degree of the transformation as much as
-# possible without compromising the accuracy of our matrix inversion. Luckily, this problem has 
+# possible without compromising the accuracy of our matrix inversion. Luckily, this problem has
 # been studied in the literature and there is an expression for an *optimal* polynomial approximation
 # of the inverse function that minimizes the target error given the maximum allowed degree [#sunderhauf2025]_.
 #
@@ -113,26 +116,27 @@ print(resources)
 # number of the matrix (:math:`\kappa`) [#sunderhauf2025]_:
 #
 # .. math:: \epsilon(a, n) = \frac{(1 - a^{n})}{a(1 + a)^{n-1}} .
-# 
+#
 # Where :math:`a = \kappa^{-1}` and :math:`n = 2(d + 1)`. We can use this expression to determine the
 # minimum degree needed to approximate the inverse function within the target error :math:`\epsilon`.
 
 import numpy as np
 
+
 def polynomial_approx_error(degree, kappa):
-    """The error in the polynomial approximation of 1/x 
+    """The error in the polynomial approximation of 1/x
     as described in Theorem 1 of https://arxiv.org/pdf/2507.15537"""
     a = 1 / kappa
-    n = (degree + 1) // 2 # the degree must be an odd number!
+    n = (degree + 1) // 2  # the degree must be an odd number!
 
-    numerator = (1 - a)**n
-    denominator = a * (1 + a)**(n-1)
+    numerator = (1 - a) ** n
+    denominator = a * (1 + a) ** (n - 1)
 
     return numerator / denominator
 
 
 def optimal_degree_from_error(error_threshold, kappa):
-    """Determine the optimal degree of the polynomial 
+    """Determine the optimal degree of the polynomial
     approximation for 1/x required to be within the given threshold"""
     degree = 3
     eps = np.inf  # set initial value as upper bound
@@ -141,6 +145,7 @@ def optimal_degree_from_error(error_threshold, kappa):
         eps = polynomial_approx_error(degree, kappa)
 
     return degree
+
 
 kappa = 10
 epsilon = 1e-5
@@ -152,7 +157,7 @@ print("Minimum degree:", optimal_degree_from_error(epsilon, kappa))
 # The other parameter that impacts the cost of QSVT is the block encoding operator. While
 # the strategy of decomposing :math:`A` into an LCU of Pauli operators works for any (square)
 # matrix in general, it suffers from a few fatal flaws (try saying that five times fast).
-# 
+#
 # The number of terms in the LCU scales as :math:`O(4^{n})` in the number of qubits, and thus
 # the cost of the block encoding also scales exponentially. Even computing the LCU decomposition
 # becomes a computational bottleneck. Furthermore, there is no way of knowing a priori how many
@@ -197,9 +202,9 @@ print("Minimum degree:", optimal_degree_from_error(epsilon, kappa))
 # Let's start with the :math:`D_{k}` operators. These are a list of diagonal operators where
 # each operator stores the normalized entries from one of the diagonals of our d-diagonal matrix :math:`A`.
 # By multiplexing over the :math:`D_{k}` operators, we can load all of the diagonals in *parallel*.
-# Typically, each diagonal operator is implemented using a product of Controlled PhaseShift gates, 
+# Typically, each diagonal operator is implemented using a product of Controlled PhaseShift gates,
 # in this case we will be leveraging the Walsh-Hadamard transformation ([#linaje2025]_, [#zylberman2025]_).
-# 
+#
 # The Walsh-Hadamard transform allows us to naturally optimize the cost of our block encoding by tuning the
 # number of Walsh coefficients within :math:`[1, N]`, where :math:`N` is the
 # the size of the matrix. If the entries in our diagonal are sparse in the Walsh basis, as is the case for
@@ -217,6 +222,7 @@ print("Minimum degree:", optimal_degree_from_error(epsilon, kappa))
 # |
 #
 
+
 def WalshHadamard_Dk(num_diags, size_diagonal, num_walsh_coeffs):
     num_diag_wires = int(np.ceil(np.log2(size_diagonal)))
     list_of_diagonal_ops = []
@@ -225,14 +231,14 @@ def WalshHadamard_Dk(num_diags, size_diagonal, num_walsh_coeffs):
         compute_op = qre.Prod([qre.Hadamard(), qre.S()])
 
         zero_ctrl_wh = qre.Controlled(
-            qre.Prod(((qre.MultiRZ(num_diag_wires//2), num_walsh_coeffs),)),
-            num_ctrl_wires = 1,
-            num_zero_ctrl = 1,
+            qre.Prod(((qre.MultiRZ(num_diag_wires // 2), num_walsh_coeffs),)),
+            num_ctrl_wires=1,
+            num_zero_ctrl=1,
         )
         one_ctrl_wh = qre.Controlled(
-            qre.Prod(((qre.MultiRZ(num_diag_wires//2), num_walsh_coeffs),)),
-            num_ctrl_wires = 1,
-            num_zero_ctrl = 0,
+            qre.Prod(((qre.MultiRZ(num_diag_wires // 2), num_walsh_coeffs),)),
+            num_ctrl_wires=1,
+            num_zero_ctrl=0,
         )
         target_op = qre.Prod([zero_ctrl_wh, one_ctrl_wh])
 
@@ -240,19 +246,20 @@ def WalshHadamard_Dk(num_diags, size_diagonal, num_walsh_coeffs):
 
     return list_of_diagonal_ops
 
+
 ##############################################################################
 # Here the `Prod <https://docs.pennylane.ai/en/stable/code/api/pennylane.estimator.ops.Prod.html>`_
 # class is used to represent a product of operators, and the
 # `Controlled <https://docs.pennylane.ai/en/stable/code/api/pennylane.estimator.ops.Controlled.html>`_
 # class represents the controlled operator.
-# 
+#
 # Shift Operator
 # ^^^^^^^^^^^^^^
 # Next we will implement the Shift operator. This is a product of three subroutines which
 # shift the diagonal entries into the correct rank of the d-diagonal block encoding. Each rank
 # has an associated shift value, the main diagonal has shift value 0. Each diagonal going up
 # is labelled :math:`(+1, +2, \ldots)` respectively and each diagonal going down is labelled
-# with its negative counterpart. The Shift operator works in three steps: 
+# with its negative counterpart. The Shift operator works in three steps:
 #
 # 1. **Load** the binary representation of the shift value for each non-zero diagonal in :math:`A`.
 #
@@ -261,32 +268,34 @@ def WalshHadamard_Dk(num_diags, size_diagonal, num_walsh_coeffs):
 # 3. **Unload** the shift values to restore the quantum register.
 #
 
+
 def ShiftOp(num_shifts, num_load_wires, wires):
     prep_wires, load_shift_wires, load_diag_wires = wires
 
     Load = qre.QROM(
-        num_bitstrings = num_shifts,
-        size_bitstring = num_load_wires,
-        restored = False,
-        select_swap_depth = 1,
-        wires = prep_wires + load_shift_wires,
+        num_bitstrings=num_shifts,
+        size_bitstring=num_load_wires,
+        restored=False,
+        select_swap_depth=1,
+        wires=prep_wires + load_shift_wires,
     )
 
     Adder = qre.SemiAdder(
-        max_register_size = num_load_wires,
-        wires = load_shift_wires + load_diag_wires,
+        max_register_size=num_load_wires,
+        wires=load_shift_wires + load_diag_wires,
     )
-    
+
     Unload = qre.Adjoint(
         qre.QROM(
-            num_bitstrings = num_shifts,
-            size_bitstring = num_load_wires,
-            restored = False,
-            select_swap_depth = 1,
-            wires = prep_wires + load_shift_wires,
+            num_bitstrings=num_shifts,
+            size_bitstring=num_load_wires,
+            restored=False,
+            select_swap_depth=1,
+            wires=prep_wires + load_shift_wires,
         )
     )
     return qre.Prod([Load, Adder, Unload])
+
 
 ##############################################################################
 # d-Diagonal Block Encoding
@@ -304,6 +313,7 @@ def ShiftOp(num_shifts, num_load_wires, wires):
 # |
 #
 
+
 def WH_Diagonal_BE(num_diagonals, matrix_size, num_walsh_coeffs):
     # Initialize qubit registers:
     num_prep_wires = int(np.ceil(np.log2(num_diagonals)))
@@ -313,24 +323,25 @@ def WH_Diagonal_BE(num_diagonals, matrix_size, num_walsh_coeffs):
     load_diag_wires = [f"load_d{i}" for i in range(num_diag_wires)]
     load_shift_wires = [f"load_s{i}" for i in range(num_diag_wires)]
 
-    # Prep: 
-    Prep = qre.QROMStatePreparation(num_prep_wires, wires = prep_wires)
+    # Prep:
+    Prep = qre.QROMStatePreparation(num_prep_wires, wires=prep_wires)
 
     # Shift:
     Shift = ShiftOp(
-        num_shifts = num_diagonals,
-        num_load_wires = num_diag_wires,
-        wires = (prep_wires, load_diag_wires, load_shift_wires)
+        num_shifts=num_diagonals,
+        num_load_wires=num_diag_wires,
+        wires=(prep_wires, load_diag_wires, load_shift_wires),
     )
-    
+
     # Multiplexed - Dk:
     diagonal_ops = WalshHadamard_Dk(num_diagonals, matrix_size, num_walsh_coeffs)
-    Dk = qre.Select(diagonal_ops, wires = load_diag_wires + prep_wires + ["target"])
+    Dk = qre.Select(diagonal_ops, wires=load_diag_wires + prep_wires + ["target"])
 
     # Prep^t:
-    Prep_dagger = qre.Adjoint(qre.QROMStatePreparation(num_prep_wires, wires = prep_wires))
+    Prep_dagger = qre.Adjoint(qre.QROMStatePreparation(num_prep_wires, wires=prep_wires))
 
     return qre.Prod([Prep, Shift, Dk, Prep_dagger])
+
 
 ##############################################################################
 # Application: Computational Fluid Dynamics
@@ -339,7 +350,7 @@ def WH_Diagonal_BE(num_diagonals, matrix_size, num_walsh_coeffs):
 # for solving a practical matrix inversion problem**. Following the CFD example [#linaje2025]_, we
 # will estimate the resources required to invert a tri-diagonal matrix of size :math:`(2^{20}, 2^{20})`.
 # This system requires a :math:`10^{8}`-degree polynomial approximation of the inverse function.
-# 
+#
 # We will also restrict the gateset to Clifford + T gates, to generate results that
 # are faithful to the ones presented in [#linaje2025]_.
 
@@ -348,21 +359,23 @@ matrix_size = 2**20  # 2^20 x 2^20
 num_diagonals = 3
 num_walsh_coeffs = 512  # imperically determined
 
+
 def matrix_inversion(degree, matrix_size, num_diagonals):
     num_state_wires = int(np.ceil(np.log2(matrix_size)))  # 20 qubits
 
-    # Prepare |b> vector: 
-    qre.MPSPrep(num_state_wires, max_bond_dim = 2**5)  # bond dimension = 2**5
+    # Prepare |b> vector:
+    qre.MPSPrep(num_state_wires, max_bond_dim=2**5)  # bond dimension = 2**5
 
     # Apply A^-1:
     qre.QSVT(
-        block_encoding = WH_Diagonal_BE(num_diagonals, matrix_size, num_walsh_coeffs),
-        encoding_dims = (matrix_size, matrix_size),
-        poly_deg = degree,
+        block_encoding=WH_Diagonal_BE(num_diagonals, matrix_size, num_walsh_coeffs),
+        encoding_dims=(matrix_size, matrix_size),
+        poly_deg=degree,
     )
     return
 
-gate_set = {'X', 'Y', 'Z', 'Hadamard', 'T', 'S', 'CNOT'}
+
+gate_set = {"X", "Y", "Z", "Hadamard", "T", "S", "CNOT"}
 res = qre.estimate(matrix_inversion, gate_set)(degree, matrix_size, num_diagonals)
 print(res)
 
@@ -372,8 +385,8 @@ print(res)
 #
 # Challenge (Next Steps)
 # ----------------------
-# In this demo, you learned how to use PennyLane's :mod:`~.pennylane.estimator` module to determine the 
-# resource requirements for **matrix inversion by QSVT**. Along the way we explored the two major factors 
+# In this demo, you learned how to use PennyLane's :mod:`~.pennylane.estimator` module to determine the
+# resource requirements for **matrix inversion by QSVT**. Along the way we explored the two major factors
 # that impact the cost of QSVT (block encoding and degree of the approximation), as well as how they can
 # be optimized to reduce the overall cost of the algorithm. We showcased how complex circuits can be
 # built from our library of primitives, making resource estimation as simple as putting together Lego
