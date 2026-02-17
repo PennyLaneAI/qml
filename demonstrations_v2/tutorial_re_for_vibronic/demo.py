7bc20a10eafe4d643627507be0aaa88bd4b24ec3
Diksha Dhawan <40900030+ddhawan11@users.noreply.github.com>
2026-02-17 16:31:51 -0500

    Update demonstrations_v2/tutorial_re_for_vibronic/demo.py
    Co-authored-by: ANT0N <39093564+AntonNI8@users.noreply.github.com>

--- demonstrations_v2/tutorial_re_for_vibronic/demo.py
+++ demonstrations_v2/tutorial_re_for_vibronic/demo.py
@@ -40,7 +40,7 @@ contains off-diagonal electronic couplings.
 To simulate the time evolution :math:`U(t) = e^{-iHt}`, we employ a product formula (Trotterization) approach as outlined in
 D. Motlagh et al. [#Motlagh2025]_. A key challenge in Trotterization is decomposing the potential energy matrix, :math:`V`, in the Hamiltonian efficiently.
 Here, we use the fragmentation scheme proposed in the reference, grouping the terms :math:`\ket{j} \bra{i} \otimes V_{ji}` such
-that they differ by a fixed bitstring :math:`m`. This grouping method results in N different fragments, which can be viewed as
+that they differ by a fixed bitstring :math:`m`. This grouping method results in :math:`N` different fragments, which can be viewed as
 blocks of the potential energy matrix as shown in Figure 1. Each of these fragments can then be block-diagonalized by using only
 Clifford gates and implemented as a sequence of evolutions controlled by the corresponding electronic states.
 
